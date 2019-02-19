d483f8772b1c2228f13276a203c3597aaaeadf2a
Abubakar Abid <a12d@stanford.edu>
2019-02-19 01:19:19 -0800

    added package data

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name = 'gradio',
-    version = '0.1.1',
-    description = 'Python library for easily interacting with trained machine learning models',
-    author = 'Abubakar Abid',
-    author_email = 'a12d@stanford.edu',
-    url = 'https://github.com/abidlabs/gradio',
+    name='gradio',
+    version='0.1.2',
+    include_package_data=True,
+    description='Python library for easily interacting with trained machine learning models',
+    author='Abubakar Abid',
+    author_email='a12d@stanford.edu',
+    url='https://github.com/abidlabs/gradio',
     packages=['gradio'],
-    keywords = ['machine learning', 'visualization', 'reproducibility'],
+    keywords=['machine learning', 'visualization', 'reproducibility'],
     install_requires=[
         'numpy',
         'websockets',
