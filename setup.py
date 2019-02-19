d8247270e1c117487e04dd13f34de352e990d1e5
Abubakar Abid <a12d@stanford.edu>
2019-02-19 11:49:13 -0800

    0.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='gradio',
-    version='0.1.5',
+    version='0.1.6',
     include_package_data=True,
     description='Python library for easily interacting with trained machine learning models',
     author='Abubakar Abid',
