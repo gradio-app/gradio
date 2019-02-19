febf3e631f78d5b4a467874d7efe48641e827207
Abubakar Abid <a12d@stanford.edu>
2019-02-18 22:01:39 -0800

    fixed link sharing bug

--- js/all-io.js
+++ js/all-io.js
@@ -1,17 +1,25 @@
+var NGROK_URL = "wss://40d65b0a.ngrok.io"
+
 try {
-  var ws = new WebSocket("ws://127.0.0.1:9200/")
+  var origin = window.location.origin;
+  if (origin.includes("ngrok")){
+      var ws = new WebSocket(NGROK_URL)
+  } else {
+      var ws = new WebSocket("ws://127.0.0.1:9200/")
+  }
   ws.onerror = function(evt) {
-    notifyError(evt)
+    console.log(evt)
   };
   ws.onclose = function(event) {
     console.log("WebSocket is closed now.");
-    $('#model-status').html('Model: closed');
-    $('#model-status').css('color', '#e23e44');
+    var model_status = $('#model-status')
+    model_status.html('Model: closed');
+    model_status.css('color', '#e23e44');
     $('#overlay').css('visibility','visible')
 };
 
 } catch (e) {
-  notifyError(e)
+  console.log(e)
 }
 
 const sleep = (milliseconds) => {
