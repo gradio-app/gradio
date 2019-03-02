e523045f6750a0c7ab1941879c3b68b1fde28dc9
Abubakar Abid <a12d@stanford.edu>
2019-03-01 20:13:02 -0800

    added commented out code for updated build-interface

--- build-interface.py
+++ build-interface.py
@@ -10,6 +10,12 @@ args = parser.parse_args()
 def launch_interface(args):
     io = gradio.Interface(inputs=args.inputs, outputs=args.outputs, model=lambda x:x, model_type='function')
     io.launch()
+    # input_interface = gradio.inputs.registry[args.inputs.lower()]()
+    # output_interface = gradio.outputs.registry[args.outputs.lower()]()
+    # temp_dir = tempfile.mkdtemp()
+    # gradio.networking.build_template(temp_dir, input_interface, output_interface)
+    # print('Open this path in your browser to access the input interface: {}'.format(
+    #     os.path.join(temp_dir, INDEX_FILE_NAME)))
 
 
 if __name__ == "__main__":
