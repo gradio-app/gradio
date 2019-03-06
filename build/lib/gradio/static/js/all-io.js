8ddbf9493f762e68a1995615cc0eb2d88cbdd3d1
Abubakar Abid <a12d@stanford.edu>
2019-03-06 12:11:58 -0800

    updated manifest.in for 0.3.2

---
+++ build/lib/gradio/static/js/all-io.js
@@ -0,0 +1,28 @@
+var NGROK_URL = "ws://0f9bffb5.ngrok.io"
+var SOCKET_PORT = 9200
+
+try {
+  var origin = window.location.origin;
+  if (origin.includes("ngrok")){
+      var ws = new WebSocket(NGROK_URL)
+  } else {
+      var ws = new WebSocket("ws://127.0.0.1:" + SOCKET_PORT + "/")
+  }
+  ws.onerror = function(evt) {
+    console.log(evt)
+  };
+  ws.onclose = function(event) {
+    console.log("WebSocket is closed now.");
+    var model_status = $('#model-status')
+    model_status.html('Model: closed');
+    model_status.css('color', '#e23e44');
+    $('#overlay').css('visibility','visible')
+};
+
+} catch (e) {
+  console.log(e)
+}
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
