d60703d356811f9f4ae4d601f1ac30e3b5269acb
Abubakar Abid <a12d@stanford.edu>
2019-03-06 12:11:58 -0800

    updated manifest.in for 0.3.2

---
+++ build/lib/gradio/static/js/class-output.js
@@ -0,0 +1,63 @@
+function notifyError(error) {
+      $.notify({
+          // options
+          message: 'Not able to communicate with model (is python code still running?)'
+      },{
+          // settings
+          type: 'danger',
+          animate: {
+              enter: 'animated fadeInDown',
+              exit: 'animated fadeOutUp'
+          },
+          placement: {
+              from: "bottom",
+              align: "right"
+          },
+          delay: 5000
+
+      });
+ }
+
+try {
+  ws.onerror = function(evt) {
+    notifyError(evt)
+  };
+
+  ws.onmessage = function (event) {
+    sleep(300).then(() => {
+//      $(".output_class").text(event.data);
+      var data = JSON.parse(event.data)
+      // data = {
+      //   label: "happy",
+      //   confidences : [
+      //    {
+      //     label : "happy",
+      //     confidence: 0.7
+      //    },
+      //    {
+      //     label : "sad",
+      //     confidence: 0.3
+      //    },
+      //   ]
+      //   }
+      $(".output_class").text(data["label"])
+      $(".confidence_intervals").empty()
+      if ("confidences" in data) {
+        data["confidences"].forEach(function (c) {
+          var confidence = c["confidence"]
+          $(".confidence_intervals").append(`<div class="confidence"><div class=
+              "label">${c["label"]}</div><div class="level" style="flex-grow:
+                ${confidence}">${Math.round(confidence * 100)}%</div></div>`)
+        })
+      }
+    })
+
+  }
+} catch (e) {
+  notifyError(e)
+}
+
+$('body').on('click', '.clear', function(e) {
+  $(".output_class").text("")
+  $(".confidence_intervals").empty()
+})
---
+++ build/lib/gradio/static/js/image-upload-input.js
@@ -0,0 +1,60 @@
+// var cropper;
+
+$('body').on('click', ".input_image.drop_mode", function (e) {
+  $(this).parent().find(".hidden_upload").click();
+})
+
+$('body').on('drag dragstart dragend dragover dragenter dragleave drop', ".input_image.drop_mode", function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+})
+
+function loadPreviewFromFiles(files) {
+  var ReaderObj = new FileReader()
+  ReaderObj.readAsDataURL(files[0])
+  ReaderObj.onloadend = function() {
+    $(".input_caption").hide()
+    $(".input_image").removeClass("drop_mode")
+    var image = $(".input_image img")
+    image.attr("src", this.result)
+    // image.cropper({aspectRatio : 1.0});
+    // if (!cropper) {
+    //   cropper = image.data('cropper');
+    // }
+  }
+}
+
+$(".input_image").on('drop', function(e) {
+  files = e.originalEvent.dataTransfer.files;
+  loadPreviewFromFiles(files)
+});
+
+$(".hidden_upload").on("change", function() {
+  var files = !!this.files ? this.files : []
+  if (!files.length || !window.FileReader) {
+    return
+  }
+  if (/^image/.test(files[0].type)) {
+    loadPreviewFromFiles(files)
+  } else {
+    alert("Invalid input")
+  }
+})
+
+$('body').on('click', '.submit', function(e) {
+  var src = $('.input_image img').attr('src');
+  src = resizeImage(src)
+  ws.send(src, function(e) {
+    notifyError(e)
+  })
+})
+
+$('body').on('click', '.clear', function(e) {
+  // if (cropper) {
+  //   cropper.destroy();
+  //   cropper = null
+  // }
+  $(".input_caption").show()
+  $(".input_image img").removeAttr("src");
+  $(".input_image").addClass("drop_mode")
+})
