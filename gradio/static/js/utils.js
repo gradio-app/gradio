ed1476b225e323d80f16c3b875636ff84331f099
Your Name <you@example.com>
2019-03-05 22:53:59 -0800

    confidence intervals, img upload bug fix

---
+++ gradio/static/js/utils.js
@@ -0,0 +1,27 @@
+function resizeImage(base64Str) {
+
+    var img = new Image();
+    img.src = base64Str;
+    var canvas = document.createElement('canvas');
+    var MAX_WIDTH = 360;
+    var MAX_HEIGHT = 360;
+    var width = img.width;
+    var height = img.height;
+
+    if (width > height) {
+      if (width > MAX_WIDTH) {
+        height *= MAX_WIDTH / width;
+        width = MAX_WIDTH;
+      }
+    } else {
+      if (height > MAX_HEIGHT) {
+        width *= MAX_HEIGHT / height;
+        height = MAX_HEIGHT;
+      }
+    }
+    canvas.width = width;
+    canvas.height = height;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, width, height);
+    return canvas.toDataURL();
+  }
