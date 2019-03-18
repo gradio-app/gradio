7739987a2e7882219d8bc69bce6fe57bcf0845a1
Abubakar Abid <a12d@stanford.edu>
2019-03-18 03:17:04 -0700

    added ability to close ports if running from same instance

--- gradio/inputs.py
+++ gradio/inputs.py
@@ -143,6 +143,7 @@ class ImageUpload(AbstractInput):
             array = im.reshape(1, self.image_width, self.image_height, self.num_channels)
         return array
 
+
 class CSV(AbstractInput):
 
     def get_template_path(self):
@@ -153,6 +154,7 @@ class CSV(AbstractInput):
         By default, no pre-processing is applied to text.
         """
         return inp
+
 
 # Automatically adds all subclasses of AbstractInput into a dictionary (keyed by class name) for easy referencing.
 registry = {cls.__name__.lower(): cls for cls in AbstractInput.__subclasses__()}
