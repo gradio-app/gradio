307f3e5b8cd0214f5165c66a72d6441fdcd649a0
Abubakar Abid <a12d@stanford.edu>
2019-04-18 03:39:44 -0700

    replaced ws with http; run test_keras notebook

--- gradio/interface.py
+++ gradio/interface.py
@@ -3,23 +3,14 @@ This is the core file in the `gradio` package, and defines the Interface class, 
 interface using the input and output types.
 '''
 
-import asyncio
-import websockets
-import nest_asyncio
 import webbrowser
 import gradio.inputs
 import gradio.outputs
 from gradio import networking, strings, inputs
 import tempfile
-import threading
 import traceback
 import urllib
-import json
-import os
-import errno
-
-
-nest_asyncio.apply()
+import tensorflow as tf
 
 LOCALHOST_IP = '127.0.0.1'
 SHARE_LINK_FORMAT = 'https://{}.gradio.app/'
@@ -105,44 +96,6 @@ class Interface:
 
         raise ValueError("model_type could not be inferred, please specify parameter `model_type`")
 
-    async def communicate(self, websocket, path):
-        """
-        Method that defines how this interface should communicates with the websocket. (1) When an input is received by
-        the websocket, it is passed into the input interface and preprocssed. (2) Then the model is called to make a
-        prediction. (3) Finally, the prediction is postprocessed to get something to be displayed by the output.
-        :param websocket: a Websocket server used to communicate with the interface frontend
-        :param path: not used, but required for compliance with websocket library
-        """
-        while True:
-            try:
-                msg = json.loads(await websocket.recv())
-                if msg['action'] == 'input':
-                    processed_input = self.input_interface.preprocess(msg['data'])
-                    prediction = self.predict(processed_input)
-                    processed_output = self.output_interface.postprocess(prediction)
-                    output = {
-                    'action': 'output',
-                    'data': processed_output,
-                    }
-                    await websocket.send(json.dumps(output))
-                if msg['action'] == 'flag':
-                    if not os.path.exists(os.path.dirname('gradio-flagged/')):
-                        try:
-                            os.makedirs(os.path.dirname('gradio-flagged/'))
-                        except OSError as exc: # Guard against race condition
-                            if exc.errno != errno.EEXIST:
-                                raise
-                    f = open('gradio-flagged/gradio-flagged.txt','a+')
-                    f.write(str(msg['data']))
-                    f.close()
-                    inp = msg['data']['input']
-                    self.input_interface.rebuild_flagged(inp)
-
-            except websockets.exceptions.ConnectionClosed:
-                pass
-            # except Exception as e:
-            #     print(e)
-
     def predict(self, preprocessed_input):
         """
         Method that calls the relevant method of the model object to make a prediction.
@@ -156,9 +109,7 @@ class Interface:
             return self.model_obj(preprocessed_input)
         elif self.model_type=='pytorch':
             import torch
-            print(preprocessed_input.dtype)
             value = torch.from_numpy(preprocessed_input)
-            print(value.dtype)
             value = torch.autograd.Variable(value)
             prediction = self.model_obj(value)
             return prediction.data.numpy()
@@ -228,15 +179,10 @@ class Interface:
 
         output_directory = tempfile.mkdtemp()
         # Set up a port to serve the directory containing the static files with interface.
-        server_port, httpd = networking.start_simple_server(output_directory)
+        server_port, httpd = networking.start_simple_server(self, output_directory)
         path_to_local_server = 'http://localhost:{}/'.format(server_port)
         networking.build_template(output_directory, self.input_interface, self.output_interface)
 
-        # Set up a port to serve a websocket that sets up the communication between the front-end and model.
-        websocket_port = networking.get_first_available_port(
-            INITIAL_WEBSOCKET_PORT, INITIAL_WEBSOCKET_PORT + TRY_NUM_PORTS)
-        start_server = websockets.serve(self.communicate, LOCALHOST_IP, websocket_port)
-        networking.set_socket_port_in_js(output_directory, websocket_port)  # sets the websocket port in the JS file.
         networking.set_interface_types_in_config_file(output_directory,
                                                       self.input_interface.__class__.__name__.lower(),
                                                       self.output_interface.__class__.__name__.lower())
@@ -258,7 +204,7 @@ class Interface:
         if share:
             try:
                 path_to_ngrok_server, ngrok_api_ports = networking.setup_ngrok(
-                    server_port, websocket_port, output_directory, self.ngrok_api_ports)
+                    server_port, output_directory, self.ngrok_api_ports)
                 self.ngrok_api_ports = ngrok_api_ports
             except RuntimeError:
                 path_to_ngrok_server = None
@@ -267,7 +213,7 @@ class Interface:
         else:
             if is_colab:  # For a colab notebook, create a public link even if share is False.
                 path_to_ngrok_server, ngrok_api_ports = networking.setup_ngrok(
-                    server_port, websocket_port, output_directory, self.ngrok_api_ports)
+                    server_port, output_directory, self.ngrok_api_ports)
                 self.ngrok_api_ports = ngrok_api_ports
                 if self.verbose:
                     print(strings.en["COLAB_NO_LOCAL"])
@@ -282,14 +228,6 @@ class Interface:
             path_to_ngrok_interface_page = SHARE_LINK_FORMAT.format(subdomain)
             if self.verbose:
                 print(strings.en["MODEL_PUBLICLY_AVAILABLE_URL"].format(path_to_ngrok_interface_page))
-
-        # Keep the server running in the background.
-        asyncio.get_event_loop().run_until_complete(start_server)
-        try:
-            _ = get_ipython()
-        except NameError:  # Runtime errors are thrown in jupyter notebooks because of async.
-            t = threading.Thread(target=asyncio.get_event_loop().run_forever, daemon=True)
-            t.start()
 
         if inline is None:
             try:  # Check if running interactively using ipython.
