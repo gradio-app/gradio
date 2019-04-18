bdcbd5e8b4e795539711ebcbe8fb7aee9f5cde29
Abubakar Abid <a12d@stanford.edu>
2019-04-18 03:39:44 -0700

    replaced ws with http; run test_keras notebook

--- gradio/static/js/all_io.js
+++ gradio/static/js/all_io.js
@@ -1,27 +1,25 @@
-var NGROK_URL = "{{ngrok_socket_url}}"
-var SOCKET_PORT = "{{socket_port}}"
-
-var origin = window.location.origin;
-if (origin.includes("ngrok") || origin.includes("gradio.app")){ //TODO(abidlabs): better way to distinguish localhost?
-    var ws = new WebSocket(NGROK_URL)
-} else {
-    var ws = new WebSocket("ws://127.0.0.1:" + SOCKET_PORT + "/")
-}
-ws.onclose = function(event) {
-  console.log("WebSocket is closed now.");
-}
-
 var io_master = {
   input: function(interface_id, data) {
     this.last_input = data;
     this.last_output = null;
-    var ws_data = {
+    var post_data = {
       'action': 'input',
       'data': data
-    }
-    ws.send(JSON.stringify(ws_data), function(e) {
-      console.log(e)
-    })
+    };
+    $.ajax({type: "POST",
+        url: "/api/predict/",
+        data: JSON.stringify(post_data),
+        success: function(output){
+            if (output['action'] == 'output') {
+              io_master.output(output['data']);
+            }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+         console.log(XMLHttpRequest);
+         console.log(textStatus);
+         console.log(errorThrown);
+        }
+    });
   },
   output: function(data) {
     this.last_output = data;
@@ -35,16 +33,16 @@ var io_master = {
         'output' : this.last_output,
         'message' : message
       }
-    }
+    };
     ws.send(JSON.stringify(ws_data), function(e) {
       console.log(e)
     })
   }
-}
+};
 
-ws.onmessage = function (event) {
-  var output = JSON.parse(event.data)
-  if (output['action'] == 'output') {
-    io_master.output(output['data']);
-  }
-}
+// ws.onmessage = function (event) {
+//   var output = JSON.parse(event.data)
+//   if (output['action'] == 'output') {
+//     io_master.output(output['data']);
+//   }
+// }
