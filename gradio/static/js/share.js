5f74c2df3dacef1e777f73dfc03fa3e203f5bd4f
Your Name <you@example.com>
2019-04-18 20:47:24 -0700

    sharing in progress

--- gradio/static/js/share.js
+++ gradio/static/js/share.js
@@ -3,23 +3,33 @@ function enable_sharing() {
     let name = $("#share_name").val()
     let email = $("#share_email").val()
     if (name && email) {
-      $.post("https://gradio.app/send_email", {
-        "url": config["ngrok_socket_url"],
-        "name": name,
-        "email": email
-      }).done(function() {
-        $("#share_message").text("Shared successfully.");
-      }).fail(function() {
-        $("#share_message").text("Failed to share.");
-      }).always(function() {
-        $("#share_form").hide();
-        $("#share_complete").show();
-      });
+      $.ajax({
+        "url" : "https://gradio.app/send_email",
+        "type": "POST",
+        "crossDomain": true,
+        "data": JSON.stringify({
+          "url": config["ngrok_socket_url"],
+          "name": name,
+          "email": email
+        }),
+        "success": function() {
+          $("#share_message").text("Shared successfully.");
+          $("#share_more").text("Share more");
+        },
+        "error": function() {
+          $("#share_message").text("Failed to share.");
+          $("#share_more").text("Try again");
+        },
+        "complete": function() {
+          $("#share_form").hide();
+          $("#share_complete").show();
+        }
+      })
     }
   })
 }
 
 $("#share_more").click(function (evt) {
   $("#share_form").show();
-  $("#share_complete").hide();  
+  $("#share_complete").hide();
 })
