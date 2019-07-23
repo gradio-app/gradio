5f232cfac5a0fd786cb3ab5c2be13ff138e18637
Abubakar Abid <a12d@stanford.edu>
2019-07-22 23:41:25 -0700

    released 0.8.1

--- build/lib/gradio/static/js/interfaces/input/sketchpad.js
+++ build/lib/gradio/static/js/interfaces/input/sketchpad.js
@@ -5,8 +5,13 @@ const sketchpad_input = {
       <div id="brush_2" size="16" class="brush selected"></div>
       <div id="brush_3" size="24" class="brush"></div>
     </div>
-    <div class="canvas_holder">
-      <canvas id="canvas"></canvas>
+    <div class="view_holders">
+      <div class="saliency_holder hide">
+        <canvas class="saliency"></canvas>
+      </div>
+      <div class="canvas_holder">
+        <canvas id="canvas"></canvas>
+      </div>
     </div>`,
   init: function() {
     var io = this;
@@ -14,10 +19,12 @@ const sketchpad_input = {
         this.target.find(".canvas_holder").height()) - 2 // dimension - border
     var id = this.id;
     this.sketchpad = new Sketchpad({
-      element: '.interface[interface_id=' + id + '] > .canvas_holder canvas',
+      element: '.interface[interface_id=' + id + '] .canvas_holder canvas',
       width: dimension,
       height: dimension
     });
+    this.target.find(".saliency")
+      .attr("width", dimension+"px").attr("height", dimension+"px");
     this.sketchpad.penSize = this.target.find(".brush.selected").attr("size");
     this.canvas = this.target.find('.canvas_holder canvas')[0];
     this.context = this.canvas.getContext("2d");
@@ -31,8 +38,28 @@ const sketchpad_input = {
     var dataURL = this.canvas.toDataURL("image/png");
     this.io_master.input(this.id, dataURL);
   },
+  output: function(data) {
+    this.target.find(".saliency_holder").removeClass("hide");
+    var ctx = this.target.find(".saliency")[0].getContext('2d');
+    let dimension = this.target.find(".saliency").width();
+    console.log(data, dimension, dimension);
+    paintSaliency(data, dimension, dimension, ctx);
+  },
   clear: function() {
     this.context.clearRect(0, 0, this.context.canvas.width, this.context.
         canvas.height);
+    this.target.find(".saliency_holder").addClass("hide");
+  },
+  renderFeatured: function(data) {
+    return `<img src=${data}>`;
+  },
+  loadFeatured: function(data) {
+    let ctx = $(".canvas_holder canvas")[0].getContext("2d");
+    var img = new Image;
+    let dimension = this.target.find(".canvas_holder canvas").width();
+    img.onload = function(){
+      ctx.drawImage(img,0,0,dimension,dimension);
+    };
+    img.src = data;
   }
 }
