e1b25b2901b961f3a414ab6fc518ec3c3ac31c91
Abubakar Abid <a12d@stanford.edu>
2019-07-25 14:14:32 -0700

    minor changes to scripts

--- examples/mnist_saliency.py
+++ examples/mnist_saliency.py
@@ -8,21 +8,21 @@ from tensorflow.keras import backend as K
 (x_train, y_train),(x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train, x_test = x_train.reshape(-1,784) / 255.0, x_test.reshape(-1,784) / 255.0
 
-model = tf.keras.models.Sequential()
 
-model.add(Reshape((28, 28, 1), input_shape=(784,)))
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation='softmax'))
-
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-model.fit(x_train, y_train, epochs=1)
+def get_trained_model(n):
+    model = tf.keras.models.Sequential()
+    model.add(Reshape((28, 28, 1), input_shape=(784,)))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x_train[:n], y_train[:n], epochs=1)
+    return model
 
 
 def saliency(interface, model, input, processed_input, output, processed_output):
@@ -40,8 +40,9 @@ def saliency(interface, model, input, processed_input, output, processed_output)
             return normalized_saliency.reshape(28, 28)
 
 
-sketchpad = gradio.inputs.Sketchpad(flatten=True, sample_inputs=x_train[:10])
-iface = gradio.Interface(inputs=sketchpad, outputs="label", model=model, model_type='keras', saliency=saliency)
-httpd, path_to_local_server, share_url = iface.launch(inline=True, share=True)
+model = get_trained_model(n=100)
+sketchpad = gradio.inputs.Sketchpad(flatten=True, sample_inputs=x_test[:10])
+io = gradio.Interface(inputs=sketchpad, outputs="label", model=model, model_type="keras", saliency=saliency)
+httpd, path_to_local_server, share_url = io.launch(inline=True, share=True, inbrowser=True)
 
-print("MNIST model with saliency: ", share_url)
+print("URL for MNIST model interface with saliency:", share_url)
