6c5e63053607ec502cb7723def3e410fa0dc207a
Ali Abid <aliabid94@gmail.com>
2020-06-26 12:41:26 -0700

    latest

--- demo/basic_text.py
+++ demo/basic_text.py
@@ -3,7 +3,11 @@ from time import sleep
 
 def answer_question(text1, text2):
     sleep(2)
-    return text1[::-1]
+    return text1[::-1], [
+        ("Value 1", 12.3),
+        ("Section", "DF3"),
+        ("Confidence", 100),
+    ]
 
 
 gr.Interface(answer_question, 
@@ -12,5 +16,6 @@ gr.Interface(answer_question,
                 gr.inputs.Textbox(label="text 2", lines=4),
             ], [
                 gr.outputs.Textbox(label="out", lines=8),
+                "key_values"
             ]
             ).launch(share=True)
--- demo/webcam.py
+++ demo/webcam.py
@@ -4,4 +4,4 @@ import numpy as np
 def snap(image):
     return np.flipud(image)
 
-gr.Interface(snap, "webcam", "image", live=True, show_input=False).launch()
+gr.Interface(snap, "webcam", "image").launch()
