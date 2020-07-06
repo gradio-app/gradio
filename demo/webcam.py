5c279d22533e2c75c75fc5060a4a6b80afdefbff
Ali Abid <aliabid94@gmail.com>
2020-07-06 11:12:48 -0700

    changes

--- demo/webcam.py
+++ demo/webcam.py
@@ -2,6 +2,6 @@ import gradio as gr
 import numpy as np
 
 def snap(image):
-    return np.flipud(image)
+    return image
 
-gr.Interface(snap, "webcam", "image").launch()
+gr.Interface(snap, gr.inputs.Webcam(shape=(50,100)), "image").launch()
