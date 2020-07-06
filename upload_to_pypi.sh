990d72c1a7fd408cf7b6ff888a6d41cf897b948e
Abubakar Abid <a12d@stanford.edu>
2020-07-06 17:19:36 -0500

    updated PyPi version

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# Before running this, go to setup.py and update the version # of the library
 
-echo "Make sure to update the version number!!!"
+echo "Make sure to update the version number in setup.py!!!"
 rm dist/*
 python setup.py sdist bdist_wheel
 python -m twine upload dist/*
