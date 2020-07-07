a1a2096a743057282856f11adaed08b56f2de3b4
Abubakar Abid <a12d@stanford.edu>
2020-07-07 00:51:07 -0500

    added detailed explanations of what each output expects along with relevant tests

--- test/test_interface.py
+++ test/test_interface.py
@@ -7,8 +7,7 @@ import gradio.outputs
 
 class TestInterface(unittest.TestCase):
     def test_input_output_mapping(self):
-        io = gr.Interface(inputs='SketCHPad', outputs='textBOX', fn=lambda
-            x: x)
+        io = gr.Interface(inputs='SketCHPad', outputs='textBOX', fn=lambda x: x)
         self.assertIsInstance(io.input_interfaces[0], gradio.inputs.Sketchpad)
         self.assertIsInstance(io.output_interfaces[0], gradio.outputs.Textbox)
 
@@ -18,52 +17,15 @@ class TestInterface(unittest.TestCase):
         self.assertEqual(io.input_interfaces[0], inp)
 
     def test_output_interface_is_instance(self):
-        # out = gradio.outputs.Label(show_confidences=False)
         out = gradio.outputs.Label()
         io = gr.Interface(inputs='SketCHPad', outputs=out, fn=lambda x: x)
         self.assertEqual(io.output_interfaces[0], out)
 
-    def test_keras_model(self):
-        try:
-            import tensorflow as tf
-        except:
-            raise unittest.SkipTest("Need tensorflow installed to do keras-based tests")
-        inputs = tf.keras.Input(shape=(3,))
-        x = tf.keras.layers.Dense(4, activation=tf.nn.relu)(inputs)
-        outputs = tf.keras.layers.Dense(5, activation=tf.nn.softmax)(x)
-        model = tf.keras.Model(inputs=inputs, outputs=outputs)
-        io = gr.Interface(inputs='SketCHPad', outputs='textBOX', fn=model)
-        # pred = io.predict(np.ones(shape=(1, 3), ))
-        # self.assertEqual(pred.shape, (1, 5))
-
-    def test_func_model(self):
+    def test_prediction(self):
         def model(x):
             return 2*x
-        io = gr.Interface(inputs='SketCHPad', outputs='textBOX', fn=model)
-        # pred = io.predict(np.ones(shape=(1, 3)))
-        # self.assertEqual(pred.shape, (1, 3))
-
-    def test_pytorch_model(self):
-        try:
-            import torch
-        except:
-            raise unittest.SkipTest("Need torch installed to do pytorch-based tests")
-
-        class TwoLayerNet(torch.nn.Module):
-            def __init__(self):
-                super(TwoLayerNet, self).__init__()
-                self.linear1 = torch.nn.Linear(3, 4)
-                self.linear2 = torch.nn.Linear(4, 5)
-
-            def forward(self, x):
-                h_relu = torch.nn.functional.relu(self.linear1(x))
-                y_pred = self.linear2(h_relu)
-                return y_pred
-
-        model = TwoLayerNet()
-        io = gr.Interface(inputs='SketCHPad', outputs='textBOX', fn=model)
-        # pred = io.predict(np.ones(shape=(1, 3), dtype=np.float32))
-        # self.assertEqual(pred.shape, (1, 5))
+        io = gr.Interface(inputs='textbox', outputs='textBOX', fn=model)
+        self.assertEqual(io.predict[0](11), 22)
 
 
 if __name__ == '__main__':
