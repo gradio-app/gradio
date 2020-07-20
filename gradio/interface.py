10279498d26c6bd4a8b72294ef23cf21c2295cc6
Abubakar Abid <a12d@stanford.edu>
2020-07-20 12:01:37 -0500

    created gradio interfaces now include metatags

--- gradio/interface.py
+++ gradio/interface.py
@@ -386,6 +386,7 @@ class Interface:
             config["examples"] = processed_examples
 
         networking.set_config(config, output_directory)
+        networking.set_meta_tags(output_directory, self.title, self.description, self.thumbnail)
 
         if debug:
             while True:
@@ -408,7 +409,7 @@ class Interface:
 
     @classmethod
     def get_instances(cls):
-        return list(Interface.instances) #Returns list of all current instances
+        return list(Interface.instances)  # Returns list of all current instances.
 
 
 def reset_all():
