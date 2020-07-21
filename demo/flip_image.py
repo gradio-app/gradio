55933718ce2b397b97e5092e2f15386c16e07c15
Ali Abid <aliabid94@gmail.com>
2020-07-21 07:24:38 -0700

    examples load fix

--- demo/flip_image.py
+++ demo/flip_image.py
@@ -12,13 +12,15 @@ def flip2(image):
     start = time()
     return np.fliplr(image)
 
+def flip10(i1, i2, i3, i4, i5):
+    return i1 + i2
 
-gr.Interface(flip, 
-             gr.inputs.Image(), 
+gr.Interface(flip10, 
+             ["image"] * 5, 
              ["image"],
              examples=[
-                 ["images/cheetah1.jpg"],
-                 ["images/cheetah2.jpg"],
-                 ["images/lion.jpg"],
+                 ["images/cheetah1.jpg"] * 5,
+                 ["images/cheetah2.jpg"] * 5,
+                 ["images/lion.jpg"] * 5,
              ]
              ).launch()
