926eccbdc9f508f18ab12b6fd25739ab5039e80d
Ali Abid <aliabid94@gmail.com>
2020-08-20 04:57:07 -0700

    demos

---
+++ demo/zip_two_files.py
@@ -0,0 +1,10 @@
+import gradio as gr
+from zipfile import ZipFile
+
+def zip_two_files(file1, file2):
+    with ZipFile('tmp.zip', 'w') as zipObj:
+        zipObj.write(file1.name, "file1")
+        zipObj.write(file2.name, "file2")
+    return "tmp.zip"
+
+gr.Interface(zip_two_files, ["file", "file"], "file").launch()
