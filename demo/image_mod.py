cbdfcd5dc731cc5677e1cfe7fa99b891dd60ae8e
Ali Abid <aliabid94@gmail.com>
2020-08-21 14:20:05 -0700

    include demos as documentation

--- demo/image_mod.py
+++ demo/image_mod.py
@@ -1,18 +1,17 @@
+# Demo: (Image) -> (Image)
+
 import gradio as gr
 from time import time
-from PIL import Image, ImageFilter
+from PIL import Image
 
 
 def image_mod(image):
-    return image.rotate(45), image.filter(ImageFilter.FIND_EDGES)
+    return image.rotate(45)
 
 
 gr.Interface(image_mod, 
              gr.inputs.Image(type="pil"), 
-             [
-                gr.outputs.Image(type="pil"), 
-                gr.outputs.Image(type="pil"), 
-             ],
+             gr.outputs.Image(type="pil"), 
              examples=[
                  ["images/cheetah1.jpg"],
                  ["images/cheetah2.jpg"],
