7df41f521730f2b0f440bb6eb6f8ddfee2d8f397
Ali Abid <aliabid94@gmail.com>
2020-11-04 07:20:37 -0800

    navigate examples with arrow keys

--- demo/calculator.py
+++ demo/calculator.py
@@ -16,10 +16,10 @@ iface = gr.Interface(calculator,
     "number",
     examples=[
         [
-            random.randint(0, 10), 
+            random.randint(1, 10), 
             random.choice(["add", "subtract", "multiply", "divide"]),
-            random.randint(0, 5),
-        ] for _ in range(40)
+            random.randint(1, 5),
+        ] for _ in range(10)
     ]
 )
 if __name__ == "__main__":
