ec6f7507dd945a0a0ad5afc5daeb7058f85531d5
Ali Abid <aliabid94@gmail.com>
2020-11-19 06:29:32 -0800

    fix webcam live mode

--- demo/image_classifier.py
+++ demo/image_classifier.py
@@ -1,7 +1,7 @@
 # Demo: (Image) -> (Label)
 
 import gradio as gr
-# import tensorflow as tf
+import tensorflow as tf
 
 import numpy as np
 from PIL import Image
@@ -15,13 +15,12 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(current_dir, "files/imagenet_labels.json")) as labels_file:
     labels = json.load(labels_file)
 
-# mobile_net = tf.keras.applications.MobileNetV2()
+mobile_net = tf.keras.applications.MobileNetV2()
 
 
 def image_classifier(im):
-    return 0
     arr = np.expand_dims(im, axis=0)
-    # arr = tf.keras.applications.mobilenet.preprocess_input(arr)
+    arr = tf.keras.applications.mobilenet.preprocess_input(arr)
     prediction = mobile_net.predict(arr).flatten()
     return {labels[i]: float(prediction[i]) for i in range(1000)}
 
@@ -38,4 +37,4 @@ iface = gr.Interface(image_classifier, image, label,
     ])
 
 if __name__ == "__main__":
-    iface.launch()
+    iface.launch(share=True)
