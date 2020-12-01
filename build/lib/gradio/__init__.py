01ebc24df93d6f8386367eca6acffc1003875012
Ali Abid <aliabid94@gmail.com>
2020-12-01 11:46:23 -0800

    removing build again

--- build/lib/gradio/__init__.py
+++
@@ -1,5 +0,0 @@
-from gradio.interface import *  # This makes it possible to import `Interface` as `gradio.Interface`.
-import pkg_resources
-
-current_pkg_version = pkg_resources.require("gradio")[0].version
-__version__ = current_pkg_version
