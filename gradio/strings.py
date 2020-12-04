671263c476293814a1c1c69630c0652ae6df799b
aliabd <ali.si3luwa@gmail.com>
2020-12-05 01:02:07 +0400

    added launch counter

--- gradio/strings.py
+++ gradio/strings.py
@@ -8,5 +8,7 @@ en = {
     "MODEL_PUBLICLY_AVAILABLE_URL": "Model available publicly at: {} (may take up to a minute for link to be usable)",
     "GENERATING_PUBLIC_LINK": "Generating public link (may take a few seconds...):",
     "TF1_ERROR": "It looks like you might be using tensorflow < 2.0. Please pass capture_session=True in Interface() to"
-                 " avoid the 'Tensor is not an element of this graph.' error."
+                 " avoid the 'Tensor is not an element of this graph.' error.",
+    "BETA_INVITE": "\nWe want to invite you to become a beta user.\nYou'll get early access to new and premium "
+                   "features (persistent links, hosting, and more).\nIf you're interested please email beta@gradio.app\n"
 }
