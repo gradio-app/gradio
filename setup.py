c38148cae4fe968b4f1babdbe8903cef05bf6380
Ali Abid <aliabid94@gmail.com>
2021-05-24 14:49:34 -0700

    updated PyPi version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='gradio',
-    version='1.7.7',
+    version='2.0.0',
     include_package_data=True,
     description='Python library for easily interacting with trained machine learning models',
     author='Abubakar Abid',
