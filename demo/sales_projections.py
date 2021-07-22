0d5057f6b8e19c9afda55640b08dc0a49ae721ee
Ali Abid <ali.abid@gradio.app>
2021-07-22 11:25:50 -0700

    updated PyPi version

---
+++ demo/sales_projections.py
@@ -0,0 +1,32 @@
+import gradio as gr
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def sales_projections(employee_data):
+    sales_data = employee_data.iloc[:, 1:4].astype("int").to_numpy()
+    regression_values = sales_data.apply_along_axis(lambda row: 
+        np.array(np.poly1d(np.polyfit([0,1,2], row, 2))))
+    projected_months = np.repeat(np.expand_dims(
+        np.arange(3,12), 0), len(sales_data), axis=0)
+    projected_values = np.array([
+        month * month * regression[0] + month * regression[1] + regression[2]
+        for month, regression in zip(projected_months, regression_values)])
+    plt.plot(projected_values.T)
+    plt.legend(employee_data["Name"])
+    return employee_data, plt.gcf(), regression_values
+
+iface = gr.Interface(sales_projections, 
+    gr.inputs.Dataframe(
+        headers=["Name", "Jan Sales", "Feb Sales", "Mar Sales"],
+        default=[["Jon", 12, 14, 18], ["Alice", 14, 17, 2], ["Sana", 8, 9.5, 12]]
+    ),
+    [
+        "dataframe",
+        "figure",
+        "numpy"
+    ],
+    description="Enter sales figures for employees to predict sales trajectory over year."
+)
+if __name__ == "__main__":
+    iface.launch()
