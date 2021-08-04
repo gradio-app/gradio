9677dd1e85ffd9799b7ba71daf4d95f715e410f3
Ubuntu <ubuntu@ip-172-31-29-79.us-east-2.compute.internal>
2021-08-04 01:15:58 +0000

    updated PyPi version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='gradio',
-    version='2.2.5',
+    version='2.2.6',
     include_package_data=True,
     description='Python library for easily interacting with trained machine learning models',
     author='Abubakar Abid',
