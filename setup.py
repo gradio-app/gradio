7325c750fcedf4aa9e1b429e3f2481b2fd478a60
root <root@ip-172-31-29-79.us-east-2.compute.internal>
2021-09-21 01:40:50 +0000

    updated PyPi version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='gradio',
-    version='2.3.4',
+    version='2.3.5b',
     include_package_data=True,
     description='Python library for easily interacting with trained machine learning models',
     author='Abubakar Abid',
