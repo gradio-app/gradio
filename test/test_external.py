98c10fcb02420b1a1c6fa08f4ab720e86d3fffd4
Abubakar Abid <a12d@stanford.edu>
2021-10-19 10:59:44 -0500

    added some more tests for external APIs

--- test/test_external.py
+++ test/test_external.py
@@ -1,11 +1,12 @@
 import unittest
+import pathlib
 import gradio as gr
 
 """
 WARNING: These tests have an external dependency: namely that Hugging Face's Hub and Space APIs do not change, and they keep their most famous models up.
 """
 
-class TestHuggingFaceModels(unittest.TestCase):
+class TestHuggingFaceModelAPI(unittest.TestCase):
     def test_gpt2(self):
         interface_info = gr.external.get_huggingface_interface("gpt2", api_key=None, alias=None)
         self.assertEqual(interface_info["fn"].__name__, "gpt2")
@@ -18,7 +19,7 @@ class TestHuggingFaceModels(unittest.TestCase):
         self.assertIsInstance(interface_info["inputs"], gr.inputs.Textbox)
         self.assertIsInstance(interface_info["outputs"], gr.outputs.Label)
 
-class TestHuggingFaceSpaces(unittest.TestCase):
+class TestHuggingFaceSpaceAPI(unittest.TestCase):
     def test_english_to_spanish(self):
         interface_info = gr.external.get_spaces_interface("abidlabs/english_to_spanish", api_key=None, alias=None)
         self.assertIsInstance(interface_info["inputs"][0], gr.inputs.Textbox)
@@ -42,6 +43,34 @@ class TestLoadInterface(unittest.TestCase):
         self.assertIsInstance(interface_info["inputs"], gr.inputs.Textbox)
         self.assertIsInstance(interface_info["outputs"], gr.outputs.Label)
 
+class TestCallingLoadInterface(unittest.TestCase):
+    def test_sentiment_model(self):
+        interface_info = gr.external.load_interface("models/distilbert-base-uncased-finetuned-sst-2-english", alias="sentiment_classifier")
+        io = gr.Interface(**interface_info)
+        output = io("I am happy, I love you.")
+        self.assertGreater(output['Positive'], 0.5)
+
+    def test_image_classification_model(self):
+        interface_info = gr.external.load_interface("models/google/vit-base-patch16-224")
+        io = gr.Interface(**interface_info)
+        output = io("images/lion.jpg")
+        self.assertGreater(output['lion'], 0.5)
+
+    def test_translation_model(self):
+        interface_info = gr.external.load_interface("models/t5-base")
+        io = gr.Interface(**interface_info)
+        output = io("My name is Sarah and I live in London")
+        self.assertEquals(output, 'Mein Name ist Sarah und ich lebe in London')
+
+    def test_image_to_image_space(self):
+        def assertIsFile(path):
+            if not pathlib.Path(path).resolve().is_file():
+                raise AssertionError("File does not exist: %s" % str(path))
+
+        interface_info = gr.external.load_interface("spaces/akhaliq/Car_Keypoints")
+        io = gr.Interface(**interface_info)
+        output = io("images/lion.jpg")
+        assertIsFile(output)
 
 if __name__ == '__main__':
     unittest.main()
