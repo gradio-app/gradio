bd80bb7221860df140fb7d5e053f8b2ed94a78c9
Abubakar Abid <a12d@stanford.edu>
2021-10-27 13:43:24 -0500

    started spaces architecture

---
+++ gradio/flagging.py
@@ -0,0 +1,47 @@
+import gradio as gr
+
+class FlaggingHandler():
+    """
+    A class for defining the methods that any FlaggingHandler should have.
+    """
+    def __init__(self, interface, **kwargs):
+        """
+        Parameters:
+        interface (gradio.Interface): The interface object that is calling the flag method.
+        """
+        self.interface = interface
+        self.kwargs = kwargs
+
+    def flag(self, input_data, output_data, flag_option=None, flag_index=None, username=None, path=None):
+        """
+        This method should be overridden by the FlaggingHandler subclass.
+        Parameters:
+        input_data: The input data to be flagged.
+        output_data: The output data to be flagged.
+        flag_option (optional): In the case that flagging_options are provided, the flag option that is being used.
+        flag_index (optional):
+        username (optional): The username of the user that is flagging the data, if logged in.
+        path (optional): The path to the logfile (if not the default path).
+        """
+        raise NotImplementedError("flag() method not implemented.")
+
+
+class DefaultFlaggingHandler(FlaggingHandler):
+    def flag():
+        pass
+        # TODO(aliabd): bring the code from networking.flag() here and tweak it so it works.
+
+# TODO(aliabd): in the Interface class, add a parameter called flagging_handler, which takes a subclass FlaggingHandler and instantiates with the interface object.
+# If the interface is a Spaces demo, then it should use HuggingFaceFlaggingHandler, otherwise it should use DefaultFlaggingHandler.
+# Also we should add a tooltip under the FLAG button that explains what will happen when they click on it (and a disclaimer saying that they should make sure no copyrighted material gets added to their dataset)
+# Can you add a parameter called "flagging_disclaimer=" to Interface() and do the frontend?
+
+class HuggingFaceFlaggingHandler(FlaggingHandler):
+    def flag(self, input_data, output_data, flag_option=None, flag_index=None, username=None, path=None):
+        pass # TODO(abidlabs): implement this as follows
+        # check if a repo exists with username/spacename (if path is None; otherwise, use path)
+        # if not, create a repo with username/spacename
+        # clone the repo and append to a log csv file
+        # push the repo 
+        # write tests
+
