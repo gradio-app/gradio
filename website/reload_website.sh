56cd9952214a05ad831bee657241cf1519d39aa8
Ali <ali.abid@huggingface.co>
2021-12-16 15:16:41 -0800

    fix reload

--- website/reload_website.sh
+++ website/reload_website.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
+git pull > /tmp/git_changes.txt
 
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date. No restart."
-elif [ $LOCAL = $BASE ]; then
-    echo "Restarting..."
-    git pull > /tmp/git_changes.txt
-    if grep "demo/" /tmp/git_changes.txt 
-    then
+if grep -q "Already up to date." /tmp/git_changes.txt; then
+    echo "Already up to date. No reload."
+else
+    echo "Reloading..."
+    if grep -q "demo/" /tmp/git_changes.txt; then
         cd upload_notebooks && python run.py && cd ..
     fi
     docker-compose build
