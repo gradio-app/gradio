789b4374dc0d5c1a633cb083b8e11c4b38ffaa25
Abubakar Abid <islamrealm@gmail.com>
2021-12-16 15:08:27 -0800

    autoreload website on changes

--- website/reload_website.sh
+++ website/reload_website.sh
@@ -9,8 +9,11 @@ if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date. No restart."
 elif [ $LOCAL = $BASE ]; then
     echo "Restarting..."
-    git pull
-    python refresh_google_credentials.py
+    git pull > /tmp/git_changes.txt
+    if grep "demo/" /tmp/git_changes.txt 
+    then
+        cd upload_notebooks && python run.py && cd ..
+    fi
     docker-compose build
-    docker-compose restart -d
+    docker-compose up -d
 fi
