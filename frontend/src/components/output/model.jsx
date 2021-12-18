d1e89af6d348810064484eb120dce8b1a1d740bb
Florian Bruggisser <florian@bildspur.ch>
2021-12-18 12:42:38 +0100

    implemented basic glb loader

--- frontend/src/components/output/model.jsx
+++ frontend/src/components/output/model.jsx
@@ -3,23 +3,47 @@ import BaseComponent from "../base_component";
 import ComponentExample from "../component_example";
 
 class ModelOutput extends BaseComponent {
-  render() {
-    if (this.props.value) {
-      return (
-          <div className="output_model">
-            <h1>example: {this.props.value["name"]}</h1>
-          </div>
-      )
-    } else {
-      return false;
+    componentDidMount() {
+        const canvas = document.getElementById("renderCanvas");
+        const engine = new BABYLON.Engine(canvas, true);
+
+        const scene = new BABYLON.Scene(engine);
+        scene.createDefaultCameraOrLight();
+
+        let base64_model_content = this.props.value["data"];
+        let raw_content = BABYLON.Tools.DecodeBase64(base64_model_content);
+        let blob = new Blob([raw_content]);
+        let url = URL.createObjectURL(blob);
+        BABYLON.SceneLoader.Append("", url, scene, function () {
+            scene.createDefaultCamera(true, true, true);
+        }, undefined, undefined, ".glb");
+
+        engine.runRenderLoop(function () {
+            scene.render();
+        });
+
+        window.addEventListener("resize", function () {
+            engine.resize();
+        });
     }
-  }
+
+    render() {
+        if (this.props.value) {
+            return (
+                <div className="output_model">
+                    <canvas id="renderCanvas"></canvas>
+                </div>
+            )
+        } else {
+            return false;
+        }
+    }
 }
 
 class ModelOutputExample extends ComponentExample {
-  render() {
-    return <div className="output_model_example">{this.props.value}</div>;
-  }
+    render() {
+        return <div className="output_model_example">{this.props.value}</div>;
+    }
 }
 
-export { ModelOutput, ModelOutputExample };
+export {ModelOutput, ModelOutputExample};
