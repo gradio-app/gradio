6867bb65bb2decb53020c34a584b5e2f6fbdb003
Florian Bruggisser <florian@bildspur.ch>
2021-12-18 13:17:49 +0100

    implemented update behaviour in component

--- frontend/src/components/output/model.jsx
+++ frontend/src/components/output/model.jsx
@@ -3,28 +3,47 @@ import BaseComponent from "../base_component";
 import ComponentExample from "../component_example";
 
 class ModelOutput extends BaseComponent {
+    constructor(props) {
+        super(props);
+        this.scene = null;
+    }
+
     componentDidMount() {
         const canvas = document.getElementById("renderCanvas");
         const engine = new BABYLON.Engine(canvas, true);
 
-        const scene = new BABYLON.Scene(engine);
-        scene.createDefaultCameraOrLight();
+        this.scene = new BABYLON.Scene(engine);
+        this.scene.createDefaultCameraOrLight();
 
+        this.addNewModel();
+
+        engine.runRenderLoop(() => {
+            this.scene.render();
+        });
+
+        window.addEventListener("resize", () => {
+            engine.resize();
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.addNewModel();
+    }
+
+    addNewModel() {
+        // remove all existing models
+        for (let mesh of this.scene.meshes) {
+            mesh.dispose();
+        }
+
+        // add new model
         let base64_model_content = this.props.value["data"];
         let raw_content = BABYLON.Tools.DecodeBase64(base64_model_content);
         let blob = new Blob([raw_content]);
         let url = URL.createObjectURL(blob);
-        BABYLON.SceneLoader.Append("", url, scene, function () {
-            scene.createDefaultCamera(true, true, true);
+        BABYLON.SceneLoader.Append("", url, this.scene, () => {
+            this.scene.createDefaultCamera(true, true, true);
         }, undefined, undefined, this.props.value["extension"]);
-
-        engine.runRenderLoop(function () {
-            scene.render();
-        });
-
-        window.addEventListener("resize", function () {
-            engine.resize();
-        });
     }
 
     render() {
