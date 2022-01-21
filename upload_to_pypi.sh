2ec44a403de0573b82b11102bd47d7f6a86976ef
Abubakar Abid <aaabid93@gmail.com>
2022-01-21 09:51:09 -0600

    updated PyPi version to 2.7.2

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -2,7 +2,7 @@
 
 git pull origin master
 
-old_version=$(grep -Po "(?<=version=')[^']+(?=')" setup.py)
+old_version=$(grep -Po "(?<=version=\")[^\"]+(?=\")" setup.py)
 echo "Current version is $old_version. New version?"
 read new_version
 sed -i "s/version='$old_version'/version='$new_version'/g" setup.py
