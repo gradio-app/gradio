efc4e25288201412c0f4738e6743dde6b468576e
Abubakar Abid <aaabid93@gmail.com>
2022-01-21 09:52:35 -0600

    updated PyPi version to 2.7.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="gradio",
-    version="2.7.1",
+    version="2.7.2",
     include_package_data=True,
     description="Python library for easily interacting with trained machine learning models",
     author="Abubakar Abid, Ali Abid, Ali Abdalla, Dawood Khan, Ahsen Khaliq",
