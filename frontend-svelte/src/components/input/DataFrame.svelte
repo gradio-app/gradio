1c1d8c9c6009c21b7bd2486e268b12cab47e1907
pngwn <hello@pngwn.io>
2022-01-25 15:50:34 +0000

    add row/col to table

--- frontend-svelte/src/components/input/DataFrame.svelte
+++ frontend-svelte/src/components/input/DataFrame.svelte
@@ -2,14 +2,23 @@
   import { tick } from "svelte";
 
   export let label = "Title";
-
   export let headers = ["one", "two", "three"];
-  // let sort_by = undefined;
+  export let setValue;
+
   let id = 0;
   let editing = false;
   let selected = false;
-
   let els = {};
+
+  function make_headers(_h) {
+    return _h.map((h) => {
+      const _id = ++id;
+      els[_id] = { cell: null, input: null };
+      return { id: _id, value: h };
+    });
+  }
+
+  let _headers = make_headers(headers);
 
   export let default_data = [
     ["Frank", 32, "Male"],
@@ -34,6 +43,8 @@
         return { value: "", id: _id };
       }),
   ];
+
+  $: setValue(data.map((r) => r.map(({ value }) => value)));
 
   function get_sort_status(name, sort) {
     if (!sort) return "none";
@@ -146,15 +157,86 @@
 
     sort(col, sort_direction);
   }
+
+  let header_edit;
+
+  async function edit_header(_id, select) {
+    header_edit = _id;
+    await tick();
+    els[_id].input.focus();
+
+    if (select) els[_id].input.select();
+  }
+
+  function end_header_edit(event) {
+    switch (event.key) {
+      case "Escape":
+        event.preventDefault();
+        header_edit = false;
+        break;
+      case "Enter":
+        event.preventDefault();
+        header_edit = false;
+    }
+  }
+
+  function add_row() {
+    data.push(
+      headers.map(() => {
+        const _id = ++id;
+        els[_id] = { cell: null, input: null };
+        return { id: _id, value: "" };
+      })
+    );
+    data = data;
+  }
+
+  async function add_col() {
+    for (let i = 0; i < data.length; i++) {
+      const _id = ++id;
+      els[_id] = { cell: null, input: null };
+      data[i].push({ id: _id, value: "" });
+    }
+
+    const _id = ++id;
+    els[_id] = { cell: null, input: null };
+    _headers.push({ id: _id, value: `Header ${_headers.length + 1}` });
+
+    data = data;
+    _headers = _headers;
+
+    await tick();
+
+    edit_header(_id, true);
+  }
+
+  const double_click = (node, { click, dblclick }) => {
+    let timer;
+
+    function handler(event) {
+      if (timer) {
+        clearTimeout(timer);
+        timer = undefined;
+        dblclick(event);
+      } else {
+        timer = setTimeout(() => {
+          click(event);
+          timer = undefined;
+        }, 250);
+      }
+    }
+
+    node.addEventListener("click", handler);
+
+    return {
+      destroy: () => node.removeEventListener("click", handler),
+    };
+  };
 </script>
 
 <h4 id="title">{label}</h4>
+
 <div class="shadow overflow-hidden border-gray-200 rounded-sm relative">
-  <!-- <button class="md:absolute h-10 w-36 right-0 text-xs">+ New Column</button>
-  <button
-    class="absolute h-9 w-full bottom-0 left-0 text-left pl-2 bg-white text-sm"
-    >+ Add New Row</button
-  > -->
   <table
     id="grid"
     role="grid"
@@ -163,21 +245,39 @@
   >
     <thead class="bg-gray-50">
       <tr>
-        {#each headers as header, i}
-          <!-- {@debug i, sort_by} -->
+        {#each _headers as { value, id }, i (id)}
           <th
-            on:click={() => handle_sort(i)}
-            aria-sort={get_sort_status(header, sort_by)}
-            class="after:absolute after:opacity-0 after:content-['▲'] after:ml-2 relative px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            use:double_click={{
+              click: () => handle_sort(i),
+              dblclick: () => edit_header(id),
+            }}
+            aria-sort={get_sort_status(value, sort_by)}
+            class="relative after:absolute after:opacity-0 after:content-['▲'] after:ml-2 after:inset-y-0 after:h-[1.05rem] after:m-auto relative px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
             class:sorted={sort_by === i}
             class:des={sort_by === i && sort_direction === "des"}
           >
-            <span tabindex="-1" role="button">{header}</span>
+            {#if header_edit === id}
+              <input
+                class="bg-transparent inset-y-0 left-6 w-full outline-none absolute p-0 w-3/4 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                tabindex="-1"
+                bind:value
+                bind:this={els[id].input}
+                on:keydown={end_header_edit}
+                on:blur={({ currentTarget }) =>
+                  currentTarget.setAttribute("tabindex", -1)}
+              />
+            {/if}
+            <span
+              tabindex="-1"
+              role="button"
+              class="min-h-full"
+              class:opacity-0={header_edit === id}>{value}</span
+            >
           </th>
         {/each}
       </tr></thead
     ><tbody class="bg-white divide-y divide-gray-200">
-      {#each data as row, i}
+      {#each data as row, i (row)}
         <tr>
           {#each row as { value, id }, j (id)}
             <td
@@ -192,7 +292,8 @@
             >
               <div
                 class:border-gray-600={selected === id}
-                class="px-5 py-3 border-transparent border-[0.125rem]"
+                class:border-transparent={selected !== id}
+                class="min-h-[3.3rem] px-5 py-3  border-[0.125rem]"
               >
                 {#if editing === id}
                   <input
@@ -208,8 +309,10 @@
                   class=" cursor-default w-full"
                   class:opacity-0={editing === id}
                   tabindex="-1"
-                  role="button">{value}</span
+                  role="button"
                 >
+                  {value}
+                </span>
               </div>
             </td>
           {/each}
@@ -218,6 +321,18 @@
     </tbody>
   </table>
 </div>
+<div class="flex justify-end ">
+  <button
+    on:click={add_col}
+    class="hover:bg-gray-100 dark:hover:bg-gray-600 shadow  py-1 px-3 rounded transition  focus:outline-none m-2 mr-0"
+    >New Column</button
+  >
+  <button
+    on:click={add_row}
+    class="bg-yellow-500 hover:bg-yellow-400 dark:bg-red-700 dark:hover:bg-red-600 text-white shadow py-1 px-3 rounded transition focus:outline-none m-2 mr-0"
+    >New Row</button
+  >
+</div>
 
 <style>
   .sorted::after {
@@ -225,6 +340,6 @@
   }
 
   .des::after {
-    transform: rotate(180deg);
+    transform: rotate(180deg) translateY(1.5px);
   }
 </style>
