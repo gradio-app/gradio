cc0965575327cfff1b5048d4ec3c6ff0cb7f329f
Ali Abid <aliabid94@gmail.com>
2022-01-25 20:26:36 -0800

    interpretation

--- frontend-svelte/src/components/utils/helpers.js
+++ frontend-svelte/src/components/utils/helpers.js
@@ -86,3 +86,32 @@ const interpolate = (val, rgb1, rgb2) => {
 const colorToString = (rgb) => {
   return "rgb(" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ")";
 }
+
+export const getObjectFitSize = (
+  contains /* true = contain, false = cover */,
+  containerWidth,
+  containerHeight,
+  width,
+  height
+) => {
+  var doRatio = width / height;
+  var cRatio = containerWidth / containerHeight;
+  var targetWidth = 0;
+  var targetHeight = 0;
+  var test = contains ? doRatio > cRatio : doRatio < cRatio;
+
+  if (test) {
+    targetWidth = containerWidth;
+    targetHeight = targetWidth / doRatio;
+  } else {
+    targetHeight = containerHeight;
+    targetWidth = targetHeight * doRatio;
+  }
+
+  return {
+    width: targetWidth,
+    height: targetHeight,
+    x: (containerWidth - targetWidth) / 2,
+    y: (containerHeight - targetHeight) / 2
+  };
+}
