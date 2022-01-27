d6b1247e2198acf7b30f9e90a4c4c3b94bc72107
aliabid94 <aabid94@gmail.com>
2022-01-26 18:33:49 -0800

    Svelte migration (WIP) (#448)
    * first migration commit
    
    * style comment
    
    * first mvp working with calculator
    
    * ali components
    
    * carousel
    
    * more changes
    
    * changes
    
    * add examples
    
    * examples support
    
    * more changes
    
    * interpretation
    
    * interpretation
    
    * submission state
    
    * first migration commit
    
    * style comment
    
    * first mvp working with calculator
    
    * ali components
    
    * carousel
    
    * more changes
    
    * changes
    
    * add examples
    
    * examples support
    
    * more changes
    
    * interpretation
    
    * interpretation
    
    * submission state
    
    * base image cropper
    
    * add image editor
    
    * css tweaks
    
    * remove dead code
    
    * finalise sketch tools
    
    * add webcam snapshot source
    
    * tweak config
    
    * tweak config
    
    * tweak config
    
    * tweaks
    
    * reset egg files
    
    * lockfile v2
    
    * image tweaks
    
    * record audio from mic
    
    * add audio input components
    
    * audio tweaks
    
    * editable table
    
    * more table tweaks
    
    * sort columns
    
    * add row/col to table
    
    * add output table
    
    * fix broken paths
    
    * fix svelte build destination
    
    * fix svelte build destination again
    
    * fix gitignore
    
    * fix css
    
    * add themes
    
    * add all themes
    
    * snake core classnames
    
    * actually fix themes this time
    
    * merge changes
    
    Co-authored-by: Ali Abid <aliabid94@gmail.com>
    Co-authored-by: Ali Abid <aliabid@Alis-MacBook-Pro.local>
    Co-authored-by: pngwn <hello@pngwn.io>

---
+++ frontend/src/components/Dummy.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let value, setValue, theme;
+    export let choices;
+</script>
+
+<div class="dummy" {theme}>DUMMY</div>
+
+<style lang="postcss">
+</style>
---
+++ frontend/src/components/output/HighlightedText/Component.svelte
@@ -0,0 +1,105 @@
+<script>
+  import { getNextColor } from "../../utils/helpers";
+
+  export let value, theme;
+  export let show_legend, color_map;
+  color_map = color_map || {};
+  let mode;
+
+  if (value.length > 0) {
+    for (let [_, label] of value) {
+      if (label !== null) {
+        if (typeof label === "string") {
+          mode = "categories";
+          if (!(label in color_map)) {
+            let color = getNextColor(Object.keys(color_map).length);
+            color_map[label] = color;
+          }
+        } else {
+          mode = "scores";
+        }
+      }
+    }
+  }
+  console.log(color_map);
+</script>
+
+<div class="output-highlightedtext" {theme}>
+  {#if mode === "categories"}
+    {#if show_legend}
+      <div class="category-legend flex flex-wrap gap-1 mb-2">
+        {#each color_map.entries() as [category, color], i}
+          <div
+            class="category-label px-2 py-1 rounded text-white font-semibold"
+            style={"background-color" + color}
+            key={i}
+          >
+            {category}
+          </div>
+        {/each}
+      </div>
+    {/if}
+    <div
+      class="textfield p-2 bg-white dark:bg-gray-800 rounded box-border max-w-full leading-8 break-all"
+    >
+      {#each value as [text, category], i}
+        <span
+          class="textspan p-1 mr-0.5 bg-opacity-20 dark:bg-opacity-80 rounded-sm"
+          title={category}
+          style={category === null
+            ? ""
+            : `color: ${color_map[category]}; background-color: ${color_map[
+                category
+              ].replace("1)", "var(--tw-bg-opacity))")}`}
+          key={i}
+        >
+          <span class="text dark:text-white">{text}</span>
+          {#if !show_legend && category !== null}
+            <span
+              class="inline-category text-xs text-white ml-0.5 px-0.5 rounded-sm"
+              style={category === null
+                ? ""
+                : `background-color: ${color_map[category]}`}
+            >
+              {category}
+            </span>
+          {/if}
+        </span>
+      {/each}
+    </div>
+  {:else}
+    {#if show_legend}
+      <div
+        class="color_legend flex px-2 py-1 justify-between rounded mb-3 font-semibold"
+        style="background: -webkit-linear-gradient(to right,#8d83d6,(255,255,255,0),#eb4d4b); background: linear-gradient(to right,#8d83d6,rgba(255,255,255,0),#eb4d4b);"
+      >
+        <span>-1</span>
+        <span>0</span>
+        <span>+1</span>
+      </div>
+    {/if}
+    <div
+      class="textfield p-2 bg-white dark:bg-gray-800 rounded box-border max-w-full leading-8 break-all"
+    >
+      {#each value as [text, score], i}
+        <span
+          class="textspan p-1 mr-0.5 bg-opacity-20 dark:bg-opacity-80 rounded-sm"
+          title={value}
+          style={"background-color: rgba(" +
+            (score < 0 ? "141, 131, 214," + -score : "235, 77, 75," + score) +
+            ")"}
+        >
+          <span class="text dark:text-white">{text}</span>
+        </span>
+      {/each}
+    </div>
+  {/if}
+</div>
+
+<style lang="postcss">
+  .output-highlightedtext[theme="default"] {
+    .textfield {
+      @apply shadow;
+    }
+  }
+</style>
