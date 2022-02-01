381bd56ff6c1e83ef0a0791e7b85fde41a9a0855
aliabid94 <aabid94@gmail.com>
2022-02-01 00:40:10 -0800

    Login frontend (#499)

--- demo/hello_world_3/run.py
+++ demo/hello_world_3/run.py
@@ -1,12 +1,10 @@
 import gradio as gr
 
-
 def greet(name, is_morning, temperature):
     salutation = "Good morning" if is_morning else "Good evening"
     greeting = "%s %s. It is %s degrees today" % (salutation, name, temperature)
     celsius = (temperature - 32) * 5 / 9
     return greeting, round(celsius, 2)
-
 
 iface = gr.Interface(
     fn=greet,
