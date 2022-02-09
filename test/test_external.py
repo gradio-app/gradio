3a9d018eab3a2e489e9cd7aebc286a43b4caaa5d
Ömer Faruk Özdemir <farukozderim@gmail.com>
2022-02-09 16:15:01 +0300

    Format-The-Codebase - rename a test

--- test/test_external.py
+++ test/test_external.py
@@ -243,7 +243,7 @@ class TestLoadInterface(unittest.TestCase):
 
 
 class TestLoadFromPipeline(unittest.TestCase):
-    def test_question_answering(self):
+    def test_text_to_text_model_from_pipeline(self):
         pipe = transformers.pipeline(model="sshleifer/bart-tiny-random")
         output = pipe("My name is Sylvain and I work at Hugging Face in Brooklyn")
         self.assertIsNotNone(output)
