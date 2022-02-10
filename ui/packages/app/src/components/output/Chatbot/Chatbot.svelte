cf81aaac819358f0e895643933d3868c69586afe
dawoodkhan82 <dawoodkhan82@gmail.com>
2022-02-10 14:14:42 -0500

    add chatbot component to ui dir

---
+++ ui/packages/app/src/components/output/Chatbot/Chatbot.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let value: Array<string>;
+	export let theme: string;
+</script>
+
+
+<div class="overflow-y-auto h-64 border border-b-0 rounded-t-lg leading-tight">
+	<div class="flex flex-col items-end space-y-4 p-3">
+		{#each value as message}
+			<div class="px-3 py-2 rounded-2xl place-self-start bg-gray-300 dark:bg-gray-850 dark:text-gray-200 mr-7">{ message[1] }</div>
+			<div class="px-3 py-2 rounded-2xl bg-yellow-500 text-white ml-7">{ message[0] }</div> 		
+		{/each}
+	</div>
+</div>
+
+<style lang="postcss">
+</style>
