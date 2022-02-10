47b4b6929017834337fa7148d136a5cd279877db
Ömer Faruk Özdemir <farukozderim@gmail.com>
2022-02-10 11:25:27 +0300

    Format-The-Codebase - update scripts

--- scripts/run_circleci.sh
+++ scripts/run_circleci.sh
@@ -3,8 +3,9 @@ if [ -z "$(ls | grep CONTRIBUTING.md)" ]; then
   echo "Please run the script from repo directory"
   exit -1
 else
-  echo "Building the frontend"
-  cd frontend
-  npm install
-  npm run build
+  echo "Running circleci locally"
+  circleci local execute
 fi
+
+
+
