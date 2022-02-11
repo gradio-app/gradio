3c59c1127bf4f0c813536b7ba03034e608ef4860
pngwn <hello@pngwn.io>
2022-02-11 17:59:12 +0000

    add unit tests

---
+++ ui/packages/app/src/components/utils/helpers.test.ts
@@ -0,0 +1,24 @@
+import { assert, describe, expect, it } from "vitest";
+import { prettyBytes } from "./helpers";
+
+describe("prettyBytes", () => {
+	it("handle B", () => {
+		assert.equal(prettyBytes(10), "10.0 B");
+	});
+
+	it("handles KB", () => {
+		assert.equal(prettyBytes(1_300), "1.3 KB");
+	});
+
+	it("handles MB", () => {
+		assert.equal(prettyBytes(1_300_000), "1.2 MB");
+	});
+
+	it("handles MB", () => {
+		assert.equal(prettyBytes(1_300_000_123), "1.2 GB");
+	});
+
+	it("handles MB", () => {
+		assert.equal(prettyBytes(1_300_000_123_000), "1.2 PB");
+	});
+});
