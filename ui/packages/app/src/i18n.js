bcdad92fe188603c34efcaec7db3169287f4cf9c
dawoodkhan82 <dawoodkhan82@gmail.com>
2022-02-11 11:22:39 -0500

    pretty formatting

--- ui/packages/app/src/i18n.js
+++ ui/packages/app/src/i18n.js
@@ -1,13 +1,13 @@
-import { addMessages, init, getLocaleFromNavigator } from 'svelte-i18n';
-import en from '../public/lang/en.json'
-import es from '../public/lang/es.json'
+import { addMessages, init, getLocaleFromNavigator } from "svelte-i18n";
+import en from "../public/lang/en.json";
+import es from "../public/lang/es.json";
 
-addMessages('en', en);
-addMessages('es', es); 
+addMessages("en", en);
+addMessages("es", es);
 
 export function setupi18n() {
-  init({
-    fallbackLocale: 'en',
-    initialLocale: getLocaleFromNavigator(),
-  });
+	init({
+		fallbackLocale: "en",
+		initialLocale: getLocaleFromNavigator()
+	});
 }
