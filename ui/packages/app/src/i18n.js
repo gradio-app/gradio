1b69d1cb9576bf8650517310801537728e736953
Pierre Fernandez <pierre.fernandezgenestier@gmail.com>
2022-02-16 01:37:15 +0100

    added french

--- ui/packages/app/src/i18n.js
+++ ui/packages/app/src/i18n.js
@@ -1,10 +1,12 @@
 import { addMessages, init, getLocaleFromNavigator } from "svelte-i18n";
 import en from "../public/lang/en.json";
 import es from "../public/lang/es.json";
+import fr from "../public/lang/fr.json";
 import ur from "../public/lang/ur.json";
 
 addMessages("en", en);
 addMessages("es", es);
+addMessages("fr", fr);
 addMessages("ur", ur);
 
 export function setupi18n() {
