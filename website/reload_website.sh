e2b4d0b9fe839695fefd0108325f20b7123c9f2c
aliabd <ali.si3luwa@gmail.com>
2022-03-09 07:06:07 +0200

    added commands to reload script

--- website/reload_website.sh
+++ website/reload_website.sh
@@ -11,6 +11,14 @@ else
     if grep -q "demo/" /tmp/git_changes.txt; then
         cd upload_notebooks && python run.py && cd ..
     fi
-    docker-compose build
-    docker-compose up -d
+    if docker-compose build && docker-compose up -d ; then
+        LATEST=$(git log -1 | fgrep commit)$(git log -1 | tail -1)
+        curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app relaoded successfully! :ship:
+        \n\nLatest:\n>`'"${LATEST}"'`"}'
+        https://hooks.slack.com/services/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
+    else
+        LOGS=$(tail -n 25 /var/mail/ubuntu)
+        curl -X POST -H 'Content-type: application/json' --data '{"text":":o: gradio.app is not tracking master\n\nLogs:\n```'"${LOGS}"'`"}'
+        https://hooks.slack.com/services/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
+    fi
 fi
