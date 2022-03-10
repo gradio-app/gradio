2635c5fbc1e40b206709a105f3b364520837c923
aliabd <ali.si3luwa@gmail.com>
2022-03-10 03:48:48 +0200

    read new webhook from os variable

--- website/reload_website.sh
+++ website/reload_website.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 . /home/ubuntu/.bashrc
 export PATH="/usr/local/bin:/usr/bin:/bin"
+WEBHOOK="${SLACK_WEBHOOK}"
+
 
 git pull > /tmp/git_changes.txt
 
@@ -10,7 +12,7 @@ else
     if grep -q "error" /tmp/git_changes.txt; then
         LOGS=`cat /tmp/git_changes.txt`
         curl -X POST -H 'Content-type: application/json' --data '{"text":":o: gradio.app is not tracking master\n\n Logs:\n```'"${LOGS}"'`"}'
-        https://hooks.slack.com/services/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
+        $WEBHOOK
     fi
     echo "Reloading..."
     if grep -q "demo/" /tmp/git_changes.txt; then
@@ -20,10 +22,10 @@ else
         LATEST=$(git log -1 | fgrep commit)$(git log -1 | tail -1)
         curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app relaoded successfully! :ship:
         \n\nLatest:\n>`'"${LATEST}"'`"}'
-        https://hooks.slack.com/services/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
+        $WEBHOOK
     else
         LOGS=$(tail -n 25 /var/mail/ubuntu)
         curl -X POST -H 'Content-type: application/json' --data '{"text":":o: gradio.app is not tracking master\n\nLogs:\n```'"${LOGS}"'`"}'
-        https://hooks.slack.com/services/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
+        $WEBHOOK
     fi
 fi
