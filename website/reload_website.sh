e6244a0f256aabd13895c24d18529b5fef316098
aliabd <ali.si3luwa@gmail.com>
2022-03-10 05:55:40 +0200

    formatting

--- website/reload_website.sh
+++ website/reload_website.sh
@@ -9,7 +9,7 @@ if grep -q "Already up to date." /tmp/git_changes.txt; then
 else
     if grep -q "error" /tmp/git_changes.txt; then
         LOGS=`cat /tmp/git_changes.txt`
-        curl -X POST -H 'Content-type: application/json' --data '{"text":":o: gradio.app is not tracking master\n\n Logs:\n```'"${LOGS}"'```"}' ${SLACK_WEBHOOK}
+        curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app is not tracking master :o:\n\n Logs:\n```'"${LOGS}"'```"}' ${SLACK_WEBHOOK}
     else
         echo "Reloading..."
         if grep -q "demo/" /tmp/git_changes.txt; then
@@ -17,11 +17,10 @@ else
         fi
         if docker-compose build && docker-compose up -d ; then
           LATEST=$(git log -1 | fgrep commit)$(git log -1 | tail -1)
-          curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app relaoded successfully! :ship:
-          \n\nLatest:\n>`'"${LATEST}"'`"}' ${SLACK_WEBHOOK}
+          curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app relaoded successfully! :ship:\n\n Latest live commit:\n>`'"${LATEST}"'`"}' ${SLACK_WEBHOOK}
         else
           LOGS=$(tail -n 25 /var/mail/ubuntu)
-          curl -X POST -H 'Content-type: application/json' --data '{"text":":o: gradio.app is not tracking master\n\nLogs:\n```'"${LOGS}"'```"}' ${SLACK_WEBHOOK}
+          curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app is not tracking master :o:\n\n Logs:\n```'"${LOGS}"'```"}' ${SLACK_WEBHOOK}
         fi
     fi
 fi
