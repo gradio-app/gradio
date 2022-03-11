a81bc4156583c28213435c4233616c5608650b06
pngwn <hello@pngwn.io>
2022-03-11 16:09:32 +0000

    fix broken thing

--- gradio/static.py
+++ gradio/static.py
@@ -1,20 +1,12 @@
 from __future__ import annotations
 
-<<<<<<< HEAD
-from gradio.component import Component
-=======
 from gradio.components import Component
->>>>>>> Blocks-Components
 
 
 class StaticComponent(Component):
     def __init__(self, label: str):
         self.component_type = "static"
-<<<<<<< HEAD
-        super().__init__(label)
-=======
         super().__init__(label=label)
->>>>>>> Blocks-Components
 
     def process(self, y):
         """
