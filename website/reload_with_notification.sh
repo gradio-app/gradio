36ec376134586a778e655723067a750409795655
aliabd <ali.si3luwa@gmail.com>
2022-03-11 14:50:06 +0200

    correct text escaping for error message

--- website/reload_with_notification.sh
+++ website/reload_with_notification.sh
@@ -5,6 +5,7 @@ export PATH="/usr/local/bin:/usr/bin:/bin"
 ERROR=$(sh ./reload_website.sh 2>&1)
 
 if ! [ $? -eq 0 ]; then
-    ESCAPED_ERROR=$(echo $ERROR | sed -e "s/'/'\\\\''/g; 1s/^/'/; \$s/\$/'/")
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app is not tracking master :o:\n\n Error:\n```'"${ESCAPED_ERROR}"'```"}' ${SLACK_WEBHOOK}
+    data=$( jo text="$(echo "gradio.app is not tracking master :o: \nError:\n\n\`\`\`'$ERROR'\`\`\`")")
+    echo "$data"
+    curl -X POST -H 'Content-type: application/json' --data "$data" ${SLACK_WEBHOOK}
 fi
