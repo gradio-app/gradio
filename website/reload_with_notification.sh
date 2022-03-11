ab86ac1c4f12758378dc62dbfa8bb92111a29617
aliabd <ali.si3luwa@gmail.com>
2022-03-11 03:32:22 +0200

    catching error from script

--- website/reload_with_notification.sh
+++ website/reload_with_notification.sh
@@ -2,7 +2,8 @@
 . /home/ubuntu/.bashrc
 export PATH="/usr/local/bin:/usr/bin:/bin"
 
-if ! sh ./reload_website.sh; then
-    LOGS=$(tail -n 25 /var/mail/ubuntu)
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app is not tracking master :o:\n\n Logs:\n```'"${LOGS}"'```"}' ${SLACK_WEBHOOK}
+ERROR=$(sh ./reload_website.sh 2>&1)
+
+if ! [ $? -eq 0 ]; then
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app is not tracking master :o:\n\n Error:\n```'"${ERROR}"'```"}' ${SLACK_WEBHOOK}
 fi
