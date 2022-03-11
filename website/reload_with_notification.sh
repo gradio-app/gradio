9720d5cd822bd591a111edd6e839829894f4f7b8
aliabd <ali.si3luwa@gmail.com>
2022-03-11 05:23:53 +0200

    escaping error text to send with curl

--- website/reload_with_notification.sh
+++ website/reload_with_notification.sh
@@ -5,5 +5,6 @@ export PATH="/usr/local/bin:/usr/bin:/bin"
 ERROR=$(sh ./reload_website.sh 2>&1)
 
 if ! [ $? -eq 0 ]; then
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app is not tracking master :o:\n\n Error:\n```'"${ERROR}"'```"}' ${SLACK_WEBHOOK}
+    ESCAPED_ERROR=$(echo $ERROR | sed -e "s/'/'\\\\''/g; 1s/^/'/; \$s/\$/'/")
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app is not tracking master :o:\n\n Error:\n```'"${ESCAPED_ERROR}"'```"}' ${SLACK_WEBHOOK}
 fi
