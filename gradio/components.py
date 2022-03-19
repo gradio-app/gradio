e95a4b3017afd78a505db5b854cb1822d7219f4a
Abubakar Abid <abubakar@huggingface.co>
2022-03-18 17:20:56 -0700

    deleted components

--- gradio/components.py
+++ gradio/components.py
@@ -52,9 +52,7 @@ class Component(Block):
         """
         :return: a dictionary with context variables for the javascript file associated with the context
         """
-        return {
-            "label": self.label,
-        }
+        return {"name": self.__class__.__name__.lower(), "label": self.label}
 
     @classmethod
     def get_shortcut_implementations(cls):
