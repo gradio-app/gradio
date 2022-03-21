457df16d6b571f184028e606053f61176880add5
Abubakar Abid <abubakar@huggingface.co>
2022-03-21 10:42:53 -0700

    added full highlighting and tests

--- gradio/utils.py
+++ gradio/utils.py
@@ -292,3 +292,20 @@ def get_default_args(func: Callable) -> Dict[str, Any]:
         v.default if v.default is not inspect.Parameter.empty else None
         for v in signature.parameters.values()
     ]
+
+
+def format_ner_list(input_string: str, ner_groups: Dict[str: str | int]):
+    if len(ner_groups) == 0:
+        return [(input_string, None)]
+    
+    output = []
+    prev_end = 0
+    
+    for group in ner_groups:
+        entity, start, end = group["entity_group"], group["start"], group["end"]
+        output.append((input_string[prev_end:start], None))
+        output.append((input_string[start:end], entity))
+        prev_end = end
+    
+    output.append((input_string[end:], None))    
+    return output
