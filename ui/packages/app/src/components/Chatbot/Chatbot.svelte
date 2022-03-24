85dbeffd5cbf0d9b69c6aab2e9ae628814e103d5
pngwn <hello@pngwn.io>
2022-03-24 11:11:49 +0000

    initialise chatbot with empty array of messages

--- ui/packages/app/src/components/Chatbot/Chatbot.svelte
+++ ui/packages/app/src/components/Chatbot/Chatbot.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { ChatBot } from "@gradio/chatbot";
-	export let value: Array<[string, string]>;
+	export let value: Array<[string, string]> = [];
+
+	$: console.log($$props);
 </script>
 
 <ChatBot {value} on:change />
