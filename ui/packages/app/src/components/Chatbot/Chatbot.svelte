017e62c95c38792f5c30b817bae807d08efee543
pngwn <hello@pngwn.io>
2022-03-24 11:12:47 +0000

    remove log

--- ui/packages/app/src/components/Chatbot/Chatbot.svelte
+++ ui/packages/app/src/components/Chatbot/Chatbot.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { ChatBot } from "@gradio/chatbot";
 	export let value: Array<[string, string]> = [];
-
-	$: console.log($$props);
 </script>
 
 <ChatBot {value} on:change />
