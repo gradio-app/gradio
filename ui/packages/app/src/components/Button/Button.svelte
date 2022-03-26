52913100a4247771cda34b142b1f4b8e37e5cb35
Ali Abid <aliabid94@gmail.com>
2022-03-26 03:47:29 +0100

    fix buttons

--- ui/packages/app/src/components/Button/Button.svelte
+++ ui/packages/app/src/components/Button/Button.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import { Button } from "@gradio/button";
 
-	export let value: string;
+	export let default_value: string;
 	export let style: string = "";
 	export let variant: "primary" | "secondary" = "primary";
 </script>
 
 <Button {variant} {style} on:click>
-	{value}
+	{default_value}
 </Button>
