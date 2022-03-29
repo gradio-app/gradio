dcc9ba87c9cfd7565e7cf16f7fba559f8c90a7c9
Ömer Faruk Özdemir <farukozderim@gmail.com>
2022-03-29 18:45:24 +0300

    blocks-tests - implement a simple test to check blocks config generation

---
+++ test/test_blocks.py
@@ -0,0 +1,47 @@
+from .test_data.blocks_configs import XRAY_CONFIG
+import gradio as gr
+import random
+
+
+class TestBlocks:
+    def test_xray(self):
+        xray_model = lambda diseases, img: {disease: random.random() for disease in diseases}
+        ct_model = lambda diseases, img: {disease: 0.1 for disease in diseases}
+
+        xray_blocks = gr.Blocks()
+
+        with xray_blocks:
+            gr.components.Markdown(
+                """
+            # Detect Disease From Scan
+            With this model you can lorem ipsum
+            - ipsum 1
+            - ipsum 2
+            """
+            )
+            disease = gr.components.CheckboxGroup(
+                choices=["Covid", "Malaria", "Lung Cancer"], label="Disease to Scan For"
+            )
+
+            with gr.Tabs():
+                with gr.TabItem("X-ray"):
+                    with gr.Row():
+                        xray_scan = gr.components.Image()
+                        xray_results = gr.components.JSON()
+                    xray_run = gr.Button(
+                        "Run", css={"background-color": "red", "--hover-color": "orange"}
+                    )
+                    xray_run.click(
+                        xray_model, inputs=[disease, xray_scan], outputs=xray_results
+                    )
+
+                with gr.TabItem("CT Scan"):
+                    with gr.Row():
+                        ct_scan = gr.components.Image()
+                        ct_results = gr.components.JSON()
+                    ct_run = gr.Button("Run")
+                    ct_run.click(ct_model, inputs=[disease, ct_scan], outputs=ct_results)
+
+            overall_probability = gr.components.Textbox()
+
+        assert XRAY_CONFIG == xray_blocks.get_config_file()
