c19042ee625e76dde2b56e6a958a33dfc3ea34ce
Abubakar Abid <abubakar@huggingface.co>
2022-04-08 07:35:55 -0700

    replaced local files with urls to images (#965)

--- demo/fake_gan/run.py
+++ demo/fake_gan/run.py
@@ -7,8 +7,15 @@ import gradio as gr
 
 
 def fake_gan(*args):
-    time.sleep(2)
-    return "demo/fake_gan/fake" + str(random.randint(1, 2)) + ".png"
+    time.sleep(1)
+    image = random.choice([
+        "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80",
+        "https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+        "https://images.unsplash.com/photo-1546456073-92b9f0a8d413?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        "https://images.unsplash.com/photo-1601412436009-d964bd02edbc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+    ])
+    return image
 
 
 demo = gr.Interface(
@@ -21,6 +28,8 @@ demo = gr.Interface(
         gr.Number(label="Respacing"),
     ],
     outputs=gr.Image(label="Generated Image"),
+    title="FD-GAN",
+    description="This is a fake demo of a GAN. In reality, the images are randomly chosen from Unsplash.",
 )
 
 if __name__ == "__main__":
