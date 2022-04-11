fbf84119675bc8203cc7d785770ae6c4cb7d0834
Abubakar Abid <abubakar@huggingface.co>
2022-04-11 13:09:39 -0700

    creating a new blocks demo

---
+++ demo/blocks_neural_instrument_coding/run.py
@@ -0,0 +1,10 @@
+# A Blocks implementation of https://erlj.notion.site/Neural-Instrument-Cloning-from-very-few-samples-2cf41d8b630842ee8c7eb55036a1bfd6
+
+import gradio as gr
+
+demo = gr.Blocks()
+
+
+
+if __name__ == "__main__":
+    demo.launch()
