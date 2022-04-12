54deaa9af392d95b92076c8e8d13fbf83504c9d7
Abubakar Abid <abubakar@huggingface.co>
2022-04-12 15:33:33 -0700

    Update test_components.py

--- test/test_components.py
+++ test/test_components.py
@@ -882,9 +882,9 @@ class TestDataframe(unittest.TestCase):
                 "col_count": 3,
                 "col_width": None,
                 "default_value": [
-                    [None, None, None],
-                    [None, None, None],
-                    [None, None, None],
+                    ["", "", ""],
+                    ["", "", ""],
+                    ["", "", ""],
                 ],
             },
         )
