b68da0812f44131bcabb5f7bc84353fa5d3d8a73
dawoodkhan82 <dawoodkhan82@gmail.com>
2022-04-14 16:45:51 -0400

    fix 3d model examples

--- demo/Image3D/run.py
+++ demo/Image3D/run.py
@@ -6,9 +6,10 @@ def load_mesh(mesh_file_name):
     return mesh_file_name
 
 iface = gr.Interface(
-    load_mesh,
-    gr.inputs.Image3D(),
-    gr.outputs.Image3D(clear_color=[0.8, 0.2, 0.2, 1.0])
+    fn=load_mesh,
+    inputs=gr.inputs.Image3D(),
+    outputs=gr.outputs.Image3D(clear_color=[0.8, 0.2, 0.2, 1.0]),
+    examples=[["files/Bunny.obj"], ["files/Duck.glb"]]
 )
 
 if __name__ == "__main__":
--- ui/packages/app/src/components/input/Image3D/Image3D.svelte
+++ ui/packages/app/src/components/input/Image3D/Image3D.svelte
@@ -1,30 +1,13 @@
 <script lang="ts">
+	import type { FileData } from "./types";
 	import Upload from "../../utils/Upload.svelte";
 	import { _ } from "svelte-i18n";
 
-	interface Data {
-		data: string;
-		name: string;
-		size: number;
-	}
-
-	interface FileData {
-		name: string;
-		size: number;
-		data: string;
-		is_example: false;
-	}
-
-	export let value: Data;
-	export let setValue: (val: null | Data) => null | Data;
+	export let value: FileData;
+	export let setValue: (val: null | FileData) => null | FileData;
 	export let theme: string;
 
-	function handle_load(v: string | FileData | (string | FileData)[] | null) {
-		setValue(v as Data);
-		return v;
-	}
-
-	import { onMount, afterUpdate } from "svelte";
+	import { afterUpdate } from "svelte";
 	import * as BABYLON from "babylonjs";
 	import "babylonjs-loaders";
 
@@ -50,10 +33,16 @@
 			engine.resize();
 		});
 
-		let base64_model_content = value["data"];
-		let raw_content = BABYLON.Tools.DecodeBase64(base64_model_content);
-		let blob = new Blob([raw_content]);
-		let url = URL.createObjectURL(blob);
+		let url: string
+		if (value["is_example"]) {
+			url = value["data"]
+		} else {
+			let base64_model_content = value["data"];
+			let raw_content = BABYLON.Tools.DecodeBase64(base64_model_content);
+			let blob = new Blob([raw_content]);
+			url = URL.createObjectURL(blob);
+		}
+
 		BABYLON.SceneLoader.Append(
 			"",
 			url,
@@ -69,7 +58,7 @@
 </script>
 
 {#if value === null}
-	<Upload load={handle_load} {theme} filetype=".obj, .gltf, .glb">
+	<Upload load={setValue} {theme} filetype=".obj, .gltf, .glb">
 		{$_("interface.drop_file")}
 		<br />- {$_("interface.or")} -<br />
 		{$_("interface.click_to_upload")}
