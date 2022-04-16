b3a692cf3515e9d6f38e9a9bdcd3b357fd14c659
Victor Muštar <victor.mustar@gmail.com>
2022-04-16 22:21:35 +0200

    form update

--- ui/packages/form/src/Radio.svelte
+++ ui/packages/form/src/Radio.svelte
@@ -20,13 +20,16 @@
 
 	<div class="flex flex-wrap gap-2">
 		{#each choices as choice, i}
-			<label class="gr-box gr-box-sm ">
+			<label
+				class:!cursor-not-allowed={disabled}
+				class="flex items-center text-gray-700 text-sm space-x-2 border py-1.5 px-3 rounded-lg cursor-pointer bg-white shadow-sm checked:shadow-inner"
+			>
 				<input
 					{disabled}
 					bind:group={value}
 					type="radio"
 					name="test"
-					class="rounded-full gr-check-radio "
+					class="rounded-full border-gray-300 text-blue-600 disabled:text-gray-400 disabled:cursor-not-allowed shadow-sm focus:border-blue-300 focus:ring focus:ring-offset-0 focus:ring-blue-200 focus:ring-opacity-50"
 					value={choice}
 				/> <span class="ml-2">{choice}</span></label
 			>
