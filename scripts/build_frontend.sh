a7a53f0a6be1fd96cd0e4aa0df8a9491bca451fe
dawoodkhan82 <dawoodkhan82@gmail.com>
2022-04-18 06:29:04 -0400

    update build script

--- scripts/build_frontend.sh
+++ scripts/build_frontend.sh
@@ -3,6 +3,7 @@ if [ -z "$(ls | grep CONTRIBUTING.md)" ]; then
   echo "Please run the script from repo directory"
   exit -1
 else
+  export NODE_OPTIONS=--max_old_space_size=4096
   echo "Building the frontend"
   cd ui
   pnpm i --frozen-lockfile
