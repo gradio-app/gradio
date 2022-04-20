d7de7f3a87264e3855775dd36f7b8044cc29b50b
Victor Muštar <victor.mustar@gmail.com>
2022-04-20 17:08:56 +0200

    static image size

--- ui/packages/audio/src/StaticAudio.svelte
+++ ui/packages/audio/src/StaticAudio.svelte
@@ -35,7 +35,7 @@
 	<BlockLabel image={audio_icon} label={label || "Audio"} />
 	{#if value === null}
 		<div class="min-h-[8rem] flex justify-center items-center">
-			<img src={audio_icon} alt="" class="h-10 opacity-30" />
+			<img src={audio_icon} alt="" class="h-6 opacity-20" />
 		</div>
 	{:else}
 		<audio
