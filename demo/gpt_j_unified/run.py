b89e76904f833ca43edaecc52a03ce9b905ac3e9
Abubakar Abid <abubakar@huggingface.co>
2022-04-28 02:44:21 -0700

    updated demos

--- demo/gpt_j_unified/run.py
+++ demo/gpt_j_unified/run.py
@@ -1,14 +1,13 @@
 import gradio as gr
 
-title = "GPT-J-6B"
-
 component = gr.Textbox(lines=5, label="Text")
+api = gr.Interface.load("huggingface/EleutherAI/gpt-j-6B")
 
-demo = gr.Interface.load(
-    "huggingface/EleutherAI/gpt-j-6B",
+demo = gr.Interface(
+    fn=lambda x: x[:-50] + api(x[-50:]),
     inputs=component,
     outputs=component,
-    title=title,
+    title="GPT-J-6B",
 )
 
 if __name__ == "__main__":
