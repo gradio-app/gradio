63138f7ce52ba32c070ecdd1dbebd93010613407
pngwn <hello@pngwn.io>
2022-05-02 11:46:22 +0100

    move interface row styling to frontend

--- ui/packages/app/src/components/Column/Column.svelte
+++ ui/packages/app/src/components/Column/Column.svelte
@@ -2,13 +2,19 @@
 	export let value: boolean = true;
 	export let default_value: boolean;
 	export let style: string = "";
+	export let variant: "default" | "panel" = "default";
 
 	if (default_value) value = default_value;
+
+	$: console.log($$props);
 </script>
 
 <div
 	{style}
 	class:hidden={!value}
+	class:bg-gray-50={variant === "panel"}
+	class:p-2={variant === "panel"}
+	class:rounded-lg={variant === "panel"}
 	class="flex flex-1 flex-col gr-gap gr-form-gap relative"
 >
 	<slot />
