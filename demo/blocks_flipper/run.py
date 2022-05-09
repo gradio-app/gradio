7264c61e2d2be4d92186f180a208ffe20fb02110
Abubakar Abid <abubakar@huggingface.co>
2022-05-09 12:46:12 -0700

    updated demos

--- demo/blocks_flipper/run.py
+++ demo/blocks_flipper/run.py
@@ -15,17 +15,20 @@ with demo:
     gr.Markdown("Flip text or image files using this demo.")
     with gr.Tabs():
         with gr.TabItem("Flip Text"):
-            with gr.Row():
-                text_input = gr.Textbox()
-                text_output = gr.Textbox()
-            text_button = gr.Button("Flip")
+            with gr.Column():
+                with gr.Row():
+                    text_input = gr.Textbox()
+                    text_output = gr.Textbox()
+                text_button = gr.Button("Flip")
         with gr.TabItem("Flip Image"):
-            with gr.Row():
-                image_input = gr.Image()
-                image_output = gr.Image()
-            image_button = gr.Button("Flip")
+            with gr.Column():
+                with gr.Row():
+                    image_input = gr.Image()
+                    image_output = gr.Image()
+                image_button = gr.Button("Flip")
     
     text_button.click(flip_text, inputs=text_input, outputs=text_output)
     image_button.click(flip_image, inputs=image_input, outputs=image_output)
     
-demo.launch()
+if __name__ == "__main__":
+    demo.launch()
