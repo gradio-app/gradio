46f7aed0f58daf24a1b6276426dd06d70008f530
Dawood <dawoodkhan82@gmail.com>
2022-05-11 14:25:03 -0400

    model3d fix

--- ui/packages/model3D/src/Model3D.svelte
+++ ui/packages/model3D/src/Model3D.svelte
@@ -17,24 +17,10 @@
 
 	let canvas: HTMLCanvasElement;
 	let scene: BABYLON.Scene;
+	let engine: BABYLON.Engine;
 
 	onMount(() => {
-		const engine = new BABYLON.Engine(canvas, true);
-		scene = new BABYLON.Scene(engine);
-		scene.createDefaultCameraOrLight();
-		scene.clearColor = clearColor
-			? (scene.clearColor = new BABYLON.Color4(
-					clearColor[0],
-					clearColor[1],
-					clearColor[2],
-					clearColor[3]
-			  ))
-			: new BABYLON.Color4(0.2, 0.2, 0.2, 1);
-
-		engine.runRenderLoop(() => {
-			scene.render();
-		});
-
+		engine = new BABYLON.Engine(canvas, true);
 		window.addEventListener("resize", () => {
 			engine.resize();
 		});
@@ -45,28 +31,50 @@
 	});
 
 	function addNewModel() {
-		for (let mesh of scene.meshes) {
-			mesh.dispose();
+		if (scene) {
+			scene.dispose()
 		}
+		if (value) {
+			scene = new BABYLON.Scene(engine);
+			scene.createDefaultCameraOrLight();
+			scene.clearColor = clearColor
+				? (scene.clearColor = new BABYLON.Color4(
+						clearColor[0],
+						clearColor[1],
+						clearColor[2],
+						clearColor[3]
+				))
+				: new BABYLON.Color4(0.2, 0.2, 0.2, 1);
+			
+			engine.runRenderLoop(() => {
+				scene.render();
+			});
 
-		let base64_model_content = value["data"];
-		let raw_content = BABYLON.Tools.DecodeBase64(base64_model_content);
-		let blob = new Blob([raw_content]);
-		let url = URL.createObjectURL(blob);
-		BABYLON.SceneLoader.Append(
-			"",
-			url,
-			scene,
-			() => {
-				scene.createDefaultCamera(true, true, true);
-			},
-			undefined,
-			undefined,
-			"." + value["name"].split(".")[1]
-		);
+			let base64_model_content = value["data"];
+			let raw_content = BABYLON.Tools.DecodeBase64(base64_model_content);
+			let blob = new Blob([raw_content]);
+			let url = URL.createObjectURL(blob);
+			BABYLON.SceneLoader.Append(
+				"",
+				url,
+				scene,
+				() => {
+					scene.createDefaultCamera(true, true, true);
+				},
+				undefined,
+				undefined,
+				"." + value["name"].split(".")[1]
+			);
+		}
 	}
 </script>
 
 <BlockLabel {show_label} Icon={File} label={label || "3D Model"} />
 
-<canvas class="w-full h-full object-contain" bind:this={canvas} />
+{#if value}
+	<canvas class="w-full h-full object-contain" bind:this={canvas} />
+{:else}
+	<div class="min-h-[16rem] flex justify-center items-center">
+		<div class="h-10 dark:text-white opacity-50"><File /></div>
+	</div>
+{/if}
