a5eb3498d0c7cbbc3f23e93aa4616930edd14757
Radames Ajna <radamajna@gmail.com>
2022-05-11 15:30:05 -0700

    input number is float, force int to multipy string

--- demo/blocks_textbox_max_lines/run.py
+++ demo/blocks_textbox_max_lines/run.py
@@ -1,12 +1,12 @@
 import gradio as gr
 
 
-def greet(name: str, repeat: int):
-    return "Hello " + name * repeat + "!!"
+def greet(name: str, repeat: float):
+    return "Hello " + name * int(repeat) + "!!"
 
 
 demo = gr.Interface(
-    fn=greet, inputs=[gr.Textbox(lines=2, max_lines=4), gr.Number()], outputs=gr.component("textarea")
+    fn=greet, inputs=[gr.Textbox(lines=2, max_lines=4), gr.Number()], outputs=gr.component("textarea")()
 )
 
 if __name__ == "__main__":
