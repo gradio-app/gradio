030953d403ce03a26c65fa7815925c6b9a270b8c
Ömer Faruk Özdemir <farukozderim@gmail.com>
2022-05-12 22:18:50 +0300

    update-shortcut-syntax
    - tweaks

--- demo/blocks_component_shortcut/run.py
+++ demo/blocks_component_shortcut/run.py
@@ -12,10 +12,14 @@ with gr.Blocks() as demo:
     2. gr.templates.Template() or gr.Template()
     All the templates are listed in gradio/templates.py
     """
-    with gr.Column():
-        input = gr.component("textarea")
-    with gr.Column():
-        output = gr.TextArea()
-        output2 = gr.templates.TextArea()
-    input.change(greet, input, output)
+    with gr.Row():
+        with gr.Column():
+            text1 = gr.component("textarea")
+        with gr.Column():
+            text2 = gr.TextArea()
+        with gr.Column():
+            text3 = gr.templates.TextArea()
+    text1.change(greet, text1, text2)
+    text2.change(greet, text2, text3)
+    text3.change(greet, text3, text1)
     demo.launch()
