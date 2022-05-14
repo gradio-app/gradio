73e98ddf150671b964a3b5c78a3a091f960f7ead
Radamés Ajna <radamajna@gmail.com>
2022-05-13 22:45:44 -0700

    Small fixes for multiple demos compatible with 3.0 (#1257)
    * add required param but None
    
    * import torch req, add chunk_length_s
    
    import torch requirement for transformers
    enable inference for longer audio files
    
    * fix compononte initialization
    
    * input number is float, force int to multipy string
    
    * no need for Templates, fix class init
    
    * expects array
    
    * add requirements.txt for demo
    
    * update with cleaner syntax
    
    * add sample csv to fraud demo
    
    * adapt to new syntax
    
    * temp fix for Slider arguments
    
    * add dep to requirements
    
    * remove gr.Markdown from Interface init
    
    * fix default value param name
    
    * upgrade deepspeech, download models onstart
    
    * use path resolution consistent with other demos
    
    * remove redundant demo
    
    * add example to interface
    
    * fixed plotting issues
    
    * plots
    
    * deprecated carousel
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- demo/blocks_speech_text_length/run.py
+++ demo/blocks_speech_text_length/run.py
@@ -1,18 +1,18 @@
-# from transformers import pipeline
+from transformers import pipeline
 
 import gradio as gr
 
-# asr = pipeline("automatic-speech-recognition", "facebook/wav2vec2-base-960h")
-# classifier = pipeline("text-classification")
+asr = pipeline("automatic-speech-recognition", "facebook/wav2vec2-base-960h")
+classifier = pipeline("text-classification")
 
 
-# def speech_to_text(speech):
-#     text = asr(speech)["text"]
-#     return text
+def speech_to_text(speech):
+    text = asr(speech)["text"]
+    return text
 
 
-# def text_to_sentiment(text):
-#     return classifier(text)[0]["label"]
+def text_to_sentiment(text):
+    return classifier(text)[0]["label"]
 
 
 demo = gr.Blocks()
@@ -25,8 +25,8 @@ with demo:
     b1 = gr.Button("Recognize Speech")
     b2 = gr.Button("Classify Sentiment")
 
-    # b1.click(speech_to_text, inputs=audio_file, outputs=text)
-    # b2.click(text_to_sentiment, inputs=text, outputs=label)
+    b1.click(speech_to_text, inputs=audio_file, outputs=text)
+    b2.click(text_to_sentiment, inputs=text, outputs=label)
 
 if __name__ == "__main__":
     demo.launch()
--- demo/model3D/run.py
+++ demo/model3D/run.py
@@ -1,18 +1,27 @@
 import time
 import gradio as gr
+import os
+
 
 def load_mesh(mesh_file_name):
     time.sleep(2)
     return mesh_file_name
 
+
 inputs = gr.Model3D()
 outputs = gr.Model3D(clear_color=[0.8, 0.2, 0.2, 1.0])
 
 demo = gr.Interface(
-    fn=load_mesh, 
-    inputs=inputs, 
+    fn=load_mesh,
+    inputs=inputs,
     outputs=outputs,
-    examples=[["files/Bunny.obj"], ["files/Duck.glb"], ["files/Fox.gltf"],["files/face.obj"]], cache_examples=True
+    examples=[
+        [os.path.join(os.path.dirname(__file__), "files/Bunny.obj")],
+        [os.path.join(os.path.dirname(__file__), "files/Duck.glb")],
+        [os.path.join(os.path.dirname(__file__), "files/Fox.gltf")],
+        [os.path.join(os.path.dirname(__file__), "files/face.obj")],
+    ],
+    cache_examples=True,
 )
 
 if __name__ == "__main__":
