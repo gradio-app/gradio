f55f40db3a947e721a6aa1d1478db9a7223f66fd
Ali Abid <aabid94@gmail.com>
2022-05-14 04:16:52 +0000

    remove index

--- website/demos/map_demos.py
+++ website/demos/map_demos.py
@@ -14,15 +14,16 @@ port = 7860
 
 DEMO_PATTERN = r'demos\["([A-Za-z0-9_]+)"]'
 
-demos_to_skip = ["no_input", "disease_report", "fake_gan", "hello_login"] # skipping some demos
-demos_to_run = [demo for demo in os.listdir(GRADIO_DEMO_DIR)
-                if os.path.isdir(os.path.join(GRADIO_DEMO_DIR, demo))
-                and demo not in demos_to_skip]
+# demos_to_skip = ["no_input", "disease_report", "fake_gan", "hello_login"] # skipping some demos
+# demos_to_run = [demo for demo in os.listdir(GRADIO_DEMO_DIR)
+#                 if os.path.isdir(os.path.join(GRADIO_DEMO_DIR, demo))
+#                 and demo not in demos_to_skip]
+demos_to_run = []
 # for guide_filename in os.listdir(GRADIO_GUIDES_DIR):
-# for guide_filename in ["getting_started.md"]:
-#     with open(os.path.join(GRADIO_GUIDES_DIR, guide_filename)) as guide_file:
-#         guide_content = guide_file.read()
-#     demos_to_run += re.findall(DEMO_PATTERN, guide_content)
+for guide_filename in ["getting_started.md"]:
+    with open(os.path.join(GRADIO_GUIDES_DIR, guide_filename)) as guide_file:
+        guide_content = guide_file.read()
+    demos_to_run += re.findall(DEMO_PATTERN, guide_content)
 
 demo_port_sets = []
 for demo_name in demos_to_run:
