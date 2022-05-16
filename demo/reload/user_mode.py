e4e3f324b9411ab67bc30e249ea3f3a8efbf031a
Ömer Faruk Özdemir <farukozderim@gmail.com>
2022-05-16 18:35:58 +0300

    reload-mode
    - add user_mode

---
+++ demo/reload/user_mode.py
@@ -0,0 +1,21 @@
+"""
+Simple Gradio example with reload feature.
+"""
+import gradio as gr
+import gradio
+import uvicorn
+
+
+def greet(str):
+    return str
+
+
+with gr.Blocks() as demo:
+    text = gr.Text()
+    text_2 = gr.Text()
+    text.change(greet, text, text_2)
+
+app = gradio.routes.App.create_app(demo)
+
+if __name__ == "__main__":
+    uvicorn.run(app="user_mode:app", reload=True, debug=True)
