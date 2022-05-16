ee52811997b63510b3d4248ed5e70e1fdca56331
Abubakar Abid <abubakar@huggingface.co>
2022-05-16 12:44:32 -0400

    Label hotfixes (#1281)
    * label default
    
    * label test fix
    
    * formatting

--- gradio/components.py
+++ gradio/components.py
@@ -2580,7 +2580,7 @@ class Label(Changeable, IOComponent):
 
     def __init__(
         self,
-        value: str = "",
+        value: str = None,
         *,
         num_top_classes: Optional[int] = None,
         label: Optional[str] = None,
@@ -2591,7 +2591,7 @@ class Label(Changeable, IOComponent):
     ):
         """
         Parameters:
-        value (str): Default string value
+        value (str): Default value to show
         num_top_classes (int): number of most confident classes to show.
         label (Optional[str]): component name in interface.
         show_label (bool): if True, will display label.
