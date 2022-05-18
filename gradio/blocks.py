4d86dfd85061dfcd761d182564422f6ae14f1853
Abubakar Abid <abubakar@huggingface.co>
2022-05-18 15:08:21 -0700

    simplifications

--- gradio/blocks.py
+++ gradio/blocks.py
@@ -415,7 +415,7 @@ class Blocks(BlockContext):
         ssl_keyfile: Optional[str] = None,
         ssl_certfile: Optional[str] = None,
         ssl_keyfile_password: Optional[str] = None,
-        verbose: bool = True,
+        quiet: bool = False,
         _frontend: bool = True,
     ) -> Tuple[FastAPI, str, str]:
         """
@@ -441,6 +441,7 @@ class Blocks(BlockContext):
         ssl_keyfile (str | None): If a path to a file is provided, will use this as the private key file to create a local server running on https.
         ssl_certfile (str | None): If a path to a file is provided, will use this as the signed certificate for https. Needs to be provided if ssl_keyfile is provided.
         ssl_keyfile_password (str | None): If a password is provided, will use this with the ssl certificate for https.
+        quiet (bool): If True, suppresses most print statements.
         Returns:
         app (FastAPI): FastAPI app object that is running the demo
         local_url (str): Locally accessible link to the demo
@@ -475,7 +476,7 @@ class Blocks(BlockContext):
 
         if self.is_running:
             self.server_app.launchable = self
-            if verbose:
+            if not(quiet):
                 print(
                     "Rerunning server... use `close()` to stop if you need to change `launch()` parameters.\n----"
                 )
@@ -519,7 +520,8 @@ class Blocks(BlockContext):
                     share_url = networking.setup_tunnel(self.server_port, None)
                     self.share_url = share_url
                 print(strings.en["SHARE_LINK_DISPLAY"].format(self.share_url))
-                print(strings.en["SHARE_LINK_MESSAGE"])
+                if not(quiet):                
+                    print(strings.en["SHARE_LINK_MESSAGE"])
             except RuntimeError:
                 if self.analytics_enabled:
                     utils.error_analytics(self.ip_address, "Not able to set up tunnel")
@@ -527,7 +529,7 @@ class Blocks(BlockContext):
                 share = False
                 print(strings.en["COULD_NOT_GET_SHARE_LINK"])
         else:
-            if verbose:
+            if not(quiet):
                 print(strings.en["PUBLIC_SHARE_TRUE"])
             self.share_url = None
 
--- gradio/ipython_ext.py
+++ gradio/ipython_ext.py
@@ -11,65 +11,6 @@ def load_ipython_extension(ipython):
 
     @register_cell_magic
     def blocks(line, cell):
-        if "imp" in line:
-            from gradio.components import (
-                HTML,
-                JSON,
-                Audio,
-                Button,
-                Carousel,
-                Chatbot,
-                Checkbox,
-                Checkboxgroup,
-                CheckboxGroup,
-                DataFrame,
-                Dataframe,
-                Dropdown,
-                File,
-                Gallery,
-                Highlightedtext,
-                HighlightedText,
-                Image,
-                Label,
-                Markdown,
-                Model3D,
-                Number,
-                Plot,
-                Radio,
-                Slider,
-                StatusTracker,
-                Textbox,
-                TimeSeries,
-                Timeseries,
-                Variable,
-                Video,
-                component,
-                update,
-            )
-            from gradio.flagging import (
-                CSVLogger,
-                FlaggingCallback,
-                HuggingFaceDatasetSaver,
-                SimpleCSVLogger,
-            )
-            from gradio.interface import Interface, TabbedInterface, close_all
-            from gradio.layouts import Box, Column, Group, Row, TabItem, Tabs
-            from gradio.mix import Parallel, Series
-            from gradio.templates import (
-                Files,
-                Highlight,
-                List,
-                Matrix,
-                Mic,
-                Microphone,
-                Numpy,
-                Pil,
-                PlayableVideo,
-                Sketchpad,
-                Text,
-                TextArea,
-                Webcam,
-            )
         with demo.clear():
             exec(cell)
-            demo.launch(verbose=False)
+            demo.launch(quiet=True)
