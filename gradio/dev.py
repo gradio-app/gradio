5c38f52b16cefe6c8e802bdfaccac1f20a9e7579
Abubakar Abid <abubakar@huggingface.co>
2022-05-21 01:15:23 -0700

    reload

--- gradio/dev.py
+++ gradio/dev.py
@@ -3,15 +3,21 @@
 
 import os
 import sys
+from gradio import networking
 
 def main():
     args = sys.argv[1:]
     
     path = args[0]
     path = os.path.normpath(path)
-    if "/" in path or "\\" in path:
+    if not(path == os.path.basename(path)):
         raise ValueError("Must provide file name in the current directory")
     filename = os.path.splitext(path)[0]   
+
+    port = networking.get_first_available_port(
+        networking.INITIAL_PORT_VALUE, 
+        networking.INITIAL_PORT_VALUE + networking.TRY_NUM_PORTS
+    )
     
-    os.system(f"uvicorn {filename}:demo.app --reload")
+    os.system(f"uvicorn {filename}:demo.app --reload --port {port}")
 
