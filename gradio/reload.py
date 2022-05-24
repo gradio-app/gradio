3d1d5eda9e0d0228b067153db6c182e30e48c4d9
Ömer Faruk Özdemir <farukozderim@gmail.com>
2022-05-24 19:00:39 +0300

    reload-dev-mode
    - add file reload to the dev-reload-mode

--- gradio/reload.py
+++ gradio/reload.py
@@ -1,13 +1,19 @@
-# Contains the function that runs when `gradio` is called from the command line. Specifically, allows
-# $ gradio app.py to run app.py in development mode where any changes reload the script.
+"""
 
+Contains the functions that run when `gradio` is called from the command line. Specifically, allows
+
+$ gradio app.py, to run app.py in user reload mode where any changes in the app.py reload the demo.
+$ gradio-dev app.py, to run app.py in developer reload mode where any changes in the Gradio library reloads the demo.
+$ gradio app.py my_demo, to use variable names other than "demo"
+$ gradio-dev app.py my_demo, to use variable names other than "demo"
+"""
 import os
 import sys
 
 from gradio import networking
 
 
-def user():
+def run_in_reload_mode(mode: str):
     args = sys.argv[1:]
     if len(args) == 0:
         raise ValueError("No file specified.")
@@ -16,43 +22,33 @@ def user():
     else:
         demo_name = args[1]
 
-    path = args[0]
-    path = path.replace("/", ".")
+    original_path = args[0]
+    path = original_path.replace("/", ".")
     filename = os.path.splitext(path)[0]
 
     port = networking.get_first_available_port(
         networking.INITIAL_PORT_VALUE,
         networking.INITIAL_PORT_VALUE + networking.TRY_NUM_PORTS,
     )
+    if mode == "user":
+        print(
+            f"\nLaunching in *reload mode* on: http://{networking.LOCALHOST_NAME}:{port} (Press CTRL+C to quit)\n"
+        )
+        os.system(
+            f"uvicorn {filename}:{demo_name}.app --reload --port {port} --log-level warning"
+        )
+    elif mode == "dev":
+        print(
+            f"\nLaunching in *developer reload mode* on: http://{networking.LOCALHOST_NAME}:{port} (Press CTRL+C to quit)\n"
+        )
+        os.system(
+            f"uvicorn {filename}:{demo_name}.app --reload --port {port} --log-level warning --reload-dir gradio --reload-dir {os.path.dirname(original_path)}"
+        )
 
-    print(
-        f"\nLaunching in *reload mode* on: http://{networking.LOCALHOST_NAME}:{port} (Press CTRL+C to quit)\n"
-    )
-    os.system(
-        f"uvicorn {filename}:{demo_name}.app --reload --port {port} --log-level warning"
-    )
+
+def user():
+    run_in_reload_mode("user")
 
 
 def dev():
-    args = sys.argv[1:]
-    if len(args) == 0:
-        raise ValueError("No file specified.")
-    if len(args) == 1:
-        demo_name = "demo"
-    else:
-        demo_name = args[1]
-
-    path = args[0]
-    path = path.replace("/", ".")
-    filename = os.path.splitext(path)[0]
-    port = networking.get_first_available_port(
-        networking.INITIAL_PORT_VALUE,
-        networking.INITIAL_PORT_VALUE + networking.TRY_NUM_PORTS,
-    )
-
-    print(
-        f"\nLaunching in *developer reload mode* on: http://{networking.LOCALHOST_NAME}:{port} (Press CTRL+C to quit)\n"
-    )
-    os.system(
-        f"uvicorn {filename}:{demo_name}.app --reload --port {port} --log-level warning --reload-dir gradio"
-    )
+    run_in_reload_mode("dev")
