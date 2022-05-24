0ab254b82f8b168c791fc1496820bb2e58cbd252
Victor Muštar <victor.mustar@gmail.com>
2022-05-24 22:08:08 +0200

    Mute examples videos (#1363)
    * mute examples videos
    
    * set attrs onmount for safari
    
    Co-authored-by: Radames Ajna <radamajna@gmail.com>

--- ui/packages/app/src/components/Dataset/ExampleComponents/Video.svelte
+++ ui/packages/app/src/components/Dataset/ExampleComponents/Video.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
 	import { playable } from "../../utils/helpers";
+	import { onMount } from "svelte";
 
 	export let value: string;
 	export let samples_dir: string;
 	let video: HTMLVideoElement;
+
+	onMount(() => {
+		video.muted = true;
+		video.playsInline = true;
+		video.controls = false;
+		video.setAttribute("muted", "");
+		video.play();
+		video.pause();
+	});
 </script>
 
 <!-- svelte-ignore a11y-media-has-caption -->
 <!-- svelte-ignore a11y-mouse-events-have-key-events -->
 {#if playable(value)}
 	<video
+		muted
+		playsinline
 		bind:this={video}
 		on:mouseover={video.play}
 		on:mouseout={video.pause}
