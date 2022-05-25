7db95bc97d740befa4b692f15f4daba89fb38789
Abubakar Abid <abubakar@huggingface.co>
2022-05-25 12:22:56 -0700

    formatting

--- gradio/blocks.py
+++ gradio/blocks.py
@@ -171,7 +171,7 @@ class class_or_instancemethod(classmethod):
     def __get__(self, instance, type_):
         descr_get = super().__get__ if instance is None else self.__func__.__get__
         return descr_get(instance, type_)
-    
+
 
 def update(**kwargs) -> dict:
     """
