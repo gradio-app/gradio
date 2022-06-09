60d5e9ca7805b33d6ca4dc6470c3fd334c6d35ef
Ali Abid <aabid94@gmail.com>
2022-06-09 00:05:26 -0700

    changes

--- demo/image_mod/run.py
+++ demo/image_mod/run.py
@@ -5,7 +5,8 @@ def image_mod(image):
     return image.rotate(45)
 
 
-demo = gr.Interface(image_mod, gr.inputs.Image(type="pil"), "image")
+demo = gr.Interface(image_mod, gr.inputs.Image(type="pil"), "image", 
+    flagging_options=["blurry", "incorrect", "other"])
 
 if __name__ == "__main__":
     demo.launch()
