e2336f1a726b4e5f9dfc93e308b3e8cce7840c70
Abubakar Abid <abubakar@huggingface.co>
2022-06-13 13:00:35 -0700

    working on github action fix (#1552)

--- scripts/gh_action_pypi.sh
+++ scripts/gh_action_pypi.sh
@@ -3,6 +3,7 @@ if [ -z "$(ls | grep CONTRIBUTING.md)" ]; then
   echo "Please run the script from repo directory"
   exit -1
 else
+  set -e
   old_version=$(grep -Po "(?<=version=\")[^\"]+(?=\")" setup.py)
   read -r new_version < gradio/version.txt
   sed -i "s/version=\"$old_version\"/version=\"$new_version\"/g" setup.py
@@ -14,7 +15,9 @@ else
   pnpm build
   GRADIO_VERSION=$new_version pnpm build:cdn
   cd ..
-  aws s3 cp gradio/templates/cdn s3://gradio/$new_version/ --recursive  # requires aws cli (contact maintainers for credentials)
+  echo "Before AWS"
+  aws s3 cp gradio/templates/cdn s3://gradio/$new_version/ --recursive 
+  echo "After AWS"
   cp gradio/templates/cdn/index.html gradio/templates/frontend/share.html
 
   rm -r dist/*
