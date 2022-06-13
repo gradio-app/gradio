21ec01ef002243ac01947b65c89eb1ebaf36a900
Abubakar Abid <abubakar@huggingface.co>
2022-06-13 13:50:49 -0700

    6th try

--- scripts/gh_action_pypi.sh
+++ scripts/gh_action_pypi.sh
@@ -3,8 +3,6 @@ if [ -z "$(ls | grep CONTRIBUTING.md)" ]; then
   echo "Please run the script from repo directory"
   exit -1
 else
-  set -e
-  aws configure list
   old_version=$(grep -Po "(?<=version=\")[^\"]+(?=\")" setup.py)
   read -r new_version < gradio/version.txt
   sed -i "s/version=\"$old_version\"/version=\"$new_version\"/g" setup.py
@@ -16,9 +14,7 @@ else
   pnpm build
   GRADIO_VERSION=$new_version pnpm build:cdn
   cd ..
-  echo "Before AWS"
   aws s3 cp gradio/templates/cdn s3://gradio/$new_version/ --recursive 
-  echo "After AWS"
   cp gradio/templates/cdn/index.html gradio/templates/frontend/share.html
 
   rm -r dist/*
