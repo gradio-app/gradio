04b27b8bddc2d9b8bea1231b08b1df5e7756556a
Abubakar Abid <abubakar@huggingface.co>
2022-06-13 18:25:24 -0700

    fix images for pypi

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ try:
 except ImportError:
     from distutils.core import setup
 from pathlib import Path
+import re
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text(encoding='utf8')
+
+# Replace relative paths to images with absolute paths
+long_description = re.sub("website/homepage/src/assets/img/", "https://raw.githubusercontent.com/gradio-app/gradio/main/website/homepage/src/assets/img/", long_description)
+long_description = re.sub(r"demo/([\S]*.gif)", r"https://raw.githubusercontent.com/gradio-app/gradio/main/demo/\g<1>", long_description)
 
 print(long_description)
 
