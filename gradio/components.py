44ddafb68a02bfc05230f92871f81b59714bf20b
Abubakar Abid <abubakar@huggingface.co>
2022-06-20 13:17:55 -0500

    Fixes json serialization issues (#1606)
    * fixed serialization
    
    * formatting
    
    * Update gradio/components.py
    
    Co-authored-by: Marcelo Trylesinski <marcelotryle@gmail.com>
    
    * Update gradio/components.py
    
    Co-authored-by: aliabid94 <aabid94@gmail.com>
    Co-authored-by: Marcelo Trylesinski <marcelotryle@gmail.com>

--- gradio/components.py
+++ gradio/components.py
@@ -19,6 +19,7 @@ from typing import Any, Callable, Dict, List, Optional, Tuple
 
 import matplotlib.figure
 import numpy as np
+import orjson
 import pandas as pd
 import PIL
 from ffmpy import FFmpeg
@@ -2702,13 +2703,25 @@ class Variable(IOComponent):
         Parameters:
         value (Any): the initial value of the state.
         """
-        self.value = value
+        self.value = orjson.loads(value) if isinstance(value, str) else value
+        try:
+            orjson.dumps(self.value, option=orjson.OPT_SERIALIZE_NUMPY)
+        except TypeError:
+            raise ValueError(
+                f"Cannot set initial value because type: {type(value)} is "
+                "not JSON serializable. Instead, you can set the initial "
+                "value to be None, and in your Python function, change it "
+                "to be the your desired value when the function is "
+                "run initially."
+            )
         self.stateful = True
         IOComponent.__init__(self, **kwargs)
 
     def get_config(self):
         return {
-            "value": self.value,
+            "value": orjson.dumps(self.value, option=orjson.OPT_SERIALIZE_NUMPY).decode(
+                "utf-8"
+            ),
             **IOComponent.get_config(self),
         }
 
