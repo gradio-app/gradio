1d134853c5fb42cfb2d7bf521c9d74cfd96e35a3
Dawood <dawoodkhan82@gmail.com>
2022-06-28 16:19:41 -0400

    add small image

--- demo/image_mod/run.py
+++ demo/image_mod/run.py
@@ -9,7 +9,8 @@ def image_mod(image):
 demo = gr.Interface(image_mod, gr.inputs.Image(type="pil"), "image", 
     flagging_options=["blurry", "incorrect", "other"], examples=[
         os.path.join(os.path.dirname(__file__), "images/cheetah1.jpg"),
-        os.path.join(os.path.dirname(__file__), "images/lion.jpg")
+        os.path.join(os.path.dirname(__file__), "images/lion.jpg"),
+        os.path.join(os.path.dirname(__file__), "images/logo.png")
         ])
 
 if __name__ == "__main__":
