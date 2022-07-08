8cecb65b07a964791f872113f763aedd8917c84c
Abubakar Abid <abubakar@huggingface.co>
2022-07-08 17:57:27 -0500

    pull before updating repo

--- gradio/flagging.py
+++ gradio/flagging.py
@@ -279,6 +279,8 @@ class HuggingFaceDatasetSaver(FlaggingCallback):
     ) -> int:
         is_new = not os.path.exists(self.log_file)
         infos = {"flagged": {"features": {}}}
+        
+        self.repo.git_pull(lfs=True)
 
         with open(self.log_file, "a", newline="") as csvfile:
             writer = csv.writer(csvfile)
