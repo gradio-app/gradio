d4db2e84f810493a628232f9d176098aebe9b341
freddyaboulton <alfonsoboulton@gmail.com>
2022-07-08 14:56:51 -0400

    Add examples for series and parallel

--- gradio/mix.py
+++ gradio/mix.py
@@ -17,6 +17,8 @@ class Parallel(gradio.Interface):
     """
     Creates a new Interface consisting of multiple models in parallel (comparing their outputs).
     The Interfaces to put in Parallel must share the same input components (but can have different output components).
+
+    Demos: interface_parallel
     """
 
     def __init__(self, *interfaces: gradio.Interface, **options):
@@ -57,6 +59,8 @@ class Series(gradio.Interface):
     """
     Creates a new Interface from multiple models in series (the output of one is fed as the input to the next,
     and so the input and output components must agree between the interfaces).
+
+    Demos: interface_series
     """
 
     def __init__(self, *interfaces: gradio.Interface, **options):
