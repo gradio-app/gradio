2bc07d685fbd67ae8703cba342add5570cba9667
pngwn <hello@pngwn.io>
2022-07-12 11:12:07 +0100

    reinstate demo

--- demo/blocks_plug/run.py
+++ demo/blocks_plug/run.py
@@ -1,19 +1,36 @@
 import gradio as gr
 
-import gradio as gr
-import pandas as pd
-import numpy as np
+
+def change_tab():
+    return gr.Tabs.update(selected=2)
 
 
-def return_empty(x):
-    return np.atleast_1d(x.astype(float).mean(axis=0))
+identity_demo, input_demo, output_demo = gr.Blocks(), gr.Blocks(), gr.Blocks()
 
+with identity_demo:
+    gr.Interface(lambda x: x, "text", "text")
 
-demo = gr.Interface(
-    return_empty,
-    gr.Dataframe(type="numpy", headers=["a"]),
-    gr.Dataframe(headers=["mean"]),
-).launch()
+with input_demo:
+    t = gr.Textbox(label="Enter your text here")
+    with gr.Row():
+        btn = gr.Button("Submit")
+        clr = gr.Button("Clear")
+    clr.click(lambda x: "", t, t)
+
+with output_demo:
+    gr.Textbox("This is a static output")
+
+with gr.Blocks() as demo:
+    gr.Markdown("Three demos in one!")
+    with gr.Tabs(selected=1) as tabs:
+        with gr.TabItem("Text Identity", id=0):
+            identity_demo.render()
+        with gr.TabItem("Text Input", id=1):
+            input_demo.render()
+        with gr.TabItem("Text Static", id=2):
+            output_demo.render()
+    btn = gr.Button("Change tab")
+    btn.click(inputs=None, outputs=tabs, fn=change_tab)
 
 if __name__ == "__main__":
     demo.launch()
