8caec11d74612820844cb2b2d52bf7aadaa93796
pngwn <hello@pngwn.io>
2022-07-12 22:05:51 +0100

    reuse port without waiting to leave TIME-WAIT state (#1765)
    * reuse port without waiting to leave TIME-WAIT state
    
    * resuse port only when running gradio cli

--- gradio/networking.py
+++ gradio/networking.py
@@ -45,7 +45,7 @@ class Server(uvicorn.Server):
         self.thread.join()
 
 
-def get_first_available_port(initial: int, final: int) -> int:
+def get_first_available_port(initial: int, final: int, reuse_port: bool = False) -> int:
     """
     Gets the first open port in a specified range of port numbers
     Parameters:
@@ -57,6 +57,8 @@ def get_first_available_port(initial: int, final: int) -> int:
     for port in range(initial, final):
         try:
             s = socket.socket()  # create a socket object
+            if reuse_port:
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             s.bind((LOCALHOST_NAME, port))  # Bind to the port
             s.close()
             return port
