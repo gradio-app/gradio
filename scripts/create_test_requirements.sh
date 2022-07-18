6b5c79d411eb9900de8d796ebc339038479a15e4
freddyaboulton <alfonsoboulton@gmail.com>
2022-07-18 17:46:42 -0400

    Use one requirements file

--- scripts/create_test_requirements.sh
+++ scripts/create_test_requirements.sh
@@ -4,8 +4,8 @@ if [ -z "$(ls | grep CONTRIBUTING.md)" ]; then
   exit -1
 else
   echo "Creating requirements under test/requirements.txt using requirements.in. Please run this script from unix or wsl in a python3.9 env!"
-    python -c "import sys; assert sys.version_info >= (3, 9) and sys.version_info < (3, 10), 'Run from python 3.9!'" || exit 1
+  python -c "import sys; assert sys.version_info < (3, 8) and sys.version_info >= (3, 7), 'Run from python 3.7!'" || exit 1
   cd test
   pip install --upgrade pip-tools
-  pip-compile --output-file requirements-python3.9.2.txt
+  pip-compile --output-file requirements.txt
 fi
