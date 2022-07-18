433b309c10083e8ea7d157c4311cefa551cc1146
freddyaboulton <alfonsoboulton@gmail.com>
2022-07-18 17:24:13 -0400

    Exit if running from wrong python version

--- scripts/create_test_requirements.sh
+++ scripts/create_test_requirements.sh
@@ -4,6 +4,7 @@ if [ -z "$(ls | grep CONTRIBUTING.md)" ]; then
   exit -1
 else
   echo "Creating requirements under test/requirements.txt using requirements.in. Please run this script from unix or wsl in a python3.9 env!"
+    python -c "import sys; assert sys.version_info >= (3, 9) and sys.version_info < (3, 10), 'Run from python 3.9!'" || exit 1
   cd test
   pip install --upgrade pip-tools
   pip-compile --output-file requirements-python3.9.2.txt
--- scripts/create_test_requirements_py37.sh
+++ scripts/create_test_requirements_py37.sh
@@ -4,6 +4,7 @@ if [ -z "$(ls | grep CONTRIBUTING.md)" ]; then
   exit -1
 else
   echo "Creating requirements under test/requirements-python3.7.12.txt using requirements.in. Please run this script from unix or wsl in a python 3.7 env!"
+  python -c "import sys; assert sys.version_info < (3, 8) and sys.version_info > (3, 6), 'Run from python 3.7!'" || exit 1
   cd test
   pip install --upgrade pip-tools
   pip-compile --output-file requirements-python3.7.12.txt
