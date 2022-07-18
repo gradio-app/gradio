69319206fe4d5f0f51f749e06a9eaddd0f02b288
freddyaboulton <alfonsoboulton@gmail.com>
2022-07-18 17:52:56 -0400

    Fix comment

--- scripts/create_test_requirements.sh
+++ scripts/create_test_requirements.sh
@@ -3,7 +3,7 @@ if [ -z "$(ls | grep CONTRIBUTING.md)" ]; then
   echo "Please run the script from repo directory"
   exit -1
 else
-  echo "Creating requirements under test/requirements.txt using requirements.in. Please run this script from unix or wsl in a python3.9 env!"
+  echo "Creating requirements under test/requirements.txt using requirements.in. Please run this script from unix or wsl in a python3.7 env!"
   python -c "import sys; assert sys.version_info < (3, 8) and sys.version_info >= (3, 7), 'Run from python 3.7!'" || exit 1
   cd test
   pip install --upgrade pip-tools
