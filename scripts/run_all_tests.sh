c0173a19f48f1b14f7cb7b628d0d60781defde47
sheikhartin <sheikhartin@gmail.com>
2022-08-01 23:15:37 +0430

    Removing commented codes

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 cd "$(dirname ${0})/.."
-# source scripts/create_test_requirements.sh
-# source scripts/install_test_requirements.sh
 
 echo "Running the tests..."
 python -m pytest --cov=gradio --durations=20 --durations-min=1 test
