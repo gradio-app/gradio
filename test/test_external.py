f691f7d3950df8fe6d0dfee3780524398bbb44e9
freddyaboulton <alfonsoboulton@gmail.com>
2022-08-04 14:50:49 -0400

    Lint

--- test/test_external.py
+++ test/test_external.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 import unittest
+from unittest.mock import patch
 
 import pytest
 import transformers
@@ -231,10 +232,8 @@ class TestLoadFromPipeline(unittest.TestCase):
 
 
 def test_interface_load_cache_examples(tmp_path):
-    import pathlib
-    from unittest import mock
     test_file_dir = pathlib.Path(pathlib.Path(__file__).parent, "test_files")
-    with mock.patch("gradio.examples.CACHED_FOLDER", tmp_path):
+    with patch("gradio.examples.CACHED_FOLDER", tmp_path):
         gr.Interface.load(
             name="models/google/vit-base-patch16-224",
             examples=[pathlib.Path(test_file_dir, "cheetah1.jpg")],
