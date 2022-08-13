7779bbf15a92d4641700691dad0f125ae9dfabd8
freddyaboulton <alfonsoboulton@gmail.com>
2022-08-13 17:45:20 -0400

    Change path

--- scripts/update_version_number.py
+++ scripts/update_version_number.py
@@ -5,11 +5,12 @@ def update_version_number():
     if "GITHUB_REF" not in os.environ:
         return
     pr_number = os.environ["GITHUB_REF"].split("/")[2]
-    version_file = pathlib.Path(pathlib.Path().absolute(), "gradio/version.txt")
-    version_number = open(version_file, "r").readline()
-    with open(version_file, "w") as version:
-        version.write(f"{version_number}.{pr_number}")
-    return f"{version_number}.{pr_number}"
+    return pr_number
+    #version_file = pathlib.Path(pathlib.Path().absolute(), "gradio/version.txt")
+    #version_number = open(version_file, "r").readline()
+    #with open(version_file, "w") as version:
+    #    version.write(f"{version_number}.{pr_number}")
+    #return f"{version_number}.{pr_number}"
     
 
 if __name__ == "__main__":
