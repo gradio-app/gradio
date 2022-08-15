b7781f925053506692cb1fbaaa878d89a7856f9b
freddyaboulton <alfonsoboulton@gmail.com>
2022-08-15 11:27:21 -0400

    Fix all_demo creation

--- demo/all_demos/run.py
+++ demo/all_demos/run.py
@@ -13,21 +13,20 @@ demo_dir = pathlib.Path(__file__).parent / "demos"
 
 
 all_demos = []
-for p in os.listdir("./demos"):
-    all_demos.append(p)
-
 demo_module = None
+for p in os.listdir("./demos"):
+    old_path = copy.deepcopy(sys.path)
+    sys.path = [os.path.join(demo_dir, p)] + sys.path
+    if demo_module is None:
+        demo_module = importlib.import_module(f"run")
+    else:
+        demo_module = importlib.reload(demo_module)
+    all_demos.append((p, demo_module.demo))
+
 with gr.Blocks() as mega_demo:
     with gr.Tabs():
-        for demo_name in all_demos:
+        for demo_name, demo in all_demos:
             with gr.TabItem(demo_name):
-                old_path = copy.deepcopy(sys.path)
-                sys.path = [os.path.join(demo_dir, demo_name)] + sys.path
-                if demo_module is None:
-                    demo_module = importlib.import_module(f"run")
-                else:
-                    demo_module = importlib.reload(demo_module)
-                demo_module.demo
-                sys.path = old_path
+                demo.render()
 
 mega_demo.launch()
