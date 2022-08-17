000d430fa1efdcae9145c5f03115f151b4e74a07
Abubakar Abid <abubakar@huggingface.co>
2022-08-17 16:47:14 -0500

    Sanitize for CSV (#2017)
    * sanitize for csv
    
    * added sanitization logic
    
    * fixed examples
    
    * turn cache off

--- demo/kitchen_sink/run.py
+++ demo/kitchen_sink/run.py
@@ -151,10 +151,9 @@ demo = gr.Interface(
     * 3,
     theme="default",
     title="Kitchen Sink",
-    cache_examples=False,
     description="Try out all the components!",
     article="Learn more about [Gradio](http://gradio.app)",
-    cache_examples=True
+    cache_examples=False
 )
 
 if __name__ == "__main__":
