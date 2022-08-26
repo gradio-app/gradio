b66069113116cd0b5506f0498c444a1384ae3b9e
Abubakar Abid <abubakar@huggingface.co>
2022-08-25 18:22:18 -0700

    fixed audio debugger

--- demo/audio_debugger/run.py
+++ demo/audio_debugger/run.py
@@ -1,12 +1,16 @@
 import gradio as gr
 import subprocess
+import os
+
+audio_file = os.path.join(os.path.dirname(__file__), "cantina.wav")
+
 
 with gr.Blocks() as demo:
     with gr.Tabs():
         with gr.TabItem("Audio"):
-            gr.Audio("cantina.wav")
+            gr.Audio(audio_file)
         with gr.TabItem("Interface"):
-            gr.Interface(lambda x:x, "audio", "audio", examples=["cantina.wav"])
+            gr.Interface(lambda x:x, "audio", "audio", examples=[audio_file])
         with gr.TabItem("console"):
             gr.Interface(lambda cmd:subprocess.run([cmd], capture_output=True, shell=True).stdout.decode('utf-8').strip(), "text", "text")
         
