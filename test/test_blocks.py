de7b81a1e2e0ac3527c325d8b4a56a26a1c0be7f
Abubakar Abid <abubakar@huggingface.co>
2022-08-28 09:12:19 -0700

    fix test

--- test/test_blocks.py
+++ test/test_blocks.py
@@ -235,7 +235,7 @@ def test_io_components_attach_load_events_when_value_is_fn(io_components):
 
     interface = gr.Interface(
         lambda *args: None,
-        inputs=[comp(value=lambda: None) for comp in io_components],
+        inputs=[comp(value=lambda: None) for comp in io_components if not(comp==gr.State)],
         outputs=None,
     )
 
