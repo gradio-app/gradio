f1c0dac680a9ef66d2501771526929d4eb15cf9f
pngwn <hello@pngwn.io>
2022-08-30 18:02:52 +0100

    improve preview of dataframe examples

--- ui/packages/app/src/components/Dataset/ExampleComponents/Dataframe.svelte
+++ ui/packages/app/src/components/Dataset/ExampleComponents/Dataframe.svelte
@@ -1,23 +1,60 @@
 <script lang="ts">
-	export let value: Array<Array<string | number>>;
+	//@ts-ignore
+	import { csvParseRows } from "d3-dsv";
+	export let value: Array<Array<string | number>> | string;
+	export let samples_dir: string;
+	let hovered = false;
+	let loaded = Array.isArray(value);
+
+	$: if (!loaded && typeof value === "string" && /\.[a-zA-Z]+$/.test(value)) {
+		fetch(samples_dir + value)
+			.then((v) => v.text())
+			.then((v) => {
+				try {
+					value = csvParseRows(v);
+					loaded = true;
+				} catch (e) {
+					console.error(e);
+				}
+			});
+	}
 </script>
 
-<table class="gr-sample-dataframe">
-	{#each value.slice(0, 3) as row}
-		<tr>
-			{#each row.slice(0, 3) as cell}
-				<td class="p-2">{cell}</td>
+{#if loaded}
+	<div
+		class="gr-sample-dataframe"
+		on:mouseenter={() => (hovered = true)}
+		on:mouseleave={() => (hovered = false)}
+	>
+		<table class="gr-sample-dataframe relative">
+			{#each value.slice(0, 3) as row, i}
+				<tr>
+					{#each row.slice(0, 3) as cell, j}
+						<td
+							class="p-2 {i < 3 ? 'border-b border-b-slate-700' : ''} 
+							{j < 3 ? 'border-r border-r-slate-700 ' : ''}">{cell}</td
+						>
+					{/each}
+					{#if row.length > 3}
+						<td
+							class="p-2  border-r border-b border-r-slate-700 border-b-slate-700"
+							>…</td
+						>
+					{/if}
+				</tr>
 			{/each}
-			{#if row.length > 3}
-				<td class="p-2">...</td>
+			{#if value.length > 3}
+				<div
+					class="absolute w-full h-[50%] bottom-0 bg-gradient-to-b from-transparent to-slate-900"
+					class:to-slate-800={hovered}
+				/>
 			{/if}
-		</tr>
-	{/each}
-	{#if value.length > 3}
-		<tr>
-			{#each Array(Math.min(4, value[0].length)) as _}
-				<td class="p-2">...</td>
-			{/each}
-		</tr>
-	{/if}
-</table>
+			<!--	{#if value[0].length > 3}
+			<div
+				class="absolute h-full w-[50%] right-0 top-0 bg-gradient-to-r from-transparent to-slate-900"
+				class:to-slate-800={hovered}
+			/>
+		{/if} -->
+		</table>
+	</div>
+{/if}
