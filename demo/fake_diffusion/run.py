d98c2af6428bf2e4ae992f961f1c6602992d6c99
Abubakar Abid <abubakar@huggingface.co>
2022-09-02 03:54:49 -0700

    demo

--- demo/fake_diffusion/run.py
+++ demo/fake_diffusion/run.py
@@ -1,15 +1,21 @@
 import gradio as gr
 import numpy as np
 import time
+import os
+
 
 def fake_diffusion(steps):
     for i in range(steps):
         time.sleep(1)
         yield np.random.random((200, 200, 3))
     time.sleep(1)
-    yield "lion.jpg"
+    yield os.path.join(os.path.dirname(__file__), "cheetah.jpg")
 
-demo = gr.Interface(fake_diffusion, gr.Slider(1, 10, 3), "image")
+with gr.Blocks() as demo:
+    slider = gr.Slider(1, 10, 3)
+    button = gr.Button()
+    image = gr.Image()
+    button.click(fake_diffusion, slider, image)
 
 if __name__ == "__main__":
     demo.launch()
