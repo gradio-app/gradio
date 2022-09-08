69c77e9e4a073ed5c072165a67ad8e61d00c6122
Abubakar Abid <abubakar@huggingface.co>
2022-09-08 16:21:16 -0700

    more tests

--- test/test_queue.py
+++ test/test_queue.py
@@ -38,3 +38,27 @@ class TestQueue():
     async def test_receive(self, queue: Queue, mock_event: Event):
         await queue.get_message(mock_event)
         assert mock_event.websocket.receive_json.called
+
+    @pytest.mark.asyncio
+    async def test_send(self, queue: Queue, mock_event: Event):
+        await queue.send_message(mock_event, {})
+        assert mock_event.websocket.send_json.called
+
+    @pytest.mark.asyncio
+    async def test_add_to_queue(self, queue: Queue, mock_event: Event):
+        queue.push(mock_event)
+        assert len(queue.event_queue) == 1
+
+    @pytest.mark.asyncio
+    async def test_add_to_queue_with_max_size(self, queue: Queue, mock_event: Event):
+        queue.max_size = 1
+        queue.push(mock_event)
+        assert len(queue.event_queue) == 1
+        queue.push(mock_event)
+        assert len(queue.event_queue) == 1
+
+    @pytest.mark.asyncio
+    async def test_clean_event(self, queue: Queue, mock_event: Event):
+        queue.push(mock_event)
+        await queue.clean_event(mock_event)
+        assert len(queue.event_queue) == 0
