7b2ae84af725a49e8f3fa0f3424c0af7371f1207
Abubakar Abid <abubakar@huggingface.co>
2022-09-08 16:11:51 -0700

    tests

--- test/test_queue.py
+++ test/test_queue.py
@@ -1,7 +1,8 @@
 import os
+from unittest.mock import MagicMock
 import pytest
 
-from gradio.queue import Queue
+from gradio.queue import Queue, Event
 
 os.environ["GRADIO_ANALYTICS_ENABLED"] = "False"
 
@@ -11,10 +12,29 @@ def queue() -> Queue:
     yield queue_object
     queue_object.close()
 
+@pytest.fixture()
+def mock_event() -> Event:
+    websocket = MagicMock()
+    event = Event(websocket=websocket)
+    yield event
+    
 
 class TestQueue():
     @pytest.mark.asyncio
-    async def test_start(self, queue):
+    async def test_start(self, queue: Queue):
         await queue.start()
+        assert queue.stopped is False
         assert queue.get_active_worker_count() == 0
-        
+
+    @pytest.mark.asyncio
+    async def test_stop_resume(self, queue: Queue):
+        await queue.start()
+        queue.close()
+        assert queue.stopped
+        queue.resume()
+        assert queue.stopped is False
+
+    @pytest.mark.asyncio
+    async def test_receive(self, queue: Queue, mock_event: Event):
+        await queue.get_message(mock_event)
+        assert mock_event.websocket.receive_json.called
