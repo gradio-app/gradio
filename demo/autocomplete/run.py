1f29b78db7a2e75c7644b2ba1fc9feeadb01dcdb
aliabd <ali.si3luwa@gmail.com>
2022-09-14 09:09:18 -0700

    fix rate limiting in asr and autocomplete

--- demo/autocomplete/run.py
+++ demo/autocomplete/run.py
@@ -2,9 +2,14 @@
 # DESCRIPTION: This text generation demo works like autocomplete. There's only one textbox and it's used for both the input and the output. The demo loads the model as an interface, and uses that interface as an API. It then uses blocks to create the UI. All of this is done in less than 10 lines of code.
 # imports
 import gradio as gr
+import os
+
+# save your HF API token from https:/hf.co/settings/tokens as an env variable to avoid rate limiting
+auth_token = os.getenv("auth_token")
 
 # load a model from https://hf.co/models as an interface, then use it as an api 
-api = gr.Interface.load("huggingface/EleutherAI/gpt-j-6B")
+# you can remove the api_key parameter if you don't care about rate limiting. 
+api = gr.Interface.load("huggingface/EleutherAI/gpt-j-6B", api_key=auth_token)
 
 # define the core function
 def complete_with_gpt(text):
--- demo/automatic-speech-recognition/run.py
+++ demo/automatic-speech-recognition/run.py
@@ -2,13 +2,19 @@
 # DESCRIPTION: Automatic speech recognition in Chinese and English. Uses a tabbed layout to give the user the choice between uploading audio or recording from a microphone.
 # imports
 import gradio as gr
+import os
+
+# save your HF API token from https:/hf.co/settings/tokens as an env variable to avoid rate limiting
+auth_token = os.getenv("auth_token")
 
 # automatically load the interface from a HF model 
+# you can remove the api_key parameter if you don't care about rate limiting. 
 demo = gr.Interface.load(
     "huggingface/facebook/wav2vec2-base-960h",
     title="Speech-to-text",
     inputs="mic",
     description="Let me try to guess what you're saying!",
+    api_key=auth_token
 )
 
 # launch
