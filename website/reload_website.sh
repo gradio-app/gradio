920e093c4c4c4a60c8f8ca2731c855ca35169632
aliabd <ali.si3luwa@gmail.com>
2022-09-15 17:06:37 -0700

    refactor all slack notifs to one file

--- website/reload_website.sh
+++ website/reload_website.sh
@@ -11,8 +11,4 @@ else
     echo "Reloading..."
     docker-compose build
     docker-compose up -d
-
-    LATEST=$(git log -1 | fgrep commit)$(git log -1 | tail -1)
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app relaoded successfully! :ship:\n\n Latest live commit:\n>`'"${LATEST}"'`"}' ${SLACK_WEBHOOK}
-
 fi
--- website/reload_with_notification.sh
+++ website/reload_with_notification.sh
@@ -2,7 +2,6 @@
 . /home/ubuntu/.bashrc
 export PATH="/usr/local/bin:/usr/bin:/bin"
 
-
 ERROR=$(bash ./reload_website.sh 2>&1)
 
 if ! [ $? -eq 0 ]; then
@@ -11,4 +10,7 @@ if ! [ $? -eq 0 ]; then
     THREAD_TS="$( jq -r  '.ts' <<< "${RESPONSE}")"
     DATA=$( jo text="$(echo "\`\`\`'$ERROR'\`\`\`")" channel=$CHANNELID thread_ts="'$THREAD_TS'")
     _=$(curl https://slack.com/api/chat.postMessage -X POST -H 'Content-type: application/json; charset=utf-8' -H "Authorization: Bearer ${TOKEN}" --data "$DATA")
+else 
+    LATEST=$(git log -1 | fgrep commit)$(git log -1 | tail -1)
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app relaoded successfully! :ship:\n\n Latest live commit:\n>`'"${LATEST}"'`"}' ${SLACK_WEBHOOK}
 fi
