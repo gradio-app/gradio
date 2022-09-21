874b3e8dd23e7ae332dfe7704c8f4d800d2390dc
aliabd <ali.si3luwa@gmail.com>
2022-09-21 10:30:11 -0700

    fix logic

--- website/reload_with_notification.sh
+++ website/reload_with_notification.sh
@@ -11,8 +11,8 @@ if ! [ $? -eq 0 ]; then
     THREAD_TS="$( jq -r  '.ts' <<< "${RESPONSE}")"
     DATA=$( jo text="$(echo "\`\`\`'$OUTPUT'\`\`\`")" channel=$CHANNELID thread_ts="'$THREAD_TS'")
     _=$(curl https://slack.com/api/chat.postMessage -X POST -H 'Content-type: application/json; charset=utf-8' -H "Authorization: Bearer ${TOKEN}" --data "$DATA")
-elif grep -q "Already up to date." /tmp/git_changes.txt; then
-    :
+elif [[ $OUTPUT == *"NO CHANGES"* ]]; then
+  :
 else 
     LATEST=$(git log -1 | fgrep commit)$(git log -1 | tail -1)
     curl -X POST -H 'Content-type: application/json' --data '{"text":"gradio.app relaoded successfully! :ship:\n\n Latest live commit:\n>`'"${LATEST}"'`"}' ${SLACK_WEBHOOK}
