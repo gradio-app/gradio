9c9e65f60a530ddc17568cb0c28b9cf60e02a987
freddyaboulton <alfonsoboulton@gmail.com>
2022-09-29 12:17:41 -0400

    Rename

--- gradio/blocks.py
+++ gradio/blocks.py
@@ -931,7 +931,7 @@ class Blocks(BlockContext):
                 js=_js,
             )
 
-    def continuous(
+    def run_forever(
         self,
         fn: Optional[Callable] = None,
         inputs: Optional[List[Component]] = None,
