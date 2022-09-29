d4cfd4a4b1162ad706e2b1c8b3c824dac8219298
aliabd <ali.si3luwa@gmail.com>
2022-09-29 04:55:52 -0700

    render from releases dir

--- website/homepage/src/changelog/__init__.py
+++ website/homepage/src/changelog/__init__.py
@@ -4,11 +4,10 @@ import shutil
 import re
 
 DIR = os.path.dirname(__file__)
-INNER_TEMPLATE_FILE = os.path.join(DIR, "inner_template.html")
-CHANGELOG_FILE = os.path.join(DIR, "..", "..", "..", "CHANGELOG.md")
+RELEASES_DIR = os.path.join(DIR, "..", "..", "..", "releases")
 
-def render_md(filepath):
-    with open(CHANGELOG_FILE, "r") as change_file:
+def render_md(version_filename):
+    with open(os.path.join(RELEASES_DIR, version_filename), "r") as change_file:
         content = change_file.read()
     
     # replace code blocks correctly
@@ -35,35 +34,6 @@ def render_md(filepath):
    
     # remove empty/unused sections
     content = re.sub(r"## [\w^:\n ]*No changes to highlight.", "", content)
-    # section_titles = [
-    #     "New Features", 
-    #     "Bug Fixes", 
-    #     "Documentation Changes", 
-    #     "Testing and Infrastructure Changes", 
-    #     "Breaking Changes",
-    #     "Full Changelog",
-    #     "Contributors Shoutout"
-    # ]
-
-    # for index, title in enumerate(section_titles):
-    #     title = f"## {title}:\n"
-    #     if title in content:
-    #         if index == len(section_titles) - 1: 
-    #             section = content.split(title)[1]
-    #         else:
-    #             next_title =  f"## {section_titles[index + 1]}:\n"
-    #             section = content.split(title)[1].split(next_title)[0]
-    #         print("\n\n\n\n----------")
-    #         print(section)
-    #         print("----------\n\n\n\n")
-    #         if not any(char.isalnum() for char in section): 
-    #             content.replace(title + section, "")
-
-        
-
-    # get versions and their correct href
-    versions = re.findall(r"# Version \d\.\d[^\n ]*", content)
-    versions = [("Upcoming Release", "upcoming-release")] + [("v" + v.strip("# Version "), "version-" + v.strip("# Version ").replace('.','')) for v in versions]
 
     content_html = markdown2.markdown(
                 content,
@@ -75,13 +45,19 @@ def render_md(filepath):
                 ],
             )
 
-    with open(INNER_TEMPLATE_FILE, "w+") as temp_html:
+    with open(os.path.join(DIR, f"{version_filename[:-3]}.html"), "w+") as temp_html:
         temp_html.write(content_html)
-    
-    return versions
+
+versions = []
+for version_filename in os.listdir(RELEASES_DIR):
+    render_md(version_filename)
+    versions.append(version_filename[:-3])
+
+versions.remove("UPCOMING")
+versions.sort(reverse=True)
+versions = ["UPCOMING"] + versions
 
 def build(output_dir, jinja_env):
-    versions = render_md(CHANGELOG_FILE)
     os.makedirs(output_dir, exist_ok=True)
     template = jinja_env.get_template("changelog/parent_template.html")
     output = template.render(versions=versions)
