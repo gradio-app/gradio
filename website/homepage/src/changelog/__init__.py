fa789e55cecab154cf8750ee58871f9aa157f182
aliabd <ali.si3luwa@gmail.com>
2022-09-30 12:16:36 -0700

    one file at root

--- website/homepage/src/changelog/__init__.py
+++ website/homepage/src/changelog/__init__.py
@@ -4,10 +4,11 @@ import shutil
 import re
 
 DIR = os.path.dirname(__file__)
-RELEASES_DIR = os.path.join(DIR, "..", "..", "..", "releases")
+INNER_TEMPLATE_FILE = os.path.join(DIR, "inner_template.html")
+CHANGELOG_FILE = os.path.join(DIR, "..", "..", "..", "..", "CHANGELOG.md")
 
-def render_md(version_filename):
-    with open(os.path.join(RELEASES_DIR, version_filename), "r") as change_file:
+def render_md():
+    with open(CHANGELOG_FILE, "r") as change_file:
         content = change_file.read()
     
     # replace code blocks correctly
@@ -17,23 +18,14 @@ def render_md(version_filename):
             content,
         )
     content = re.sub(r"```", "</code></pre></div>", content)
-
-    # replace pr tags with links
-    content = re.sub(
-            r"\:pr\:(\d[^\n ]*)",
-            lambda x: f"<a href='https://github.com/gradio-app/gradio/pull/{x.group(1)}'>https://github.com/gradio-app/gradio/pull/{x.group(1)}</a>",
-            content,
-        )
-    
-    # replace @ usernames with links
-    content = re.sub(
-            r"\@([^\n ]*)",
-            lambda x: f"<a href='https://github.com/{x.group(1)}'>@{x.group(1)}</a>",
-            content,
-        ) 
    
     # remove empty/unused sections
     content = re.sub(r"## [\w^:\n ]*No changes to highlight.", "", content)
+
+    # get versions and their correct href
+    versions = re.findall(r"# Version \d\.\d[^\n ]*", content)
+    versions = [("Upcoming Release", "upcoming-release")] + [("v" + v.strip("# Version "), "version-" + v.strip("# Version ").replace('.','')) for v in versions]
+
 
     content_html = markdown2.markdown(
                 content,
@@ -45,21 +37,14 @@ def render_md(version_filename):
                 ],
             )
 
-    with open(os.path.join(DIR, f"{version_filename[:-3]}.html"), "w+") as temp_html:
+    with open(INNER_TEMPLATE_FILE, "w+") as temp_html:
         temp_html.write(content_html)
 
-versions = []
-for version_filename in os.listdir(RELEASES_DIR):
-    if version_filename == "TEMPLATE.md":
-        continue
-    render_md(version_filename)
-    versions.append(version_filename[:-3])
+    return versions
 
-versions.remove("UPCOMING")
-versions.sort(reverse=True)
-versions = ["UPCOMING"] + versions
 
 def build(output_dir, jinja_env):
+    versions = render_md()
     os.makedirs(output_dir, exist_ok=True)
     template = jinja_env.get_template("changelog/parent_template.html")
     output = template.render(versions=versions)
