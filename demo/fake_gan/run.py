25c1ebe333ba5954c678ef8162f600fe708fd598
Ali Abid <aabid94@gmail.com>
2022-10-03 01:15:27 -0500

    changes

--- demo/fake_gan/run.py
+++ demo/fake_gan/run.py
@@ -8,35 +8,47 @@ import gradio as gr
 
 def fake_gan():
     images = [
-        random.choice(
-            [
-                "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-                "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80",
-                "https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
-                "https://images.unsplash.com/photo-1546456073-92b9f0a8d413?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-                "https://images.unsplash.com/photo-1601412436009-d964bd02edbc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
-            ]
-        )
+        "files/cheetah1.jpg"
         for _ in range(3)
     ]
     return images
 
 
 with gr.Blocks() as demo:
-    with gr.Row(variant="box"):
-        text = gr.Textbox(
-            label="Enter your prompt",
-            show_label=False,
-            max_lines=1,
-            placeholder="Enter your prompt",
-        ).style(
-            container=False,
-        )
-        btn = gr.Button("Generate image").style(full_width=False)
+    # with gr.Row(variant="box"):
+    #     text = gr.Textbox(
+    #         label="Enter your prompt",
+    #         show_label=False,
+    #         max_lines=1,
+    #         placeholder="Enter your prompt",
+    #     ).style(
+    #         container=False,
+    #     )
+    #     btn = gr.Button("Generate image").style(full_width=False)
+    with gr.Group():
+        with gr.Box():
+            with gr.Row(elem_id="prompt-container").style(mobile_collapse=False, equal_height=True):
+                text = gr.Textbox(
+                    label="Enter your prompt",
+                    show_label=False,
+                    max_lines=1,
+                    placeholder="Enter your prompt",
+                    elem_id="prompt-text-input",
+                ).style(
+                    border=(True, False, True, True),
+                    rounded=(True, False, False, True),
+                    container=False,
+                )
+                btn = gr.Button("Generate image").style(
+                    margin=False,
+                    rounded=(False, True, True, False),
+                    full_width=False,
+                )
 
-    gallery = gr.Gallery(
-        label="Generated images", show_label=False, elem_id="gallery"
-    ).style(grid=[2], height="auto")
+
+        gallery = gr.Gallery(
+            label="Generated images", show_label=False, elem_id="gallery"
+        ).style(grid=[2], height="auto")
 
     btn.click(fake_gan, None, gallery)
 
