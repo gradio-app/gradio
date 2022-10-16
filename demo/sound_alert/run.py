95e5f0098b0721e1b63d1fafb6d822323531719f
Abubakar Abid <abubakar@huggingface.co>
2022-10-16 10:01:48 -0500

    add sound alert demo

---
+++ demo/sound_alert/run.py
@@ -0,0 +1,17 @@
+import time
+import gradio as gr
+
+
+js_function = "() => {new Audio('file=beep.mp3').play();}"
+
+def task(x):
+    time.sleep(2)
+    return "Hello, " + x 
+
+with gr.Blocks() as demo:
+    name = gr.Textbox(label="name")
+    greeting = gr.Textbox(label="greeting")
+    name.blur(task, name, greeting)
+    greeting.change(None, None, None, _js=js_function)  # Note that _js is a special arugment whose usage may change in the future
+    
+demo.launch()
