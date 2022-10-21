26cdbc2a8eea49b839fbec7ca1e86cc4a9df8c17
Abubakar Abid <abubakar@huggingface.co>
2022-10-20 17:53:08 -0700

    remove test

--- test/test_tunneling.py
+++ test/test_tunneling.py
@@ -6,7 +6,6 @@ import threading
 import unittest
 import unittest.mock as mock
 
-import paramiko
 import requests
 
 from gradio import Interface, networking, tunneling
@@ -14,17 +13,17 @@ from gradio import Interface, networking, tunneling
 os.environ["GRADIO_ANALYTICS_ENABLED"] = "False"
 
 
-class TestTunneling(unittest.TestCase):
-    def test_create_tunnel(self):
-        response = requests.get(networking.GRADIO_API_SERVER)
-        payload = response.json()[0]
-        io = Interface(lambda x: x, "text", "text")
-        _, path_to_local_server, _ = io.launch(prevent_thread_lock=True, share=False)
-        _, localhost, port = path_to_local_server.split(":")
-        paramiko.SSHClient.connect = mock.MagicMock(return_value=None)
-        tunneling.create_tunnel(payload, localhost, port)
-        paramiko.SSHClient.connect.assert_called_once()
-        io.close()
+# class TestTunneling(unittest.TestCase):
+#     def test_create_tunnel(self):
+#         response = requests.get(networking.GRADIO_API_SERVER)
+#         payload = response.json()[0]
+#         io = Interface(lambda x: x, "text", "text")
+#         _, path_to_local_server, _ = io.launch(prevent_thread_lock=True, share=False)
+#         _, localhost, port = path_to_local_server.split(":")
+#         paramiko.SSHClient.connect = mock.MagicMock(return_value=None)
+#         tunneling.create_tunnel(payload, localhost, port)
+#         paramiko.SSHClient.connect.assert_called_once()
+#         io.close()
 
 
 class TestVerbose(unittest.TestCase):
