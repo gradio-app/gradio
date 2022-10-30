cfc1123685a1bdb1d26e2a0cff5a05c91644d84e
pngwn <hello@pngwn.io>
2022-10-30 14:12:55 +0000

    always disregard embedding apps path when embedding

--- ui/packages/app/src/api.ts
+++ ui/packages/app/src/api.ts
@@ -125,7 +125,8 @@ export const fn =
 				? location.href
 				: api_endpoint;
 			var ws_protocol = ws_endpoint.startsWith("https") ? "wss:" : "ws:";
-			var ws_path = location.pathname === "/" ? "/" : location.pathname;
+			var ws_path =
+				is_embed || location.pathname === "/" ? "/" : location.pathname;
 			var ws_host =
 				BUILD_MODE === "dev" || location.origin === "http://localhost:3000"
 					? BACKEND_URL.replace("http://", "").slice(0, -1)
