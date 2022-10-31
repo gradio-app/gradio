3a10ff1aaf5849f7eade299b181d860dfc1b59a3
Vladimir Repin <32306715+mezotaken@users.noreply.github.com>
2022-10-31 23:02:09 +0300

    Fixed upload dialog being showed on clear #2476

--- ui/packages/upload/src/ModifyUpload.svelte
+++ ui/packages/upload/src/ModifyUpload.svelte
@@ -21,5 +21,8 @@
 		<IconButton Icon={Edit} on:click={() => dispatch("edit")} />
 	{/if}
 
-	<IconButton Icon={Clear} on:click={() => dispatch("clear")} />
+	<IconButton Icon={Clear} on:click={(event) => {
+		dispatch("clear");
+		event.stopPropagation();
+	}} />
 </div>
