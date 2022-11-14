9d51814030c98cca2297be663b5a2b20e0dd6d14
freddyaboulton <alfonsoboulton@gmail.com>
2022-11-14 15:03:05 -0500

    Do not call update twice

--- test/test_blocks.py
+++ test/test_blocks.py
@@ -391,6 +391,27 @@ class TestComponentsInBlocks:
             "value": gr.media_data.BASE64_IMAGE,
         }
 
+    @pytest.mark.asyncio
+    async def test_blocks_update_interactive(
+        self,
+    ):
+        def infer():
+            return [gr.update(interactive=True), gr.update(interactive=True)]
+
+        with gr.Blocks() as demo:
+            run = gr.Button(value="Make interactive")
+            image = gr.Image()
+            textbox = gr.Text()
+            run.click(infer, None, [image, textbox])
+
+        output = await demo.process_api(0, [])
+        assert output["data"][0] == {
+            "interactive": True,
+            "__type__": "update",
+            "mode": "dynamic",
+        }
+        assert output["data"][1] == {"__type__": "update", "mode": "dynamic"}
+
 
 class TestCallFunction:
     @pytest.mark.asyncio
