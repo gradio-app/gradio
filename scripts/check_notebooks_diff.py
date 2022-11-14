e45d3da91d2e61a21abd01f09a1f56004d8ccf67
aliabd <ali.si3luwa@gmail.com>
2022-11-14 15:54:28 +0000

    remove prit

--- scripts/check_notebooks_diff.py
+++ scripts/check_notebooks_diff.py
@@ -3,7 +3,6 @@ import sys
 
 with open("diff.txt") as f:
     diff = f.read()
-print(diff[:1000])
 
 regexp = re.compile(r"^((\+|\-)   )((?!id).)*$",  re.MULTILINE)
 if bool(re.findall(regexp, diff)):
