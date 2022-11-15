0d7691420844ee0372b0e4ea9306ed0ee4e7dd69
pngwn <hello@pngwn.io>
2022-11-15 21:52:41 +0100

    whatever

--- gradio/components.py
+++ gradio/components.py
@@ -297,7 +297,7 @@ class Textbox(
             elem_id: An optional string that is assigned as the id of this component in the HTML DOM. Can be used for targeting CSS styles.
             type: The type of textbox. One of: 'text', 'password', 'email', Default is 'text'.
         """
-        if not type in ["text", "password", "email"]:
+        if type not in ["text", "password", "email"]:
             raise ValueError('`type` must be one of "text", "password", or "email".')
 
         #
