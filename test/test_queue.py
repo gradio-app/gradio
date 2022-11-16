3fb02710db4257973d2969f8118f362dba72b948
Abubakar Abid <abubakar@huggingface.co>
2022-11-15 22:35:31 -0800

    fixed tests

--- test/test_queue.py
+++ test/test_queue.py
@@ -144,7 +144,7 @@ class TestQueueProcessEvents:
         reason="Mocks of async context manager don't work for 3.7",
     )
     @pytest.mark.asyncio
-    @patch("gradio.queue.Request", new_callable=AsyncMock)
+    @patch("gradio.queue.AsyncRequest", new_callable=AsyncMock)
     async def test_process_event(self, mock_request, queue: Queue, mock_event: Event):
         queue.gather_event_data = AsyncMock()
         queue.gather_event_data.return_value = True
@@ -254,7 +254,7 @@ class TestQueueProcessEvents:
         reason="Mocks of async context manager don't work for 3.7",
     )
     @pytest.mark.asyncio
-    @patch("gradio.queue.Request", new_callable=AsyncMock)
+    @patch("gradio.queue.AsyncRequest", new_callable=AsyncMock)
     async def test_process_event_handles_exception_during_disconnect(
         self, mock_request, queue: Queue, mock_event: Event
     ):
