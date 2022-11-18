0a2ac3bf67f974cc3c871f8711d08bc3123a0e8b
aliabid94 <aabid94@gmail.com>
2022-11-18 06:47:30 -0800

    Update gradio/strings.py
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- gradio/strings.py
+++ gradio/strings.py
@@ -19,7 +19,7 @@ en = {
     "COLAB_DEBUG_TRUE": "Colab notebook detected. This cell will run indefinitely so that you can see errors and logs. "
     "To turn off, set debug=False in launch().",
     "COLAB_DEBUG_FALSE": "Colab notebook detected. To show errors in colab notebook, set debug=True in launch()",
-    "COLAB_WARNING": "Opening Chrome Inspector may crash Colab Embedded Mode.",
+    "COLAB_WARNING": "Note: opening Chrome Inspector may crash demo inside Colab notebooks.",
     "SHARE_LINK_MESSAGE": "\nThis share link expires in 72 hours. For free permanent hosting and GPU upgrades (NEW!), check out Spaces: https://huggingface.co/spaces",
     "PRIVATE_LINK_MESSAGE": "Since this is a private endpoint, this share link will never expire.",
     "INLINE_DISPLAY_BELOW": "Interface loading below...",
