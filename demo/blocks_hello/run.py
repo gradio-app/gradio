8e6f6f4162240f1007b5ec5fbc751173c5ab171f
Ali Abid <aabid94@gmail.com>
2022-11-28 13:20:54 -0700

    fix

--- demo/blocks_hello/run.py
+++ demo/blocks_hello/run.py
@@ -5,21 +5,8 @@ def welcome(name):
 
 with gr.Blocks() as demo:
     gr.Markdown(
-    r"""
-    # Hello World! $\frac{\sqrt{x + y}}{4}$ is today's lesson
-
-    ## the $\sqrt{x + y}$ is first
-
-    Start with $\frac{\frac{x+1}{x+2}}{x+3}$ then we get $x+2$
-    
-    There are three formulas to know:
-    
-    the first is $\gamma^2 + \theta^2 = \omega^2$
-    
-    $\sqrt{x^2+1}$ is next
-    
-    Integral $\int_{a}^{b} x^2 \,dx$ is last
-
+    """
+    # Hello World!
     Start typing below to see the output.
     """)
     inp = gr.Textbox(placeholder="What is your name?")
