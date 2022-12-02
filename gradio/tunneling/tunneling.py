72db09142a02ca65a768b015fbed88b716d2f85e
Adrien <adrien@huggingface.co>
2022-12-02 14:28:50 +0100

    Transform into object

--- gradio/tunneling/tunneling.py
+++ gradio/tunneling/tunneling.py
@@ -3,186 +3,162 @@ import asyncio
 import hashlib
 import json
 import struct
-import sys
 from time import time
-from typing import Tuple
 
 from gradio.tunneling.pyyamux import Conn, Session, Stream
 
 
-async def handle_req_work_conn(
-    run_id: int, local_host: str, local_port: int, session: Session
-):
-    stream = session.open()
+class Tunnel:
+    def __init__(
+            self, remote_host: str, remote_port: int, local_host: str, local_port: int
+    ):
+        self.running_worker = 0
+        self.remote_host = remote_host
+        self.remote_port = remote_port
+        self.local_host = local_host
+        self.local_port = local_port
+        self.url = ""
+        self.run_id = None
+        self.expiry = time() + 60 * 60 * 72
+        self.session = None
+        self.control_stream = None
 
-    # Send the run id (TypeNewWorkConn)
-    await _send(stream, {"run_id": run_id}, 119)
+    async def start(self) -> str:
+        reader, writer = await asyncio.open_connection(
+            self.remote_host, self.remote_port
+        )
+        conn = Conn(reader, writer)
+        self.session = Session(conn)
 
-    # Wait for the server to ask to connect
-    # We don't use the message as we don't need his content
-    # Useful if the client implement multiple proxy
-    # In our use case only one that we know
-    await _read(stream)
+        asyncio.create_task(self.session.init())
+        self.control_stream = self.session.open()
 
-    # Cleanup the reader that contains previous data
-    stream.reset_reader()
+        # Send `TypeLogin`
+        timestamp, privilege_key = self._generate_privilege_key()
 
-    # Connect to the running app
-    reader, writer = await asyncio.open_connection(local_host, local_port)
-    pipe2 = pipe(reader, stream)
-    pipe1 = pipe(stream.reader, writer)
-    await asyncio.gather(pipe1, pipe2, return_exceptions=True)
+        self._send(
+            self.control_stream,
+            {
+                "version": "0.44.0",
+                "pool_count": 1,
+                "privilege_key": privilege_key,
+                "timestamp": timestamp,
+            },
+            111,
+        )
 
+        asyncio.create_task(self.loop())
 
-async def pipe(reader, writer):
-    while not reader.at_eof():
-        await asyncio.sleep(0)
-        read_ = await reader.read(1024)
-        writer.write(read_)
-        await writer.drain()
-    writer.close()
+        # Sending proxy information `TypeNewProxy`
+        self._send(self.control_stream, {"proxy_type": "http"}, 112)
 
+        while self.url == "":
+            await asyncio.sleep(0)
 
-async def _send(stream: Stream, msg: dict, type: int):
-    """Send a message to frps.
+        return self.url
 
-    First byte is the message type
-    https://github.com/fatedier/frp/blob/6ecc97c8571df002dd7cf42522e3f2ce9de9a14d/pkg/msg/msg.go#L20-L20
-    8 next bytes are the message length.
-    Then it's the json body.
-    """
-    binary_message = bytearray(0)
-    binary_message.extend([type])
-    json_raw = json.dumps(msg).encode("utf-8")
-    binary_message.extend(struct.pack(">q", len(json_raw)))
-    binary_message.extend(json_raw)
-    return stream.write(binary_message)
+    @staticmethod
+    def _generate_privilege_key() -> tuple[int, str]:
+        """Generate and return a (timestamp, privilege_key) hash.
 
+        Privilege Key and Timestamp should be generate like:
+        https://github.com/fatedier/frp/blob/6ecc97c8571df002dd7cf42522e3f2ce9de9a14d/pkg/util/util/util.go#L46-L46
+        """
+        timestamp = round(time())
+        return timestamp, hashlib.md5(f"{timestamp}".encode()).hexdigest()
 
-async def _read(stream: Stream):
-    """Read message from frps
+    async def handle_req_work_conn(self):
+        stream = self.session.open()
 
-    First byte is the message type
-    https://github.com/fatedier/frp/blob/6ecc97c8571df002dd7cf42522e3f2ce9de9a14d/pkg/msg/msg.go#L20-L20
-    8 next bytes are the message length.
-    Then it's the json body.
-    """
-    data = await stream.reader.read(1)
-    if not data:
-        return None, None
-    binary_type = list(data)[0]
+        # Send the run id (TypeNewWorkConn)
+        self._send(stream, {"run_id": self.run_id}, 119)
 
-    size = await stream.reader.read(8)
-    if not size:
-        return None, None
-    size = struct.unpack(">Q", size)[0]
-    json_raw = await stream.reader.read(size)
-    return json.loads(json_raw), binary_type
+        # Wait for the server to ask to connect
+        # We don't use the message as we don't need his content
+        # Useful if the client implement multiple proxy
+        # In our use case only one that we know
+        await self._read(stream)
 
+        # Cleanup the reader that contains previous data
+        stream.reset_reader()
 
-async def _client_loop(
-    session: Session,
-    _stream: Stream,
-    run_id: str,
-    local_host: str,
-    local_port: int,
-    expiry: int,
-):
-    while True:
-        await asyncio.sleep(0)
-        _, type = await _read(_stream)
-        if not type or time() > expiry:
-            break
-        # TypeReqWorkConn
-        if type == 114:
-            _create_task(handle_req_work_conn(run_id, local_host, local_port, session))
-    _stream.close()
+        # Connect to the running app
+        reader, writer = await asyncio.open_connection(self.local_host, self.local_port)
+        pipe2 = self.pipe(reader, stream)
+        pipe1 = self.pipe(stream.reader, writer)
+        await asyncio.gather(pipe1, pipe2, return_exceptions=True)
+        self.running_worker -= 1
 
+    @staticmethod
+    async def pipe(reader, writer):
+        while not reader.at_eof():
+            await asyncio.sleep(0)
+            read_ = await reader.read(1024)
+            writer.write(read_)
+            await writer.drain()
+        writer.close()
 
-def _generate_privilege_key() -> Tuple[int, str]:
-    """Generate and return a (timestamp, privilege_key) hash.
+    @staticmethod
+    def _send(stream: Stream, msg: dict, type: int):
+        """Send a message to frps.
 
-    Privilege Key and Timestamp should be generate like:
-    https://github.com/fatedier/frp/blob/6ecc97c8571df002dd7cf42522e3f2ce9de9a14d/pkg/util/util/util.go#L46-L46
-    """
-    timestamp = round(time())
-    return timestamp, hashlib.md5(f"{timestamp}".encode()).hexdigest()
+        First byte is the message type
+        https://github.com/fatedier/frp/blob/6ecc97c8571df002dd7cf42522e3f2ce9de9a14d/pkg/msg/msg.go#L20-L20
+        8 next bytes are the message length.
+        Then it's the json body.
+        """
+        binary_message = bytearray(0)
+        binary_message.extend([type])
+        json_raw = json.dumps(msg).encode("utf-8")
+        binary_message.extend(struct.pack(">q", len(json_raw)))
+        binary_message.extend(json_raw)
+        return stream.write(binary_message)
+
+    @staticmethod
+    async def _read(stream: Stream):
+        """Read message from frps
+
+        First byte is the message type
+        https://github.com/fatedier/frp/blob/6ecc97c8571df002dd7cf42522e3f2ce9de9a14d/pkg/msg/msg.go#L20-L20
+        8 next bytes are the message length.
+        Then it's the json body.
+        """
+        data = await stream.reader.read(1)
+        if not data:
+            return None, None
+        binary_type = list(data)[0]
+
+        size = await stream.reader.read(8)
+        if not size:
+            return None, None
+        size = struct.unpack(">Q", size)[0]
+        json_raw = await stream.reader.read(size)
+        return json.loads(json_raw), binary_type
+
+    async def loop(self):
+        while True:
+            await asyncio.sleep(0)
+            data, type = await self._read(self.control_stream)
+            if not type or time() > self.expiry:
+                break
+            if type == 50:
+                self.url = data["remote_addr"]
+                continue
+            # TypeLoginResp
+            if type == 49:
+                self.run_id = data["run_id"]
+                continue
+            # TypeReqWorkConn
+            if type == 114:
+                if self.running_worker < 32:
+                    self.running_worker += 1
+                    asyncio.create_task(self.handle_req_work_conn())
+                continue
+        self.control_stream.close()
 
 
 async def _create_tunnel(
-    remote_host: str, remote_port: int, local_host: str, local_port: int
+        remote_host: str, remote_port: int, local_host: str, local_port: int
 ) -> str:
-    """
-    Creates a tunnel between a local server/port and a remote server/port. Returns
-    the URL of the share link that is connected to the local server/port.
-    """
-    # Connect to frps
-    # Connect to remote frps
-    reader, writer = await asyncio.open_connection(remote_host, remote_port)
-    conn = Conn(reader, writer)
-    session = Session(conn)
-
-    _create_task(session.init())
-    stream = session.open()
-
-    # Send `TypeLogin`
-    timestamp, privilege_key = _generate_privilege_key()
-    expiry = timestamp + 60 * 60 * 72
-
-    await _send(
-        stream,
-        {
-            "version": "0.44.0",
-            "pool_count": 1,
-            "privilege_key": privilege_key,
-            "timestamp": timestamp,
-        },
-        111,
-    )
-
-    # Wait for response `TypeLoginResp`
-    login_response, _ = await _read(stream)
-
-    if not login_response:
-        # TODO Handle this correctly
-        print("error getting response")
-        sys.exit(1)
-
-    if "error" in login_response:
-        # TODO Handle this correctly
-        print("error during login")
-        sys.exit(1)
-
-    run_id = login_response["run_id"]
-
-    # Server will ask to warm connection
-    msg, msg_type = await _read(stream)
-
-    if msg_type != 114:
-        # TODO Handle this correctly
-        sys.exit(1)
-
-    # Sending proxy information `TypeNewProxy`
-    await _send(stream, {"proxy_type": "http"}, 112)
-    msg, msg_type = await _read(stream)
-
-    if msg_type != 50:
-        # TODO Handle this correctly
-        print("error during proxy registration")
-        sys.exit(1)
-
-    _create_task(_client_loop(session, stream, run_id, local_host, local_port, expiry))
-
-    return msg["remote_addr"]
-
-
-def _create_task(coroutine):
-    async def _inner_coroutine():
-        try:
-            await coroutine
-        except Exception as e:
-            print("\n\n\nEEEERRRROOOO\n\n\n")
-            print(e)
-            raise
-
-    asyncio.create_task(_inner_coroutine())
+    tunnel = Tunnel(remote_host, remote_port, local_host, local_port)
+    return await tunnel.start()
