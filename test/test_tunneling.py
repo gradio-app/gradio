81a27ed5daec1e03ff73fd4d3138e2809940d856
Abubakar Abid <abubakar@huggingface.co>
2022-12-08 20:25:08 -0800

    formatting

---
+++ test/test_tunneling.py
@@ -0,0 +1,11 @@
+import pytest
+
+from gradio import Interface, networking
+
+
+@pytest.mark.flaky
+def test_setup_tunnel():
+    io = Interface(lambda x: x, "number", "number")
+    io.launch(show_error=True, prevent_thread_lock=True)
+    share_url = networking.setup_tunnel(io.server_name, io.server_port)
+    assert isinstance(share_url, str)
