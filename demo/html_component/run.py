9a259cb8d0d002b445c4a36e0b60821d8b1ca052
aliabd <ali.si3luwa@gmail.com>
2022-12-20 20:50:14 +0400

    add css to demos and remove footer js from html

--- demo/html_component/run.py
+++ demo/html_component/run.py
@@ -1,6 +1,8 @@
 import gradio as gr 
 
-with gr.Blocks() as demo:
+css = "footer {display: none !important;} .gradio-container {min-height: 0px !important;}"
+
+with gr.Blocks(css=css) as demo:
     gr.HTML(value="<p style='margin-top: 1rem, margin-bottom: 1rem'>Gradio Docs Readers: <img src='https://visitor-badge.glitch.me/badge?page_id=gradio-docs-visitor-badge' alt='visitor badge' style='display: inline-block'/></p>")
 
 demo.launch()
