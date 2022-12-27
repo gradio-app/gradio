8579da619bdafcf39289525be69fe1f1d3641e3d
freddyaboulton <alfonsoboulton@gmail.com>
2022-12-27 11:42:25 -0500

    Add back container

--- demo/blocks_style/run.py
+++ demo/blocks_style/run.py
@@ -2,28 +2,23 @@ import gradio as gr
 
 with gr.Blocks(title="Styling Examples") as demo:
     with gr.Column(variant="box"):
-        txt = gr.Textbox(label="Small Textbox", lines=1).style(container=False)
-        num = gr.Number(label="Number", show_label=False).style(container=False)
-        slider = gr.Slider(label="Slider", show_label=False).style(
-            container=False,
-        )
-        check = gr.Checkbox(label="Checkbox", show_label=False).style(
-            container=False
-        )
+        txt = gr.Textbox(label="Small Textbox", lines=1)
+        num = gr.Number(label="Number", show_label=False)
+        slider = gr.Slider(label="Slider", show_label=False)
+        check = gr.Checkbox(label="Checkbox", show_label=False)
         check_g = gr.CheckboxGroup(
             label="Checkbox Group",
             choices=["One", "Two", "Three"],
             show_label=False,
-        ).style(container=False)
+        )
         radio = gr.Radio(
             label="Radio", choices=["One", "Two", "Three"], show_label=False
         ).style(
             item_container=False,
-            container=False,
         )
         drop = gr.Dropdown(
             label="Dropdown", choices=["One", "Two", "Three"], show_label=False
-        ).style(container=False)
+        )
         image = gr.Image(show_label=False)
         video = gr.Video(show_label=False)
         audio = gr.Audio(show_label=False)
