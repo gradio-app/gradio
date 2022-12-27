68c52b626eab9a8de8c397a113509566b23c0e74
freddyaboulton <alfonsoboulton@gmail.com>
2022-12-27 11:15:36 -0500

    Fix API + notebook

--- demo/blocks_style/run.py
+++ demo/blocks_style/run.py
@@ -2,100 +2,63 @@ import gradio as gr
 
 with gr.Blocks(title="Styling Examples") as demo:
     with gr.Column():
-        txt = gr.Textbox(label="Small Textbox", lines=1).style(
-            rounded=False,
-            border=False,
-            container=False,
-        )
+        with gr.Column(variant="box"):
+            txt = gr.Textbox(label="Small Textbox", lines=1).style(container=False)
+            num = gr.Number(label="Number", show_label=False).style(container=False)
+            slider = gr.Slider(label="Slider", show_label=False).style(
+                container=False,
+            )
+            check = gr.Checkbox(label="Checkbox", show_label=False).style(
+                container=False
+            )
+            check_g = gr.CheckboxGroup(
+                label="Checkbox Group",
+                choices=["One", "Two", "Three"],
+                show_label=False,
+            ).style(container=False)
+            radio = gr.Radio(
+                label="Radio", choices=["One", "Two", "Three"], show_label=False
+            ).style(
+                item_container=False,
+                container=False,
+            )
+            drop = gr.Dropdown(
+                label="Dropdown", choices=["One", "Two", "Three"], show_label=False
+            ).style(container=False)
+            image = gr.Image(show_label=False)
+            video = gr.Video(show_label=False)
+            audio = gr.Audio(show_label=False)
+            file = gr.File(show_label=False)
+            df = gr.Dataframe(show_label=False)
+            ts = gr.Timeseries(show_label=False)
+            label = gr.Label().style(
+                container=False,
+            )
+            highlight = gr.HighlightedText(
+                "+ hello. - goodbye",
+                show_label=False,
+                color_map={"+": "green", "-": "red"},
+            ).style(container=False)
+            json = gr.JSON().style(container=False)
+            html = gr.HTML(show_label=False)
+            gallery = gr.Gallery().style(
+                grid=(3, 3, 1),
+                height="auto",
+                container=False,
+            )
+            chat = gr.Chatbot([("hi", "good bye")]).style(color_map=("pink", "blue"))
 
-        num = gr.Number(label="Number", show_label=False).style(
-            rounded=False,
-            border=False,
-            container=False,
-        )
-        slider = gr.Slider(label="Slider", show_label=False).style(
-            container=False,
-        )
-        check = gr.Checkbox(label="Checkbox", show_label=False).style(
-            rounded=False,
-            border=False,
-            container=False,
-        )
-        check_g = gr.CheckboxGroup(
-            label="Checkbox Group", choices=["One", "Two", "Three"], show_label=False
-        ).style(rounded=False, container=False, item_container=False)
-        radio = gr.Radio(
-            label="Radio", choices=["One", "Two", "Three"], show_label=False
-        ).style(
-            item_container=False,
-            container=False,
-        )
-        drop = gr.Dropdown(
-            label="Dropdown", choices=["One", "Two", "Three"], show_label=False
-        ).style(
-            rounded=False,
-            border=False,
-            container=False,
-        )
-        image = gr.Image(show_label=False).style(
-            rounded=False,
-        )
-        video = gr.Video(show_label=False).style(
-            rounded=False,
-        )
-        audio = gr.Audio(show_label=False).style(
-            rounded=False,
-        )
-        file = gr.File(show_label=False).style(
-            rounded=False,
-        )
-        df = gr.Dataframe(show_label=False).style(
-            rounded=False,
-        )
+            model = gr.Model3D()
 
-        ts = gr.Timeseries(show_label=False).style(
-            rounded=False,
-        )
-        label = gr.Label().style(
-            container=False,
-        )
-        highlight = gr.HighlightedText(
-            "+ hello. - goodbye", show_label=False, color_map={"+": "green", "-": "red"}
-        ).style(rounded=False, container=False)
-        json = gr.JSON().style(container=False)
-        html = gr.HTML(show_label=False).style()
-        gallery = gr.Gallery().style(
-            rounded=False,
-            grid=(3, 3, 1),
-            height="auto",
-            container=False,
-        )
-        chat = gr.Chatbot([("hi", "good bye")], color_map=("pink", "blue")).style(
-            rounded=False,
-        )
+            md = gr.Markdown(show_label=False)
 
-        model = gr.Model3D().style(
-            rounded=False,
-        )
+            highlight = gr.HighlightedText()
 
-        gr.Plot().style()
-        md = gr.Markdown(show_label=False).style()
+            btn = gr.Button("Run").style(
+                full_width=True,
+            )
 
-        highlight = gr.HighlightedText().style(
-            rounded=False,
-        )
-
-        btn = gr.Button("Run").style(
-            rounded=False,
-            full_width=True,
-            border=False,
-        )
-
-        gr.Dataset(components=[txt, num]).style(
-            rounded=False,
-            margin=False,
-            border=False,
-        )
+            gr.Dataset(components=[txt, num])
 
 
 if __name__ == "__main__":
