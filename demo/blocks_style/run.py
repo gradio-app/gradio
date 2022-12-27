e7cca92831feedd7fb030df7bf8904fe1bf11aaf
Freddy Boulton <alfonsoboulton@gmail.com>
2022-12-27 11:51:08 -0500

    Fix blocks_style demo (#2890)
    * Fix blocks_style demo
    
    * Add changelog
    
    * Fix API + notebook
    
    * Remove nested column
    
    * moved highlighttext colormap to style
    
    * Add back container
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- demo/blocks_style/run.py
+++ demo/blocks_style/run.py
@@ -1,103 +1,57 @@
 import gradio as gr
 
 with gr.Blocks(title="Styling Examples") as demo:
-    with gr.Column():
-        txt = gr.Textbox(label="Small Textbox", lines=1).style(
-            rounded=False,
-            border=False,
-            container=False,
-        )
-
-        num = gr.Number(label="Number", show_label=False).style(
-            rounded=False,
-            border=False,
-            container=False,
-        )
-        slider = gr.Slider(label="Slider", show_label=False).style(
-            container=False,
-        )
-        check = gr.Checkbox(label="Checkbox", show_label=False).style(
-            rounded=False,
-            border=False,
-            container=False,
-        )
+    with gr.Column(variant="box"):
+        txt = gr.Textbox(label="Small Textbox", lines=1)
+        num = gr.Number(label="Number", show_label=False)
+        slider = gr.Slider(label="Slider", show_label=False)
+        check = gr.Checkbox(label="Checkbox", show_label=False)
         check_g = gr.CheckboxGroup(
-            label="Checkbox Group", choices=["One", "Two", "Three"], show_label=False
-        ).style(rounded=False, container=False, item_container=False)
+            label="Checkbox Group",
+            choices=["One", "Two", "Three"],
+            show_label=False,
+        )
         radio = gr.Radio(
             label="Radio", choices=["One", "Two", "Three"], show_label=False
         ).style(
             item_container=False,
-            container=False,
         )
         drop = gr.Dropdown(
             label="Dropdown", choices=["One", "Two", "Three"], show_label=False
-        ).style(
-            rounded=False,
-            border=False,
-            container=False,
         )
-        image = gr.Image(show_label=False).style(
-            rounded=False,
-        )
-        video = gr.Video(show_label=False).style(
-            rounded=False,
-        )
-        audio = gr.Audio(show_label=False).style(
-            rounded=False,
-        )
-        file = gr.File(show_label=False).style(
-            rounded=False,
-        )
-        df = gr.Dataframe(show_label=False).style(
-            rounded=False,
-        )
-
-        ts = gr.Timeseries(show_label=False).style(
-            rounded=False,
-        )
+        image = gr.Image(show_label=False)
+        video = gr.Video(show_label=False)
+        audio = gr.Audio(show_label=False)
+        file = gr.File(show_label=False)
+        df = gr.Dataframe(show_label=False)
+        ts = gr.Timeseries(show_label=False)
         label = gr.Label().style(
             container=False,
         )
         highlight = gr.HighlightedText(
-            "+ hello. - goodbye", show_label=False, color_map={"+": "green", "-": "red"}
-        ).style(rounded=False, container=False)
+            "+ hello. - goodbye",
+            show_label=False,
+        ).style(color_map={"+": "green", "-": "red"}, container=False)
         json = gr.JSON().style(container=False)
-        html = gr.HTML(show_label=False).style()
+        html = gr.HTML(show_label=False)
         gallery = gr.Gallery().style(
-            rounded=False,
             grid=(3, 3, 1),
             height="auto",
             container=False,
         )
-        chat = gr.Chatbot("hi", color_map=("pink", "blue")).style(
-            rounded=False,
-        )
+        chat = gr.Chatbot([("hi", "good bye")]).style(color_map=("pink", "blue"))
 
-        model = gr.Model3D().style(
-            rounded=False,
-        )
+        model = gr.Model3D()
 
-        gr.Plot().style()
-        md = gr.Markdown(show_label=False).style()
+        md = gr.Markdown(show_label=False)
 
-        highlight = gr.HighlightedText().style(
-            rounded=False,
-        )
+        highlight = gr.HighlightedText()
 
         btn = gr.Button("Run").style(
-            rounded=False,
             full_width=True,
-            border=False,
         )
 
-        # Not currently public
-        # TODO: Uncomment at next release
-        # gr.Dataset().style(
-        #     rounded=False,
-        #     margin=False,
-        #     border=False,
-        # )
+        gr.Dataset(components=[txt, num])
 
 
 if __name__ == "__main__":
