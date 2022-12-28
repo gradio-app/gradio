8735f9e10c0f0587b3d95efba424ccee5dd7e5f9
Felix Dombek <felics_dombek@hotmail.com>
2022-12-28 06:01:38 +0100

    Fix "audio used before assigned" bug
    audio_from_file did not correctly handle the case where the file cannot be opened. This PR fixes e.g. the crash mentioned in [TheAlly's Riffusion tutorial](https://theally.notion.site/theally/Quick-n-Dirty-Riffusion-txt2audio-Tutorial-18e57df9ef214c3280efc5998bbf774d).

--- gradio/processing_utils.py
+++ gradio/processing_utils.py
@@ -192,6 +192,7 @@ def audio_from_file(filename, crop_min=0, crop_max=100):
             print(
                 "Please install `ffmpeg` in your system to use non-WAV audio file formats."
             )
+        return 0, np.empty(0)
     if crop_min != 0 or crop_max != 100:
         audio_start = len(audio) * crop_min / 100
         audio_end = len(audio) * crop_max / 100
