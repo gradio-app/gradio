de797407a2560d1a10d25a359582157a9363b801
pngwn <hello@pngwn.io>
2022-12-31 01:58:54 +0000

    tweaks to padding/ lineheight

--- ui/packages/theme/src/pollen.config.cjs
+++ ui/packages/theme/src/pollen.config.cjs
@@ -6,6 +6,10 @@ module.exports = defineConfig((pollen) => {
 		selector: ":host, :root",
 		output: "./src/pollen.css",
 		modules: {
+			line: {
+				...pollen.line,
+				sm: "1.4"
+			},
 			font: {
 				...pollen.font,
 				sans: `Source Sans Pro, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`,
