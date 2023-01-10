c061191549568c0d2518afe6ff83d3b3becadd83
Greysuki <tomchang25@gmail.com>
2023-01-10 16:51:18 +0800

    add demo and changelog

---
+++ demo/video_with_caption/run.py
@@ -0,0 +1,10 @@
+import gradio as gr
+
+css = (
+    "footer {display: none !important;} .gradio-container {min-height: 0px !important;}"
+)
+
+with gr.Blocks(css=css) as demo:
+    gr.Video()
+
+demo.launch()
