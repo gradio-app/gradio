f71ae3154abe2c5cf1adb5c0d804a75c73c423e2
Ali Abid <aabid94@gmail.com>
2023-01-11 01:37:03 -0600

    change

--- test/test_routes.py
+++ test/test_routes.py
@@ -203,9 +203,9 @@ class TestRoutes:
         file_response = client.get(f"/file={created_file}")
         assert file_response.is_success
 
-        file_response_with_full_range_header = client.get(f"/file={created_file}", headers={"Range": "bytes=0-"})
-        assert file_response_with_full_range_header.is_success
-        assert file_response.text == file_response_with_full_range_header.text
+        file_response_with_full_range = client.get(f"/file={created_file}", headers={"Range": "bytes=0-"})
+        assert file_response_with_full_range.is_success
+        assert file_response.text == file_response_with_full_range.text
 
         file_response_with_partial_range = client.get(f"/file={created_file}", headers={"Range": "bytes=0-10"})
         assert file_response_with_partial_range.is_success
