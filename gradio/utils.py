77d57903e6802c2aa6571e8e83d9b08b2a4c30b8
freddyaboulton <alfonsoboulton@gmail.com>
2023-01-18 05:32:20 -0500

    Use head instead of get to check url

--- gradio/utils.py
+++ gradio/utils.py
@@ -710,7 +710,7 @@ def append_unique_suffix(name: str, list_of_names: List[str]):
 def validate_url(possible_url: str) -> bool:
     headers = {"User-Agent": "gradio (https://gradio.app/; team@gradio.app)"}
     try:
-        return requests.get(possible_url, headers=headers).ok
+        return requests.head(possible_url, headers=headers).ok
     except Exception:
         return False
 
