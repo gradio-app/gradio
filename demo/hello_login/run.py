2e13342b5d2d71db06d89cc877966b6d8d1b5072
freddyaboulton <alfonsoboulton@gmail.com>
2023-01-19 07:28:52 -0500

    Update test

--- demo/hello_login/run.py
+++ demo/hello_login/run.py
@@ -9,6 +9,6 @@ def greet(name):
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 if __name__ == "__main__":
-    demo.launch(enable_queue=False,
+    demo.launch(enable_queue=True,
         auth=lambda u, p: user_db.get(u) == p,
         auth_message="This is a welcome message")
