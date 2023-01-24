2adc0f0f8724fdf23635c8fd535cbbf2d5cd4161
aliabd <ali.si3luwa@gmail.com>
2023-01-24 13:06:55 -0800

    remove natgeo img in gallery demo

--- demo/gallery_component/run.py
+++ demo/gallery_component/run.py
@@ -10,7 +10,6 @@ with gr.Blocks(css=css) as demo:
         "https://nationalzoo.si.edu/sites/default/files/animals/cheetah-002.jpg",
         "https://images.theconversation.com/files/375893/original/file-20201218-13-a8h8uq.jpg?ixlib=rb-1.1.0&rect=16%2C407%2C5515%2C2924&q=45&auto=format&w=496&fit=clip",
         "https://www.lifegate.com/app/uploads/ghepardo-primo-piano.jpg",
-        "https://i.natgeofe.com/n/60004bcc-cd85-4401-8bfa-6f96551557db/cheetah-extinction-3_3x4.jpg",
         "https://qph.cf2.quoracdn.net/main-qimg-0bbf31c18a22178cb7a8dd53640a3d05-lq"
     ]
     gr.Gallery(value=cheetahs)
