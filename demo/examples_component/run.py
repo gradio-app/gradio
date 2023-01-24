70bbe8d553c0e0a22507fb9cdd4c60fdc91551fb
aliabd <ali.si3luwa@gmail.com>
2023-01-24 13:05:37 -0800

    change example component demo to images

--- demo/examples_component/run.py
+++ demo/examples_component/run.py
@@ -1,29 +1,29 @@
 import gradio as gr
+import os
 
+def flip(i):
+    return i.rotate(180)
 
-def combine(a, b):
-    return a + " " + b
-
-css = "footer {display: none !important;} .gradio-container {min-height: 0px !important;} #col_1 {min-width: 0 !important; max-width: 25%; height: 100%;} #col_2 {min-width: 0 !important; max-width: 25%; height: 100%;} #col_3 {min-width: 0 !important; max-width: 25%; height: 100%;} #col_4 {min-width: 0 !important; max-width: 25%; height: 100%;}"
+css = "footer {display: none !important;} .gradio-container {min-height: 0px !important;}"
 
 with gr.Blocks(css=css) as demo:
     with gr.Row():
-        with gr.Column(elem_id="col_1"):
-            txt = gr.Textbox(label="Input 1", elem_id="input_1_text")
-        with gr.Column(elem_id="col_2"):
-            txt_2 = gr.Textbox(label="Input 2", elem_id="input_2_text")
-        with gr.Column(elem_id="col_3"):
-            btn = gr.Button(value="Combine Inputs", elem_id="combine_button")
-        with gr.Column(elem_id="col_4"):
-            txt_3 = gr.Textbox(value="", label="Output", elem_id="output_text")
-    btn.click(combine, inputs=[txt, txt_2], outputs=[txt_3])
+        with gr.Column():
+            img_i = gr.Image(label="Input Image", type="pil")
+        with gr.Column():
+            img_o = gr.Image(label="Output Image")
+    with gr.Row():
+        btn = gr.Button(value="Flip Image")
+    btn.click(flip, inputs=[img_i], outputs=[img_o])
 
     gr.Examples(
-        [["Hello", "World"], ["Machine", "Learning"], ["Gradio", "Docs"]],
-        [txt, txt_2],
-        txt_3,
-        combine,
-        cache_examples=True,
+        [ 
+            os.path.join(os.path.dirname(__file__), "images/cheetah1.jpg"),
+            os.path.join(os.path.dirname(__file__), "images/lion.jpg"),
+        ],
+        img_i,
+        img_o,
+        flip
     )
     
 demo.launch()
