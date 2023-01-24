2667eb65b1902a5b5512b82eee733c87cafdd454
pngwn <hello@pngwn.io>
2023-01-24 19:51:51 +0000

    fix form focus styles (#3042)
    * fix form focus styles
    
    * changelog

--- ui/packages/form/src/Range.svelte
+++ ui/packages/form/src/Range.svelte
@@ -66,7 +66,7 @@
 		position: relative;
 		outline: none !important;
 		box-shadow: 0 0 0 var(--shadow-spread) var(--ring-color),
-			var(--input-shadow);
+			var(--shadow-inset);
 		border: 1px solid var(--input-border-color-base);
 		border-radius: var(--radius-lg);
 		background: var(--input-background-base);
