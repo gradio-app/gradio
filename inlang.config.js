3d9a148ca415f8e359f3e293528d54cfe125806f
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-25 02:19:46 +0100

    add languages to inlang.config

--- inlang.config.js
+++ inlang.config.js
@@ -1,20 +1,43 @@
 // filename: inlang.config.js
 
 export async function initializeConfig(env) {
-    const plugin = await env.$import(
-      "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
-    );
-  
-    const pluginConfig = {
-      pathPattern: "./ui/packages/app/src/lang/{language}.json",
-    };
-  
-    return {
-      referenceLanguage: "en-us",
-      languages: ["en-us", "zh-cn"],
-      readResources: (args) =>
-        plugin.readResources({ ...args, ...env, pluginConfig }),
-      writeResources: (args) =>
-        plugin.writeResources({ ...args, ...env, pluginConfig }),
-    };
-  }
+  const plugin = await env.$import(
+    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
+  );
+
+  const pluginConfig = {
+    pathPattern: "./ui/packages/app/src/lang/{language}.json",
+  };
+
+  return {
+    referenceLanguage: "en",
+    languages: [
+      "ar",
+      "de",
+      "en",
+      "es",
+      "fa",
+      "fr",
+      "he",
+      "hi",
+      "ja",
+      "ko",
+      "lt",
+      "nl",
+      "pl",
+      "pt-BR",
+      "ru",
+      "ta",
+      "tr",
+      "uk",
+      "ur",
+      "uz",
+      "zh-cn",
+      "zh-tw",
+    ],
+    readResources: (args) =>
+      plugin.readResources({ ...args, ...env, pluginConfig }),
+    writeResources: (args) =>
+      plugin.writeResources({ ...args, ...env, pluginConfig }),
+  };
+}
