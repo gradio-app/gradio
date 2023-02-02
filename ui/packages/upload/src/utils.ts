92cdd40f16b743e1c43f2f58324830d5680f1269
Greysuki <tomchang25@gmail.com>
2023-02-02 17:38:12 +0800

    Final?

--- ui/packages/upload/src/utils.ts
+++ ui/packages/upload/src/utils.ts
@@ -10,12 +10,32 @@ export function normalise_file(
 			name: "file_data",
 			data: file
 		};
-	} else if (file.is_file) {
-		file.data = root + "file=" + file.name;
 	} else if (Array.isArray(file)) {
 		for (const x of file) {
 			normalise_file(x, root);
 		}
+	} else if (file.is_file) {
+		file.data = root + "file=" + file.name;
+	}
+	return file;
+}
+
+export function normalise_files(
+	file: string | FileData | Array<FileData> | null,
+	root: string
+): FileData | Array<FileData> | null {
+	if (file == null) return null;
+	if (typeof file === "string") {
+		return {
+			name: "file_data",
+			data: file
+		};
+	} else if (Array.isArray(file)) {
+		for (const x of file) {
+			normalise_file(x, root);
+		}
+	} else if (file.is_file) {
+		file.data = root + "file=" + file.name;
 	}
 	return file;
 }
