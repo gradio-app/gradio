e29cfcd19f8666f26a8bf8f0046e412bf977a1e1
pngwn <hello@pngwn.io>
2023-02-03 09:20:06 +0000

    cleanup

--- demo/video_component/run.py
+++ demo/video_component/run.py
@@ -1,18 +1,10 @@
 import gradio as gr
 import os
 
-# css = (
-#     "footer {display: none !important;} .gradio-container {min-height: 0px !important;}"
-# )
 
 a = os.path.join(os.path.dirname(__file__), "files/world.mp4")  # Video
 b = os.path.join(os.path.dirname(__file__), "files/a.mp4")  # Video
 c = os.path.join(os.path.dirname(__file__), "files/b.mp4")  # Video
-# with gr.Blocks(css=css) as demo:
-#     gr.Video(video)
-#     gr.Video()
-
-# demo.launch()
 
 
 demo = gr.Interface(
