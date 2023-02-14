3154466de33fa0db388d8c94b5ab8fb478e67325
Ali Abid <aabid94@gmail.com>
2023-02-14 11:31:39 -0800

    changes

--- ui/packages/app/src/components/UploadButton/UploadButton.svelte
+++ ui/packages/app/src/components/UploadButton/UploadButton.svelte
@@ -3,21 +3,30 @@
 	import type { Styles } from "@gradio/utils";
 	import type { FileData } from "@gradio/upload";
 	import { UploadButton } from "@gradio/upload-button";
+	import { upload_files } from "../../api";
 	import { _ } from "svelte-i18n";
 
 	export let style: Styles = {};
 	export let elem_id: string = "";
 	export let visible: boolean = true;
 	export let label: string;
-	export let value: null | FileData | Array<FileData>;
+	export let value: null | FileData;
 	export let file_count: string;
 	export let file_types: Array<string> = ["file"];
+	export let root: string;
 
 	async function handle_upload({ detail }: CustomEvent<FileData>) {
 		value = detail;
 		await tick();
-		dispatch("change", value);
-		dispatch("upload", detail);
+		upload_files(root, [detail.blob!]).then((response) => {
+			if (!response.error) {
+				detail.orig_name = detail.name;
+				detail.name = response.files![0];
+				detail.is_file = true;
+			}
+			dispatch("change", value);
+			dispatch("upload", detail);
+		});
 	}
 
 	const dispatch = createEventDispatcher<{
