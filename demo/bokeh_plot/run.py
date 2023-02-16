155f9188f50219660a790352b6c3dc73cad2ef8a
freddyaboulton <alfonsoboulton@gmail.com>
2023-02-16 16:46:13 -0500

    remove lorenz

--- demo/bokeh_plot/run.py
+++ demo/bokeh_plot/run.py
@@ -2,58 +2,11 @@ import gradio as gr
 import xyzservices.providers as xyz
 from bokeh.plotting import figure
 from bokeh.tile_providers import get_provider
-import numpy as np
-from scipy.integrate import odeint
 from bokeh.models import ColumnDataSource, Whisker
 from bokeh.plotting import figure
 from bokeh.sampledata.autompg2 import autompg2 as df
 from bokeh.sampledata.penguins import data
 from bokeh.transform import factor_cmap, jitter, factor_mark
-
-
-def get_lorenz_curve():
-    sigma = 10
-    rho = 28
-    beta = 8.0 / 3
-    theta = 3 * np.pi / 4
-
-    def lorenz(xyz, t):
-        x, y, z = xyz
-        x_dot = sigma * (y - x)
-        y_dot = x * rho - x * z - y
-        z_dot = x * y - beta * z
-        return [x_dot, y_dot, z_dot]
-
-    initial = (-10, -7, 35)
-    t = np.arange(0, 100, 0.006)
-
-    solution = odeint(lorenz, initial, t)
-
-    x = solution[:, 0]
-    y = solution[:, 1]
-    z = solution[:, 2]
-    xprime = np.cos(theta) * x - np.sin(theta) * y
-
-    colors = [
-        "#C6DBEF",
-        "#9ECAE1",
-        "#6BAED6",
-        "#4292C6",
-        "#2171B5",
-        "#08519C",
-        "#08306B",
-    ]
-
-    p = figure(title="Lorenz attractor example", background_fill_color="#fafafa")
-
-    p.multi_line(
-        np.array_split(xprime, 7),
-        np.array_split(z, 7),
-        line_color=colors,
-        line_alpha=0.8,
-        line_width=1.5,
-    )
-    return p
 
 
 def get_plot(plot_type):
@@ -67,8 +20,6 @@ def get_plot(plot_type):
         )
         plot.add_tile(tile_provider)
         return plot
-    elif plot_type == "lorenz":
-        return get_lorenz_curve()
     elif plot_type == "whisker":
         classes = list(sorted(df["class"].unique()))
 
@@ -133,7 +84,7 @@ def get_plot(plot_type):
 
 with gr.Blocks() as demo:
     with gr.Row():
-        plot_type = gr.Radio(value="scatter", choices=["scatter", "lorenz", "whisker", "map"])
+        plot_type = gr.Radio(value="scatter", choices=["scatter", "whisker", "map"])
         plot = gr.Plot()
     plot_type.change(get_plot, inputs=[plot_type], outputs=[plot])
 
