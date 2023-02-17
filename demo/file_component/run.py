df1b5ff221e751fded849ddef97b2cae9b6034fa
pngwn <hello@pngwn.io>
2023-02-17 12:19:40 +0000

    formatting

--- demo/file_component/run.py
+++ demo/file_component/run.py
@@ -1,8 +1,6 @@
 import gradio as gr
 
-css = (
-    "footer {display: none !important;} .gradio-container {min-height: 0px !important;}"
-)
+css = "footer {display: none !important;} .gradio-container {min-height: 0px !important;}"
 
 with gr.Blocks(css=css) as demo:
     gr.File()
