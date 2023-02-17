06f7531c52db5ae0c174d6422a00c25be0c3f7ea
pngwn <hello@pngwn.io>
2023-02-17 12:18:32 +0000

    revert demo change

--- demo/file_component/run.py
+++ demo/file_component/run.py
@@ -1,7 +1,10 @@
 import gradio as gr
 
+css = (
+    "footer {display: none !important;} .gradio-container {min-height: 0px !important;}"
+)
 
-with gr.Blocks() as demo:
+with gr.Blocks(css=css) as demo:
     gr.File()
 
 demo.launch()
