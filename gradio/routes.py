5df113a4d65910c2b6fbc15989d7971fae3c17e3
Freddy Boulton <alfonsoboulton@gmail.com>
2023-02-21 16:44:18 -0500

    Add timeouts to queue messages (#3196)
    * Fix + test
    
    * Remove print statements + fix import for 3.7
    
    * CHANGELOG
    
    * Remove more print statements
    
    * Add 60 second timeout for uploading data
    
    * Fix test
    
    ---------
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- gradio/routes.py
+++ gradio/routes.py
@@ -12,6 +12,7 @@ import posixpath
 import secrets
 import tempfile
 import traceback
+from asyncio import TimeoutError as AsyncTimeOutError
 from collections import defaultdict
 from copy import deepcopy
 from typing import Any, Dict, List, Optional, Type
@@ -479,8 +480,20 @@ class App(FastAPI):
             await websocket.accept()
             # In order to cancel jobs, we need the session_hash and fn_index
             # to create a unique id for each job
-            await websocket.send_json({"msg": "send_hash"})
-            session_info = await websocket.receive_json()
+            try:
+                await asyncio.wait_for(
+                    websocket.send_json({"msg": "send_hash"}), timeout=1
+                )
+            except AsyncTimeOutError:
+                return
+
+            try:
+                session_info = await asyncio.wait_for(
+                    websocket.receive_json(), timeout=1
+                )
+            except AsyncTimeOutError:
+                return
+
             event = Event(
                 websocket, session_info["session_hash"], session_info["fn_index"]
             )
