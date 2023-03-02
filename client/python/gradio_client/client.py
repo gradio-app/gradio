125b4c2a5d8524b48876e10bcd99580d636fa351
Abubakar Abid <abubakar@huggingface.co>
2023-03-02 02:31:40 -0800

    serialization

--- client/python/gradio_client/client.py
+++ client/python/gradio_client/client.py
@@ -3,7 +3,11 @@ from __future__ import annotations
 
 import concurrent.futures
 from concurrent.futures import Future
-from typing import Callable
+import json
+import re
+from typing import Callable, Dict
+
+import requests
 
 from gradio_client import utils
 
@@ -14,7 +18,7 @@ class Client:
         space: str | None = None, 
         src: str | None = None, 
         access_token: str | None = None, 
-        max_workers: int = 5
+        max_workers: int = 40
     ):
         if space is None and src is None:
             raise ValueError("Either `space` or `src` must be provided")
@@ -22,22 +26,67 @@ class Client:
             raise ValueError("Only one of `space` or `src` must be provided")
         
         self.space = space
-        self.src = src or utils.space_name_to_src(space)
+        self.src = src or self._space_name_to_src()
+        if self.src is None:
+            raise ValueError(f"Could not find Space: {space}. If it is a private Space, please provide an access_token.")
+        self.access_token = access_token
+        self.headers = {"Authorization": "Bearer {access_token}"} if access_token else {}
         self.api_url = utils.API_URL.format(self.src)        
         self.ws_url = utils.WS_URL.format(self.src).replace("https", "wss")
+        self.config = self._get_config()
         
         # Create a pool of threads to handle the requests
         self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
 
-    def get_predict_fn(self, api_name: str | None) -> Callable:
-        # Should make the connection with the remote API and return the result
+    def predict(self, args, api_name: str | None = None, fn_index: int = 0) -> Future:
+        predict_fn = self._get_predict_fn(api_name, fn_index)
+        future = self.executor.submit(predict_fn, *args)
+        return future
+
+    #################
+    # Helper methods
+    #################
+    
+    def _get_predict_fn(self, api_name: str | None, fn_index: int) -> Callable:
+        if api_name is not None:
+            inferred_fn_index = next(
+                (
+                    i
+                    for i, d in enumerate(self.config["dependencies"])
+                    if d.get("api_name") == api_name
+                ),
+                None,
+            )
+            if inferred_fn_index is None:
+                raise ValueError(f"Cannot find a function with api_name: {api_name}")
+            fn_index = inferred_fn_index        
+        
         return lambda *args: None
 
     def __del__(self):
         if hasattr(self, "executor"):
             self.executor.shutdown(wait=True)
+   
+    def _space_name_to_src(self) -> str | None:
+        return (
+            requests.get(
+                f"https://huggingface.co/api/spaces/{self.space}/host", headers=self.headers
+            )
+            .json()
+            .get("host")
+        )
 
-    def predict(self, args, api_name: str | None = None) -> Future:
-        predict_fn = self.get_predict_fn(api_name)
-        future = self.executor.submit(predict_fn, *args)
-        return future
+    def _get_config(self) -> Dict:
+        assert self.src is not None
+        r = requests.get(self.src, headers=self.headers)        
+        # some basic regex to extract the config
+        result = re.search(r"window.gradio_config = (.*?);[\s]*</script>", r.text)
+        try:
+            config = json.loads(result.group(1))  # type: ignore
+        except AttributeError:
+            raise ValueError(f"Could not get Gradio config from: {self.src}")
+        if "allow_flagging" in config:
+            raise ValueError(f"Gradio 2.x is not supported by this client. Please upgrade this app to Gradio 3.x.")
+        return config
+        
+
--- client/python/gradio_client/utils.py
+++ client/python/gradio_client/utils.py
@@ -4,12 +4,4 @@ import requests
 API_URL = "{}/api/predict/"
 WS_URL = "{}/queue/join"
 
-def space_name_to_src(space_name, access_token=None):
-    headers = {} if access_token is None else {"Authorization": "Bearer {api_key}"}
-    return (
-        requests.get(
-            f"https://huggingface.co/api/spaces/{space_name}/host", headers=headers
-        )
-        .json()
-        .get("host")
-    )
+
