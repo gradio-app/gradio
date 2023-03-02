130ad3c5ad484f2948b2084e9dca244a15c0245e
Jialei <jialeicui@126.com>
2023-03-02 15:11:45 +0800

    Fix error when using backen_fn and custom js at the same time

--- ui/packages/app/src/Blocks.svelte
+++ ui/packages/app/src/Blocks.svelte
@@ -60,11 +60,12 @@
 	const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
 	dependencies.forEach((d) => {
 		if (d.js) {
+			const wrap = d.backend_fn ? d.inputs.length === 1 : d.outputs.length === 1;
 			try {
 				d.frontend_fn = new AsyncFunction(
 					"__fn_args",
 					`let result = await (${d.js})(...__fn_args);
-					return ${d.outputs.length} === 1 ? [result] : result;`
+					return ${wrap} ? [result] : result;`
 				);
 			} catch (e) {
 				console.error("Could not parse custom js method.");
