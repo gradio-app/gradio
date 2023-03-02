0e0b459d0ff64dd6d7a6e575b638f921acb25fb9
Abubakar Abid <abubakar@huggingface.co>
2023-03-01 17:10:57 -0800

    utils

--- client/python/client.py
+++ client/python/client.py
@@ -3,9 +3,18 @@
 import concurrent.futures
 from concurrent.futures import Future
 
+import utils
 
 class Client:
-    def __init__(self, space, src, max_workers=5):
+    def __init__(self, space=None, src=None, max_workers=5):
+        if space is None and src is None:
+            raise ValueError('Either `space` or `src` must be provided')
+        elif space and src:
+            raise ValueError('Only one of `space` or `src` must be provided')
+        else:
+            self.space = space
+            self.src = src or utils.space_name_to_src(space)
+        
         # Create persistent websocket connection
         self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
     
@@ -14,7 +23,8 @@ class Client:
         pass
     
     def __del__(self):
-        self.executor.shutdown(wait=True)
+        if hasattr(self, 'executor'):
+            self.executor.shutdown(wait=True)
     
     def predict(self, args, api_name=None) -> Future:
         future = self.executor.submit(self.predict_api, *args)
---
+++ client/python/utils.py
@@ -0,0 +1,10 @@
+import requests
+
+def space_name_to_src(space_name, access_token=None):
+    headers = {} if access_token is None else {"Authorization": "Bearer {api_key}"}
+
+    return requests.get(
+            f"https://huggingface.co/api/spaces/{space_name}/host", headers=headers
+        ).json().get("host")
+    
+    
