b37e8838a3458f284ba40a8afdf59738163a9616
Ali Abid <aabid94@gmail.com>
2023-03-02 10:56:01 -0800

    changes

--- demo/hello_world/run.py
+++ demo/hello_world/run.py
@@ -1,13 +1,9 @@
 import gradio as gr
 
-
 def greet(name):
     return "Hello " + name + "!"
 
-
-theme = gr.themes.Default(primary_hue=gr.themes.utils.purple).set(loader_color="orange")
-
-demo = gr.Interface(fn=greet, inputs="text", outputs="text", theme=theme)
-
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+    
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch()   
