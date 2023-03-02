a7da8e0f992a61bdd5c4086ec1bc66953bc3c21a
Abubakar Abid <abubakar@huggingface.co>
2023-03-01 17:16:22 -0800

    lint

--- client/python/utils.py
+++ client/python/utils.py
@@ -3,7 +3,6 @@ import requests
 
 def space_name_to_src(space_name, access_token=None):
     headers = {} if access_token is None else {"Authorization": "Bearer {api_key}"}
-
     return (
         requests.get(
             f"https://huggingface.co/api/spaces/{space_name}/host", headers=headers
