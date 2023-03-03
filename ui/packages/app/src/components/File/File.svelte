99945c951b123633ab92e222699c9faa88b0f55b
Freddy Boulton <alfonsoboulton@gmail.com>
2023-03-03 15:14:56 -0500

    Fix File updating (#3375)
    * Fix
    
    * Remove line
    
    * Add unit test
    
    * CHANGELOG
    
    * Remove comment

--- ui/packages/app/src/components/File/File.svelte
+++ ui/packages/app/src/components/File/File.svelte
@@ -43,6 +43,12 @@
 			if (_value === null) {
 				dispatch("change");
 				pending_upload = false;
+			} else if (
+				!(Array.isArray(_value) ? _value : [_value]).every(
+					(file_data) => file_data.blob
+				)
+			) {
+				pending_upload = false;
 			} else if (mode === "dynamic") {
 				let files = (Array.isArray(_value) ? _value : [_value]).map(
 					(file_data) => file_data.blob!
