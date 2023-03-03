4208135289b18f3c2ac0db7af6a8a7aac5f6760d
freddyaboulton <alfonsoboulton@gmail.com>
2023-03-03 14:52:05 -0500

    Remove comment

--- ui/packages/app/src/components/File/File.test.ts
+++ ui/packages/app/src/components/File/File.test.ts
@@ -61,34 +61,4 @@ describe("File", () => {
 
 		expect(spy).not.toHaveBeenCalled();
 	});
-
-	// test("renders additional message as they are passed", async () => {
-	// 	const { component, getAllByTestId } = render(Chatbot, {
-	// 		loading_status,
-	// 		label: "hello",
-	// 		value: [["user message one", "bot message one"]]
-	// 	});
-
-	// 	const bot = getAllByTestId("user");
-	// 	const user = getAllByTestId("bot");
-
-	// 	assert.equal(user.length, 1);
-	// 	assert.equal(bot.length, 1);
-
-	// 	await component.$set({
-	// 		value: [
-	// 			["user message one", "bot message one"],
-	// 			["user message two", "bot message two"]
-	// 		]
-	// 	});
-
-	// 	const user_2 = getAllByTestId("user");
-	// 	const bot_2 = getAllByTestId("bot");
-
-	// 	assert.equal(user_2.length, 2);
-	// 	assert.equal(bot_2.length, 2);
-
-	// 	assert.equal(get_text(user_2[1]), "user message two");
-	// 	assert.equal(get_text(bot_2[1]), "bot message two");
-	// });
 });
