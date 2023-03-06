dbfccfde94341e09d67b9301a4cfa2cf8e8d4f67
Abubakar Abid <abubakar@huggingface.co>
2023-03-06 14:31:19 -0800

    lint

--- gradio/blocks.py
+++ gradio/blocks.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import copy
-import getpass
 import inspect
 import json
 import os
--- gradio/flagging.py
+++ gradio/flagging.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import csv
 import datetime
-import io
 import json
 import os
 import time
