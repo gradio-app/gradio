f612ddf31a5334d813f6171a7890c47b08b50685
Abubakar Abid <abubakar@huggingface.co>
2023-03-09 12:02:24 -0800

    fix lint

--- gradio/blocks.py
+++ gradio/blocks.py
@@ -1320,7 +1320,7 @@ class Blocks(BlockContext):
             import gradio as gr
             def reverse(text):
                 return text[::-1]
-            with gr.Blocks() as demo: 
+            with gr.Blocks() as demo:
                 button = gr.Button(value="Reverse")
                 button.click(reverse, gr.Textbox(), gr.Textbox())
             demo.launch(share=True, auth=("username", "password"))
