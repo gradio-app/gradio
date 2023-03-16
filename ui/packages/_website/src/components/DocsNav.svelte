78b5d7cc9fc3edc5f41611d9f29db88afde43186
aliabd <ali.si3luwa@gmail.com>
2023-03-16 13:35:42 -0700

    menu bar on docs mobile

--- ui/packages/_website/src/components/DocsNav.svelte
+++ ui/packages/_website/src/components/DocsNav.svelte
@@ -1,12 +1,34 @@
 <script>
+    import {clickOutside} from './clickOutside.js';
+
     export let components;
     export let helpers;
     export let routes;
+    
 
     export let current_nav_link = "";
+    
+    let show_nav=false;
 </script>
 
-<div class="navigation mobile-nav overflow-y-auto hidden fixed backdrop-blur-lg z-50 bg-gray-200/50 pr-6 pl-4 py-4 -ml-4 h-full inset-0 w-5/6 lg:inset-auto lg:h-auto lg:ml-0 lg:z-0 lg:backdrop-blur-none lg:navigation lg:p-0 lg:pb-4 lg:h-screen lg:leading-relaxed lg:sticky lg:top-0 lg:text-md lg:block rounded-t-xl lg:bg-gradient-to-r lg:from-white lg:to-gray-50 lg:overflow-x-clip lg:w-2/12 lg:min-w-2/12" id="mobile-nav">
+<section class="top-0 fixed -ml-4 flex items-center p-4 rounded-br-lg backdrop-blur-lg z-50 bg-gray-200/50 lg:hidden" id="menu-bar">
+    <button 
+    on:click={() => show_nav = !show_nav}
+    type="button" class="text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300">
+        <svg width="24" height="24"><path d="M5 6h14M5 12h14M5 18h14" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path></svg>
+    </button>
+</section>
+
+<div 
+use:clickOutside on:click_outside={() => show_nav = false}
+class:hidden={!show_nav}
+class="navigation mobile-nav overflow-y-auto fixed backdrop-blur-lg z-50 bg-gray-200/50 pr-6 pl-4 py-4 -ml-4 h-full inset-0 w-5/6 lg:inset-auto lg:h-auto lg:ml-0 lg:z-0 lg:backdrop-blur-none lg:navigation lg:p-0 lg:pb-4 lg:h-screen lg:leading-relaxed lg:sticky lg:top-0 lg:text-md lg:block rounded-t-xl lg:bg-gradient-to-r lg:from-white lg:to-gray-50 lg:overflow-x-clip lg:w-2/12 lg:min-w-2/12" id="mobile-nav">
+
+<button 
+on:click={() => show_nav = !show_nav}
+type="button" class="absolute z-10 top-4 right-4 w-2/12 h-4 flex items-center justify-center text-grey-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300 p-4 lg:hidden" tabindex="0">
+    <svg viewBox="0 0 10 10" class="overflow-visible" style="width: 10px"><path d="M0 0L10 10M10 0L0 10" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path></svg>
+  </button>
 
     <a class="link px-4 my-2 block" href="/docs/">Building Demos</a>
     <a class:current-nav-link={current_nav_link == 'interface'} 
---
+++ ui/packages/_website/src/components/clickOutside.js
@@ -0,0 +1,19 @@
+/** Dispatch event on click outside of node */
+export function clickOutside(node) {
+  
+    const handleClick = event => {
+      if (node && !node.contains(event.target) && !event.defaultPrevented) {
+        node.dispatchEvent(
+          new CustomEvent('click_outside', node)
+        )
+      }
+    }
+  
+      document.addEventListener('click', handleClick, true);
+    
+    return {
+      destroy() {
+        document.removeEventListener('click', handleClick, true);
+      }
+      }
+  }
