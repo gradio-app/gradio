59f873d2d790e36fcde824a0ed46cdf2f2d31273
space-nuko <24979496+space-nuko@users.noreply.github.com>
2023-03-16 17:05:59 -0400

    Update test requirements and script

--- scripts/create_test_requirements.sh
+++ scripts/create_test_requirements.sh
@@ -6,7 +6,8 @@ source scripts/helpers.sh
 pip_required
 
 echo "Creating test requirements...
-It's recommended to run this script from a Unix-like system in Python 3.7 (or higher) environment."
+It's recommended to run this script in a Python 3.7 (or higher) environment."
 cd test
 pip install --upgrade pip-tools
+rm requirements.txt
 pip-compile --output-file requirements.txt
