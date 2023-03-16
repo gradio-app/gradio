e513d228225cfa5bbdb46eeaf6a308abf3ad0645
Abubakar Abid <abubakar@huggingface.co>
2023-03-16 05:08:21 -0700

    added tests

--- client/python/test/test_client.py
+++ client/python/test/test_client.py
@@ -1,2 +1,20 @@
+import json
+
 import pytest
 
+from gradio_client import Client
+
+
+class TestPredictionsFromSpaces:
+    @pytest.mark.flaky
+    def test_numerical_to_label_space(self):
+        client = Client(space="abidlabs/titanic-survival")
+        output = client.predict("male", 77, 10).result()
+        assert json.load(open(output))["label"] == "Perishes"
+
+    @pytest.mark.flaky
+    def test_private_space(self):
+        access_token = "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"  # Intentionally revealing this key for testing purposes
+        client = Client(space="gradio-tests/not-actually-private-space", access_token=access_token)
+        output = client.predict("abc").result()
+        assert output == "abc"
