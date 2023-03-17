95eecddfaf7fdc93d0c6c9db0d8c1990bb4a09db
Ali Abid <aabid94@gmail.com>
2023-03-17 15:12:14 -0700

    undo merge break

--- gradio/blocks.py
+++ gradio/blocks.py
@@ -71,7 +71,6 @@ class Block:
         self._skip_init_processing = _skip_init_processing
         self._style = {}
         self.parent: BlockContext | None = None
-        self.root = ""
 
         if render:
             self.render()
@@ -497,8 +496,7 @@ class Blocks(BlockContext):
             warnings.warn("Theme should be a class loaded from gradio.themes")
             theme = DefaultTheme()
         self.theme = theme
-        self.theme_css = theme._get_theme_css()
-        self.stylesheets = theme._stylesheets
+        self.theme_css = self.theme._get_theme_css()
         self.encrypt = False
         self.share = False
         self.enable_queue = None
@@ -1104,7 +1102,6 @@ class Blocks(BlockContext):
             "show_error": getattr(self, "show_error", False),
             "show_api": self.show_api,
             "is_colab": utils.colab_check(),
-            "stylesheets": self.stylesheets,
         }
 
         def getLayout(block):
