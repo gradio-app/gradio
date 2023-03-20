1fb9a497f3c3b0c7b6febb758a1f4bd61bf448ab
freddyaboulton <alfonsoboulton@gmail.com>
2023-03-20 16:59:56 -0400

    Linting

--- demo/barplot_component/run.py
+++ demo/barplot_component/run.py
@@ -1,15 +1,21 @@
 import gradio as gr
 import pandas as pd
 
-simple = pd.DataFrame({
-    'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
-    'b': [28, 55, 43, 91, 81, 53, 19, 87, 52]
-})
+simple = pd.DataFrame(
+    {
+        "a": ["A", "B", "C", "D", "E", "F", "G", "H", "I"],
+        "b": [28, 55, 43, 91, 81, 53, 19, 87, 52],
+    }
+)
 
-css = "footer {display: none !important;} .gradio-container {min-height: 0px !important;}"
+css = (
+    "footer {display: none !important;} .gradio-container {min-height: 0px !important;}"
+)
 
 with gr.Blocks(css=css) as demo:
-    gr.BarPlot(value=simple, x="a", y="b", title="Simple Bar Plot")
+    gr.BarPlot(value=simple, x="a", y="b", title="Simple Bar Plot").style(
+        container=False,
+    )
 
 if __name__ == "__main__":
     demo.launch()
