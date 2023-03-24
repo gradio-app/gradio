85e836401e653ef0370908d4f49b44b5c33bcb1f
Abubakar Abid <abubakar@huggingface.co>
2023-03-24 15:22:47 -0700

    doc

--- gradio/components.py
+++ gradio/components.py
@@ -3113,7 +3113,7 @@ class State(IOComponent, SimpleSerializable):
     ):
         """
         Parameters:
-            value: the initial value of the state. If callable, the function will be called whenever the app loads to set the initial value of the component.
+            value: the initial value (of abitrary type) of the state. The provided argument is deepcopied. If a callable is provided, the function will be called whenever the app loads to set the initial value of the state.
         """
         self.stateful = True
         IOComponent.__init__(self, value=deepcopy(value), **kwargs)
