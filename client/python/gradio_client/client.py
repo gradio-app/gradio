99c78639562bd66cef7a5f8b22777da065efb9b9
Abubakar Abid <abubakar@huggingface.co>
2023-04-01 14:30:36 -0500

    api endpoint changes

--- client/python/gradio_client/client.py
+++ client/python/gradio_client/client.py
@@ -71,25 +71,24 @@ class Client:
         self,
         *args,
         api_name: str | None = None,
-        fn_index: int = 0,
+        fn_index: int | None = None,
         result_callbacks: Callable | List[Callable] | None = None,
     ) -> Future:
         """
         Parameters:
             *args: The arguments to pass to the remote API. The order of the arguments must match the order of the inputs in the Gradio app.
-            api_name: The name of the API endpoint to call. If not provided, the first API will be called. Takes precedence over fn_index.
-            fn_index: The index of the API endpoint to call. If not provided, the first API will be called.
+            api_name: The name of the API endpoint to call starting with a leading slash, e.g. "/predict". Does not need to be provided if the Gradio app has only one API endpoint.
+            fn_index: The index of the API endpoint to call, e.g. 0. Does not need to be provided if the Gradio app has only one API endpoint. Both api_name and fn_index can be provided, but if they conflict, api_name will take precedence.
             result_callbacks: A callback function, or list of callback functions, to be called when the result is ready. If a list of functions is provided, they will be called in order. The return values from the remote API are provided as separate parameters into the callback. If None, no callback will be called.
         Returns:
             A Job object that can be used to retrieve the status and result of the remote API call.
         """
-        if api_name:
-            fn_index = self._infer_fn_index(api_name)
+        inferred_fn_index = self._infer_fn_index(api_name, fn_index)
 
         helper = None
-        if self.endpoints[fn_index].use_ws:
+        if self.endpoints[inferred_fn_index].use_ws:
             helper = Communicator(Lock(), JobStatus())
-        end_to_end_fn = self.endpoints[fn_index].make_end_to_end_fn(helper)
+        end_to_end_fn = self.endpoints[inferred_fn_index].make_end_to_end_fn(helper)
         future = self.executor.submit(end_to_end_fn, *args)
 
         job = Job(future, communicator=helper)
@@ -235,11 +234,27 @@ class Client:
         except Exception:
             pass
 
-    def _infer_fn_index(self, api_name: str) -> int:
-        for i, d in enumerate(self.config["dependencies"]):
-            if d.get("api_name") == api_name:
-                return i
-        raise ValueError(f"Cannot find a function with api_name: {api_name}")
+    def _infer_fn_index(self, api_name: str | None, fn_index: int | None) -> int:
+        inferred_fn_index = None
+        if api_name is not None:
+            for i, d in enumerate(self.config["dependencies"]):
+                if "/" + d.get("api_name", "") == api_name:
+                    inferred_fn_index = i
+                    break
+            else:
+                error_message = f"Cannot find a function with api_name: {api_name}."
+                if not api_name.startswith("/"):
+                    error_message += " Did you mean to use a leading slash?"
+                raise ValueError(error_message)
+        elif fn_index is not None:
+            inferred_fn_index = fn_index
+        else:
+            valid_endpoints = [e for e in self.endpoints if e.is_valid]
+            if len(valid_endpoints) == 1:
+                inferred_fn_index = valid_endpoints[0].fn_index
+            else:
+                raise ValueError("This Gradio app has multiple endpoints. Please specify an `api_name` or `fn_index`")
+        return inferred_fn_index
 
     def __del__(self):
         if hasattr(self, "executor"):
@@ -273,6 +288,8 @@ class Endpoint:
         self.fn_index = fn_index
         self.dependency = dependency
         self.api_name: str | None = dependency.get("api_name")
+        if self.api_name:
+            self.api_name = "/" + self.api_name
         self.headers = client.headers
         self.config = client.config
         self.use_ws = self._use_websocket(self.dependency)
