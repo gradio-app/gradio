070b304e0981699e83327a04f3acc41fb29abf3c
Freddy Boulton <alfonsoboulton@gmail.com>
2023-04-04 11:02:20 -0700

    CI tweaks (#3752)

--- client/python/scripts/format.sh
+++ client/python/scripts/format.sh
@@ -5,3 +5,6 @@ cd "$(dirname ${0})/.."
 echo "Formatting the client library.. Our style follows the Black code style."
 ruff --fix test gradio_client
 black test gradio_client
+
+echo "Type checking the client library with pyright"
+pyright gradio_client/*.py
