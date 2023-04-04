d1ac85c04be9113ee6bbf73d6fa41c326662a850
freddyaboulton <alfonsoboulton@gmail.com>
2023-04-04 13:26:05 -0400

    CI tweaks

--- client/python/scripts/ci.sh
+++ client/python/scripts/ci.sh
@@ -6,6 +6,7 @@ cd "$(dirname ${0})/.."
 echo "Linting..."
 ruff test gradio_client
 black --check test gradio_client
+pyright gradio_client/*.py
 
 echo "Testing..."
 python -m pip install -e ../../.  # Install gradio from local source (as the latest version may not yet be published to PyPI)
