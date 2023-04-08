f3b58a1e521d358f2b687f1fbacbe002fb0dd258
Ali Abid <aabid94@gmail.com>
2023-04-08 13:58:44 -0700

    changes

--- demo/image_selections/run.py
+++ demo/image_selections/run.py
@@ -2,23 +2,44 @@ import gradio as gr
 import numpy as np
 
 with gr.Blocks() as demo:
+    tolerance = gr.Slider(label="Tolerance", info="How different colors can be in a segment.", minimum=0, maximum=256*3, value=50)
     with gr.Row():
-        non_selectable_input_image = gr.Image(label="No select input")
-        non_selectable_output_image = gr.Image(label="No select output")
-    with gr.Row():
-        selectable_input_image = gr.Image(label="Select input")
-        selectable_output_image = gr.Image(label="Select out", interactive=False).style(height=200)
-    with gr.Row():
-        img_input_coords = gr.Textbox(label="Input coords")
-        img_output_coords = gr.Textbox(label="Output coords")
+        input_img = gr.Image(label="Input")
+        output_img = gr.Image(label="Selected Segment")
 
-    non_selectable_input_image.change(lambda x:x, non_selectable_input_image, non_selectable_output_image)
-    selectable_input_image.change(lambda x:x, selectable_input_image, selectable_output_image)
+    def get_select_coords(img, tolerance, evt: gr.SelectData):
+        visited_pixels = set()
+        pixels_in_queue = set()
+        pixels_in_segment = set()
+        start_pixel = img[evt.index[1], evt.index[0]]
+        pixels_in_queue.add((evt.index[1], evt.index[0]))
+        while len(pixels_in_queue) > 0:
+            pixel = pixels_in_queue.pop()
+            visited_pixels.add(pixel)
+            neighbors = []
+            if pixel[0] > 0:
+                neighbors.append((pixel[0] - 1, pixel[1]))
+            if pixel[0] < img.shape[0] - 1:
+                neighbors.append((pixel[0] + 1, pixel[1]))
+            if pixel[1] > 0:
+                neighbors.append((pixel[0], pixel[1] - 1))
+            if pixel[1] < img.shape[1] - 1:
+                neighbors.append((pixel[0], pixel[1] + 1))
+            for neighbor in neighbors:
+                if neighbor in visited_pixels:
+                    continue
+                neighbor_pixel = img[neighbor[0], neighbor[1]]
+                if np.abs(neighbor_pixel - start_pixel).sum() < tolerance:
+                    pixels_in_queue.add(neighbor)
+                    pixels_in_segment.add(neighbor)
 
-    def get_select_coords(evt: gr.SelectData):
-        return evt.index
-    selectable_input_image.select(get_select_coords, None, img_input_coords)
-    selectable_output_image.select(get_select_coords, None, img_output_coords)
+        out = img.copy() * 0.2
+        out = out.astype(np.uint8)
+        for pixel in pixels_in_segment:
+            out[pixel[0], pixel[1]] = img[pixel[0], pixel[1]]
+        return out
+    
+    input_img.select(get_select_coords, [input_img, tolerance], output_img)
 
 if __name__ == "__main__":
     demo.launch()
