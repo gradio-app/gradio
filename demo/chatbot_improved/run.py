6cdd06c972be8357b507808dbd14d5f0d4fa8239
Abubakar Abid <abubakar@huggingface.co>
2023-04-18 07:42:39 -0700

    added guides

--- demo/chatbot_improved/run.py
+++ demo/chatbot_improved/run.py
@@ -11,10 +11,12 @@ with gr.Blocks() as demo:
         return "", history + [[user_message, None]]
 
     def bot(history):
-        bot_message = random.choice(["Yes", "No"])
-        history[-1][1] = bot_message
-        time.sleep(1)
-        return history
+        bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
+        history[-1][1] = ""
+        for word in bot_message:
+            history[-1][1].append(word)
+            time.sleep(0.33)
+            yield history
 
     msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
         bot, chatbot, chatbot
@@ -22,4 +24,4 @@ with gr.Blocks() as demo:
     clear.click(lambda: None, None, chatbot, queue=False)
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.queue().launch()
