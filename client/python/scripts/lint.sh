24a1433396ad7ecf9525fde9b8548a2c1855725d
Aarni Koskela <akx@iki.fi>
2023-04-25 00:14:40 +0300

    CI: only run backend lints once, not in all test environments

--- client/python/scripts/lint.sh
+++ client/python/scripts/lint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-set -e
 
 cd "$(dirname ${0})/.."
 
 echo "Linting..."
 ruff test gradio_client
 black --check test gradio_client
-pyright gradio_client/*.py
 
-echo "Testing..."
-python -m pytest test
+echo "Type checking the client library with pyright"
+pyright gradio_client/*.py
