e65649d14d0959d47892eac39a5538503773af09
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-05-11 16:40:37 +0300

    Add a WebWorker file with an empty impl

--- wasm/src/index.ts
+++ wasm/src/index.ts
@@ -1,4 +1,8 @@
 export function wasmClient() {
 	// TODO: Implement wasm client
 	console.log("Hello, Wasm world!");
+
+	// Loading a worker here relies on Vite's support for WebWorkers (https://vitejs.dev/guide/features.html#web-workers),
+	// assuming that this module is imported from the Gradio frontend, which is compiled with Vite.
+	const worker = new Worker(new URL('./webworker.js', import.meta.url));
 }
