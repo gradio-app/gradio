f1817df4c46734d4f13d4fe1e52a25d50d3173a5
Abubakar Abid <abubakar@huggingface.co>
2023-05-11 09:57:57 -0500

    update release note format

--- scripts/format_release_notes.py
+++ scripts/format_release_notes.py
@@ -12,21 +12,11 @@ No changes to highlight.
 ## Bug Fixes:
 No changes to highlight.
 
-## Documentation Changes:
-No changes to highlight.
-
-## Testing and Infrastructure Changes:
+## Other Changelog:
 No changes to highlight.
 
 ## Breaking Changes:
 No changes to highlight.
-
-## Full Changelog:
-No changes to highlight.
-
-## Contributors Shoutout:
-No changes to highlight.
-
 
 """
 
