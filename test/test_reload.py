c28867ae7ec941113511ed748136539aaa075a0d
Ali Abid <aabid94@gmail.com>
2023-05-11 22:18:17 -0700

    changes

--- test/test_reload.py
+++ test/test_reload.py
@@ -2,15 +2,15 @@ from pathlib import Path
 from unittest.mock import patch
 
 import gradio
-from gradio.reload import run_in_reload_mode
+from gradio.cli import cli
 
 
 @patch("gradio.reload.os.system")
-@patch("gradio.reload.sys")
+@patch("gradio.cli.sys")
 def test_run_in_reload_mode(mock_sys, mock_system_call):
 
     mock_sys.argv = ["gradio", "demo/calculator/run.py"]
-    run_in_reload_mode()
+    cli()
     reload_command = mock_system_call.call_args[0][0]
     gradio_dir = Path(gradio.__file__).parent
     demo_dir = Path("demo/calculator/run.py").resolve().parent
