223b8695636796adee305929192356df725d97ef
aliabd <ali.si3luwa@gmail.com>
2023-05-18 14:45:52 +0400

    prerender

--- js/_website/src/routes/+layout.server.js
+++ js/_website/src/routes/+layout.server.js
@@ -1,6 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 import { redirects } from './redirects.js';
 
+export const prerender = true;
+
 export async function load({ url }) {
     if (url.pathname in redirects) {
         throw redirect(302, redirects[url.pathname]);
