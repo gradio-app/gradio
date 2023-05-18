32a772ad960180244725c66c08e4661b5ec09e55
aliabd <ali.si3luwa@gmail.com>
2023-05-18 12:37:38 +0400

    convert to adapter static

--- js/_website/svelte.config.js
+++ js/_website/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
