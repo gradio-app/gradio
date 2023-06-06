bc786ae4daaaa9f058d306dda7d4deea6c17f71e
aliabid94 <aabid94@gmail.com>
2023-06-05 18:11:53 -0700

    Corner status tracker (#4396)
    * changes
    
    * first commit
    
    * Update gradio/upload.py
    
    Co-authored-by: Aarni Koskela <akx@iki.fi>
    
    * Update gradio/upload.py
    
    Co-authored-by: Aarni Koskela <akx@iki.fi>
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * chnages
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * chagnes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    ---------
    
    Co-authored-by: Aarni Koskela <akx@iki.fi>
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- demo/chatbot_simple/run.py
+++ demo/chatbot_simple/run.py
@@ -2,15 +2,6 @@ import gradio as gr
 import random
 import time
 
-md = """This is some code:
-
-<h1>hello</h1>
-
-```py
-def fn(x, y, z):
-    print(x, y, z)
-"""
-
 with gr.Blocks() as demo:
     chatbot = gr.Chatbot()
     msg = gr.Textbox()
@@ -18,8 +9,8 @@ with gr.Blocks() as demo:
 
     def respond(message, chat_history):
         bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
-        chat_history.append((message, md))
-        time.sleep(1)
+        chat_history.append((message, bot_message))
+        time.sleep(2)
         return "", chat_history
 
     msg.submit(respond, [msg, chatbot], [msg, chatbot])
