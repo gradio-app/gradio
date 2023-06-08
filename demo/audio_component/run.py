303d6a36f0de19d34603ec6182a83fdab2afa6eb
pngwn <hello@pngwn.io>
2023-06-08 21:45:09 +0900

    fix

--- demo/audio_component/run.py
+++ demo/audio_component/run.py
@@ -1,25 +1,8 @@
-# import gradio as gr
-
-# css = "footer {display: none !important;} .gradio-container {min-height: 0px !important;}"
-
-# with gr.Blocks(css=css) as demo:
-#     gr.Audio()
-
-# demo.launch()
-
-
 import gradio as gr
 
-with gr.Blocks(analytics_enabled=False) as interface:
-    with gr.Row():
-        with gr.Tab('Chat history'):
-            with gr.Row():
-                with gr.Column():
-                    gr.Markdown('Upload')
-                    upload_chat_history = gr.File(type='binary', file_types=['.json', '.txt'])
-                with gr.Column():
-                    gr.Markdown('Download')
-                    download = gr.File()
-                    download_button = gr.Button(value='Click me')
+css = "footer {display: none !important;} .gradio-container {min-height: 0px !important;}"
 
-interface.launch()
+with gr.Blocks(css=css) as demo:
+    gr.Audio()
+
+demo.launch()
