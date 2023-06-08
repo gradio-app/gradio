e3192c5e07e08a5af76416ffaf7d808fcb75ab94
pngwn <hello@pngwn.io>
2023-06-08 16:53:41 +0900

    revert demo

--- demo/kitchen_sink/run.py
+++ demo/kitchen_sink/run.py
@@ -113,7 +113,7 @@ demo = gr.Interface(
         gr.Textbox(label="Textbox"),
         gr.Label(label="Label"),
         gr.Audio(label="Audio"),
-        gr.Image(label="Image", height=120),
+        gr.Image(label="Image"),
         gr.Video(label="Video"),
         gr.HighlightedText(label="HighlightedText").style(
             color_map={"punc": "pink", "test 0": "blue"}
