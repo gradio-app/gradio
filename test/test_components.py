a03d6919c57c5a6e8ee46379e3cb332013b761c2
Abubakar Abid <abubakar@huggingface.co>
2023-06-15 06:38:35 -0500

    fix test

--- test/test_components.py
+++ test/test_components.py
@@ -1922,6 +1922,7 @@ class TestChatbot:
             "height": None,
             "root_url": None,
             "selectable": False,
+            'latex_delimiters': [{'display': True, 'left': '$$', 'right': '$$'}]
         }
 
 
