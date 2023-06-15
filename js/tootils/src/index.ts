ca4f03f8e9bdb011943870b61e6bbecec052f27f
pngwn <hello@pngwn.io>
2023-06-15 21:28:27 +0100

    changes

--- js/tootils/src/index.ts
+++ js/tootils/src/index.ts
@@ -1,3 +1,5 @@
+import { test as base } from "@playwright/test";
+
 export function get_text<T extends HTMLElement>(el: T) {
 	return el.innerText.trim();
 }
@@ -7,3 +9,21 @@ export function wait(n: number) {
 }
 
 export * from "./render";
+
+export const test = base.extend<{ setup: typeof base.beforeAll }>({
+	setup: [
+		async ({}, use, workerInfo) => {
+			console.log("FIXTURE RUN", workerInfo.workerIndex);
+			return;
+		},
+		{ auto: true, scope: "test" }
+	]
+	// log: [
+	// 	async ({}, use, workerInfo) => {
+	// 		console.log("FIXTURE RUN");
+	// 	},
+	// 	{ auto: true, scope: "worker" }
+	// ]
+});
+
+export { expect } from "@playwright/test";
