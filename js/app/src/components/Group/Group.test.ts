8f556e37a57f5c007bec066e08495c851a0d7ad1
Abubakar Abid <abubakar@huggingface.co>
2023-06-19 09:50:41 -0500

    visible=False in Group

---
+++ js/app/src/components/Group/Group.test.ts
@@ -0,0 +1,26 @@
+import { test, describe, assert, afterEach, vi } from "vitest";
+import { cleanup, render } from "@gradio/tootils";
+
+import Group from "./Group.svelte";
+
+describe("Group", () => {
+	afterEach(() => {
+		cleanup();
+		vi.restoreAllMocks();
+	});
+
+	test("setting visible to false hides the Group", async () => {
+		render(Group, {
+			elem_id: "group",
+			visible: false
+		});
+
+		const groupElement = document.getElementById("group");
+
+		assert(groupElement !== null, "Group element not found.");
+		assert(
+			groupElement.classList.contains("hide"),
+			"Group element is not hidden."
+		);
+	});
+});
