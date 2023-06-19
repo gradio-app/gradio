d6b6c87c4f1af904df6c68cd36dcc7fa1d7582a7
pngwn <hello@pngwn.io>
2023-06-19 21:02:03 +0100

    Remove mocks (#4550)
    * changes
    
    * changes
    
    * remove mocks
    
    * fix scripts
    
    * tweak port
    
    * add logs
    
    * add logs
    
    * do tests again
    
    * tweaks
    
    * fixes
    
    * regen notebooks
    
    * change workers
    
    * change workers
    
    * try to speed up ci
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * fix actions
    
    * again
    
    * again
    
    * again
    
    * again
    
    * again
    
    * again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * add names to frontend ci steps

--- js/app/test/blocks_page_load.spec.ts
+++ js/app/test/blocks_page_load.spec.ts
@@ -1,12 +1,6 @@
-import { test, expect, Page } from "@playwright/test";
-import { mock_theme, wait_for_page, mock_api, mock_demo } from "./utils";
+import { test, expect } from "@gradio/tootils";
 
 test("renders the correct elements", async ({ page }) => {
-	await mock_demo(page, "blocks_page_load");
-	await mock_api(page, [["Welcome! This page has loaded for Frank"]]);
-	await mock_theme(page);
-	await wait_for_page(page);
-
 	const textbox = await page.getByLabel("Name");
 
 	await textbox.fill("Frank");
