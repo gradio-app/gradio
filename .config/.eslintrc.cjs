d665dfee300bae20a4eda2b323fe74977ac8a1e9
pngwn <hello@pngwn.io>
2023-06-21 16:27:46 +0100

    fix

--- .config/.eslintrc.cjs
+++ .config/.eslintrc.cjs
@@ -8,6 +8,7 @@ const js_rules_disabled = Object.fromEntries(
 	Object.keys(js.configs.all.rules).map((rule) => [rule, "off"])
 );
 
+console.log(js_rules_disabled, ts_rules_disabled);
 module.exports = {
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
@@ -45,25 +46,25 @@ module.exports = {
 		// we want the rules available but we want them mostly switched off
 		...ts_rules_disabled,
 		...js_rules_disabled,
-		"no-console": ["off", { allow: ["warn", "error"] }],
-		"no-constant-condition": "off",
-		"no-dupe-args": "off",
-		"no-extra-boolean-cast": "off",
-		"no-unexpected-multiline": "off",
-		"no-unreachable": "off",
-		"valid-jsdoc": "off",
-		"array-callback-return": "off",
-		complexity: "off",
-		"no-else-return": "off",
-		"no-useless-return": "off",
-		"no-shadow": "off",
-		"no-undef": "off",
-		"@typescript-eslint/adjacent-overload-signatures": "off",
-		"@typescript-eslint/explicit-function-return-type": "off",
-		"@typescript-eslint/consistent-type-exports": "off",
-		"@typescript-eslint/ban-types": "off",
-		"@typescript-eslint/array-type": "off",
-		"@typescript-eslint/no-inferrable-types": "off"
+		"no-console": ["error", { allow: ["warn", "error"] }],
+		"no-constant-condition": "error",
+		"no-dupe-args": "error",
+		"no-extra-boolean-cast": "error",
+		"no-unexpected-multiline": "error",
+		"no-unreachable": "error",
+		"valid-jsdoc": "error",
+		"array-callback-return": "error",
+		complexity: "error",
+		"no-else-return": "error",
+		"no-useless-return": "error",
+		"no-shadow": "error",
+		"no-undef": "error",
+		"@typescript-eslint/adjacent-overload-signatures": "error",
+		"@typescript-eslint/explicit-function-return-type": "error",
+		"@typescript-eslint/consistent-type-exports": "error",
+		"@typescript-eslint/ban-types": "error",
+		"@typescript-eslint/array-type": "error",
+		"@typescript-eslint/no-inferrable-types": "error"
 
 		// override/add rules settings here, such as:
 		// 'svelte/rule-name': 'error'
