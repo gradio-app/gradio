62701f8dceedf596df9602ceb1f9a8f22d2e75ea
Dawood Khan <dawoodkhan82@gmail.com>
2023-06-22 10:32:48 -0400

    Remove unnecessary mock json files (#4625)
    * remove mock json files
    
    * changelog
    
    * Update CHANGELOG.md

--- js/app/test/blocks_kinematics.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from "@gradio/tootils";
-
-test("renders the correct elements", async ({ page }) => {
-	await Promise.all([
-		page.click("button:has-text('Run')"),
-		page.waitForResponse("**/run/predict")
-	]);
-});
