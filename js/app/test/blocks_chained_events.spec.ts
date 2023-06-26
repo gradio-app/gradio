c6257b646c9a0aabd6c705670f31a74fd836ee75
freddyaboulton <alfonsoboulton@gmail.com>
2023-06-26 18:05:12 -0400

    Fix

--- js/app/test/blocks_chained_events.spec.ts
+++ js/app/test/blocks_chained_events.spec.ts
@@ -1,19 +1,19 @@
 import { test, expect } from "@gradio/tootils";
 
 test("Success event", async ({ page }) => {
-	const value = page.getByLabel("Result");
-	expect(value.inputValue()).toEqual("");
+	const textbox = page.getByLabel("Result");
+	await expect(textbox).toHaveValue("");
 
 	await Promise.all([
 		page.click("text=Trigger Failure"),
 		page.waitForResponse("**/run/predict")
 	]);
-	expect(value.inputValue()).toEqual("");
+	expect(textbox).toHaveValue("");
 
 	await Promise.all([
 		page.click("text=Trigger Success"),
 		page.waitForResponse("**/run/predict")
 	]);
 
-	expect(value.inputValue()).toEqual("Success event triggered");
+	expect(textbox).toHaveValue("");
 });
