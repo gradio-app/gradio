bcda88f1e5878f392bcf3601a504e3c1c686e0ef
freddyaboulton <alfonsoboulton@gmail.com>
2023-06-26 14:41:02 -0400

    Fix test

--- js/app/src/components/Slider/Slider.component.spec.ts
+++ js/app/src/components/Slider/Slider.component.spec.ts
@@ -4,5 +4,5 @@ import Slider from "./Slider.svelte";
 test('should work', async ({ mount }) => {
     // <Slider value=3 minimum = 0 maximum = 10 step = 1 label = "My Slider" show_label = true />
     const component = await mount(Slider, { props: { value: 3, minimum: 0, maximum: 10, label: "My Slider", show_label: true } });
-    await expect(component).toContainText('My Slidersss');
+    await expect(component).toContainText('My Slider');
 });
