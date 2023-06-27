37d672933ef83ff53315b9afdfcd74adb4a0c8bd
Abubakar Abid <abubakar@huggingface.co>
2023-06-26 19:21:04 -0500

    overflow y

--- js/app/src/components/Form/Form.svelte
+++ js/app/src/components/Form/Form.svelte
@@ -23,7 +23,7 @@
 		border: var(--block-border-width) solid var(--border-color-primary);
 		border-radius: var(--block-radius);
 		background: var(--border-color-primary);
-		overflow: visible;
+		overflow-y: visible;
 	}
 
 	div :global(.block) {
