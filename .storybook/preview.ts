d5d3a1fdedb05c822f9507af5316925212ca50c0
Hannah <hannahblair@users.noreply.github.com>
2023-06-27 17:59:07 +0100

    add test story

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/svelte";
+import "../js/theme/src/pollen.css";
 
 const preview: Preview = {
 	parameters: {
---
+++ js/app/src/components/Button/Button.stories.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+	import { Button } from "@gradio/button";
+</script>
+
+<Meta title="Button" component={Button} />
+
+<Template let:args>
+	<Button size="lg" variant="primary">Test Button</Button>
+</Template>
+
+<Story name="Primary" />
