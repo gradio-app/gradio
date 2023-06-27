f489f6aff4bf0b513847d5ed60f367f8df8f47e7
Abubakar Abid <abubakar@huggingface.co>
2023-06-26 20:30:56 -0500

    update demo

---
+++ js/app/test/cancel_events.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@gradio/tootils";
+import { BASE64_IMAGE } from "./media_data";
+
+test("iterator", async ({ page }) => {
+    const start_button = await page.locator("button", { hasText: /Start Iterating/ });
+    await start_button.click()
+
+    page.on('websocket', ws => {
+        console.log(`WebSocket opened: ${ws.url()}>`);
+        ws.on('framesent', event => console.log(event.payload));
+        ws.on('framereceived', event => console.log(event.payload));
+        ws.on('close', () => console.log('WebSocket closed'));
+    });
+
+    // page.waitForResponse("**/*")
+
+    // const textbox = await page.getByLabel("Textbox").nth(0);
+    // await expect(textbox).toHaveValue("Lorem ipsum");
+
+});
+
