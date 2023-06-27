1d0ec29fb3ebad1a7276eeb03d827cc7357de368
Abubakar Abid <abubakar@huggingface.co>
2023-06-26 20:52:31 -0500

    websocket

--- js/app/test/cancel_events.spec.ts
+++ js/app/test/cancel_events.spec.ts
@@ -1,26 +1,22 @@
 import { test, expect } from "@gradio/tootils";
-import { BASE64_IMAGE } from "./media_data";
 
-test("iterator", async ({ page }) => {
+test("yielding", async ({ page }) => {
     const start_button = await page.locator("button", { hasText: /Start Iterating/ });
+    const textbox = await page.getByLabel("Textbox").nth(0);
     await start_button.click()
 
     page.on('websocket', ws => {
-        console.log(`WebSocket opened: ${ws.url()}>`);
         ws.on('framesent', async event => {
             console.log(event.payload)
-            const textbox = await page.getByLabel("Textbox").nth(0);
-            await expect(textbox).toHaveValue(event.payload.toString());
         });
-        ws.on('framereceived', event => console.log(event.payload));
-        ws.on('close', () => console.log('WebSocket closed'));
+
+        ws.on('framereceived', async event => {
+            console.log(event.payload)
+            await expect(textbox).toHaveValue("abcd");
+        });
 
     });
 
-    // page.waitForResponse("**/*")
-
-    // const textbox = await page.getByLabel("Textbox").nth(0);
-    // await expect(textbox).toHaveValue("Lorem ipsum");
-
+    await expect(textbox).toHaveValue("abcd");
 });
 
