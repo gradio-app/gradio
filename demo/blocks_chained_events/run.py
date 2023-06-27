0be079ba41f81bb60acf5293354ff8cb04d52670
freddyaboulton <alfonsoboulton@gmail.com>
2023-06-27 12:44:05 -0400

    Make modifications

--- demo/blocks_chained_events/run.py
+++ demo/blocks_chained_events/run.py
@@ -2,20 +2,30 @@ import gradio as gr
 
 
 def failure():
-    raise ValueError("This should fail!")
+    raise gr.Error("This should fail!")
+
+def exception():
+    raise ValueError("Something went wrong")
 
 def success():
     return True
 
 with gr.Blocks() as demo:
-    gr.Markdown("Used in E2E tests of success event trigger. The then event covered in chatbot E2E tests.")
+    gr.Markdown("Used in E2E tests of success event trigger. The then event covered in chatbot E2E tests."
+                " Also testing that the status modals show up.")
     with gr.Row():
         result = gr.Textbox(label="Result")
+        result_2 = gr.Textbox(label="Consecutive Event")
     with gr.Row():
         success_btn = gr.Button(value="Trigger Success")
+        success_btn_2 = gr.Button(value="Trigger Consecutive Success")
         failure_btn = gr.Button(value="Trigger Failure")
+        failure_exception = gr.Button(value="Trigger Failure With ValueError")
+
+        success_btn_2.click(success, None, None).then(success, None, None).success(lambda: "Consecutive Event Triggered", None, result_2)
         success_btn.click(success, None, None).success(lambda: "Success event triggered", inputs=None, outputs=result)
         failure_btn.click(failure, None, None).success(lambda: "Should not be triggered", inputs=None, outputs=result)
+        failure_exception.click(exception, None, None)
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(show_error=True)
--- js/app/test/blocks_chained_events.spec.ts
+++ js/app/test/blocks_chained_events.spec.ts
@@ -18,3 +18,44 @@ test("Success event", async ({ page }) => {
 
 	expect(textbox).toHaveValue("Success event triggered");
 });
+
+test("Consecutive success event", async ({ page }) => {
+	const textbox = page.getByLabel("Consecutive Event");
+
+	await page.click("text=Trigger Consecutive Success");
+	// need to wait for all the events to fire
+	await page.waitForTimeout(1000);
+
+	expect(textbox).toHaveValue("Consecutive Event Triggered");
+});
+
+test("gr.Error Triggers modal", async ({ page }) => {
+	await Promise.all([
+		page.click("text=Trigger Failure"),
+		page.waitForResponse("**/run/predict")
+	]);
+
+	const toast = page.getByTestId("error-toast");
+	expect(toast).toContainText("Something went wrong");
+	const close = page.getByTestId("error-close");
+	await close.click();
+	// need to let the toast fade out
+	await page.waitForTimeout(2000);
+	expect(await page.getByTestId("error-toast").count()).toEqual(0);
+});
+
+test("ValueError Triggers modal", async ({ page }) => {
+	// The demo has show_error=True so ValueError should show the modal
+	await Promise.all([
+		page.click("text=Trigger Failure With ValueError"),
+		page.waitForResponse("**/run/predict")
+	]);
+
+	const toast = page.getByTestId("error-toast");
+
+	expect(toast).toContainText("Something went wrong");
+	const close = page.getByTestId("error-close");
+	await close.click();
+	await page.waitForTimeout(2000);
+	expect(await page.getByTestId("error-toast").count()).toEqual(0);
+});
