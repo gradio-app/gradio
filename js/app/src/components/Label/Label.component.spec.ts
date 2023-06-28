46ce17cd494cbcdc49ec1e927eefad1742c4b60b
freddyaboulton <alfonsoboulton@gmail.com>
2023-06-28 15:26:35 -0400

    Add tests

--- js/app/src/components/Label/Label.component.spec.ts
+++ js/app/src/components/Label/Label.component.spec.ts
@@ -16,7 +16,10 @@ const loading_status: LoadingStatus = {
 	show_progress: "full"
 };
 
-test("Label Default Value And Label rendered", async ({ mount, page }) => {
+test("gr.Label default value and label rendered with confidences", async ({
+	mount,
+	page
+}) => {
 	const component = await mount(Label, {
 		props: {
 			value: {
@@ -32,36 +35,104 @@ test("Label Default Value And Label rendered", async ({ mount, page }) => {
 		}
 	});
 	await expect(component).toContainText("My Label");
+	await expect(component.getByTestId("block-label")).toBeVisible();
 	await expect(page.getByTestId("label-output-value")).toContainText("Good");
 	await expect(page.getByTestId("Good-confidence-set")).toContainText("90");
+	await expect(page.getByTestId("Bad-confidence-set")).toContainText("10");
 });
 
-// test("Slider Change event", async ({ mount, page }) => {
-//     let change = spy();
-//     let release = spy();
-//     const component = await mount(Slider, {
-//         props: {
-//             value: 3,
-//             minimum: 0,
-//             maximum: 10,
-//             label: "My Slider",
-//             show_label: true,
-//             step: 1,
-//             mode: "dynamic",
-//             loading_status: loading_status
-//         },
-//         on: {
-//             change: change,
-//             release: release
-//         }
-//     });
+test("gr.Label hides label when show_label=false", async ({ mount, page }) => {
+	const component = await mount(Label, {
+		props: {
+			value: {
+				label: "Good",
+				confidences: [
+					{ label: "Good", confidence: 0.9 },
+					{ label: "Bad", confidence: 0.1 }
+				]
+			},
+			label: "My Label",
+			show_label: false,
+			loading_status: loading_status
+		}
+	});
+	await expect(component.getByTestId("block-label")).toBeHidden();
+});
 
-//     const slider = page.getByLabel("Slider");
+test("gr.Label confidence bars not rendered without confidences", async ({
+	mount
+}) => {
+	const component = await mount(Label, {
+		props: {
+			value: {
+				label: "Good"
+			},
+			label: "My Label",
+			show_label: true,
+			loading_status: loading_status
+		}
+	});
+	await expect(component).toContainText("My Label");
+	expect(await component.getByTestId("Good-confidence-set").count()).toEqual(0);
+});
 
-//     await changeSlider(page, slider, slider, 0.7);
-//     await expect(component.getByLabel("My Slider")).toHaveValue("7");
+test("gr.Label confidence bars trigger select event when clicked", async ({
+	mount,
+	page
+}) => {
+	const select = spy();
+	const component = await mount(Label, {
+		props: {
+			value: {
+				label: "Good",
+				confidences: [
+					{ label: "Good", confidence: 0.9 },
+					{ label: "Bad", confidence: 0.1 }
+				]
+			},
+			label: "My Label",
+			show_label: true,
+			loading_status: loading_status
+		},
+		on: {
+			select: select
+		}
+	});
+	await expect(component).toContainText("My Label");
+	await component.getByTestId("Bad-confidence-set").click();
+	expect(select.callCount).toEqual(1);
+	expect(select.calls[0][0]).toEqual({ index: 1, value: "Bad" });
+});
 
-//     // More than one change event and one release event.
-//     await expect(change.callCount).toBeGreaterThan(1);
-//     await expect(release.callCount).toEqual(1);
-// });
+test("gr.Label triggers change event", async ({ mount, page }) => {
+	const change = spy();
+	const component = await mount(Label, {
+		props: {
+			value: {
+				label: "Good",
+				confidences: [
+					{ label: "Good", confidence: 0.9 },
+					{ label: "Bad", confidence: 0.1 }
+				]
+			},
+			label: "My Label",
+			show_label: true,
+			loading_status: loading_status
+		},
+		on: {
+			change: change
+		}
+	});
+	await component.update({
+		props: {
+			value: {
+				label: "Good",
+				confidences: [
+					{ label: "Good", confidence: 0.1 },
+					{ label: "Bad", confidence: 0.9 }
+				]
+			}
+		}
+	});
+	expect(change.callCount).toEqual(1);
+});
