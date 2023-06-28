5c70c45c70ecea77b4bfbe0c7879d7b146c769ea
pngwn <hello@pngwn.io>
2023-06-28 11:34:20 +0100

    revert demo

--- demo/audio_component/run.py
+++ demo/audio_component/run.py
@@ -1,21 +1,8 @@
 import gradio as gr
 
-TEST_AUDIO_A = "a.wav"
-TEST_AUDIO_B = "b.mp3"
+css = "footer {display: none !important;} .gradio-container {min-height: 0px !important;}"
 
-audio_files = [TEST_AUDIO_A, TEST_AUDIO_B]
-idx = 0
+with gr.Blocks(css=css) as demo:
+    gr.Audio()
 
-def output():
-    global idx
-    file = audio_files[idx]
-    idx = 1 if idx == 0 else 0
-    print(idx)
-    return file
-
-with gr.Blocks() as demo:
-    audio = gr.Audio(type="filepath", label="Audio", autoplay=True)
-    button = gr.Button(value="play")
-    button.click(output, outputs=[audio])
-    
 demo.launch()
