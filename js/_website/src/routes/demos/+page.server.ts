02efc51318be6d16fb7a7c8a3b560e23dc1e50c2
aliabd <ali.si3luwa@gmail.com>
2023-06-28 08:57:35 +0400

    typing fix

--- js/_website/src/routes/demos/+page.server.ts
+++ js/_website/src/routes/demos/+page.server.ts
@@ -4,11 +4,21 @@ import demos_by_category from "./demos.json"
 
 let language = 'python';
 
+type Demo = {
+    name: string;
+    dir: string;
+    code: string;
+    highlighted_code?: string;
+}
+type Category = {
+    category: string;
+    demos: Demo[];
+}
 
 export async function load() {
 
-    demos_by_category.forEach(category => {
-        category.demos.forEach((demo: { name: string; dir: string; code: string; highlighted_code?: string }) => {
+    demos_by_category.forEach((category: Category) => {
+        category.demos.forEach((demo: Demo) => {
             demo.highlighted_code = Prism.highlight(demo.code, Prism.languages[language], "python");
         })
     })
