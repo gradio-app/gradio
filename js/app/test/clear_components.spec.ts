42339e3ea40daec68024dd1cb5fb0e7741607f7a
freddyaboulton <alfonsoboulton@gmail.com>
2023-06-30 18:17:30 -0400

    Add non-None callable value

--- js/app/test/clear_components.spec.ts
+++ js/app/test/clear_components.spec.ts
@@ -1,5 +1,18 @@
 import { test, expect } from "@gradio/tootils";
 
+test("Components value can be set via callable to a non-None value", async ({
+	page
+}) => {
+	const textBoxValue = await page.getByLabel(`component_00`).inputValue();
+	expect(textBoxValue.length).toBeGreaterThan(1);
+
+	const sliderValue = await page.getByLabel(`component_01`).inputValue();
+	expect(parseFloat(sliderValue)).toBeGreaterThan(0);
+
+	const dropDownValue = await page.getByLabel(`component_07`).inputValue();
+	expect(Array("a", "b", "c").includes(dropDownValue)).toBeTruthy();
+});
+
 test("gr.ClearButton clears every component's value", async ({ page }) => {
 	await Promise.all([
 		page.waitForResponse("**/run/predict"),
