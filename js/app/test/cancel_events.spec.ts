4167485f45e42bce065630ea4fddc02db19d5ef4
Abubakar Abid <abubakar@huggingface.co>
2023-06-29 19:25:36 -0500

    cancel events

--- js/app/test/cancel_events.spec.ts
+++ js/app/test/cancel_events.spec.ts
@@ -5,6 +5,11 @@ test("yielding", async ({ page }) => {
 	let fourth_iteration;
 	let last_iteration;
 
+	const start_button = await page.locator("button", {
+		hasText: /Start Iterating/
+	});
+	const textbox = await page.getByLabel("Iterative Output");
+
 	page.on("websocket", (ws) => {
 		first_iteration = ws.waitForEvent("framereceived", {
 			predicate: (event) => {
@@ -25,10 +30,6 @@ test("yielding", async ({ page }) => {
 		});
 	});
 
-	const start_button = await page.locator("button", {
-		hasText: /Start Iterating/
-	});
-	const textbox = await page.getByLabel("Iterative Output");
 	await start_button.click();
 
 	await first_iteration;
@@ -37,4 +38,32 @@ test("yielding", async ({ page }) => {
 	await expect(textbox).toHaveValue("3");
 	await last_iteration;
 	await expect(textbox).toHaveValue("8");
+});
+
+test("cancel", async ({ page }) => {
+	let first_iteration;
+	const start_button = await page.locator("button", {
+		hasText: /Start Iterating/
+	});
+	const stop_button = await page.locator("button", {
+		hasText: /Stop Iterating/
+	});
+	const textbox = await page.getByLabel("Iterative Output");
+
+	page.on("websocket", (ws) => {
+		first_iteration = ws.waitForEvent("framereceived", {
+			predicate: (event) => {
+				return JSON.parse(event.payload as string).msg === "process_generating";
+			}
+		});
+	});
+
+	await start_button.click();
+
+	await first_iteration;
+	await expect(textbox).toHaveValue("0");
+	await stop_button.click();
+	await expect(textbox).toHaveValue("0");
+	await page.waitForTimeout(1000);
+	await expect(textbox).toHaveValue("0");
 });
