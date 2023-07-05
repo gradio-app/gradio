daf19912a9875c74f7b8da8a5abdad5dfc2e0533
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-07-05 17:47:13 +0900

    Build theme.css at the build time of Gradio-lite and update the frontend app to load it at the initialization phase

--- js/app/src/lite/index.ts
+++ js/app/src/lite/index.ts
@@ -2,7 +2,7 @@ import "@gradio/theme";
 import { WorkerProxy, type WorkerProxyOptions } from "@gradio/wasm";
 import { api_factory } from "@gradio/client";
 import { wasm_proxied_fetch } from "./fetch";
-import { wasm_proxied_mount_css } from "./css";
+import { wasm_proxied_mount_css, mount_prebuilt_css } from "./css";
 import type { mount_css } from "../css";
 import Index from "../Index.svelte";
 import type { ThemeMode } from "../components/types";
@@ -72,6 +72,8 @@ export function create(options: Options): GradioAppController {
 	// (see the await in the `onmessage` callback in the webworker code)
 	// So we don't await this promise because we want to mount the `Index` immediately and start the app initialization asynchronously.
 	worker_proxy.runPythonAsync(options.pyCode);
+
+	mount_prebuilt_css(document.head);
 
 	const overridden_fetch: typeof fetch = (input, init?) => {
 		return wasm_proxied_fetch(worker_proxy, input, init);
