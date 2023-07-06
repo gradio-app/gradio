34a1763f7a66bc22e95a469cb547313b3027a512
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-07-06 22:04:03 +0900

    Update the dev editor to deal with multiple files

--- js/app/src/lite/dev/App.svelte
+++ js/app/src/lite/dev/App.svelte
@@ -2,9 +2,20 @@
 	import type { create as createType } from ".."
 	// @ts-ignore
 	const create: typeof createType = globalThis.createGradioApp
+	type CreateOptions = Parameters<typeof create>[0];
+
 	import { onMount, onDestroy } from "svelte"
 
-	let code: string = `import gradio as gr
+	interface File {
+		name: string;
+		content: string | ArrayBufferView;
+	}
+	let editorFiles: File[] = [
+		{
+			name: "app.py",
+			content: `import gradio as gr
+
+from greeting import hi
 
 def greet(name):
     return "Hello " + name + "!"
@@ -18,6 +29,8 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
     output = gr.Textbox(label="Output Box")
     greet_btn = gr.Button("Greet")
     greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
+    say_hi_btn = gr.Button("Say hi")
+    say_hi_btn.click(fn=hi, inputs=name, outputs=output, api_name="hi")
 
     gr.File()
 
@@ -25,68 +38,121 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
     upload_button = gr.UploadButton("Click to Upload a File", file_types=["image", "video"], file_count="multiple")
     upload_button.upload(upload_file, upload_button, file_output)
 
-demo.launch()`
-
-		let requirements_txt = "";
-
-		function parse_requirements(text: string) {
-			return text
-				.split("\n")
-				.map((line) => line.trim())
-				.filter((line) => line.length > 0 && !line.startsWith("#"));
+demo.launch()`,
+		},
+		{
+			name: "greeting.py",
+			content: `
+def hi(name):
+    return "Hi " + name + "!"`,
 		}
+	]
+	let entrypoint = editorFiles[0].name;
 
-		const requirements = parse_requirements(requirements_txt);
+	$: files = editorFiles.reduce<NonNullable<CreateOptions["files"]>>((acc, file) => {
+		acc[file.name] = {
+			data: file.content,
+		};
+		return acc;
+	}, {})
 
-		let controller: ReturnType<typeof create>;
-		onMount(() => {
-			controller = create({
-				target: document.getElementById("gradio-app")!,
-				files: {
-					"images/logo.png": {
-						url: "https://raw.githubusercontent.com/gradio-app/gradio/main/guides/assets/logo.png"
-					}
-				},
-				code,
-				requirements,
-				info: true,
-				container: true,
-				isEmbed: false,
-				initialHeight: "300px",
-				eager: false,
-				themeMode: null,
-				autoScroll: false,
-				controlPageTitle: false,
-				appMode: true
-			});
-		})
-		onDestroy(() => {
-			controller.unmount();
-		})
+	let requirements_txt = "";
 
-		function handleExecClick () {
-			console.debug("exec_button.onclick");
-			controller.run_code(code);
-			console.debug("Rerun finished")
-		}
+	function parse_requirements(text: string) {
+		return text
+			.split("\n")
+			.map((line) => line.trim())
+			.filter((line) => line.length > 0 && !line.startsWith("#"));
+	}
 
-		function handleInstallClick() {
-			console.debug("install_button.onclick");
-			const requirements = parse_requirements(requirements_txt)
-			console.debug("requirements", requirements)
-			controller.install(requirements);
-			console.debug("Install finished")
-		}
+	const requirements = parse_requirements(requirements_txt);
+
+	let controller: ReturnType<typeof create>;
+	onMount(() => {
+		controller = create({
+			target: document.getElementById("gradio-app")!,
+			files,
+			entrypoint,
+			requirements,
+			info: true,
+			container: true,
+			isEmbed: false,
+			initialHeight: "300px",
+			eager: false,
+			themeMode: null,
+			autoScroll: false,
+			controlPageTitle: false,
+			appMode: true
+		});
+	})
+	onDestroy(() => {
+		controller.unmount();
+	})
+
+	function handleExecClick () {
+		console.debug("exec_button.onclick");
+		editorFiles.forEach((file) => {
+			controller.write(file.name, file.content, {});
+		});
+		controller.run_file(entrypoint);
+		console.debug("Rerun finished")
+	}
+
+	function handleInstallClick() {
+		console.debug("install_button.onclick");
+		const requirements = parse_requirements(requirements_txt)
+		console.debug("requirements", requirements)
+		controller.install(requirements);
+		console.debug("Install finished")
+	}
+
+	let newFileName = "";
+	function addFile() {
+		controller.write(newFileName, "", {});
+		editorFiles = editorFiles.concat({
+			name: newFileName,
+			content: "",
+		});
+		newFileName = ""
+	}
+
+	function deleteFile(deleteFileName: string) {
+		controller.unlink(deleteFileName);
+		editorFiles = editorFiles.filter((file) => file.name !== deleteFileName);
+	}
 </script>
 
 <div class="container">
-	<div class="panel left">
-		<textarea bind:value={code}></textarea>
+	<div class="panel">
+		<h2>Files</h2>
+
+		{#each editorFiles as file (file.name)}
+			<div class="file-cell">
+				<div class="cell-header">
+					<h3 class="cell-title">{file.name}</h3>
+					<div>
+						<label>
+							<input type="radio" name="entrypoint" bind:group={entrypoint} value={file.name} />
+							Set as an entrypoint file
+						</label>
+						<button on:click={() => deleteFile(file.name)}>Delete</button>
+					</div>
+				</div>
+				<textarea class="code-edit" bind:value={file.content}></textarea>
+			</div>
+		{/each}
 		<button on:click={handleExecClick}>Execute</button>
+
+		<div>
+			<h3>Create a new file</h3>
+			<input type="text" bind:value={newFileName} />
+			<button on:click={addFile}>Create</button>
+		</div>
 	</div>
 
-	<div class="panel right">
-		<textarea bind:value={requirements_txt}></textarea>
+	<div class="panel">
+		<h2>Install requirements</h2>
+		<textarea class="code-edit" bind:value={requirements_txt}></textarea>
 		<button on:click={handleInstallClick}>Install</button>
 	</div>
 </div>
@@ -94,21 +160,34 @@ demo.launch()`
 <style>
 	.container {
 		height: 100%;
+		overflow: scroll;
+	}
+
+	.panel {
+		padding: 8px;
+		box-sizing: border-box;
+	}
+
+	.panel .file-cell {
+		width: 100%;
+	}
+
+	.cell-header {
 		display: flex;
 		flex-direction: row;
+		justify-content: space-between;
 	}
-	.panel {
-		display: flex;
-		flex-direction: column;
-		height: 100%;
+
+	.cell-title {
+		font-size: medium;
+		font-weight: bold;
+		margin: 0;
+		padding: 0;
 	}
-	.panel textarea {
-		flex-grow: 1;
-	}
-	.right {
-		width: 300px;
-	}
-	.left {
-		flex-grow: 1;
+
+	.code-edit {
+		width: 100%;
+		height: 200px;
+		box-sizing: border-box;
 	}
 </style>
