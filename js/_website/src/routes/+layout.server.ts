47ddb9da09a33c0874a19c7f86f11b1394bf99a0
pngwn <hello@pngwn.io>
2023-07-06 19:03:31 +0100

    changes

--- js/_website/src/routes/+layout.server.ts
+++ js/_website/src/routes/+layout.server.ts
@@ -4,7 +4,16 @@ import { redirects } from "./redirects.js";
 export const prerender = true;
 
 export async function load({ url }: any) {
-	if (url.pathname in redirects) {
-		throw redirect(308, redirects[url.pathname as keyof typeof redirects]);
+	console.log(url.pathname);
+
+	for (const key in redirects) {
+		console.log(key);
+		
+
+		if (url.pathname.indexOf(key) !== -1 && url.pathname.indexOf('guides/') === -1) {
+			console.log('redirect')
+			throw redirect(308, redirects[key as keyof typeof redirects]);
+		}
 	}
+	
 }
