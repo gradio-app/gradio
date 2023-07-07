4777fc2e6b9a17632db790aedc1f28e58fc5da76
freddyaboulton <alfonsoboulton@gmail.com>
2023-07-07 11:40:31 -0400

    Remove css

--- demo/clearbutton_component/run.py
+++ demo/clearbutton_component/run.py
@@ -1,10 +1,7 @@
 import gradio as gr
 
-css = (
-    "footer {display: none !important;} .gradio-container {min-height: 0px !important;}"
-)
 
-with gr.Blocks(css=css) as demo:
+with gr.Blocks() as demo:
     textbox = gr.Textbox(value="This is some text")
     gr.ClearButton(textbox)
 
