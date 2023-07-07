270e908997b2f36f239971eb4da510b8a306e1ee
aliabd <ali.si3luwa@gmail.com>
2023-07-07 18:18:13 +0300

    testing meta tags

--- js/_website/src/components/MetaTags.svelte
+++ js/_website/src/components/MetaTags.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import image from "../assets/img/meta-image.png";
 	export let title = "Gradio";
 	export let description = "Build & Share Delightful Machine Learning Apps";
 	export let url: string;
@@ -16,12 +15,10 @@
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content={url} />
 	<meta property="og:description" content={description} />
-	<meta property="og:image" content={image} />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:creator" content="@Gradio" />
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content="{description}." />
-	<meta name="twitter:image" content={image} />
 
 	<link rel="canonical" href={canonical} />
 </svelte:head>
