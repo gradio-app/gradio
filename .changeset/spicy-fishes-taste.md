76acf3cb0b258c0e6bb38d611d766e5e54b68437
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-07-10 18:03:21 +0900

    Lite: Fix the markdown-it package name spec on the Wasm worker (#4846)
    * Fix the package name spec of markdown-it on the Wasm worker
    
    * Add changeset

---
+++ .changeset/spicy-fishes-taste.md
@@ -0,0 +1,5 @@
+---
+"@gradio/lite": patch
+---
+
+Fix the package name spec of markdown-it on the Wasm worker
--- js/wasm/src/webworker/index.ts
+++ js/wasm/src/webworker/index.ts
@@ -75,7 +75,7 @@ async function loadPyodideAndPackages(
 	await micropip.add_mock_package("aiohttp", "3.8.4");
 	await micropip.add_mock_package("multidict", "4.7.6");
 	await pyodide.loadPackage(["ssl", "distutils", "setuptools"]);
-	await micropip.install(["markdown-it-py~=2.2.0"]); // On 3rd June 2023, markdown-it-py 3.0.0 has been released. The `gradio` package depends on its `>=2.0.0` version so its 3.x will be resolved. However, it conflicts with `mdit-py-plugins`'s dependency `markdown-it-py >=1.0.0,<3.0.0` and micropip currently can't resolve it. So we explicitly install the compatible version of the library here.
+	await micropip.install(["markdown-it-py[linkify]~=2.2.0"]); // On 3rd June 2023, markdown-it-py 3.0.0 has been released. The `gradio` package depends on its `>=2.0.0` version so its 3.x will be resolved. However, it conflicts with `mdit-py-plugins`'s dependency `markdown-it-py >=1.0.0,<3.0.0` and micropip currently can't resolve it. So we explicitly install the compatible version of the library here.
 	await micropip.install.callKwargs(gradioWheelUrls, {
 		keep_going: true
 	});
