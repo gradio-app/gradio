4ffa8ad693aef6a7c2b956a579c61b23e378341a
Abubakar Abid <abubakar@huggingface.co>
2023-07-12 20:54:45 -0400

    chat interface

--- gradio/chat_interface.py
+++ gradio/chat_interface.py
@@ -5,6 +5,7 @@ This file defines a useful high-level abstraction to build Gradio chatbots: Chat
 
 from __future__ import annotations
 
+import inspect
 from typing import Callable
 
 from gradio_client.documentation import document, set_documentation_group
@@ -93,15 +94,27 @@ class ChatInterface(Blocks):
                     self.textbox = Textbox(show_label=False, placeholder="Type a message...")
             
             with Row():
-                for b, btn in enumerate([submit_btn, retry_btn, delete_last_btn, clear_btn]):
+                if submit_btn:
+                    if isinstance(submit_btn, Button):
+                        submit_btn.render()
+                    elif isinstance(submit_btn, str):
+                        submit_btn = Button(submit_btn, variant="primary")
+                    else:
+                        raise ValueError(f"The submit_btn parameter must be a gr.Button, string, or None, not {type(submit_btn)}")
+                    self.buttons.append(submit_btn)
+                self.stop_btn = Button("Stop", variant="stop", visible=False)
+                
+                for btn in [retry_btn, delete_last_btn, clear_btn]:
                     if btn:
                         if isinstance(btn, Button):
                             btn.render()
                         elif isinstance(btn, str):
-                            btn = Button(btn, variant="primary" if b == 0 else "secondary")
+                            btn = Button(btn, variant="secondary")
                         else:
-                            raise ValueError("The `submit_btn` parameter must be a gr.Button or a string")
+                            raise ValueError(f"All the _btn parameters must be a gr.Button, string, or None, not {type(btn)}")
                     self.buttons.append(btn)
+                
+                self.fake_api_btn = Button("Fake API", visible=False)
                 self.submit_btn, self.retry_btn, self.delete_last_btn, self.clear_btn = self.buttons
             
             if examples:
@@ -116,8 +129,14 @@ class ChatInterface(Blocks):
             self.saved_input = State()
             
             self._setup_events()
+            self._setup_api()
 
-    def _setup_events(self):            
+    def _setup_events(self):
+        if inspect.isgeneratorfunction(self.fn):
+            submit_fn = self._stream_fn
+        else:
+            submit_fn = self._submit_fn
+
         self.textbox.submit(
             self._clear_and_save_textbox,
             [self.textbox],
@@ -125,7 +144,13 @@ class ChatInterface(Blocks):
             api_name=False,
             queue=False,
         ).then(
-            self._submit_fn,
+            self._display_input,
+            [self.saved_input, self.chatbot],
+            [self.chatbot],
+            api_name=False,
+            queue=False,
+        ).then(
+            submit_fn,
             [self.saved_input, self.chatbot],
             [self.chatbot],
             api_name=False,
@@ -139,7 +164,13 @@ class ChatInterface(Blocks):
                 api_name=False,
                 queue=False,
             ).then(
-                self._submit_fn,
+                self._display_input,
+                [self.saved_input, self.chatbot],
+                [self.chatbot],
+                api_name=False,
+                queue=False,
+            ).then(
+                submit_fn,
                 [self.saved_input, self.chatbot],
                 [self.chatbot],
                 api_name=False,
@@ -153,7 +184,13 @@ class ChatInterface(Blocks):
                 api_name=False,
                 queue=False,
             ).then(
-                self._submit_fn,
+                self._display_input,
+                [self.saved_input, self.chatbot],
+                [self.chatbot],
+                api_name=False,
+                queue=False,
+            ).then(
+                submit_fn,
                 [self.saved_input, self.chatbot],
                 [self.chatbot],
                 api_name=False,
@@ -174,16 +211,51 @@ class ChatInterface(Blocks):
                 None, 
                 [self.chatbot, self.saved_input],
                 queue=False,
-                api_name="clear")
+                api_name=False)
+            
+    def _setup_api(self):
+        if inspect.isgeneratorfunction(self.fn):
+            api_fn = self._api_stream_fn
+        else:
+            api_fn = self._api_submit_fn
+
+        # Use a gr.State() instead of self.chatbot so that the API doesn't require passing forth
+        # a chat history, instead it is just stored internally in the state.
+        history = State([])
+
+        self.fake_api_btn.click(
+            api_fn,
+            [self.saved_input, history],
+            [history],
+            api_name="chat",
+        )
 
     def _clear_and_save_textbox(self, message):
         return "", message
 
-    def _submit_fn(self, message: str, history: list[list[str]]):
-        # Need to handle streaming case
-        response = self.fn(message, history)
-        history.append((message, response))
+    def _display_input(self, message: str, history: list[list[str]]):
+        history.append((message, None))
         return history
+
+    def _submit_fn(self, message: str, history: list[list[str]]):
+        response = self.fn(message, history)
+        history[-1][1] = response
+        return history
+
+    def _stream_fn(self, message: str, history: list[list[str]]):
+        for response in self.fn(message, history):
+            history[-1][1] = response
+            yield history
+
+    def _api_submit_fn(self, message: str, history: list[list[str]]):
+        history = self._display_input(message, history)
+        history = self._submit_fn(message, history)
+        return history
+        
+    def _api_stream_fn(self, message: str, history: list[list[str]]):
+        history = self._display_input(message, history)
+        for response in self._stream_fn(message, history):
+            return response
 
     def _delete_prev_fn(self, history):
         try:
