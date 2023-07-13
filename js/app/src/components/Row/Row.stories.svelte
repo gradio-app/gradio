5cca76e1f0713fafd2fef1dade988032fd63a6cf
Hannah <hannahblair@users.noreply.github.com>
2023-07-13 11:56:37 +0100

    add (sort of a) row test

---
+++ js/app/src/components/Row/Row.stories.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+	import Row from "./Row.svelte";
+    import Image from '../Image/Image.svelte';
+    import Blocks from '../../Blocks.svelte'
+</script>
+
+<Meta
+	title="Layout Elements/Row"
+	component={Row}
+/>
+
+<Template let:args>
+    <Row {...args}>
+        <Image {...args} value="https://i.ibb.co/6BgKdSj/groot.jpg" />
+        <Image {...args} value="https://i.ibb.co/6BgKdSj/groot.jpg" />
+    </Row>
+</Template>
+
+<Story name="Row" />
