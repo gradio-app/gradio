2bfb57350270d22b20eafea29b59f1a1279b7ce9
Abubakar Abid <abubakar@huggingface.co>
2023-07-16 18:32:15 -0400

    fix textbox story

--- js/app/src/components/Textbox/Textbox.stories.svelte
+++ js/app/src/components/Textbox/Textbox.stories.svelte
@@ -1,7 +1,44 @@
 <script>
-	import {Template, Story } from "@storybook/addon-svelte-csf";
+	import {Meta, Template, Story } from "@storybook/addon-svelte-csf";
 	import Textbox from "./Textbox.svelte";
 </script>
+
+<Meta
+	title="Components/Textbox"
+	component={Textbox}
+	argTypes={{
+		label: {
+			control: "text",
+			description: "The gallery label",
+			name: "label",
+			value: "Gradio Button"
+		},
+		show_label: {
+			options: [true, false],
+			description: "Whether to show the label",
+			control: { type: "boolean" },
+			defaultValue: true
+		},
+		type: {
+			options: ["text", "email", "password"],
+			description: "The type of textbox",
+			control: { type: "select" },
+			defaultValue: "text"
+		},
+		text_align: {
+			options: ["left", "right"],
+			description: "Whether to align the text left or right",
+			control: { type: "select" },
+			defaultValue: "left"
+		},
+		lines: {
+			options: [1, 5, 10, 20],
+			description: "The number of lines to show in the textbox",
+			control: { type: "select" },
+			defaultValue: 1
+		},
+	}}
+/>
 
 
 <Template let:args>
