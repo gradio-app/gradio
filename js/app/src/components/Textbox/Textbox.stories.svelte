7968d0daf19541b8aedc1a12e13211d78bab4cdd
Abubakar Abid <abubakar@huggingface.co>
2023-07-16 18:21:20 -0400

    add textbox story

---
+++ js/app/src/components/Textbox/Textbox.stories.svelte
@@ -0,0 +1,30 @@
+<script>
+	import {Template, Story } from "@storybook/addon-svelte-csf";
+	import Textbox from "./Textbox.svelte";
+</script>
+
+
+<Template let:args>
+	<Textbox
+		{...args}
+		value="hello world"
+	/>
+</Template>
+
+<Story
+	name="Textbox with label"
+	args={{ label: "My simple label", show_label: true }}
+/>
+<Story
+	name="Textbox with lines specified"
+	args={{ lines: 10 }}
+/>
+<Story
+	name="Password input"
+	args={{ type: "password" }}
+/>
+<Story
+	name="Right aligned textbox"
+	args={{ text_align: "right" }}
+/>
+
