b32665ba52aeea93ac493e91a551488dd8b817ae
Dawood <dawoodkhan82@gmail.com>
2023-07-18 10:26:19 +0300

    add animate flag

--- gradio/helpers.py
+++ gradio/helpers.py
@@ -757,6 +757,7 @@ def make_waveform(
     bars_color: str | tuple[str, str] = ("#fbbf24", "#ea580c"),
     bar_count: int = 50,
     bar_width: float = 0.6,
+    animate_waveform: bool = False,
 ) -> str:
     """
     Generates a waveform video from an audio file. Useful for creating an easy to share audio visualization. The output should be passed into a `gr.Video` component.
@@ -768,6 +769,7 @@ def make_waveform(
         bars_color: Color of waveform bars. Can be a single color or a tuple of (start_color, end_color) of gradient
         bar_count: Number of bars in waveform
         bar_width: Width of bars in waveform. 1 represents full width, 0.5 represents half width, etc.
+        animate_waveform: Whether to animate the waveform
     Returns:
         A filepath to the output video in mp4 format.
     """
@@ -827,44 +829,82 @@ def make_waveform(
         plt.axis("off")
         plt.margins(x=0)
 
+        bar_alpha = fg_alpha if animate_waveform else 1.0
         barcollection = plt.bar(
             np.arange(0, bar_count),
             samples * 2,
             bottom=(-1 * samples),
             width=bar_width,
             color=color,
+            alpha=bar_alpha
         )
 
-        def animate(_):
-            for idx, b in enumerate(barcollection):
-                rand_height = np.random.uniform(0.8, 1.2)
-                b.set_height(samples[idx] * rand_height * 2)
-                b.set_y((-rand_height * samples)[idx])
-
-        frames = int(duration * 10)
-        anim = animation.FuncAnimation(
-            fig, animate, repeat=False, blit=False, frames=frames, interval=100
-        )
         tmp_img = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
 
         savefig_kwargs: dict[str, Any] = {"bbox_inches": "tight"}
         if bg_image is not None:
             savefig_kwargs["transparent"] = True
-            savefig_kwargs["facecolor"] = "none"
+            if animate_waveform:
+                savefig_kwargs["facecolor"] = "none"
         else:
             savefig_kwargs["facecolor"] = bg_color
-        anim.save(
-            tmp_img.name,
-            writer="pillow",
-            fps=10,
-            codec="png",
-            savefig_kwargs=savefig_kwargs,
-        )
+        
+        plt.savefig(tmp_img.name, **savefig_kwargs)
+
+        if not animate_waveform:
+            waveform_img = PIL.Image.open(tmp_img.name)
+            waveform_img = waveform_img.resize((1000, 200))
+
+            # Composite waveform with background image
+            if bg_image is not None:
+                waveform_array = np.array(waveform_img)
+                waveform_array[:, :, 3] = waveform_array[:, :, 3] * fg_alpha
+                waveform_img = PIL.Image.fromarray(waveform_array)
+
+                bg_img = PIL.Image.open(bg_image)
+                waveform_width, waveform_height = waveform_img.size
+                bg_width, bg_height = bg_img.size
+                if waveform_width != bg_width:
+                    bg_img = bg_img.resize(
+                        (waveform_width, 2 * int(bg_height * waveform_width / bg_width / 2))
+                    )
+                    bg_width, bg_height = bg_img.size
+                composite_height = max(bg_height, waveform_height)
+                composite = PIL.Image.new(
+                    "RGBA", (waveform_width, composite_height), "#FFFFFF"
+                )
+                composite.paste(bg_img, (0, composite_height - bg_height))
+                composite.paste(
+                    waveform_img, (0, composite_height - waveform_height), waveform_img
+                )
+                composite.save(tmp_img.name)
+                img_width, img_height = composite.size
+            else:
+                img_width, img_height = waveform_img.size
+                waveform_img.save(tmp_img.name)    
+        else:
+            def animate(_):
+                for idx, b in enumerate(barcollection):
+                    rand_height = np.random.uniform(0.8, 1.2)
+                    b.set_height(samples[idx] * rand_height * 2)
+                    b.set_y((-rand_height * samples)[idx])
+
+            frames = int(duration * 10)
+            anim = animation.FuncAnimation(
+                fig, animate, repeat=False, blit=False, frames=frames, interval=100
+            )
+            anim.save(
+                tmp_img.name,
+                writer="pillow",
+                fps=10,
+                codec="png",
+                savefig_kwargs=savefig_kwargs,
+            )
 
     # Convert waveform to video with ffmpeg
     output_mp4 = tempfile.NamedTemporaryFile(suffix=".mp4", delete=False)
 
-    if bg_image is not None:
+    if animate_waveform and bg_image is not None:
         ffmpeg_cmd = [
             ffmpeg,
             "-loop",
@@ -891,7 +931,7 @@ def make_waveform(
             "-y",
             output_mp4.name,
         ]
-    else:
+    elif animate_waveform and bg_image is None:
         ffmpeg_cmd = [
             ffmpeg,
             "-i",
@@ -909,6 +949,22 @@ def make_waveform(
             "-c:a",
             "aac",
             "-shortest",
+            "-y",
+            output_mp4.name,
+        ]
+    else:
+        ffmpeg_cmd = [
+            ffmpeg,
+            "-loop",
+            "1",
+            "-i",
+            tmp_img.name,
+            "-i",
+            audio_file,
+            "-vf",
+            f"color=c=#FFFFFF77:s={img_width}x{img_height}[bar];[0][bar]overlay=-w+(w/{duration})*t:H-h:shortest=1",
+            "-t",
+            str(duration),
             "-y",
             output_mp4.name,
         ]
