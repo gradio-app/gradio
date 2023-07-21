8e49ae71077a72dcee3994435bd763989bce69ef
Freddy Boulton <alfonsoboulton@gmail.com>
2023-07-20 23:01:05 -0400

    Update client/python/gradio_client/templates/discord_chat.py
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- client/python/gradio_client/templates/discord_chat.py
+++ client/python/gradio_client/templates/discord_chat.py
@@ -151,7 +151,7 @@ if not DISCORD_TOKEN:
     
     ## 4. Optionally make the bot public if you want anyone to be able to add it to their servers
     
-    ## 5. On the left hand side under 'Bot', enable 'Message Content Intent' under 'Priviledged Gateway Intents'
+    ## 5. Scroll down and enable 'Message Content Intent' under 'Priviledged Gateway Intents'
     
     ![](https://gradio-builds.s3.amazonaws.com/demo-files/discordbots/MessageContentIntent.png)
 
