e549bf10c7674e7def73aa24d09de240d3c8ac0d
fabiencro <fabien.cromieres@gmail.com>
2023-07-24 21:39:27 +0200

    Documentation: made correct example for the .then() event listener method (#5003)
    * Documentation: made correct example for the .then() event listener method
    
    * Fix notebook
    
    ---------
    
    Co-authored-by: Freddy Boulton <alfonsoboulton@gmail.com>

---
+++ demo/chatbot_consecutive/run.py
@@ -0,0 +1,26 @@
+import gradio as gr
+import random
+import time
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.Button("Clear")
+
+    def user(user_message, history):
+        return "", history + [[user_message, None]]
+
+    def bot(history):
+        bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
+        time.sleep(2)
+        history[-1][1] = bot_message
+        return history
+
+    msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
+        bot, chatbot, chatbot
+    )
+    clear.click(lambda: None, None, chatbot, queue=False)
+    
+demo.queue()
+if __name__ == "__main__":
+    demo.launch()
