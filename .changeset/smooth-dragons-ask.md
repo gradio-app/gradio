44ac8ad08d82ea12c503dde5c78f999eb0452de2
aliabid94 <aabid94@gmail.com>
2023-07-25 11:39:51 -0700

    Allow setting sketch color default (#4979)
    * changes
    
    * add changeset
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    ---------
    
    Co-authored-by: pngwn <hello@pngwn.io>
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

---
+++ .changeset/smooth-dragons-ask.md
@@ -0,0 +1,7 @@
+---
+"@gradio/app": minor
+"@gradio/image": minor
+"gradio": minor
+---
+
+feat:Allow setting sketch color default
--- js/app/src/components/Image/Image.svelte
+++ js/app/src/components/Image/Image.svelte
@@ -25,6 +25,8 @@
 	export let mirror_webcam: boolean;
 	export let shape: [number, number];
 	export let brush_radius: number;
+	export let brush_color: string;
+	export let mask_opacity: number;
 	export let selectable = false;
 	export let container = true;
 	export let scale: number | null = null;
@@ -78,11 +80,13 @@
 	{:else}
 		<Image
 			{brush_radius}
+			{brush_color}
 			{shape}
 			bind:value
 			{source}
 			{tool}
 			{selectable}
+			{mask_opacity}
 			on:edit
 			on:clear
 			on:stream
