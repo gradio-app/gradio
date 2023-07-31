3de080d91746674a7486c51c27d13879405fe6dd
Hannah <hannahblair@hotmail.co.uk>
2023-07-31 14:47:38 +0200

    add highlighted text stories

---
+++ js/app/src/components/HighlightedText/HighlightedText.stories.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+	import HighlightedText from "./HighlightedText.svelte";
+</script>
+
+<Meta title="Components/HighlightedText" component={HighlightedText} />
+
+<Template let:args>
+	<HighlightedText
+		{...args}
+		value={[
+			["zebras", "+"],
+			["dogs", "-"],
+			["elephants", "+"],
+		]}
+	/>
+</Template>
+
+<Story name="Highlighted Text" />
+<Story name="Highlighted Text with legend" args={{ show_legend: true }} />
+<Story name="Highlighted Text with label" args={{ label: "animals" }} />
+<Story
+	name="Highlighted Text with new line"
+	value={[["zebras", "+"], ["\n"], ["dogs", "-"], ["\n"], ["elephants", "+"]]}
+/>
+<Story
+	name="Highlighted Text with color map"
+	args={{ color_map: { "+": "green", "-": "red" } }}
+/>
