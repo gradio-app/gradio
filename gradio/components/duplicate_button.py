9642b00db3dcc2f48e06ca79bbc743931d611210
pngwn <hello@pngwn.io>
2023-08-07 14:59:34 +0100

    asd

--- gradio/components/duplicate_button.py
+++ gradio/components/duplicate_button.py
@@ -37,6 +37,18 @@ class DuplicateButton(Button):
         _activate: bool = True,
         **kwargs,
     ):
+        """
+        Parameters:
+            value: Default text for the button to display. If callable, the function will be called whenever the app loads to set the initial value of the component.
+            variant: 'primary' for main call-to-action, 'secondary' for a more subdued style, 'stop' for a stop button.
+            size: Size of the button. Can be "sm" or "lg".
+            visible: If False, component will be hidden.
+            interactive: If False, the Button will be in a disabled state.
+            elem_id: An optional string that is assigned as the id of this component in the HTML DOM. Can be used for targeting CSS styles.
+            elem_classes: An optional list of strings that are assigned as the classes of this component in the HTML DOM. Can be used for targeting CSS styles.
+            scale: relative width compared to adjacent Components in a Row. For example, if Component A has scale=2, and Component B has scale=1, A will be twice as wide as B. Should be an integer.
+            min_width: minimum pixel width, will wrap if not sufficient screen space to satisfy this value. If a certain scale value results in this Component being narrower than min_width, the min_width parameter will be respected first.
+        """
         super().__init__(
             value,
             variant=variant,
