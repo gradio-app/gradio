8c14bcc5595740de8c35ea9e01647acd9b633fd5
pngwn <hello@pngwn.io>
2023-08-10 20:32:14 +0100

    fix

--- js/_website/svelte.config.js
+++ js/_website/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-vercel";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import _version from "./src/lib/json/version.json" assert { type: "json" };
 
