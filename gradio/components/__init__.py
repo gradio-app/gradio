3d9bb9bd90128b49bb5ad5cf3c330014ba7bbdcd
Ali Abid <aabid94@gmail.com>
2023-08-15 16:53:04 -0700

    changes

--- gradio/components/__init__.py
+++ gradio/components/__init__.py
@@ -1,10 +1,6 @@
-from gradio.blocks import updateable
 from gradio.components.annotated_image import AnnotatedImage
-from gradio.blocks import updateable
 from gradio.components.audio import Audio
-from gradio.blocks import updateable
 from gradio.components.bar_plot import BarPlot
-from gradio.blocks import updateable
 from gradio.components.base import (
     Column,
     Component,
@@ -16,77 +12,41 @@ from gradio.components.base import (
     component,
     get_component_instance,
 )
-from gradio.blocks import updateable
 from gradio.components.button import Button
-from gradio.blocks import updateable
 from gradio.components.carousel import Carousel
-from gradio.blocks import updateable
 from gradio.components.chatbot import Chatbot
-from gradio.blocks import updateable
 from gradio.components.checkbox import Checkbox
-from gradio.blocks import updateable
 from gradio.components.checkboxgroup import CheckboxGroup
-from gradio.blocks import updateable
 from gradio.components.clear_button import ClearButton
-from gradio.blocks import updateable
 from gradio.components.code import Code
-from gradio.blocks import updateable
 from gradio.components.color_picker import ColorPicker
-from gradio.blocks import updateable
 from gradio.components.dataframe import Dataframe
-from gradio.blocks import updateable
 from gradio.components.dataset import Dataset
-from gradio.blocks import updateable
 from gradio.components.dropdown import Dropdown
-from gradio.blocks import updateable
 from gradio.components.duplicate_button import DuplicateButton
-from gradio.blocks import updateable
 from gradio.components.file import File
-from gradio.blocks import updateable
 from gradio.components.gallery import Gallery
-from gradio.blocks import updateable
 from gradio.components.highlighted_text import HighlightedText
-from gradio.blocks import updateable
 from gradio.components.html import HTML
-from gradio.blocks import updateable
 from gradio.components.image import Image
-from gradio.blocks import updateable
 from gradio.components.interpretation import Interpretation
-from gradio.blocks import updateable
 from gradio.components.json_component import JSON
-from gradio.blocks import updateable
 from gradio.components.label import Label
-from gradio.blocks import updateable
 from gradio.components.line_plot import LinePlot
-from gradio.blocks import updateable
 from gradio.components.login_button import LoginButton
-from gradio.blocks import updateable
 from gradio.components.logout_button import LogoutButton
-from gradio.blocks import updateable
 from gradio.components.markdown import Markdown
-from gradio.blocks import updateable
 from gradio.components.model3d import Model3D
-from gradio.blocks import updateable
 from gradio.components.number import Number
-from gradio.blocks import updateable
 from gradio.components.plot import Plot
-from gradio.blocks import updateable
 from gradio.components.radio import Radio
-from gradio.blocks import updateable
 from gradio.components.scatter_plot import ScatterPlot
-from gradio.blocks import updateable
 from gradio.components.slider import Slider
-from gradio.blocks import updateable
 from gradio.components.state import State, Variable
-from gradio.blocks import updateable
 from gradio.components.status_tracker import StatusTracker
-from gradio.blocks import updateable
 from gradio.components.textbox import Textbox
-from gradio.blocks import updateable
 from gradio.components.timeseries import Timeseries
-from gradio.blocks import updateable
 from gradio.components.upload_button import UploadButton
-from gradio.blocks import updateable
 from gradio.components.video import Video
 
 Text = Textbox
