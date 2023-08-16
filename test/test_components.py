18fe999950bc3917cd04227c71f7259f43793b60
Artem Kotov <breengles@gmail.com>
2023-08-16 07:40:09 +0000

    add simple test for #5229 similar to default one

--- test/test_components.py
+++ test/test_components.py
@@ -1331,6 +1331,11 @@ class TestVideo:
         assert isinstance(output1, str)
         output2 = video_input.preprocess(x_video)
         assert output1 == output2
+        
+        video_input = gr.Video(include_audio=False)
+        output1 = video_input.preprocess(x_video)
+        output2 = video_input.preprocess(x_video)
+        assert output1 == output2
 
         video_input = gr.Video(label="Upload Your Video")
         assert video_input.get_config() == {
