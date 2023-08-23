d78e3ad5ae0a92ac823df808a7b344f9e3d50ef6
Hannah <hannahblair@hotmail.co.uk>
2023-08-23 22:34:11 +0200

    test test

--- js/app/test/blocks_page_load.spec.ts
+++ js/app/test/blocks_page_load.spec.ts
@@ -9,3 +9,16 @@ test("renders the correct elements", async ({ page }) => {
 		"Welcome! This page has loaded for Frank"
 	);
 });
+
+test("renders the footer", async ({ page }) => {
+	const footer = page.locator("footer");
+
+	await expect(footer).toBeVisible();
+	await expect(footer).toHaveText("Use via API");
+	await expect(footer).toHaveText("Built with Gradio");
+});
+
+test("renders the text", async ({ page }) => {
+	await expect(await page.getByText("Built with Gradio")).toBeVisible();
+	await expect(await page.getByText("Use via API")).toBeVisible();
+});
