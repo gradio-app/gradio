adcfd6d2c2980840fc32ff5871c140f452881178
Ali Abid <aabid94@gmail.com>
2023-08-24 11:01:12 -0700

    changes

--- gradio/blocks.py
+++ gradio/blocks.py
@@ -434,12 +434,6 @@ class BlockFunction:
         return str(self)
 
 
-class class_or_instancemethod(classmethod):  # noqa: N801
-    def __get__(self, instance, type_):
-        descr_get = super().__get__ if instance is None else self.__func__.__get__
-        return descr_get(instance, type_)
-
-
 def postprocess_update_dict(block: Block, update_dict: dict, postprocess: bool = True):
     """
     Converts a dictionary of updates into a format that can be sent to the frontend.
@@ -1538,9 +1532,8 @@ Received outputs:
         self.progress_tracking = any(block_fn.tracks_progress for block_fn in self.fns)
         self.exited = True
 
-    @class_or_instancemethod
     def load(
-        self_or_cls,  # noqa: N805
+        self: Blocks | None = None,
         fn: Callable | None = None,
         inputs: list[Component] | None = None,
         outputs: list[Component] | None = None,
@@ -1565,9 +1558,7 @@ Received outputs:
         For reverse compatibility reasons, this is both a class method and an instance
         method, the two of which, confusingly, do two completely different things.
 
-
         Class method: loads a demo from a Hugging Face Spaces repo and creates it locally and returns a block instance. Warning: this method will be deprecated. Use the equivalent `gradio.load()` instead.
-
 
         Instance method: adds event that runs as soon as the demo loads in the browser. Example usage below.
         Parameters:
@@ -1597,7 +1588,7 @@ Received outputs:
                 demo.load(get_time, inputs=None, outputs=dt)
             demo.launch()
         """
-        if isinstance(self_or_cls, type):
+        if self is None:
             warn_deprecation(
                 "gr.Blocks.load() will be deprecated. Use gr.load() instead."
             )
@@ -1611,7 +1602,7 @@ Received outputs:
         else:
             from gradio.events import Dependency
 
-            dep, dep_index = self_or_cls.set_event_trigger(
+            dep, dep_index = self.set_event_trigger(
                 event_name="load",
                 fn=fn,
                 inputs=inputs,
@@ -1628,7 +1619,7 @@ Received outputs:
                 every=every,
                 no_target=True,
             )
-            return Dependency(self_or_cls, dep, dep_index)
+            return Dependency(self, dep, dep_index)
 
     def clear(self):
         """Resets the layout of the Blocks object."""
