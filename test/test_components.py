e65a5c0ae4c12740f3e099eab9bea378afff927f
Abubakar Abid <abubakar@huggingface.co>
2023-08-29 13:13:22 -0700

    fix test

--- test/test_components.py
+++ test/test_components.py
@@ -2136,7 +2136,7 @@ class TestModel3D:
         """
         component = gr.components.Model3D(None, label="Model")
         assert {
-            "clearColor": [0, 0, 0, 0],
+            "clear_color": [0, 0, 0, 0],
             "value": None,
             "label": "Model",
             "show_label": True,
