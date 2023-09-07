5212493919791fddfc3282de0cd8512079f2d756
pngwn <hello@pngwn.io>
2023-09-07 19:32:21 +0100

    fix demos

--- demo/hello_blocks/run.py
+++ demo/hello_blocks/run.py
@@ -1,23 +1,13 @@
-# import gradio as gr
-
-# def greet(name):
-#     return "Hello " + name + "!"
-
-# with gr.Blocks() as demo:
-#     name = gr.Textbox(label="Name")
-#     output = gr.Textbox(label="Output Box")
-#     greet_btn = gr.Button("Greet")
-#     greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
-
-# if __name__ == "__main__":
-#     demo.launch()
-
-
 import gradio as gr
 
+def greet(name):
+    return "Hello " + name + "!"
 
 with gr.Blocks() as demo:
-    gr.DataFrame(value=[[]], interactive=False)
+    name = gr.Textbox(label="Name")
+    output = gr.Textbox(label="Output Box")
+    greet_btn = gr.Button("Greet")
+    greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
 
 if __name__ == "__main__":
     demo.launch()
