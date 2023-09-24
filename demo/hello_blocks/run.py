3c53a99357e1c5ef025013c49edacb2860bbf8fd
Ali Abid <aabid94@gmail.com>
2023-09-23 22:56:09 -0700

    changes

--- demo/hello_blocks/run.py
+++ demo/hello_blocks/run.py
@@ -7,14 +7,7 @@ with gr.Blocks() as demo:
     name = gr.Textbox(label="Name")
     output = gr.Textbox(label="Output Box")
     greet_btn = gr.Button("Greet")
-
-    gr.on(
-        triggers=[name.submit, greet_btn.click],
-        fn=greet,
-        inputs=name,
-        outputs=output,
-        api_name="greet",
-    )
+    greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
 
 if __name__ == "__main__":
     demo.launch()
