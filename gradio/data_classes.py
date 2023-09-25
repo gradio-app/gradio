1731b2b33b64fa1e03559e99203d5971d163e853
freddyaboulton <alfonsoboulton@gmail.com>
2023-09-25 19:13:15 -0400

    Fix 3.8

--- gradio/data_classes.py
+++ gradio/data_classes.py
@@ -7,7 +7,7 @@ import secrets
 import shutil
 from abc import ABC, abstractmethod
 from enum import Enum, auto
-from typing import Any, Optional, Union
+from typing import Any, Optional, Union, List
 
 from gradio_client.utils import traverse
 from pydantic import BaseModel, RootModel, ValidationError
@@ -17,14 +17,14 @@ from typing_extensions import Literal
 class PredictBody(BaseModel):
     session_hash: Optional[str] = None
     event_id: Optional[str] = None
-    data: list[Any]
+    data: List[Any]
     event_data: Optional[Any] = None
     fn_index: Optional[int] = None
     batched: Optional[
         bool
     ] = False  # Whether the data is a batch of samples (i.e. called from the queue if batch=True) or a single sample (i.e. called from the UI)
     request: Optional[
-        Union[dict, list[dict]]
+        Union[dict, List[dict]]
     ] = None  # dictionary of request headers, query parameters, url, etc. (used to to pass in request for queuing)
 
 
@@ -60,7 +60,7 @@ class ProgressUnit(BaseModel):
 
 class Progress(BaseModel):
     msg: str = "progress"
-    progress_data: list[ProgressUnit] = []
+    progress_data: List[ProgressUnit] = []
 
 
 class LogMessage(BaseModel):
