cf163ea5004be5a517071b56fcd77d86e94b596a
Abubakar Abid <abubakar@huggingface.co>
2023-09-26 11:24:26 -0700

    setting share=True on Spaces or in wasm should raise warning instead of error

--- gradio/blocks.py
+++ gradio/blocks.py
@@ -2117,9 +2117,13 @@ Received outputs:
 
         if self.share:
             if self.space_id:
-                raise RuntimeError("Share is not supported when you are in Spaces")
+                warnings.warn("Share is not supported when you are in Spaces")
+                self.share = False
             if wasm_utils.IS_WASM:
-                raise RuntimeError("Share is not supported in the Wasm environment")
+                warnings.warn("Share is not supported in the Wasm environment")
+                self.share = False
+
+        if self.share:
             try:
                 if self.share_url is None:
                     self.share_url = networking.setup_tunnel(
