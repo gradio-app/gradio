d6a4c2bec6576f971ddeace7e81d4acd08ba5f6d
freddyaboulton <alfonsoboulton@gmail.com>
2023-09-28 15:39:52 -0400

    Fix templates

--- test/test_gradio_component_cli.py
+++ test/test_gradio_component_cli.py
@@ -3,6 +3,27 @@ import pytest
 from gradio.cli.commands.components.create import _create
 
 
+def test_template_layout_component(tmp_path):
+    _create("MyRow", tmp_path, template="Row", overwrite=True)
+    assert (
+        (tmp_path / "demo" / "app.py").read_text()
+        == """
+import gradio as gr
+from gradio_myrow import MyRow
+
+
+with gr.Blocks() as demo:
+    with MyRow():
+        gr.Textbox(value="foo", interactive=True)
+        gr.Number(value=10, interactive=True)
+ 
+
+demo.launch()
+"""
+    )
+    assert (tmp_path / "backend" / "gradio_myrow" / "myrow.py").exists()
+
+
 def test_raise_error_component_template_does_not_exist(tmp_path):
     with pytest.raises(
         ValueError,
