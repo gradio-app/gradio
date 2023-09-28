4472f1e67cd20581836e8260e528a8b9116b9e59
freddyaboulton <alfonsoboulton@gmail.com>
2023-09-28 18:50:35 -0400

    Add test

--- test/test_gradio_component_cli.py
+++ test/test_gradio_component_cli.py
@@ -71,6 +71,7 @@ def test_do_not_replace_class_name_in_import_statement(tmp_path):
 
 
 def test_raises_if_directory_exists(tmp_path):
-    with pytest.raises(ValueError,
-                       match=f"The directory {tmp_path.resolve()} already exists."):
+    with pytest.raises(
+        ValueError, match=f"The directory {tmp_path.resolve()} already exists."
+    ):
         _create("MyComponent", tmp_path)
