438d08e39e396c74b16533afc61a01502c593b02
Freddy Boulton <alfonsoboulton@gmail.com>
2023-09-28 15:37:45 -0400

    Fix component regex (#5739)
    * Add code
    
    * Fix component regex
    
    * add changeset
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- test/test_gradio_component_cli.py
+++ test/test_gradio_component_cli.py
@@ -11,3 +11,11 @@ def test_raise_error_component_template_does_not_exist(tmp_path):
         _create(
             "MyComponent", tmp_path, template="NonExistentComponent", overwrite=True
         )
+
+
+def test_do_not_replace_class_name_in_import_statement(tmp_path):
+    _create("MyImage", template="Image", directory=tmp_path, overwrite=True)
+    code = (tmp_path / "backend" / "gradio_myimage" / "myimage.py").read_text()
+    assert "from PIL import Image as _Image" in code
+    assert "class MyImage" in code
+    assert "_Image.Image" in code
