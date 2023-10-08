a93f6a945afcca28cd465674394e696aed040fc6
Kit PANG <pangkit888@gmail.com>
2023-10-09 01:07:57 +0800

    Fix error when scrolling dropdown with scrollbar

--- js/dropdown/shared/Dropdown.svelte
+++ js/dropdown/shared/Dropdown.svelte
@@ -117,6 +117,10 @@
 
 	function handle_option_selected(e: any): void {
 		selected_index = parseInt(e.detail.target.dataset.index);
+		if (isNaN(selected_index)) {
+			selected_index = null;
+			return;
+		}
 		show_options = false;
 		active_index = null;
 		filter_input.blur();
