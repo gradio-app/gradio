01e176193e59b4ae6afd38afc2cc8769c11386db
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-10-09 03:09:06 +0900

    Add a comment

--- js/wasm/src/webworker/index.ts
+++ js/wasm/src/webworker/index.ts
@@ -320,7 +320,7 @@ self.onmessage = async (event: MessageEvent<InMessage>): Promise<void> => {
 		// so it must be converted to a plain object before sending it to the main thread.
 		// Otherwise, the following error will be thrown:
 		// `Uncaught (in promise) DOMException: Failed to execute 'postMessage' on 'MessagePort': #<Object> could not be cloned.`
-		const cloneableError = JSON.parse(JSON.stringify(error));
+		const cloneableError = JSON.parse(JSON.stringify(error)); // Ref: https://stackoverflow.com/a/42376465/13103190
 		const replyMessage: ReplyMessageError = {
 			type: "reply:error",
 			error: cloneableError
