3618238960d54df65c34895f4eb69d08acc3f9b6
Dawood Khan <dawoodkhan82@gmail.com>
2023-10-16 14:36:22 -0400

    Fix dataframe `line_breaks` (#5930)
    * fix
    
    * format
    
    * add changeset
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- js/dataframe/shared/Table.svelte
+++ js/dataframe/shared/Table.svelte
@@ -702,6 +702,7 @@
 								<EditableCell
 									{value}
 									{latex_delimiters}
+									{line_breaks}
 									datatype={Array.isArray(datatype) ? datatype[j] : datatype}
 									edit={false}
 									el={null}
@@ -743,6 +744,7 @@
 									bind:value={_headers[i].value}
 									bind:el={els[id].input}
 									{latex_delimiters}
+									{line_breaks}
 									edit={header_edit === i}
 									on:keydown={end_header_edit}
 									on:dblclick={() => edit_header(i)}
@@ -791,6 +793,7 @@
 									bind:el={els[id].input}
 									display_value={display_value?.[index]?.[j]}
 									{latex_delimiters}
+									{line_breaks}
 									{editable}
 									edit={dequal(editing, [index, j])}
 									datatype={Array.isArray(datatype) ? datatype[j] : datatype}
