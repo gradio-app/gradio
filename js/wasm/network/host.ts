edbff5a7366a9671ef42415636d88a5dbdee9781
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-10-18 15:16:43 +0900

    Fix `is_self_host()`

--- js/wasm/network/host.ts
+++ js/wasm/network/host.ts
@@ -3,6 +3,6 @@ export function is_self_host(url: URL): boolean {
 		url.host === window.location.host ||
 		url.host === "localhost:7860" ||
 		url.host === "127.0.0.1:7860" || // Ref: https://github.com/gradio-app/gradio/blob/v3.32.0/js/app/src/Index.svelte#L194
-		url.origin === "http://lite.local" // A special host name set when the endpoint is a local file (`file:/*`). Ref: `determine_protocol()` in `client/js/src/utils.ts`
+		url.host === "lite.local" // A special hostname set when the endpoint is a local file (`file:/*`). See `determine_protocol()` in `client/js/src/utils.ts`
 	);
 }
