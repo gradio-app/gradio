184834d02d448bff387eeb3aef64d9517962f146
Freddy Boulton <alfonsoboulton@gmail.com>
2023-10-20 15:21:30 -0400

    Add a cli command to list available templates (#6018)
    * Add code
    
    * dataset
    
    * Add test
    
    * add changeset
    
    * add changeset
    
    * add changeset
    
    * add changeset
    
    * Exclude FormComponent
    
    * Paginate the output
    
    * No colors but emojis:
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- gradio/cli/commands/components/app.py
+++ gradio/cli/commands/components/app.py
@@ -3,6 +3,7 @@ from typer import Typer
 from .build import _build
 from .create import _create
 from .dev import _dev
+from .show import _show
 
 app = Typer(help="Create and publish a new Gradio component")
 
@@ -12,3 +13,4 @@ app.command(
     help="Build the component for distribution. Must be called from the component directory.",
 )(_build)
 app.command("dev", help="Launch the custom component demo in development mode.")(_dev)
+app.command("show", help="Show the list of available templates")(_show)
