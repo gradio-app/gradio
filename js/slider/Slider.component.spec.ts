8d29516f9b5ad793da578b0b8a36c0d4f1b26f64
pngwn <hello@pngwn.io>
2023-10-22 14:25:38 +0100

    fix playwright component tests

--- js/slider/Slider.component.spec.ts
+++ js/slider/Slider.component.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/experimental-ct-svelte";
 import type { Page, Locator } from "@playwright/test";
-import Slider from "./interactive";
+import Slider from "./Index.svelte";
 import { spy } from "tinyspy";
 
 import type { LoadingStatus } from "@gradio/statustracker";
@@ -58,7 +58,10 @@ test("Slider Default Value And Label rendered", async ({ mount }) => {
 			show_label: true,
 			step: 1,
 			mode: "dynamic",
-			loading_status: loading_status
+			loading_status: loading_status,
+			gradio: {
+				dispatch() {}
+			}
 		}
 	});
 	await expect(component).toContainText("My Slider");
