9053c95a10de12aef572018ee37c71106d2da675
Freddy Boulton <alfonsoboulton@gmail.com>
2023-10-23 12:54:56 -0400

    Simplify File Component (#6044)
    * Add code
    
    * Add code
    
    * Add code
    
    * Reuse code UploadButton
    
    * Use two files for UploadButton
    
    * Address feedback
    
    * Lint
    
    * add changeset
    
    * Update js/uploadbutton/shared/UploadButton.svelte
    
    Co-authored-by: pngwn <hello@pngwn.io>
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>
    Co-authored-by: pngwn <hello@pngwn.io>

--- js/file/Index.svelte
+++ js/file/Index.svelte
@@ -6,15 +6,11 @@
 
 <script lang="ts">
 	import type { Gradio, SelectData } from "@gradio/utils";
-	import { getContext } from "svelte";
 	import File from "./shared/File.svelte";
 	import FileUpload from "./shared/FileUpload.svelte";
-	import { blobToBase64 } from "@gradio/upload";
 	import type { FileData } from "@gradio/upload";
 	import { normalise_file } from "@gradio/upload";
 	import { Block, UploadText } from "@gradio/atoms";
-
-	import { upload_files as default_upload_files } from "@gradio/client";
 
 	import { StatusTracker } from "@gradio/statustracker";
 	import type { LoadingStatus } from "@gradio/statustracker";
@@ -23,7 +19,6 @@
 	export let elem_classes: string[] = [];
 	export let visible = true;
 	export let value: null | FileData | FileData[];
-	let old_value: null | FileData | FileData[];
 
 	export let mode: "static" | "interactive";
 	export let root: string;
@@ -46,68 +41,16 @@
 	}>;
 	export let file_count: string;
 	export let file_types: string[] = ["file"];
-
-	const upload_files =
-		getContext<typeof default_upload_files>("upload_files") ??
-		default_upload_files;
-
 	$: _value = normalise_file(value, root, root_url);
+
+	let old_value = _value;
+	$: if (JSON.stringify(old_value) !== JSON.stringify(_value)) {
+		gradio.dispatch("change");
+		old_value = _value;
+	}
 
 	let dragging = false;
 	let pending_upload = false;
-
-	$: {
-		if (JSON.stringify(_value) !== JSON.stringify(old_value)) {
-			old_value = _value;
-			if (_value === null) {
-				gradio.dispatch("change");
-				pending_upload = false;
-			} else if (
-				!(Array.isArray(_value) ? _value : [_value]).every(
-					(file_data) => file_data.blob
-				)
-			) {
-				pending_upload = false;
-				gradio.dispatch("change");
-			} else if (mode === "interactive") {
-				let files = (Array.isArray(_value) ? _value : [_value]).map(
-					(file_data) => file_data.blob!
-				);
-				let upload_value = _value;
-				pending_upload = true;
-				upload_files(root, files).then((response) => {
-					if (upload_value !== _value) {
-						// value has changed since upload started
-						return;
-					}
-
-					pending_upload = false;
-					if (response.error) {
-						(Array.isArray(_value) ? _value : [_value]).forEach(
-							async (file_data, i) => {
-								file_data.data = await blobToBase64(file_data.blob!);
-								file_data.blob = undefined;
-							}
-						);
-					} else {
-						(Array.isArray(_value) ? _value : [_value]).forEach(
-							(file_data, i) => {
-								if (response.files) {
-									file_data.orig_name = file_data.name;
-									file_data.name = response.files[i];
-									file_data.is_file = true;
-									file_data.blob = undefined;
-								}
-							}
-						);
-						old_value = _value = normalise_file(value, root, root_url);
-					}
-					gradio.dispatch("change");
-					gradio.dispatch("upload");
-				});
-			}
-		}
-	}
 </script>
 
 <Block
@@ -150,13 +93,16 @@
 			{selectable}
 			{root}
 			{height}
-			on:change={({ detail }) => (value = detail)}
+			on:change={({ detail }) => {
+				value = detail;
+			}}
 			on:drag={({ detail }) => (dragging = detail)}
 			on:clear={() => gradio.dispatch("clear")}
 			on:select={({ detail }) => gradio.dispatch("select", detail)}
+			on:upload={() => gradio.dispatch("upload")}
 			i18n={gradio.i18n}
 		>
-		<UploadText i18n={gradio.i18n} type="file" />
+			<UploadText i18n={gradio.i18n} type="file" />
 		</FileUpload>
 	{/if}
 </Block>
