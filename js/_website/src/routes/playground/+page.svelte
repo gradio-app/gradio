5524e590577769b0444a5332b8d444aafb0c5c12
pngwn <hello@pngwn.io>
2023-10-25 10:25:12 +0100

    playground proposal (#6040)
    * tweaks
    
    * add changeset
    
    * fix demo overflowing
    
    * make clear divider
    
    * fix demos tab minimizing increasing width
    
    * display width and breakpoints
    
    * mobile responsive
    
    * add delay and skip queue
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>
    Co-authored-by: Ali Abdalla <ali.si3luwa@gmail.com>

--- js/_website/src/routes/playground/+page.svelte
+++ js/_website/src/routes/playground/+page.svelte
@@ -20,7 +20,6 @@
 	let show_nav = true;
 
 	$: show_nav;
-
 </script>
 
 <MetaTags
@@ -30,50 +29,68 @@
 	description="Play Around with Gradio Demos"
 />
 
-<main class="container mx-auto px-4 gap-4">
-	<h2 class="text-4xl font-light mb-2 pt-2 text-orange-500 group">Playground</h2> 
-	<p class="mt-4 mb-8 text-lg text-gray-600">
-			All the demos on this page are interactive - meaning you can change the code and the embedded demo will update automatically. 
-					Use this as a space to explore and play around with Gradio. This is made possible thanks to the 
-					<a
-					class="link text-black"
-					target="_blank"
-					href="https://gradio.app/guides/gradio-lite">
-					Gradio Lite 
-					</a>
-					package.
-	</p>
+<main class="px-6 flex flex-col justify-between">
+	<div class="container mx-auto px-4 gap-4">
+		<h2
+			class="text-4xl font-light mb-2 pt-2 text-orange-500 group container mx-auto gap-4"
+		>
+			Playground
+		</h2>
+		<p class="mt-4 mb-8 text-lg text-gray-600">
+			All the demos on this page are interactive - meaning you can change the
+			code and the embedded demo will update automatically. Use this as a space
+			to explore and play around with Gradio. This is made possible thanks to
+			the
+			<a
+				class="link text-black"
+				target="_blank"
+				href="https://gradio.app/guides/gradio-lite"
+			>
+				Gradio Lite
+			</a>
+			package.
+		</p>
 
-	<p class="mt-4 mb-8 text-lg text-gray-600 md:hidden">
-		Playground renders best on desktop.
-	</p>
-		
-	<div class="flex w-full border border-gray-200 shadow-xl rounded-xl p-4 mb-3">
-		<div class:w-10={!show_nav} class:xl:w-10={!show_nav} class:w-[33%]={show_nav} class:xl:w-[15%]={show_nav} class="mr-4 overflow-y-scroll mb-0 p-0 pb-4 text-md block rounded-t-xl bg-gradient-to-r from-white to-gray-50 overflow-x-clip" style="height: 70vh; word-break: normal; overflow-wrap: break-word; white-space:nowrap">
-			<button
-						on:click={() => (show_nav = !show_nav)}
-						class="float-right p-1 px-2 text-gray-600"
-						>{#if show_nav}&larr;{:else}&rarr;{/if}</button
-					>
+		<p class="mt-4 mb-8 text-lg text-gray-600 md:hidden">
+			Playground renders best on desktop.
+		</p>
+	</div>
+
+	<div
+		class="w-full border border-gray-200 shadow-xl rounded-xl mb-3 h-full relative"
+	>
+		<div
+			class="w-[200px] h-full rounded-tr-none rounded-bl-xl overflow-y-scroll mb-0 p-0 pb-4 text-md block rounded-t-xl bg-gradient-to-r from-white to-gray-50 overflow-x-clip"
+			style="word-break: normal; overflow-wrap: break-word; white-space:nowrap; width: {show_nav
+				? 200
+				: 37}px;"
+		>
+			<div class="flex justify-between align-middle h-8 border-b px-2">
+				{#if show_nav}
+					<h3 class="pl-2 pt-1">Demos</h3>
+				{/if}
+				<button
+					on:click={() => (show_nav = !show_nav)}
+					class="float-right text-gray-600 pl-1"
+					>{#if show_nav}&larr;{:else}&rarr;{/if}</button
+				>
+			</div>
 			{#if show_nav}
-			{#each data.demos_by_category as { category, demos } (category)}
-				<p class="px-4 my-2">{category}</p>
-				{#each demos as demo, i}	
-					<button
-						on:click={() => (current_selection = demo.name)}
-						class:current-playground-demo={current_selection == demo.name}
-						class="thin-link font-light px-4 block"
-						>{demo.name}</button
-					>
+				{#each data.demos_by_category as { category, demos } (category)}
+					<p class="px-4 my-2">{category}</p>
+					{#each demos as demo, i}
+						<button
+							on:click={() => (current_selection = demo.name)}
+							class:current-playground-demo={current_selection == demo.name}
+							class="thin-link font-light px-4 block">{demo.name}</button
+						>
+					{/each}
 				{/each}
-			{/each}
 			{/if}
 		</div>
 
-			<DemosLite
-				demos={all_demos}
-				current_selection={current_selection}
-			/>
+		<DemosLite demos={all_demos} {current_selection} {show_nav} />
+	</div>
 </main>
 
 <style>
