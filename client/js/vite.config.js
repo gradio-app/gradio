af06569e06f614873117f66fe1cc3880511345dd
pngwn <hello@pngwn.io>
2023-10-26 13:33:53 +0100

    fix client build

--- client/js/vite.config.js
+++ client/js/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { fileURLToPath } from "url";
+import path from "path";
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
 	build: {
@@ -14,7 +17,18 @@ export default defineConfig({
 			}
 		}
 	},
-	plugins: [svelte()],
+	plugins: [
+		svelte(),
+		{
+			name: "resolve-gradio-client",
+			enforce: "pre",
+			resolveId(id) {
+				if (id === "@gradio/client") {
+					return path.join(__dirname, "src", "index.ts");
+				}
+			}
+		}
+	],
 
 	ssr: {
 		target: "node",
