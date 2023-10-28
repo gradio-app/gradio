3c8bffb366791b4686ea80254572f22949123dc3
Abubakar Abid <abubakar@huggingface.co>
2023-10-27 23:06:11 -0700

    reduce'

--- gradio/blocks_events.py
+++ gradio/blocks_events.py
@@ -8,14 +8,12 @@ BLOCKS_EVENTS: list[EventListener | str] = [Events.load]
 
 class BlocksMeta(type):
     def __new__(cls, name, bases, attrs):
-        new_events = []
         for event in BLOCKS_EVENTS:
             trigger = (
                 event
                 if isinstance(event, EventListener)
                 else EventListener(event_name=event)
             ).copy()
-            new_events.append(trigger)
             trigger.set_doc(component=name)
             attrs[event] = trigger.listener
         component_class = super().__new__(cls, name, bases, attrs)
