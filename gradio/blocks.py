198d7fba875b1899f417dd29bde9edd4f3d5b5ef
Abubakar Abid <abubakar@huggingface.co>
2023-10-29 11:18:44 -0700

    Small change to make `api_open=False` by default (#6138)
    * set api_open to False by default
    
    * add changeset
    
    * tests
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- gradio/blocks.py
+++ gradio/blocks.py
@@ -544,7 +544,7 @@ class Blocks(BlockContext, BlocksEvents, metaclass=BlocksMeta):
         self.share_url = None
         self.width = None
         self.height = None
-        self.api_open = True
+        self.api_open = False
 
         self.space_id = utils.get_space()
         self.favicon_path = None
@@ -1598,7 +1598,7 @@ Received outputs:
         self,
         concurrency_count: int = 1,
         status_update_rate: float | Literal["auto"] = "auto",
-        api_open: bool = True,
+        api_open: bool = False,
         max_size: int | None = None,
     ):
         """
