10ba73f33ba886dcff6f547ea99ae349bb1c48fa
freddyaboulton <alfonsoboulton@gmail.com>
2023-10-29 20:59:14 -0400

    Chatbot

--- js/chatbot/Index.svelte
+++ js/chatbot/Index.svelte
@@ -1,3 +1,7 @@
+<script context="module" lang="ts">
+	export { default as BaseButton } from "./shared/ChatBot.svelte";
+</script>
+
 <script lang="ts">
 	import type { Gradio, SelectData, LikeData } from "@gradio/utils";
 
