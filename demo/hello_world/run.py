d35a890d5f51d862cd41d2efe856f33aa37d92e6
Ali Abid <aabid94@gmail.com>
2023-10-29 19:43:12 -0700

    changes

--- demo/hello_world/run.py
+++ demo/hello_world/run.py
@@ -3,7 +3,7 @@ import gradio as gr
 def greet(name):
     return "Hello " + name + "!"
 
-demo = gr.Interface(fn=greet, inputs=["number"], outputs="text")
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
     
 if __name__ == "__main__":
     demo.launch(show_api=False)   
