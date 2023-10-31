b0111a961883838898f39ff74f7757aeb4a3aa24
Abubakar Abid <abubakar@huggingface.co>
2023-10-31 04:28:39 -0700

    Remove gr.mix (#6184)
    * remove mix
    
    * add changeset
    
    * fix script
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- gradio/__init__.py
+++ gradio/__init__.py
@@ -79,7 +79,6 @@ from gradio.helpers import create_examples as Examples  # noqa: N812
 from gradio.interface import Interface, TabbedInterface, close_all
 from gradio.ipython_ext import load_ipython_extension
 from gradio.layouts import Accordion, Column, Group, Row, Tab, TabItem, Tabs
-from gradio.mix import Parallel, Series
 from gradio.oauth import OAuthProfile
 from gradio.routes import Request, mount_gradio_app
 from gradio.templates import (
