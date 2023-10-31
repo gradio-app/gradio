438fa8f39a48edc9ce3d6cbb32b1e9753b1cca6d
Dawood <dawoodkhan82@gmail.com>
2023-10-30 22:39:45 -0400

    fixes

--- gradio/events.py
+++ gradio/events.py
@@ -360,7 +360,7 @@ def on(
         postprocess: If False, will not run postprocessing of component data before returning 'fn' output to the browser.
         cancels: A list of other events to cancel when this listener is triggered. For example, setting cancels=[click_event] will cancel the click_event, where click_event is the return value of another components .click method. Functions that have not yet run (or generators that are iterating) will be cancelled, but functions that are currently running will be allowed to finish.
         every: Run this event 'every' number of seconds while the client connection is open. Interpreted in seconds. Queue must be enabled.
-        js: Optional frontend js method to run before running 'fn'. Input arguments for js method are values of 'inputs' and 'outputs', return should be a list of values for output components.
+        js: Optional frontend js method to run before running 'fn'. Input arguments for js method are values of 'inputs', return should be a list of values for output components.
     """
     from gradio.components.base import Component
 
