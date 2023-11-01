27b4ebeb81074411011a69118495544ac7cd9d3e
pngwn <hello@pngwn.io>
2023-11-01 19:37:16 +0000

    fix checkboxgroup dispatches

--- demo/hello_blocks/run.py
+++ demo/hello_blocks/run.py
@@ -1,38 +1,15 @@
-# import gradio as gr
-
-
-# def greet(name):
-#     return "Hello " + name + "!"
-
-
-# with gr.Blocks() as demo:
-#     name = gr.Textbox(label="Name")
-#     output = gr.Textbox(label="Output Box")
-#     greet_btn = gr.Button("Greet")
-#     greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
-
-# if __name__ == "__main__":
-#     demo.launch()
-
 import gradio as gr
 
-CHOICES = ["(NONE)", "a", "b", "c"]
 
-
-def fn(choices):
-    print(choices)
-    if "(NONE)" in choices:
-        print("NONE")
-        return []
-    else:
-        return choices
+def greet(name):
+    return "Hello " + name + "!"
 
 
 with gr.Blocks() as demo:
-    checkbox = gr.CheckboxGroup(
-        choices=CHOICES, value=["a", "b", "c"], interactive=True
-    )
-    checkbox.change(fn=fn, inputs=checkbox, outputs=checkbox)
+    name = gr.Textbox(label="Name")
+    output = gr.Textbox(label="Output Box")
+    greet_btn = gr.Button("Greet")
+    greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
 
 if __name__ == "__main__":
-    demo.queue().launch()
+    demo.launch()
