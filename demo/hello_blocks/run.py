f67633a45c596bca4c3c7270647f218199d1691a
pngwn <hello@pngwn.io>
2023-11-01 15:55:12 +0000

    asd

--- demo/hello_blocks/run.py
+++ demo/hello_blocks/run.py
@@ -1,15 +1,35 @@
+# import gradio as gr
+
+
+# def greet(name):
+#     return "Hello " + name + "!"
+
+
+# with gr.Blocks() as demo:
+#     name = gr.Textbox(label="Name")
+#     output = gr.Textbox(label="Output Box")
+#     greet_btn = gr.Button("Greet")
+#     greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
+
+# if __name__ == "__main__":
+#     demo.launch()
+
 import gradio as gr
 
+CHOICES = ["(NONE)", "a", "b", "c"]
 
-def greet(name):
-    return "Hello " + name + "!"
+
+def fn(choices):
+    print(choices)
+    if "(NONE)" in choices:
+        return []
+    else:
+        return choices
 
 
 with gr.Blocks() as demo:
-    name = gr.Textbox(label="Name")
-    output = gr.Textbox(label="Output Box")
-    greet_btn = gr.Button("Greet")
-    greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
+    checkbox = gr.CheckboxGroup(choices=CHOICES, value=["a", "b", "c"])
+    checkbox.change(fn=fn, inputs=checkbox, outputs=checkbox)
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.queue().launch()
