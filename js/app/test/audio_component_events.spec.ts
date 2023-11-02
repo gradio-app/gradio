9598478d3d9310097cc3d5d12ead1f8067baf09f
freddyaboulton <alfonsoboulton@gmail.com>
2023-11-02 14:53:23 -0400

    Add code

--- js/app/test/audio_component_events.spec.ts
+++ js/app/test/audio_component_events.spec.ts
@@ -1,32 +1,32 @@
 import { test, expect, drag_and_drop_file } from "@gradio/tootils";
 
-// test("Audio click-to-upload uploads audio successfuly.", async ({ page }) => {
-// 	await page
-// 		.getByRole("button", { name: "Drop Audio Here - or - Click to Upload" })
-// 		.click();
-// 	const uploader = await page.locator("input[type=file]");
-// 	await Promise.all([
-// 		uploader.setInputFiles(["../../test/test_files/audio_sample.wav"]),
-// 		page.waitForResponse("**/upload")
-// 	]);
+test("Audio click-to-upload uploads audio successfuly.", async ({ page }) => {
+	await page
+		.getByRole("button", { name: "Drop Audio Here - or - Click to Upload" })
+		.click();
+	const uploader = await page.locator("input[type=file]");
+	await Promise.all([
+		uploader.setInputFiles(["../../test/test_files/audio_sample.wav"]),
+		page.waitForResponse("**/upload")
+	]);
 
-// 	await expect(page.getByLabel("# Change Events")).toHaveValue("1");
-// 	await expect(page.getByLabel("# Upload Events")).toHaveValue("1");
+	await expect(page.getByLabel("# Change Events")).toHaveValue("1");
+	await expect(page.getByLabel("# Upload Events")).toHaveValue("1");
 
-// 	await page.getByLabel("Clear").click();
-// 	await expect(page.getByLabel("# Change Events")).toHaveValue("2");
-// 	await page
-// 		.getByRole("button", { name: "Drop Audio Here - or - Click to Upload" })
-// 		.click();
+	await page.getByLabel("Clear").click();
+	await expect(page.getByLabel("# Change Events")).toHaveValue("2");
+	await page
+		.getByRole("button", { name: "Drop Audio Here - or - Click to Upload" })
+		.click();
 
-// 	await Promise.all([
-// 		uploader.setInputFiles(["../../test/test_files/audio_sample.wav"]),
-// 		page.waitForResponse("**/upload")
-// 	]);
+	await Promise.all([
+		uploader.setInputFiles(["../../test/test_files/audio_sample.wav"]),
+		page.waitForResponse("**/upload")
+	]);
 
-// 	await expect(page.getByLabel("# Change Events")).toHaveValue("3");
-// 	await expect(page.getByLabel("# Upload Events")).toHaveValue("2");
-// });
+	await expect(page.getByLabel("# Change Events")).toHaveValue("3");
+	await expect(page.getByLabel("# Upload Events")).toHaveValue("2");
+});
 
 test("Audio drag-and-drop uploads a file to the server correctly.", async ({
 	page
