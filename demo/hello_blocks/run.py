1577253b89bbbd7e40d323f02d1b760605def0ee
pngwn <hello@pngwn.io>
2023-11-02 14:18:35 +0000

    reinstate demo

--- demo/hello_blocks/run.py
+++ demo/hello_blocks/run.py
@@ -1,28 +1,15 @@
-# import gradio as gr
-
-
-# def greet(name):
-#     return "Hello " + name + "!"
-
-
-# with gr.Blocks() as demo:
-#     name = gr.Textbox(label="Name")
-#     output = gr.Textbox(label="Output Box")
-#     greet_btn = gr.Button("Greet")
-#     greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
-
-# if __name__ == "__main__":
-#     demo.launch()
-
-
 import gradio as gr
 
+
+def greet(name):
+    return "Hello " + name + "!"
+
+
 with gr.Blocks() as demo:
-    with gr.Row():
-        with gr.Column():
-            Drug = gr.Radio(["Yes", "No"])
-            Family = gr.Radio(["Yes", "No"])
+    name = gr.Textbox(label="Name")
+    output = gr.Textbox(label="Output Box")
+    greet_btn = gr.Button("Greet")
+    greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
 
 if __name__ == "__main__":
-    demo.queue()
     demo.launch()
