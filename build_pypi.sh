5e80cb79fa568e0ac9e7b301d736bdd06e859a83
pngwn <hello@pngwn.io>
2023-11-02 19:48:32 +0000

    remove final references to cdn

--- build_pypi.sh
+++ build_pypi.sh
@@ -11,8 +11,7 @@ GRADIO_VERSION=$new_version
 rm -rf gradio/templates/frontend
 pnpm i --frozen-lockfile --ignore-scripts
 GRADIO_VERSION=$new_version pnpm build
-aws s3 cp gradio/templates/cdn "s3://gradio/${new_version}/" --recursive --region us-west-2
-cp gradio/templates/cdn/index.html gradio/templates/frontend/share.html
+aws s3 cp gradio/templates/frontend "s3://gradio/${new_version}/" --recursive --region us-west-2
 
 rm -rf dist/*
 rm -rf build/*
