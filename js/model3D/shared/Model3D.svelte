40a7e7983cca560d56ef1099238776d68fa75b98
Hannah <hannahblair@users.noreply.github.com>
2023-11-02 17:12:54 +0000

    formatting

--- js/model3D/shared/Model3D.svelte
+++ js/model3D/shared/Model3D.svelte
@@ -20,7 +20,7 @@
 	export let camera_position: [number | null, number | null, number | null] = [
 		null,
 		null,
-		null,
+		null
 	];
 
 	$: {
@@ -46,7 +46,7 @@
 	});
 
 	$: ({ path } = value || {
-		path: undefined,
+		path: undefined
 	});
 
 	$: canvas && mounted && path && dispose();
