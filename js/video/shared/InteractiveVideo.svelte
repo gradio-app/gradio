42ebaf0805b7442e73b77d73b218c8403f23d1b6
Hannah <hannahblair@users.noreply.github.com>
2023-11-02 14:54:40 +0000

    prevent resetting source when clearing value

--- js/video/shared/InteractiveVideo.svelte
+++ js/video/shared/InteractiveVideo.svelte
@@ -48,7 +48,6 @@
 
 	function handle_clear(): void {
 		value = null;
-		active_source = sources[0];
 		dispatch("change", null);
 		dispatch("clear");
 	}
@@ -125,7 +124,7 @@
 		>
 		<button
 			class="icon"
-			aria-label="Record audio"
+			aria-label="Record video"
 			on:click={() => {
 				handle_clear();
 				active_source = "webcam";
