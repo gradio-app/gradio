f77e3f5c741bd8ff74ecdcb1caefba1b174b4549
freddyaboulton <alfonsoboulton@gmail.com>
2023-11-03 18:11:36 -0400

    Use urls from our s3 bucket

--- demo/gallery_component_events/run.py
+++ demo/gallery_component_events/run.py
@@ -2,11 +2,9 @@ import gradio as gr
 
 with gr.Blocks() as demo:
     cheetahs = [
-        "https://upload.wikimedia.org/wikipedia/commons/0/09/TheCheethcat.jpg",
-        "https://nationalzoo.si.edu/sites/default/files/animals/cheetah-003.jpg",
-        "https://img.etimg.com/thumb/msid-50159822,width-650,imgsize-129520,,resizemode-4,quality-100/.jpg",
-        "https://nationalzoo.si.edu/sites/default/files/animals/cheetah-002.jpg",
-        "https://images.theconversation.com/files/375893/original/file-20201218-13-a8h8uq.jpg?ixlib=rb-1.1.0&rect=16%2C407%2C5515%2C2924&q=45&auto=format&w=496&fit=clip",
+        "https://gradio-builds.s3.amazonaws.com/assets/cheetah-003.jpg",
+        "https://gradio-builds.s3.amazonaws.com/assets/lite-logo.png",
+        "https://gradio-builds.s3.amazonaws.com/assets/TheCheethcat.jpg",
     ]
     with gr.Row():
         with gr.Column():
@@ -15,10 +13,13 @@ with gr.Blocks() as demo:
             gallery = gr.Gallery()
         with gr.Column():
             select_output = gr.Textbox(label="Select Data")
-        btn.click(lambda: cheetahs, None, [gallery])
-        def select(select_data: gr.SelectData):
-            return select_data.value['image']['url']
-        gallery.select(select, None, select_output)
+    btn.click(lambda: cheetahs, None, [gallery])
+
+    def select(select_data: gr.SelectData):
+        return select_data.value['image']['url']
+
+    gallery.select(select, None, select_output)
+
 
 if __name__ == "__main__":
     demo.launch()
--- js/app/test/gallery_component_events.spec.ts
+++ js/app/test/gallery_component_events.spec.ts
@@ -4,30 +4,21 @@ test("Gallery preview mode displays all images correctly.", async ({
 	page
 }) => {
 	await page.getByRole("button", { name: "Run" }).click();
-	await page.getByLabel("Thumbnail 2 of 5").click();
+	await page.getByLabel("Thumbnail 2 of 3").click();
 
 	expect(await page.getByTestId("detailed-image").getAttribute("src")).toEqual(
-		"https://nationalzoo.si.edu/sites/default/files/animals/cheetah-003.jpg"
+		"https://gradio-builds.s3.amazonaws.com/assets/lite-logo.png"
 	);
 
 	expect(await page.getByTestId("thumbnail 1").getAttribute("src")).toEqual(
-		"https://upload.wikimedia.org/wikipedia/commons/0/09/TheCheethcat.jpg"
+		"https://gradio-builds.s3.amazonaws.com/assets/cheetah-003.jpg"
 	);
 });
 
 test("Gallery select event returns the right value", async ({ page }) => {
 	await page.getByRole("button", { name: "Run" }).click();
-	await page.getByLabel("Thumbnail 2 of 5").click();
-
+	await page.getByLabel("Thumbnail 2 of 3").click();
 	expect(await page.getByLabel("Select Data")).toHaveValue(
-		"https://nationalzoo.si.edu/sites/default/files/animals/cheetah-003.jpg"
+		"https://gradio-builds.s3.amazonaws.com/assets/lite-logo.png"
 	);
 });
-
-// test("Video click-to-upload uploads video successfuly. Clear, play, and pause buttons dispatch events correctly.", async ({
-// 	page
-// }) => {
-// 	await page
-// 		.getByRole("button", { name: "Drop Video Here - or - Click to Upload" })
-// 		.click();
-// });
