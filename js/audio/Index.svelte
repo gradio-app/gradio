8c2a078b20e1fb80c79c6f03b899398949e1ec5f
Hannah <hannahblair@users.noreply.github.com>
2023-11-07 15:47:27 +0100

    remove reundant params

--- js/audio/Index.svelte
+++ js/audio/Index.svelte
@@ -134,7 +134,6 @@
 
 		<StaticAudio
 			i18n={gradio.i18n}
-			{autoplay}
 			{show_label}
 			{show_download_button}
 			{show_share_button}
@@ -177,7 +176,6 @@
 			{active_source}
 			{pending}
 			{streaming}
-			{autoplay}
 			{handle_reset_value}
 			bind:dragging
 			on:edit={() => gradio.dispatch("edit")}
