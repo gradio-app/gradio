bed2eade4b6ef9af3a943d8162ee63ce2eaaffcb
Abubakar Abid <abubakar@huggingface.co>
2023-11-08 16:33:14 -0800

    format

--- test/test_gradio_component_cli.py
+++ test/test_gradio_component_cli.py
@@ -76,7 +76,9 @@ def test_do_not_replace_class_name_in_import_statement(tmp_path):
 
 
 def test_raises_if_directory_exists(tmp_path):
-    with pytest.raises(Exception):  # Keeping it a general exception since the specific exception seems to differ between operating systems
+    with pytest.raises(
+        Exception
+    ):  # Keeping it a general exception since the specific exception seems to differ between operating systems
         _create("MyComponent", tmp_path)
 
 
