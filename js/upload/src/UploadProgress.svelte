03e407093983d2d50fb8f89a9af3055b330d4f7a
Hannah <hannahblair@users.noreply.github.com>
2023-11-09 12:47:17 +0100

    add progress bar and show current file uploading

--- js/upload/src/UploadProgress.svelte
+++ js/upload/src/UploadProgress.svelte
@@ -10,6 +10,7 @@
 
 	let event_source: EventSource;
 	let progress = false;
+	let current_file_upload: FileDataWithProgress;
 
 	let files_with_progress: FileDataWithProgress[] = files.map((file) => {
 		return {
@@ -17,8 +18,6 @@
 			progress: 0,
 		};
 	});
-
-	let lowestProgressFile: FileData | null = "";
 
 	const dispatch = createEventDispatcher();
 
@@ -28,6 +27,7 @@
 			if (file.orig_name === filename) {
 				file.progress += chunk_size;
 			}
+			current_file_upload = file;
 			return file;
 		});
 	}
@@ -53,7 +53,7 @@
 		};
 	});
 
-	function calculateTotalProgress(files: FileData[]): string {
+	function calculateTotalProgress(files: FileData[]): number {
 		let totalProgress = 0;
 		files.forEach((file) => {
 			totalProgress += getProgress(file as FileDataWithProgress);
@@ -64,37 +64,34 @@
 			(totalProgress / files.length).toFixed(2) + "%"
 		);
 
-		document.documentElement.style.setProperty(
-			"--upload-progress",
-			(totalProgress / files.length).toFixed(2)
-		);
-
-		files.forEach((file) => {
-			if (
-				getProgress(file as FileDataWithProgress) <
-				getProgress(lowestProgressFile as FileDataWithProgress)
-			) {
-				lowestProgressFile = file;
-			}
-		});
-
-		return (totalProgress / files.length).toFixed(2);
+		return totalProgress / files.length;
 	}
 
 	$: calculateTotalProgress(files_with_progress);
 </script>
 
 <div class="wrap" class:progress>
-	<span class="file-wrap">
-		<div class="file-info">
-			<span>Uploading {files_with_progress.length} files...</span>
+	<span class="uploading"
+		>Uploading {files_with_progress.length}
+		{files_with_progress.length > 1 ? "files" : "file"}...</span
+	>
+
+	{#if current_file_upload}
+		<div class="file">
+			<span>
+				<div class="progress-bar">
+					<progress
+						style="visibility:hidden;height:0;width:0;"
+						value={getProgress(current_file_upload)}
+						max="100">{getProgress(current_file_upload)}</progress
+					>
+				</div>
+			</span>
+			<span class="file-name">
+				{current_file_upload.orig_name}
+			</span>
 		</div>
-		{#if lowestProgressFile}
-			<div class="file-name">
-				{lowestProgressFile.orig_name}
-			</div>
-		{/if}
-	</span>
+	{/if}
 </div>
 
 <style>
@@ -102,53 +99,77 @@
 		overflow-y: auto;
 		transition: opacity 0.5s ease-in-out;
 		background: var(--block-background-fill);
-		min-height: var(--size-40);
-		position: relative;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.wrap::after {
-		content: "";
-		top: 0;
-		left: 0;
-		position: absolute;
-		background: var(--background-fill-primary);
-		width: var(--upload-progress-width, 0%);
-		height: 100%;
-		z-index: 1;
-	}
-
-	.wrap.progress {
-		opacity: 1;
-	}
-
-	.file-info {
-		height: 100%;
-		justify-content: center;
-		text-align: center;
-		width: 100%;
-		z-index: 2;
 		position: relative;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		justify-content: center;
+		min-height: var(--size-40);
 	}
 
-	.file-info > span {
-		color: var(--body-text-color);
+	.wrap::after {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: var(--upload-progress-width);
+		height: 100%;
+		background: var(--background-fill-secondary);
+		transition: all 0.5s ease-in-out;
+		z-index: 1;
+	}
+
+	.uploading {
 		font-size: var(--text-lg);
 		font-family: var(--font);
-		margin: var(--spacing-lg);
+		z-index: 2;
 	}
 
 	.file-name {
-		color: var(--body-text-color-subdued);
+		margin: var(--spacing-md);
 		font-size: var(--text-lg);
-		font-family: var(--font);
-		margin: var(--spacing-lg);
+		color: var(--body-text-color-subdued);
+	}
+
+	.file {
+		font-size: var(--text-md);
 		z-index: 2;
-		position: relative;
+		display: flex;
+		align-items: center;
+	}
+
+	.file progress {
+		display: inline;
+		height: var(--size-1);
+		width: 100%;
+		transition: all 0.5s ease-in-out;
+		color: var(--color-accent);
+		border: none;
+	}
+
+	.file progress[value]::-webkit-progress-value {
+		background-color: var(--color-accent);
+		border-radius: 20px;
+	}
+
+	.file progress[value]::-webkit-progress-bar {
+		background-color: var(--border-color-accent);
+		border-radius: 20px;
+	}
+
+	.progress-bar {
+		width: 14px;
+		height: 14px;
+		border-radius: 50%;
+		background: radial-gradient(
+				closest-side,
+				var(--block-background-fill) 64%,
+				transparent 53% 100%
+			),
+			conic-gradient(
+				var(--color-accent) var(--upload-progress-width),
+				var(--border-color-accent) 0
+			);
+		transition: all 0.5s ease-in-out;
 	}
 </style>
