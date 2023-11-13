0d65dddc1a52561f15a09c65c3fabade391593d9
Ali Abid <aabid94@gmail.com>
2023-11-13 15:05:10 -0800

    changes

--- js/app/test/on_listener_test.spec.ts
+++ js/app/test/on_listener_test.spec.ts
@@ -1,12 +1,10 @@
 import { test, expect } from "@gradio/tootils";
 
-test("On listener works.", async ({
-	page
-}) => {
-	const name_box = await page.locator("textarea").nth(0);	
-	const output_box = await page.locator("textarea").nth(1);	
-	const trigger1_box = await page.locator("textarea").nth(2);		
-	const trigger2_box = await page.locator("textarea").nth(3);		
+test("On listener works.", async ({ page }) => {
+	const name_box = await page.locator("textarea").nth(0);
+	const output_box = await page.locator("textarea").nth(1);
+	const trigger1_box = await page.locator("textarea").nth(2);
+	const trigger2_box = await page.locator("textarea").nth(3);
 
 	name_box.fill("Jimmy");
 	await page.click("text=Greet");
@@ -14,7 +12,7 @@ test("On listener works.", async ({
 	await expect(trigger1_box).toHaveValue("Button");
 	await expect(name_box).toHaveValue("");
 	await expect(trigger2_box).toHaveValue("Button");
-	
+
 	await name_box.fill("Sally");
 	await name_box.press("Enter");
 	await expect(output_box).toHaveValue("Hello Sally!");
