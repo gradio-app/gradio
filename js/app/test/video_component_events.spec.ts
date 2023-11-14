c92d972fafb980e1c42d5548120125059cb5346c
freddyaboulton <alfonsoboulton@gmail.com>
2023-11-14 12:00:15 -0500

    Add download tests for audio/video

--- js/app/test/video_component_events.spec.ts
+++ js/app/test/video_component_events.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect, drag_and_drop_file } from "@gradio/tootils";
 
-test("Video click-to-upload uploads video successfuly. Clear, play, and pause buttons dispatch events correctly.", async ({
+test("Video click-to-upload uploads video successfuly. Clear, play, and pause buttons dispatch events correctly. Downloading the file works and has the correct name.", async ({
 	page
 }) => {
 	await page
@@ -38,6 +38,11 @@ test("Video click-to-upload uploads video successfuly. Clear, play, and pause bu
 	await page.getByLabel("play-pause-replay-button").first().click();
 	await expect(page.getByLabel("# Play Events")).toHaveValue("2");
 	await expect(page.getByLabel("# Pause Events")).toHaveValue("2");
+
+	const downloadPromise = page.waitForEvent("download");
+	await page.getByLabel("Download").click();
+	const download = await downloadPromise;
+	await expect(download.suggestedFilename()).toBe("file_test.ogg");
 });
 
 test("Video drag-and-drop uploads a file to the server correctly.", async ({
