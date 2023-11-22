d3ce1242e918717cd29ce12431c83180772c1e74
D V <divyadityasnaruka@gmail.com>
2023-11-22 15:02:50 +0530

    migrate from black to ruff

--- scripts/format_backend.sh
+++ scripts/format_backend.sh
@@ -2,10 +2,10 @@
 
 cd "$(dirname ${0})/.."
 
-echo "Formatting the backend... Our style follows the Black code style."
+echo "Formatting the backend... Our style follows the ruff code style."
 python -c "import gradio"
 ruff --fix gradio test
-black gradio test
+ruff format gradio test
 bash scripts/type_check_backend.sh
 
 bash client/python/scripts/format.sh  # Call the client library's formatting script
