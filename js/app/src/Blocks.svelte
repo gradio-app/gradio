e1e5222c3322e235b96cdf68be44df6d7ca96b34
Dawood <dawoodkhan82@gmail.com>
2023-11-22 18:05:55 -0500

    format

--- js/app/src/Blocks.svelte
+++ js/app/src/Blocks.svelte
@@ -491,7 +491,7 @@
 						outputs.forEach((id) => {
 							if (
 								instance_map[id].props.interactive &&
-								status.stage === "pending" && 
+								status.stage === "pending" &&
 								dep.targets[0][1] !== "focus"
 							) {
 								pending_outputs.push(id);
