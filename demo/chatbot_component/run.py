67b898fece53938387b0f3855b5242e133d5bd8b
Hannah <hannahblair@users.noreply.github.com>
2023-11-24 14:45:04 +0100

    add like/dislike to chatbot demo and add e2e test

--- demo/chatbot_component/run.py
+++ demo/chatbot_component/run.py
@@ -1,6 +1,15 @@
 import gradio as gr
 
+
+def test(x: gr.LikeData):
+    print(x.index, x.value, x.liked)
+
+
 with gr.Blocks() as demo:
-    gr.Chatbot(value=[["Hello World","Hey Gradio!"],["❤️","😍"],["🔥","🤗"]])
+    chatbot = gr.Chatbot(
+        value=[["Hello World", "Hey Gradio!"], ["❤️", "😍"], ["🔥", "🤗"]]
+    )
+
+    chatbot.like(test, None, None)
 
 demo.launch()
