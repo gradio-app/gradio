9bcb1da189a9738d023ef6daad8c6c827e3f6371
Xiang Liao <liao1120x@gmail.com>
2023-11-27 17:31:21 +0800

    Feat: make UploadButton accept icon (#6584)
    * feat: make UploadButton accept icon
    
    * add changeset
    
    * add proxy url prop
    
    * add stories
    
    ---------
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

---
+++ js/uploadbutton/UploadButton.stories.svelte
@@ -0,0 +1,80 @@
+<script>
+	import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+	import UploadButton from "./Index.svelte";
+</script>
+
+<Meta
+	title="Components/UploadButton"
+	component={UploadButton}
+	argTypes={{
+		label: {
+			control: "text",
+			description: "The text to display on the button",
+			name: "label",
+			value: "Gradio Button"
+		},
+		variant: {
+			options: ["primary", "secondary", "stop"],
+			description: "The variant of the button",
+			control: { type: "select" },
+			defaultValue: "primary"
+		},
+		size: {
+			options: ["sm", "lg"],
+			description: "The size of the button",
+			control: { type: "select" },
+			defaultValue: "lg"
+		},
+		visible: {
+			options: [true, false],
+			description: "Sets the visibility of the button",
+			control: { type: "boolean" },
+			defaultValue: true
+		},
+		interactive: {
+			options: [true, false],
+			description: "If false, the button will be in a disabled state",
+			control: { type: "boolean" },
+			defaultValue: true
+		},
+		disabled: {
+			options: [true, false],
+			control: { type: "boolean" },
+			defaultValue: false
+		},
+		scale: {
+			options: [null, 0.5, 1, 2],
+			description:
+				"relative width compared to adjacent Components in a Row. For example, if Component A has scale=2, and Component B has scale=1, A will be twice as wide as B. Should be an integer.",
+			control: { type: "select" }
+		}
+	}}
+/>
+
+<Template let:args>
+	<UploadButton value="Gradio Button" {...args} />
+</Template>
+
+<Story
+	name="Primary"
+	args={{ label: "Upload", variant: "primary", size: "lg", scale: 1 }}
+/>
+<Story
+	name="Secondary"
+	args={{ label: "Upload", variant: "secondary", size: "lg" }}
+/>
+<Story name="Stop" args={{ label: "Upload", variant: "stop", size: "lg" }} />
+<Story
+	name="Button with external image icon"
+	args={{
+		label: "Upload",
+		icon: "https://huggingface.co/front/assets/huggingface_logo-noborder.svg"
+	}}
+/>
+<Story
+	name="Button with visible equal to false"
+	args={{
+		label: "Upload",
+		visible: false
+	}}
+/>
