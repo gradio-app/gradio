d548202d2b5bd8a99e3ebc5bf56820b0282ce0f5
Hannah <hannahblair@users.noreply.github.com>
2023-12-04 21:38:55 +0000

    Improve video trimming and error handling (#6566)
    * amend trimming logic and return original file when error occurs
    
    * add interactive story test
    
    * add changeset
    
    * add changeset
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- js/video/Video.stories.svelte
+++ js/video/Video.stories.svelte
@@ -3,30 +3,15 @@
 	import Video from "./Index.svelte";
 	import { format } from "svelte-i18n";
 	import { get } from "svelte/store";
+	import { userEvent, within } from "@storybook/testing-library";
+	import { waitFor } from "@testing-library/dom";
 </script>
 
-<Meta
-	title="Components/Video"
-	component={Video}
-	argTypes={{
-		video: {
-			control: "text",
-			description:
-				"A path or URL for the default value that Video component is going to take. Can also be a tuple consisting of (video filepath, subtitle filepath). If a subtitle file is provided, it should be of type .srt or .vtt. Or can be callable, in which case the function will be called whenever the app loads to set the initial value of the component.",
-			name: "value"
-		},
-		autoplay: {
-			control: [true, false],
-			description: "Whether to autoplay the video on load",
-			name: "autoplay",
-			value: true
-		}
-	}}
-/>
+<Meta title="Components/Video" component={Video} />
 
 <div>
 	<Template let:args>
-		<Video {...args} i18n={get(format)} />
+		<Video i18n={get(format)} {...args} />
 	</Template>
 </div>
 
@@ -70,5 +55,44 @@
 		width: 400,
 		height: 400,
 		value: null
+	}}
+/>
+
+<Story
+	name="Trim video"
+	args={{
+		value: {
+			video: {
+				path: "https://gradio-static-files.s3.us-west-2.amazonaws.com/world.mp4",
+				url: "https://gradio-static-files.s3.us-west-2.amazonaws.com/world.mp4",
+				orig_name: "world.mp4"
+			}
+		},
+		label: "world video",
+		show_label: true,
+		interactive: "true",
+		sources: ["upload"],
+		width: 400
+	}}
+	play={async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+
+		const trimButton = canvas.getByLabelText("Trim video to selection");
+
+		userEvent.click(trimButton);
+
+		await new Promise((r) => setTimeout(r, 4000));
+
+		const rightHandle = canvas.getByLabelText(
+			"end drag handle for trimming video"
+		);
+
+		userEvent.click(rightHandle);
+
+		for (let i = 0; i < 4; i++) {
+			await userEvent.keyboard("[ArrowRight]");
+		}
+
+		userEvent.click(canvas.getByText("Trim"));
 	}}
 />
