b6ecf8e5ea2347fab565773f79d3854fa3043ae0
Abubakar Abid <abubakar@huggingface.co>
2023-12-05 18:36:53 -0800

    buttons

--- test/test_buttons.py
+++ test/test_buttons.py
@@ -9,15 +9,25 @@ class TestClearButton:
     def test_clear_event_setup_correctly(self):
         with gr.Blocks() as demo:
             chatbot = gr.Chatbot([["Hello", "How are you?"]])
-            state = gr.State("")
             with gr.Row():
                 textbox = gr.Textbox(scale=3, interactive=True)
-                gr.ClearButton([state, textbox, chatbot], scale=1)
+                gr.ClearButton([textbox, chatbot], scale=1)
+
+        clear_event_trigger = demo.dependencies.pop()
+        assert not clear_event_trigger["backend_fn"]
+        assert clear_event_trigger["js"]
+        assert clear_event_trigger["outputs"] == [textbox._id, chatbot._id]
+
+    def test_clear_event_setup_correctly_with_state(self):
+        with gr.Blocks() as demo:
+            chatbot = gr.Chatbot([["Hello", "How are you?"]])
+            state = gr.State("")
+            gr.ClearButton([state, chatbot], scale=1)
 
         clear_event_trigger = demo.dependencies.pop()
         assert clear_event_trigger["backend_fn"]
         assert clear_event_trigger["js"]
-        assert clear_event_trigger["outputs"] == [state._id, textbox._id, chatbot._id]
+        assert clear_event_trigger["outputs"] == [state._id, chatbot._id]
 
 
 class TestOAuthButtons:
