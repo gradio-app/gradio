90cd53effd1e9de7a8ca1ae683188e3236f264a6
D V <divyadityasnaruka@gmail.com>
2023-12-07 01:00:37 +0530

    fix script and dependencies

--- scripts/format_backend.sh
+++ scripts/format_backend.sh
@@ -4,8 +4,8 @@ cd "$(dirname ${0})/.."
 
 echo "Formatting the backend... Our style follows the ruff code style."
 python -c "import gradio"
-ruff --fix gradio test
-ruff format gradio test
+python -m ruff --fix gradio test
+python -m ruff format gradio test
 bash scripts/type_check_backend.sh
 
 bash client/python/scripts/format.sh  # Call the client library's formatting script
