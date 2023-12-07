a30cad75d07c1572261820406de0b148c1dbe8cb
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-12-07 23:51:46 +0100

    Fix  js/image/shared/Image.svelte

--- js/image/shared/Image.svelte
+++ js/image/shared/Image.svelte
@@ -6,15 +6,20 @@
 
 	export let src: HTMLImgAttributes["src"] = undefined;
 
-	// Use `src` to render <img/> immediately without waiting
-	// for the asynchronous Wasm src resolving.
-	// This is necessary to avoid flickering of the image element.
-	let resolved_src = src;
+	let resolved_src: typeof src;
+
 	// The `src` prop can be updated before the Promise from `resolve_wasm_src` is resolved.
 	// In such a case, the resolved value for the old `src` has to be discarded,
 	// This variable `latest_src` is used to pick up only the value resolved for the latest `src` prop.
 	let latest_src: typeof src;
 	$: {
+		// In normal (non-Wasm) Gradio, the `<img>` element should be rendered with the passed `src` props immediately
+		// without waiting for `resolve_wasm_src()` to resolve.
+		// If it waits, a black image is displayed until the async task finishes
+		// and it leads to undesirable flickering.
+		// So set `src` to `resolved_src` here.
+		resolved_src = src;
+
 		latest_src = src;
 		const resolving_src = src;
 		resolve_wasm_src(resolving_src).then((s) => {
