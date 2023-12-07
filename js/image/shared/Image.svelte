63b70c9c4d37e3ed5ed1e609cc5b181c984be0d8
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-12-07 23:02:48 +0100

    Fix js/image/shared/Image.svelte to avoid race condition

--- js/image/shared/Image.svelte
+++ js/image/shared/Image.svelte
@@ -10,9 +10,20 @@
 	// for the asynchronous Wasm src resolving.
 	// This is necessary to avoid flickering of the image element.
 	let resolved_src = src;
-	$: resolve_wasm_src(src).then(s => {
-		resolved_src = s;
-	});
+	// The `src` prop can be updated before the Promise from `resolve_wasm_src` is resolved.
+	// In such a case, the promise dispatched for the old `src` has to be ignored,
+	// This variable `latest_promise` is used for that purpose.
+	let latest_promise: ReturnType<typeof resolve_wasm_src>;
+	$: {
+		const current_promise = resolve_wasm_src(src);
+		latest_promise = current_promise;
+
+		current_promise.then((s) => {
+			if (latest_promise === current_promise) {
+				resolved_src = s;
+			}
+		});
+	}
 </script>
 
 <!-- svelte-ignore a11y-missing-attribute -->
