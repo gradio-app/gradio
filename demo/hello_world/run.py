9db0cbcca9c93b9b3c165fb34076c4c6334956d5
freddyaboulton <alfonsoboulton@gmail.com>
2023-12-13 13:45:38 -0500

    Revert demo

--- demo/hello_world/run.py
+++ demo/hello_world/run.py
@@ -1,11 +1,9 @@
 import gradio as gr
 
 def greet(name):
-    import time
-    time.sleep(10)
     return "Hello " + name + "!"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
     
 if __name__ == "__main__":
-    demo.queue(max_size=1).launch(show_api=False)   
+    demo.launch(show_api=False)   
