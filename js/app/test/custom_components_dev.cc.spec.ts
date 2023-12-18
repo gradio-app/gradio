1185271f7c7e70baa1e78630aa5683e7ea92e800
freddyaboulton <alfonsoboulton@gmail.com>
2023-12-18 16:33:16 -0500

    Log port to see

--- js/app/test/custom_components_dev.cc.spec.ts
+++ js/app/test/custom_components_dev.cc.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from "@playwright/test";
 import { spawnSync } from "node:child_process";
 import { launch_app_background, kill_process } from "./utils";
 import { join } from "path";
+import which from "which";
 
 test.beforeAll(() => {
 	// copy pnpm-lock.yml to copy function
@@ -62,11 +63,16 @@ test.afterAll(() => {
 
 test("gradio cc dev correcty launches and is interactive", async ({ page }) => {
 	test.setTimeout(60 * 1000);
-	console.log("cwd", process.cwd());
 
 	// create a new python virtual environment and activate it
 	const venv_create = spawnSync(
-		`python3 -m venv ${join(process.cwd(), "..", "preview", "test", "venv")}`,
+		`${which.sync("python")} -m venv ${join(
+			process.cwd(),
+			"..",
+			"preview",
+			"test",
+			"venv"
+		)}`,
 		{
 			shell: true,
 			stdio: "pipe",
@@ -107,7 +113,11 @@ test("gradio cc dev correcty launches and is interactive", async ({ page }) => {
 	}
 	// install local copies of gradio and gradio client
 	const gradio_install = spawnSync(
-		`pip install -e ${join(process.cwd(), "..", "..")}`,
+		`${which.sync("python")} -m pip install -e ${join(
+			process.cwd(),
+			"..",
+			".."
+		)}`,
 		{
 			shell: true,
 			stdio: "pipe",
@@ -123,7 +133,13 @@ test("gradio cc dev correcty launches and is interactive", async ({ page }) => {
 	}
 	// install local copies of gradio and gradio client
 	const client_install = spawnSync(
-		`pip install -e ${join(process.cwd(), "..", "..", "client", "python")}`,
+		`${which.sync("python")} -m pip install -e ${join(
+			process.cwd(),
+			"..",
+			"..",
+			"client",
+			"python"
+		)}`,
 		{
 			shell: true,
 			stdio: "pipe",
@@ -138,7 +154,9 @@ test("gradio cc dev correcty launches and is interactive", async ({ page }) => {
 		console.log("client install stderr", client_install.stderr.toString());
 	}
 	const create = spawnSync(
-		`gradio cc create MyComponent --no-configure-metadata --template SimpleTextbox --overwrite`,
+		`${which(
+			"gradio"
+		)} cc create MyComponent --no-configure-metadata --template SimpleTextbox --overwrite`,
 		{
 			shell: true,
 			stdio: "pipe",
@@ -157,9 +175,10 @@ test("gradio cc dev correcty launches and is interactive", async ({ page }) => {
 	let _process;
 	try {
 		const { port, process: _process } = await launch_app_background(
-			`gradio cc dev`,
+			`${which.sync("gradio")} cc dev`,
 			join(process.cwd(), "..", "preview", "test", "mycomponent")
 		);
+		console.log("Connected to port", port);
 		await page.goto(`http://localhost:${port}`);
 		await page.getByLabel("x").fill("foo");
 		await page.getByRole("button", { name: "Submit" }).click();
