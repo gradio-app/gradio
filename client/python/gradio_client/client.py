298651bf965b4a610047dd07b600c83338a21574
Abubakar Abid <abubakar@huggingface.co>
2023-12-21 15:59:42 -0800

    format

--- client/python/gradio_client/client.py
+++ client/python/gradio_client/client.py
@@ -683,7 +683,12 @@ class Client:
                 raise ValueError(f"Invalid function index: {fn_index}.")
         else:
             valid_endpoints = [
-                e for e in self.endpoints if e.is_valid and e.api_name is not None and e.backend_fn is not None
+                e
+                for e in self.endpoints
+                if e.is_valid
+                and e.api_name is not None
+                and e.backend_fn is not None
+                and e.show_api
             ]
             if len(valid_endpoints) == 1:
                 inferred_fn_index = valid_endpoints[0].fn_index
@@ -922,6 +927,7 @@ class Endpoint:
         # Disallow hitting endpoints that the Gradio app has disabled
         self.is_valid = self.api_name is not False
         self.backend_fn = dependency.get("backend_fn")
+        self.show_api = dependency.get("show_api")
 
     def _get_component_type(self, component_id: int):
         component = next(
@@ -1219,6 +1225,7 @@ class EndpointV3Compatibility:
         except SerializationSetupError:
             self.is_valid = False
         self.backend_fn = dependency.get("backend_fn")
+        self.show_api = True
 
     def __repr__(self):
         return f"Endpoint src: {self.client.src}, api_name: {self.api_name}, fn_index: {self.fn_index}"
