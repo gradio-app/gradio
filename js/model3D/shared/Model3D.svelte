1e28b5167da458eaaa75075860316ba420a80bd0
Hannah <hannahblair@users.noreply.github.com>
2023-12-22 21:42:54 +0100

    check for changes when resetting scene

--- js/model3D/shared/Model3D.svelte
+++ js/model3D/shared/Model3D.svelte
@@ -7,6 +7,7 @@
 	import * as BABYLON from "babylonjs";
 	import * as BABYLON_LOADERS from "babylonjs-loaders";
 	import type { I18nFormatter } from "@gradio/utils";
+	import { dequal } from "dequal";
 
 	export let value: FileData | null;
 	export let clear_color: [number, number, number, number] = [0, 0, 0, 0];
@@ -15,15 +16,14 @@
 	export let i18n: I18nFormatter;
 	export let zoom_speed = 1;
 	export let pan_speed = 1;
-
-	let previous_scene: BABYLON.Scene | null = null; // To track the previous scene
-
 	// alpha, beta, radius
 	export let camera_position: [number | null, number | null, number | null] = [
 		null,
 		null,
 		null
 	];
+
+	let current_settings = { camera_position, zoom_speed, pan_speed };
 
 	$: {
 		if (
@@ -83,9 +83,14 @@
 	}
 
 	$: {
-		if (scene && scene !== previous_scene) {
+		if (
+			scene &&
+			(!dequal(current_settings.camera_position, camera_position) ||
+				current_settings.zoom_speed !== zoom_speed ||
+				current_settings.pan_speed !== pan_speed)
+		) {
 			reset_camera_position(scene, camera_position, zoom_speed, pan_speed);
-			previous_scene = scene;
+			current_settings = { camera_position, zoom_speed, pan_speed };
 		}
 	}
 </script>
