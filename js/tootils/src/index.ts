b24a3645617ba97126a3eac05375391138a79ce9
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-12-28 11:27:24 +0900

    Use the same Page instance for all the tests in the case of lite

--- js/tootils/src/index.ts
+++ js/tootils/src/index.ts
@@ -21,77 +21,96 @@ const ROOT_DIR = path.resolve(
 	"../../../.."
 );
 
-export const test = base.extend<{ setup: void }>({
+const is_lite = !!process.env.GRADIO_E2E_TEST_LITE;
+
+const test_normal = base.extend<{ setup: void }>({
 	setup: [
-		!!process.env.GRADIO_E2E_TEST_LITE
-			? async ({ page }, use, testInfo) => {
-					const { file } = testInfo;
-					const test_name = path.basename(file, ".spec.ts");
-					const demo_dir = path.resolve(ROOT_DIR, `./demo/${test_name}`);
-					const demo_filenames = await fsPromises
-						.readdir(demo_dir, { withFileTypes: true, recursive: true })
-						.then((dirents) =>
-							dirents
-								.filter(
-									(dirent) =>
-										dirent.isFile() &&
-										!dirent.name.endsWith(".ipynb") &&
-										!dirent.name.endsWith(".pyc")
-								)
-								.map((dirent) => dirent.name)
-						);
-					const demo_files = await Promise.all(
-						demo_filenames.map(
-							(filename) =>
-								fsPromises
-									.readFile(path.join(demo_dir, filename))
-									.then((buffer) => [filename, buffer.toString("base64")]) // To pass to the browser, we need to convert the buffer to base64.
-						)
-					);
+		async ({ page }, use, testInfo): Promise<void> => {
+			const port = process.env.GRADIO_E2E_TEST_PORT;
+			const { file } = testInfo;
+			const test_name = path.basename(file, ".spec.ts");
 
-					await page.goto("http://localhost:9876/lite.html");
+			await page.goto(`localhost:${port}/${test_name}`);
 
-					const controllerHandle = await page.waitForFunction(
-						// @ts-ignore
-						() => window.controller // This controller object is set in the dev app.
-					);
-					await controllerHandle.evaluate(
-						async (controller: any, files: [string, string][]) => {
-							function base64ToUint8Array(base64: string): Uint8Array {
-								// Ref: https://stackoverflow.com/a/21797381/13103190
-								const binaryString = atob(base64);
-								const bytes = new Uint8Array(binaryString.length);
-								for (var i = 0; i < binaryString.length; i++) {
-									bytes[i] = binaryString.charCodeAt(i);
-								}
-								return bytes;
-							}
-
-							files.forEach(([filepath, data_b64]) => {
-								controller.write(filepath, base64ToUint8Array(data_b64), {});
-							});
-
-							await controller.run_file("run.py");
-						},
-						demo_files
-					);
-
-					await use();
-
-					controllerHandle.dispose();
-			  }
-			: async ({ page }, use, testInfo): Promise<void> => {
-					const port = process.env.GRADIO_E2E_TEST_PORT;
-					const { file } = testInfo;
-					const test_name = path.basename(file, ".spec.ts");
-
-					await page.goto(`localhost:${port}/${test_name}`);
-
-					await use();
-			  },
+			await use();
+		},
 		{ auto: true }
 	]
 });
+
+const lite_url = "http://localhost:9876/lite.html";
+// LIte taks a long time to initialize, so we share the page across tests.
+let shared_page_for_lite: Page;
+const test_lite = base.extend<{ setup: void }>({
+	page: async ({ browser }, use) => {
+		if (shared_page_for_lite == null) {
+			shared_page_for_lite = await browser.newPage();
+		}
+		if (shared_page_for_lite.url() !== lite_url) {
+			await shared_page_for_lite.goto(lite_url);
+		}
+		await use(shared_page_for_lite);
+	},
+	setup: [
+		async ({ page }, use, testInfo) => {
+			const { file } = testInfo;
+			const test_name = path.basename(file, ".spec.ts");
+			const demo_dir = path.resolve(ROOT_DIR, `./demo/${test_name}`);
+			const demo_filenames = await fsPromises
+				.readdir(demo_dir, { withFileTypes: true, recursive: true })
+				.then((dirents) =>
+					dirents
+						.filter(
+							(dirent) =>
+								dirent.isFile() &&
+								!dirent.name.endsWith(".ipynb") &&
+								!dirent.name.endsWith(".pyc")
+						)
+						.map((dirent) => dirent.name)
+				);
+			const demo_files = await Promise.all(
+				demo_filenames.map(
+					(filename) =>
+						fsPromises
+							.readFile(path.join(demo_dir, filename))
+							.then((buffer) => [filename, buffer.toString("base64")]) // To pass to the browser, we need to convert the buffer to base64.
+				)
+			);
+
+			const controllerHandle = await page.waitForFunction(
+				// @ts-ignore
+				() => window.controller // This controller object is set in the dev app.
+			);
+			await controllerHandle.evaluate(
+				async (controller: any, files: [string, string][]) => {
+					function base64ToUint8Array(base64: string): Uint8Array {
+						// Ref: https://stackoverflow.com/a/21797381/13103190
+						const binaryString = atob(base64);
+						const bytes = new Uint8Array(binaryString.length);
+						for (var i = 0; i < binaryString.length; i++) {
+							bytes[i] = binaryString.charCodeAt(i);
+						}
+						return bytes;
+					}
+
+					files.forEach(([filepath, data_b64]) => {
+						controller.write(filepath, base64ToUint8Array(data_b64), {});
+					});
+
+					await controller.run_file("run.py");
+				},
+				demo_files
+			);
+
+			await use();
+
+			controllerHandle.dispose();
+		},
+		{ auto: true }
+	]
+});
+
+export const test = is_lite ? test_lite : test_normal;
 
 export async function wait_for_event(
 	component: SvelteComponent,
