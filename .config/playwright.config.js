c9a534f93e3cbb866e3afd5ba448c985c67b3371
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-12-28 16:13:47 +0900

    Add testIgnore for lite

--- .config/playwright.config.js
+++ .config/playwright.config.js
@@ -32,7 +32,11 @@ const lite = defineConfig(base, {
 		command: "pnpm --filter @gradio/app dev:lite",
 		url: "http://localhost:9876/lite.html",
 		reuseExistingServer: !process.env.CI
-	}
+	},
+	testIgnore: [
+		"**/clear_components.spec.ts", // `gr.Image()` with remote image is not supported in lite because it calls `httpx.stream` through `processing_utils.save_url_to_cache()`.
+		"**/load_space.spec.ts" // `gr.load()`, which calls `httpx.get` is not supported in lite.
+	]
 });
 lite.projects = undefined; // Explicitly unset this field due to https://github.com/microsoft/playwright/issues/28795
 
