8af7375e7ab5480606b3d862e59a34d929c876d0
Ali Abid <aliabid@Alis-MacBook-Pro.local>
2024-01-03 23:15:02 -0800

    changes

--- js/app/test/cancel_events.spec.ts
+++ js/app/test/cancel_events.spec.ts
@@ -8,11 +8,22 @@ test("when using an iterative function the UI should update over time as iterati
 	});
 	const textbox = await page.getByLabel("Iterative Output");
 
-	await start_button.click();
+	let output_values: string[] = [];
+	let last_output_value = "";
+	let interval = setInterval(async () => {
+		let value = await textbox.inputValue();
+		if (value !== last_output_value) {
+			output_values.push(value);
+			last_output_value = value;
+		}
+	}, 100);
 
-	await expect(textbox).toHaveValue("0");
-	await expect(textbox).toHaveValue("3");
+	await start_button.click();
 	await expect(textbox).toHaveValue("8");
+	clearInterval(interval);
+	for (let i = 1; i < 8; i++) {
+		expect(output_values).toContain(i.toString());
+    }	
 });
 
 test("when using an iterative function it should be possible to cancel the function, after which the UI should stop updating", async ({
