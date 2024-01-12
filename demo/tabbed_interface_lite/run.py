94976320da18fab8cdef6737dc9fb6245ec867ca
Hannah <hannahblair@users.noreply.github.com>
2024-01-12 15:38:26 +0100

    add tab e2e test

--- demo/tabbed_interface_lite/run.py
+++ demo/tabbed_interface_lite/run.py
@@ -2,8 +2,10 @@ import gradio as gr
 
 hello_world = gr.Interface(lambda name: "Hello " + name, "text", "text")
 bye_world = gr.Interface(lambda name: "Bye " + name, "text", "text")
+hidden_tab = gr.Interface(lambda name: "Hidden " + name, "text", "text")
+secret_tab = gr.Interface(lambda name: "Secret " + name, "text", "text")
 
-demo = gr.TabbedInterface([hello_world, bye_world], ["Hello World", "Bye World"])
+demo = gr.TabbedInterface([secret_tab, hello_world, bye_world, hidden_tab], ["Secret Tab", "Hello World", "Bye World", "Hidden Tab"], visible_tabs=[0,1,2], interactive_tabs=[1,2])
 
 if __name__ == "__main__":
     demo.launch()
