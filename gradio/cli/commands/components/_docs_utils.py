0636fbdfbd4fe4145690b08a5c52a11580343d5b
pngwn <hello@pngwn.io>
2024-01-18 11:22:15 +0000

    tweaks

--- gradio/cli/commands/components/_docs_utils.py
+++ gradio/cli/commands/components/_docs_utils.py
@@ -567,6 +567,13 @@ def make_user_fn(
 
 ### User function
 
+The impact on the users predict function varies depending on whether the component is used as an input or output for an event (or both).
+
+- When used as an Input, the component only impacts the input signature of the user function. 
+- When used as an output, the component only impacts the return signature of the user function. 
+
+The code snippet below is accurate in cases where the component is used as both an input and an output.
+
 """
 
     md += (
