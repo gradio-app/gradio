09888a072ce6aa4201653d545076009f78d39864
Dawood <dawoodkhan82@gmail.com>
2024-01-24 22:17:06 -0500

    format

--- js/markdown/Markdown.test.ts
+++ js/markdown/Markdown.test.ts
@@ -5,47 +5,48 @@ import Markdown from "./Index.svelte";
 import type { LoadingStatus } from "@gradio/statustracker";
 
 const loading_status: LoadingStatus = {
-    eta: 0,
-    queue_position: 1,
-    queue_size: 1,
-    status: "complete" as LoadingStatus["status"],
-    scroll_to_output: false,
-    visible: true,
-    fn_index: 0,
-    show_progress: "full"
+	eta: 0,
+	queue_position: 1,
+	queue_size: 1,
+	status: "complete" as LoadingStatus["status"],
+	scroll_to_output: false,
+	visible: true,
+	fn_index: 0,
+	show_progress: "full"
 };
 
 describe("Markdown", () => {
-    afterEach(() => cleanup());
+	afterEach(() => cleanup());
 
-    test("renders valid URL", async () => {
-        const { getByText } = await render(Markdown, {
-            show_label: true,
-            max_lines: 1,
-            loading_status,
-            lines: 1,
-            value: "Visit [Gradio](https://www.gradio.app/) for more information.",
-            label: "Markdown",
-            interactive: false
-        });
+	test("renders valid URL", async () => {
+		const { getByText } = await render(Markdown, {
+			show_label: true,
+			max_lines: 1,
+			loading_status,
+			lines: 1,
+			value: "Visit [Gradio](https://www.gradio.app/) for more information.",
+			label: "Markdown",
+			interactive: false
+		});
 
-        const link: HTMLAnchorElement = getByText("Gradio") as HTMLAnchorElement;
-        assert.equal(link.href, "https://www.gradio.app/");
-    });
+		const link: HTMLAnchorElement = getByText("Gradio") as HTMLAnchorElement;
+		assert.equal(link.href, "https://www.gradio.app/");
+	});
 
-    test("renders invalid URL", async () => {
-        const { getByText } = await render(Markdown, {
-            show_label: true,
-            max_lines: 1,
-            loading_status,
-            lines: 1,
-            value: "Visit [Invalid URL](https://) for more information.",
-            label: "Markdown",
-            interactive: false
-        });
+	test("renders invalid URL", async () => {
+		const { getByText } = await render(Markdown, {
+			show_label: true,
+			max_lines: 1,
+			loading_status,
+			lines: 1,
+			value: "Visit [Invalid URL](https://) for more information.",
+			label: "Markdown",
+			interactive: false
+		});
 
-        const link: HTMLAnchorElement = getByText("Invalid URL") as HTMLAnchorElement;
-        assert.equal(link.href, "https://");
-    });
+		const link: HTMLAnchorElement = getByText(
+			"Invalid URL"
+		) as HTMLAnchorElement;
+		assert.equal(link.href, "https://");
+	});
 });
-
