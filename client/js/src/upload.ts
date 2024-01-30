f8c34eef6a8fb3390d13e89084045fd214cd8c6c
Abubakar Abid <abubakar@huggingface.co>
2024-01-30 15:00:38 -0800

    clean

--- client/js/src/upload.ts
+++ client/js/src/upload.ts
@@ -30,41 +30,41 @@ export function normalise_file(
 	proxy_url: string | null // root_url: string | null
 ): FileData[] | FileData | null {
 	return file;
-	if (file == null) {
-		return null;
-	}
+	// if (file == null) {
+	// 	return null;
+	// }
 
-	if (Array.isArray(file)) {
-		const normalized_file: (FileData | null)[] = [];
+	// if (Array.isArray(file)) {
+	// 	const normalized_file: (FileData | null)[] = [];
 
-		for (const x of file) {
-			if (x == null) {
-				normalized_file.push(null);
-			} else {
-				normalized_file.push(normalise_file(x, server_url, proxy_url));
-			}
-		}
+	// 	for (const x of file) {
+	// 		if (x == null) {
+	// 			normalized_file.push(null);
+	// 		} else {
+	// 			normalized_file.push(normalise_file(x, server_url, proxy_url));
+	// 		}
+	// 	}
 
-		return normalized_file as FileData[];
-	}
+	// 	return normalized_file as FileData[];
+	// }
 
-	if (file.is_stream) {
-		if (proxy_url == null) {
-			return new FileData({
-				...file,
-				url: server_url + "/stream/" + file.path
-			});
-		}
-		return new FileData({
-			...file,
-			url: "/proxy=" + proxy_url + "stream/" + file.path
-		});
-	}
+	// if (file.is_stream) {
+	// 	if (proxy_url == null) {
+	// 		return new FileData({
+	// 			...file,
+	// 			url: server_url + "/stream/" + file.path
+	// 		});
+	// 	}
+	// 	return new FileData({
+	// 		...file,
+	// 		url: "/proxy=" + proxy_url + "stream/" + file.path
+	// 	});
+	// }
 
-	return new FileData({
-		...file,
-		url: get_fetchable_url_or_file(file.path, server_url, proxy_url)
-	});
+	// return new FileData({
+	// 	...file,
+	// 	url: get_fetchable_url_or_file(file.path, server_url, proxy_url)
+	// });
 }
 
 function is_url(str: string): boolean {
