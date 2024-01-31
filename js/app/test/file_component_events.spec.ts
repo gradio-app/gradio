b1408b6427367ffed09f3fd1a95dfe65da9bc93c
Dawood <dawoodkhan82@gmail.com>
2024-01-31 12:22:14 -0500

    test fixes

--- js/app/test/file_component_events.spec.ts
+++ js/app/test/file_component_events.spec.ts
@@ -7,7 +7,7 @@ test("File component properly dispatches load event for the single file case.", 
 		.getByRole("button", { name: "Drop File Here - or - Click to Upload" })
 		.first()
 		.click();
-	const uploader = await page.getByTestId("file-upload").first();
+	const uploader = await page.locator("input[type=file]").first();
 	await uploader.setInputFiles(["./test/files/cheetah1.jpg"]);
 
 	await expect(page.getByLabel("# Load Upload Single File")).toHaveValue("1");
@@ -21,40 +21,31 @@ test("File component properly dispatches load event for the single file case.", 
 test("File component drag-and-drop uploads a file to the server correctly.", async ({
 	page
 }) => {
-	await drag_and_drop_file(
-		page,
-		"input[type=file]",
-		"./test/files/alphabet.txt",
-		"alphabet.txt"
-	);
+	const uploader = await page.locator("input[type=file]").nth(1);
+	await uploader.setInputFiles(["./test/files/alphabet.txt"]);
+
+	await expect(
+		page.getByLabel("# Load Upload Multiple Files").first()
+	).toHaveValue("1");
 });
 
 test("File component properly handles drag and drop of image and video files.", async ({
 	page
 }) => {
-	// Simulate dragging and dropping an image file
-	await drag_and_drop_file(
-		page,
-		"input[type=file]",
-		"./test/files/cheetah1.jpg",
-		"cheetah1.jpg"
-	);
+	const uploader = await page.locator("input[type=file]").last();
+	await uploader.setInputFiles(["./test/files/cheetah1.jpg"]);
 
 	// Check that the image file was uploaded
-	await expect(page.getByLabel("# Load Upload Multiple Files")).toHaveValue(
-		"1"
-	);
+	await expect(
+		page.getByLabel("# Load Upload Multiple Files Image/Video")
+	).toHaveValue("1");
 
-	// Simulate dragging and dropping a video file
-	await drag_and_drop_file(
-		page,
-		"input[type=file]",
-		"./test/files/video1.mp4",
-		"video1.mp4"
-	);
+	await page.getByLabel("Clear").click();
+
+	await uploader.setInputFiles(["./test/files/world.mp4"]);
 
 	// Check that the video file was uploaded
-	await expect(page.getByLabel("# Load Upload Multiple Files")).toHaveValue(
-		"2"
-	);
+	await expect(
+		page.getByLabel("# Load Upload Multiple Files Image/Video")
+	).toHaveValue("2");
 });
