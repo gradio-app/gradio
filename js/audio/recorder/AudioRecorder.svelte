8a97031698e2ab5b04ef5660fab4259afc77bde0
snow-fox <taijunjet@hotmail.com>
2024-02-01 18:32:28 +0000

    fix minutes

--- js/audio/recorder/AudioRecorder.svelte
+++ js/audio/recorder/AudioRecorder.svelte
@@ -63,7 +63,7 @@
 
 	const format_time = (seconds: number): string => {
 		const hours = Math.floor(seconds / 3600);
-		const minutes = Math.floor(seconds / 60);
+		const minutes = Math.floor(seconds / 60) % 60;
 		const secondsRemainder = Math.round(seconds) % 60;
 		const paddedSeconds = `0${secondsRemainder}`.slice(-2);
 		return `${hours}:${minutes}:${paddedSeconds}`;
