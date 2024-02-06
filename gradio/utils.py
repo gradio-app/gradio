aea14c44961be8b0e732e05799df8104d7b906ed
Abubakar Abid <abubakar@huggingface.co>
2024-02-06 12:27:44 -0800

    Refactor Inference API and rename it to Serverless Inference Endpoints (#7295)
    * changes
    
    * changes
    
    * add changeset
    
    * add changeset
    
    * changes
    
    * all pipelines
    
    * format
    
    * clean
    
    * add examples
    
    * fix audio classification
    
    * format
    
    * format
    
    * fix all pipelines
    
    * fixes
    
    * fixes
    
    * fix tabular
    
    * add changeset
    
    * added future
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- gradio/utils.py
+++ gradio/utils.py
@@ -386,24 +386,6 @@ def assert_configs_are_equivalent_besides_ids(
     return True
 
 
-def format_ner_list(input_string: str, ner_groups: list[dict[str, str | int]]):
-    if len(ner_groups) == 0:
-        return [(input_string, None)]
-
-    output = []
-    end = 0
-    prev_end = 0
-
-    for group in ner_groups:
-        entity, start, end = group["entity_group"], group["start"], group["end"]
-        output.append((input_string[prev_end:start], None))
-        output.append((input_string[start:end], entity))
-        prev_end = end
-
-    output.append((input_string[end:], None))
-    return output
-
-
 def delete_none(_dict: dict, skip_value: bool = False) -> dict:
     """
     Delete keys whose values are None from a dictionary
