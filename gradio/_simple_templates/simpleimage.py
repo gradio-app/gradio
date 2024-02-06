edfd05d18d20542d350d883d5e7f84d2774ad99e
aliabid94 <aabid94@gmail.com>
2024-02-06 13:42:30 -0800

    Expand chatinterface to full window height (#7313)
    * changes
    
    * changes
    
    * add changeset
    
    * changes
    
    * changes
    
    * changs
    
    * changes
    
    ---------
    
    Co-authored-by: Ali Abid <aliabid94@gmail.com>
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- gradio/_simple_templates/simpleimage.py
+++ gradio/_simple_templates/simpleimage.py
@@ -51,7 +51,7 @@ class SimpleImage(Component):
             show_label: if True, will display label.
             show_download_button: If True, will display button to download image.
             container: If True, will place the component in a container - providing some extra padding around the border.
-            scale: relative width compared to adjacent Components in a Row. For example, if Component A has scale=2, and Component B has scale=1, A will be twice as wide as B. Should be an integer.
+            scale: relative size compared to adjacent Components. For example if Components A and B are in a Row, and A has scale=2, and B has scale=1, A will be twice as wide as B. Should be an integer. scale applies in Rows, and to top-level Components in Blocks where fill_height=True.
             min_width: minimum pixel width, will wrap if not sufficient screen space to satisfy this value. If a certain scale value results in this Component being narrower than min_width, the min_width parameter will be respected first.
             interactive: if True, will allow users to upload and edit an image; if False, can only be used to display images. If not provided, this is inferred based on whether the component is used as an input or output.
             visible: If False, component will be hidden.
