bee71aedfab5d263a9bdd51a3928d3ba2a369ae1
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-02-06 20:12:07 +0900

    Create <Canvas3D /> and use it from <Model3D /> and <Model3DUpload />

--- js/model3D/shared/Model3D.svelte
+++ js/model3D/shared/Model3D.svelte
@@ -2,12 +2,8 @@
 	import type { FileData } from "@gradio/client";
 	import { BlockLabel, IconButton } from "@gradio/atoms";
 	import { File, Download, Undo } from "@gradio/icons";
-	import { add_new_model, reset_camera_position } from "./utils";
-	import { onMount } from "svelte";
-	import * as BABYLON from "babylonjs";
-	import * as BABYLON_LOADERS from "babylonjs-loaders";
+	import Canvas3D from "./Canvas3D.svelte";
 	import type { I18nFormatter } from "@gradio/utils";
-	import { resolve_wasm_src } from "@gradio/wasm/svelte";
 	import { dequal } from "dequal";
 
 	export let value: FileData | null;
@@ -26,101 +22,20 @@
 
 	let current_settings = { camera_position, zoom_speed, pan_speed };
 
-	$: {
-		if (
-			BABYLON_LOADERS.OBJFileLoader != undefined &&
-			!BABYLON_LOADERS.OBJFileLoader.IMPORT_VERTEX_COLORS
-		) {
-			BABYLON_LOADERS.OBJFileLoader.IMPORT_VERTEX_COLORS = true;
-		}
-	}
-
-	let canvas: HTMLCanvasElement;
-	let scene: BABYLON.Scene;
-	let engine: BABYLON.Engine | null;
-	let mounted = false;
-
-	let resolved_value: typeof value;
-	/* URL resolution for the Wasm mode. */
-	// The `value` prop can be updated before the Promise from `resolve_wasm_src` is resolved.
-	// In such a case, the resolved value for the old `value` has to be discarded,
-	// This variable `latest_value` is used to pick up only the value resolved for the latest `value` prop.
-	let latest_value: typeof value;
-	$: {
-		// In normal (non-Wasm) Gradio, the original `value` should be used immediately
-		// without waiting for `resolve_wasm_src()` to resolve.
-		// If it waits, a blank element is displayed until the async task finishes
-		// and it leads to undesirable flickering.
-		// So set `resolved_value` immediately above, and update it with the resolved values below later.
-		resolved_value = value;
-
-		if (value?.url) {
-			latest_value = value;
-			const resolving_value = value;
-			resolve_wasm_src(value.url).then((resolved_url) => {
-				if (latest_value === resolving_value) {
-					resolved_value = {
-						...resolving_value,
-						url: resolved_url ?? undefined
-					};
-				} else {
-					resolved_url && URL.revokeObjectURL(resolved_url);
-				}
-			});
-		}
-	}
-
-	onMount(() => {
-		engine = new BABYLON.Engine(canvas, true);
-		window.addEventListener("resize", () => {
-			engine?.resize();
-		});
-		mounted = true;
-	});
-
-	$: ({ path } = resolved_value || {
-		path: undefined
-	});
-
-	$: canvas && mounted && path && resolved_value && dispose();
-
-	function dispose(): void {
-		if (scene && !scene.isDisposed) {
-			scene.dispose();
-			engine?.stopRenderLoop();
-			engine?.dispose();
-			engine = null;
-			engine = new BABYLON.Engine(canvas, true);
-			window.addEventListener("resize", () => {
-				engine?.resize();
-			});
-		}
-		if (engine !== null) {
-			scene = add_new_model(
-				canvas,
-				scene,
-				engine,
-				resolved_value,
-				clear_color,
-				camera_position,
-				zoom_speed,
-				pan_speed
-			);
-		}
-	}
+	let canvas3d: Canvas3D;
+	let resolved_url: string | undefined;
 
 	function handle_undo(): void {
-		reset_camera_position(scene, camera_position, zoom_speed, pan_speed);
+		canvas3d.reset_camera_position(camera_position, zoom_speed, pan_speed);
 	}
 
 	$: {
 		if (
-			scene &&
-			(!dequal(current_settings.camera_position, camera_position) ||
-				current_settings.zoom_speed !== zoom_speed ||
-				current_settings.pan_speed !== pan_speed)
+			!dequal(current_settings.camera_position, camera_position) ||
+			current_settings.zoom_speed !== zoom_speed ||
+			current_settings.pan_speed !== pan_speed
 		) {
-			reset_camera_position(scene, camera_position, zoom_speed, pan_speed);
+			canvas3d.reset_camera_position(camera_position, zoom_speed, pan_speed);
 			current_settings = { camera_position, zoom_speed, pan_speed };
 		}
 	}
@@ -131,22 +46,28 @@
 	Icon={File}
 	label={label || i18n("3D_model.3d_model")}
 />
-{#if resolved_value}
+{#if value}
 	<div class="model3D">
 		<div class="buttons">
 			<IconButton Icon={Undo} label="Undo" on:click={() => handle_undo()} />
 			<a
-				href={resolved_value.url}
+				href={resolved_url}
 				target={window.__is_colab__ ? "_blank" : null}
-				download={window.__is_colab__
-					? null
-					: resolved_value.orig_name || resolved_value.path}
+				download={window.__is_colab__ ? null : value.orig_name || value.path}
 			>
 				<IconButton Icon={Download} label={i18n("common.download")} />
 			</a>
 		</div>
 
-		<canvas bind:this={canvas} />
+		<Canvas3D
+			bind:this={canvas3d}
+			bind:resolved_url
+			{value}
+			{clear_color}
+			{camera_position}
+			{zoom_speed}
+			{pan_speed}
+		/>
 	</div>
 {/if}
 
@@ -157,7 +78,7 @@
 		width: var(--size-full);
 		height: var(--size-full);
 	}
-	canvas {
+	.model3D :global(canvas) {
 		width: var(--size-full);
 		height: var(--size-full);
 		object-fit: contain;
--- js/model3D/shared/Model3DUpload.svelte
+++ js/model3D/shared/Model3DUpload.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import { createEventDispatcher, tick, onMount } from "svelte";
+	import { createEventDispatcher, tick } from "svelte";
 	import { Upload, ModifyUpload } from "@gradio/upload";
 	import type { FileData } from "@gradio/client";
 	import { BlockLabel } from "@gradio/atoms";
 	import { File } from "@gradio/icons";
-	import { add_new_model, reset_camera_position } from "./utils";
-	import { resolve_wasm_src } from "@gradio/wasm/svelte";
+	import type { I18nFormatter } from "@gradio/utils";
+	import Canvas3D from "./Canvas3D.svelte";
 
 	export let value: null | FileData;
 	export let clear_color: [number, number, number, number] = [0, 0, 0, 0];
@@ -23,91 +23,25 @@
 		null
 	];
 
-	let resolved_value: typeof value;
-
-	/* URL resolution for the Wasm mode. */
-	// The `value` prop can be updated before the Promise from `resolve_wasm_src` is resolved.
-	// In such a case, the resolved value for the old `value` has to be discarded,
-	// This variable `latest_value` is used to pick up only the value resolved for the latest `value` prop.
-	let latest_value: typeof value;
-	$: {
-		// In normal (non-Wasm) Gradio, the original `value` should be used immediately
-		// without waiting for `resolve_wasm_src()` to resolve.
-		// If it waits, a blank element is displayed until the async task finishes
-		// and it leads to undesirable flickering.
-		// So set `resolved_value` immediately above, and update it with the resolved values below later.
-		resolved_value = value;
-
-		if (value?.url) {
-			latest_value = value;
-			const resolving_value = value;
-			resolve_wasm_src(value.url).then((resolved_url) => {
-				if (latest_value === resolving_value) {
-					resolved_value = {
-						...resolving_value,
-						url: resolved_url ?? undefined
-					};
-				} else {
-					resolved_url && URL.revokeObjectURL(resolved_url);
-				}
-			});
-		}
-	}
-
-	let mounted = false;
-	let canvas: HTMLCanvasElement;
-	let scene: BABYLON.Scene;
-	let engine: BABYLON.Engine;
-
-	function reset_scene(): void {
-		scene = add_new_model(
-			canvas,
-			scene,
-			engine,
-			resolved_value,
-			clear_color,
-			camera_position,
-			zoom_speed,
-			pan_speed
-		);
-	}
-
-	onMount(() => {
-		if (resolved_value != null) {
-			reset_scene();
-		}
-		mounted = true;
-	});
-
-	$: ({ path } = resolved_value || {
-		path: undefined
-	});
-
-	$: canvas && mounted && path != null && resolved_value && reset_scene();
-
 	async function handle_upload({
 		detail
 	}: CustomEvent<FileData>): Promise<void> {
 		value = detail;
 		await tick();
-		reset_scene();
 		dispatch("change", value);
 		dispatch("load", value);
 	}
 
 	async function handle_clear(): Promise<void> {
-		if (scene && engine) {
-			scene.dispose();
-			engine.dispose();
-		}
 		value = null;
 		await tick();
 		dispatch("clear");
 		dispatch("change");
 	}
 
+	let canvas3D: Canvas3D;
 	async function handle_undo(): Promise<void> {
-		reset_camera_position(scene, camera_position, zoom_speed, pan_speed);
+		canvas3D.reset_camera_position(camera_position, zoom_speed, pan_speed);
 	}
 
 	const dispatch = createEventDispatcher<{
@@ -118,19 +52,6 @@
 	}>();
 
 	let dragging = false;
-
-	import * as BABYLON from "babylonjs";
-	import * as BABYLON_LOADERS from "babylonjs-loaders";
-	import type { I18nFormatter } from "@gradio/utils";
-
-	$: {
-		if (
-			BABYLON_LOADERS.OBJFileLoader != undefined &&
-			!BABYLON_LOADERS.OBJFileLoader.IMPORT_VERTEX_COLORS
-		) {
-			BABYLON_LOADERS.OBJFileLoader.IMPORT_VERTEX_COLORS = true;
-		}
-	}
 
 	$: dispatch("drag", dragging);
 </script>
@@ -155,7 +76,14 @@
 			on:undo={handle_undo}
 			absolute
 		/>
-		<canvas bind:this={canvas} />
+		<Canvas3D
+			bind:this={canvas3D}
+			{value}
+			{clear_color}
+			{camera_position}
+			{zoom_speed}
+			{pan_speed}
+		></Canvas3D>
 	</div>
 {/if}
 
@@ -169,7 +97,7 @@
 		height: var(--size-full);
 	}
 
-	canvas {
+	.input-model :global(canvas) {
 		width: var(--size-full);
 		height: var(--size-full);
 		object-fit: contain;
