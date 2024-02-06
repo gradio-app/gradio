f0564ee4d825dbf10bcf47fa4faac11dcfa76609
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-02-06 15:17:21 +0900

    Make Model3D.svelte Wasm-compatible

--- js/model3D/shared/Model3D.svelte
+++ js/model3D/shared/Model3D.svelte
@@ -7,6 +7,7 @@
 	import * as BABYLON from "babylonjs";
 	import * as BABYLON_LOADERS from "babylonjs-loaders";
 	import type { I18nFormatter } from "@gradio/utils";
+	import { resolve_wasm_src } from "@gradio/wasm/svelte";
 	import { dequal } from "dequal";
 
 	export let value: FileData | null;
@@ -39,6 +40,36 @@
 	let engine: BABYLON.Engine | null;
 	let mounted = false;
 
+	let resolved_value: typeof value;
+	/* URL resolution for the Wasm mode. */
+	// The `value` prop can be updated before the Promise from `resolve_wasm_src` is resolved.
+	// In such a case, the resolved value for the old `value` has to be discarded,
+	// This variable `latest_value` is used to pick up only the value resolved for the latest `value` prop.
+	let latest_value: typeof value;
+	$: {
+		// In normal (non-Wasm) Gradio, the original `value` should be used immediately
+		// without waiting for `resolve_wasm_src()` to resolve.
+		// If it waits, a blank element is displayed until the async task finishes
+		// and it leads to undesirable flickering.
+		// So set `resolved_value` immediately above, and update it with the resolved values below later.
+		resolved_value = value;
+
+		if (value?.url) {
+			latest_value = value;
+			const resolving_value = value;
+			resolve_wasm_src(value.url).then((resolved_url) => {
+				if (latest_value === resolving_value) {
+					resolved_value = {
+						...resolving_value,
+						url: resolved_url ?? undefined
+					};
+				} else {
+					resolved_url && URL.revokeObjectURL(resolved_url);
+				}
+			});
+		}
+	}
+
 	onMount(() => {
 		engine = new BABYLON.Engine(canvas, true);
 		window.addEventListener("resize", () => {
@@ -47,11 +78,11 @@
 		mounted = true;
 	});
 
-	$: ({ path } = value || {
+	$: ({ path } = resolved_value || {
 		path: undefined
 	});
 
-	$: canvas && mounted && path && dispose();
+	$: canvas && mounted && path && resolved_value && dispose();
 
 	function dispose(): void {
 		if (scene && !scene.isDisposed) {
@@ -69,7 +100,7 @@
 				canvas,
 				scene,
 				engine,
-				value,
+				resolved_value,
 				clear_color,
 				camera_position,
 				zoom_speed,
@@ -100,14 +131,16 @@
 	Icon={File}
 	label={label || i18n("3D_model.3d_model")}
 />
-{#if value}
+{#if resolved_value}
 	<div class="model3D">
 		<div class="buttons">
 			<IconButton Icon={Undo} label="Undo" on:click={() => handle_undo()} />
 			<a
-				href={value.url}
+				href={resolved_value.url}
 				target={window.__is_colab__ ? "_blank" : null}
-				download={window.__is_colab__ ? null : value.orig_name || value.path}
+				download={window.__is_colab__
+					? null
+					: resolved_value.orig_name || resolved_value.path}
 			>
 				<IconButton Icon={Download} label={i18n("common.download")} />
 			</a>
