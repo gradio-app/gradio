0dccf825c4b6e4b1124e1177aa0e767c0dfed188
Ari Roffe <aroffe9@gmail.com>
2024-02-12 12:06:46 -0600

    Add step parameter to slider (#7399)
    * Add step parameter to slider
    
    * fix demos
    
    * changes
    
    * tweaks to demo
    
    ---------
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- demo/hello_world_2/run.py
+++ demo/hello_world_2/run.py
@@ -1,11 +1,11 @@
 import gradio as gr
 
 def greet(name, intensity):
-    return "Hello " * intensity + name + "!"
+    return "Hello, " + name + "!" * intensity
 
 demo = gr.Interface(
     fn=greet,
-    inputs=["text", gr.Slider(value=2, minimum=1, maximum=10)],
+    inputs=["text", gr.Slider(value=2, minimum=1, maximum=10, step=1)],
     outputs=[gr.Textbox(label="greeting", lines=3)],
 )
 
