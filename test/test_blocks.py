1ff032ab4a9769ee86bfc604c1c9d549ba969f1b
Abubakar Abid <abubakar@huggingface.co>
2024-02-12 13:08:19 -0800

    add test

--- test/test_blocks.py
+++ test/test_blocks.py
@@ -7,6 +7,7 @@ import random
 import sys
 import time
 import uuid
+import warnings
 from concurrent.futures import wait
 from contextlib import contextmanager
 from functools import partial
@@ -822,6 +823,29 @@ class TestStateHolder:
         assert (
             "error" not in session_1.json()
         )  # no error because sesssion 1 block config was lost when session 3 was added
+
+    def test_state_holder_is_used_in_postprocess(self):
+        with gr.Blocks() as demo:
+            dropdown = gr.Dropdown(label="list", choices=["Choice 1"], interactive=True)
+            button = gr.Button("Get dropdown value")
+            button2 = gr.Button("Convert dropdown to multiselect")
+            button.click(lambda x:x, inputs=dropdown, outputs=dropdown)
+            button2.click(lambda :gr.Dropdown(multiselect=True), outputs=dropdown)
+
+        app, _, _ = demo.launch(prevent_thread_lock=True)
+        client = TestClient(app)
+
+        response = client.post(
+            "/api/predict/", json={"data": ["Choice 1"], "session_hash": "1", "fn_index": 0}
+        )
+        assert response.json()["data"][0] == "Choice 1"
+        response = client.post(
+            "/api/predict/", json={"data": [], "session_hash": "1", "fn_index": 1}
+        )
+        response = client.post(
+            "/api/predict/", json={"data": ["Choice 1"], "session_hash": "1", "fn_index": 0}
+        )
+        assert response.json()["data"][0] == ["Choice 1"]
 
 
 class TestCallFunction:
