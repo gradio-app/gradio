3e0f194efcfb91614f525f2aa8638761f4051a90
Abubakar Abid <abubakar@huggingface.co>
2024-02-13 08:28:23 -0800

    fix

--- js/app/src/Blocks.svelte
+++ js/app/src/Blocks.svelte
@@ -529,7 +529,7 @@
 							if (
 								instance_map[id].props.interactive &&
 								status.stage === "pending" &&
-								!["focus", "type"].includes(dep.targets[0][1])
+								!["focus", "key_up"].includes(dep.targets[0][1])
 							) {
 								pending_outputs.push(id);
 								instance_map[id].props.interactive = false;
