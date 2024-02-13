65437ce832f806da316aa074539b6263e1d8b7ac
aliabid94 <aabid94@gmail.com>
2024-02-13 16:51:47 -0600

    Improve File Explorer performance (#7337)
    * changes
    
    * add changeset
    
    * changes
    
    * changes
    
    * add changeset
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * Update gradio/components/file_explorer.py
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    
    * Update gradio/components/file_explorer.py
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    
    * Update demo/file_explorer_component_events/run.py
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    
    * changes
    
    * changes
    
    * changes
    
    ---------
    
    Co-authored-by: Ali Abid <aliabid94@gmail.com>
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- scripts/copy_demos.py
+++ scripts/copy_demos.py
@@ -23,6 +23,7 @@ def copy_all_demos(source_dir: str, dest_dir: str):
         "code",
         "fake_gan",
         "fake_diffusion_with_gif",
+        "file_explorer_component_events",
         "image_mod_default_image",
         "image_segmentation",
         "interface_random_slider",
