f1917867916647d383b8d7ce15e0c17f2abbdec1
Hannah <hannahblair@users.noreply.github.com>
2024-02-16 19:57:12 +0100

    Improve responsiveness of `gr.Audio()` controls (#6991)
    * improve audio container in mobile
    
    * check for denied device access
    
    * add changeset
    
    * add changeset
    
    * tweak controls placements
    
    * add changeset
    
    * add viewport modes to test
    
    * add changeset
    
    * add changeset
    
    * add changeset
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>
    Co-authored-by: pngwn <hello@pngwn.io>

---
+++ .changeset/nine-states-sip.md
@@ -0,0 +1,7 @@
+---
+"@gradio/audio": patch
+"@gradio/icons": patch
+"gradio": patch
+---
+
+fix:Improve responsiveness of `gr.Audio()` controls
--- js/icons/src/Trim.svelte
+++ js/icons/src/Trim.svelte
@@ -1,7 +1,7 @@
 <svg
 	xmlns="http://www.w3.org/2000/svg"
-	width="20"
-	height="20"
+	width="100%"
+	height="100%"
 	viewBox="0 0 24 24"
 	fill="none"
 	stroke="currentColor"
