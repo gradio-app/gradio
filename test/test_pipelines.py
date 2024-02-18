43f8b49bcae8757521dda94ad821c52ee9c2e958
shubhamofbce <sraj13169@gmail.com>
2024-02-18 18:30:29 +0530

    Add unit tests for one transformer and diffuser load from pipeline

--- test/test_pipelines.py
+++ test/test_pipelines.py
@@ -2,6 +2,7 @@ import pytest
 import transformers
 
 import gradio as gr
+from gradio.interface import Interface
 
 
 def test_text_to_text_model_from_pipeline():
@@ -22,3 +23,26 @@ def test_interface_in_blocks():
             gr.Interface.from_pipeline(pipe2)
     demo.launch(prevent_thread_lock=True)
     demo.close()
+
+
+def test_transformers_load_from_pipeline():
+    from transformers import pipeline
+
+    pipe = pipeline(model="deepset/roberta-base-squad2")
+    io = Interface.from_pipeline(pipe)
+    assert io.input_components[0].label == "Context"
+    assert io.input_components[1].label == "Question"
+    assert io.output_components[0].label == "Answer"
+    assert io.output_components[1].label == "Score"
+
+
+def test_diffusers_load_from_pipeline():
+    from diffusers import StableDiffusionPipeline
+
+    pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+    io = Interface.from_pipeline(pipe)
+    assert io.input_components[0].label == "Prompt"
+    assert io.input_components[1].label == "Negative prompt"
+    assert io.input_components[2].label == "Number of inference steps"
+    assert io.input_components[3].label == "Guidance scale"
+    assert io.output_components[0].label == "Generated Image"
