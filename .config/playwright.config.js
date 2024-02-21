6588d3c8cb61c5a3469486e5ffd3416c9048c591
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-02-21 19:28:56 +0900

    Revert "comment out failed tests"
    This reverts commit 3580d7988714f49a21e221fe230b26ee86b66a68.

--- .config/playwright.config.js
+++ .config/playwright.config.js
@@ -39,10 +39,7 @@ const lite = defineConfig(base, {
 		"**/custom_css.spec.ts", // Media queries is not working somehow.
 		"**/clear_components.spec.ts", // `gr.Image()` with remote image is not supported in lite because it calls `httpx.stream` through `processing_utils.save_url_to_cache()`.
 		"**/load_space.spec.ts", // `gr.load()`, which calls `httpx.get` is not supported in lite.
-		"**/gradio_pdf_demo.spec.ts", // The PDF component is not working on Lite, https://github.com/gradio-app/gradio/issues/7476
-		"**/file_explorer_component_events.spec.ts",
-		"**/gallery_component_events.spec.ts",
-		"**/queue_full_e2e_test.spec.ts"
+		"**/gradio_pdf_demo.spec.ts" // The PDF component is not working on Lite, https://github.com/gradio-app/gradio/issues/7476
 	],
 	expect: { timeout: 60000 },
 	timeout: 60000,
