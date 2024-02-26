2cf34c19cb4ffcb1ddfbf6b41c71e6256b1b6e5f
Abubakar Abid <abubakar@huggingface.co>
2024-02-26 11:58:57 -0800

    quick fix

--- js/app/src/Login.stories.svelte
+++ js/app/src/Login.stories.svelte
@@ -25,3 +25,7 @@
 	name="Login page with auth message"
 	args={{ auth_message: "Log in to this great gradio app" }}
 />
+<Story
+	name="Login page with HTML auth message"
+	args={{ auth_message: "<em>Log in to this <strong>great</strong> gradio app</em>" }}
+/>
