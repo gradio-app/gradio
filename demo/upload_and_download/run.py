3418df4812ca31967009dbd286821eceb4e88ad7
Abubakar Abid <abubakar@huggingface.co>
2024-02-27 12:26:42 -0800

    download

--- demo/upload_and_download/run.py
+++ demo/upload_and_download/run.py
@@ -3,7 +3,7 @@ import gradio as gr
 
 def upload_file(filepath):
     name = Path(filepath).name
-    return [gr.UploadButton(visible=False), gr.DownloadButton(label=f"Download {name}", value=filepath, visible=True, icon="cheetah.jpg")]
+    return [gr.UploadButton(visible=False), gr.DownloadButton(label=f"Download {name}", value=filepath, visible=True)]
 
 def download_file():
     return [gr.UploadButton(visible=True), gr.DownloadButton(visible=False)]
@@ -12,7 +12,7 @@ with gr.Blocks() as demo:
     gr.Markdown("First upload a file and and then you'll be able download it (but only once!)")
     with gr.Row():
         u = gr.UploadButton("Upload a file", file_count="single")
-        d = gr.DownloadButton("Download a file", visible=False)
+        d = gr.DownloadButton("Download the file", visible=False)
 
     u.upload(upload_file, u, [u, d])
     d.click(download_file, None, [u, d])
