afcc6c3f59c352eb3dc2a86c98b6735a4ba05bdb
Dawood <dawoodkhan82@gmail.com>
2024-03-05 15:18:06 -0500

    revert

--- js/dropdown/shared/Dropdown.svelte
+++ js/dropdown/shared/Dropdown.svelte
@@ -56,7 +56,7 @@
 			old_input_text = input_text;
 		}
 		set_input_text();
-	} else if (choices.length > 0 && value != "") {
+	} else if (choices.length > 0) {
 		old_selected_index = 0;
 		selected_index = 0;
 		[input_text, value] = choices[selected_index];
