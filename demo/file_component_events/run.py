26356a623c4196f48ca236d973a597831743cdb8
Dawood Khan <dawoodkhan82@gmail.com>
2024-03-05 16:52:22 -0500

    Fixes: Invalid file_type breaks drag and drop (#7559)
    * file type fix
    
    * add changeset
    
    * pdf test
    
    * format
    
    * test fixes
    
    * file test fix
    
    * file path fix
    
    * new test
    
    * format
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>
    Co-authored-by: Hannah <hannahblair@users.noreply.github.com>

--- demo/file_component_events/run.py
+++ demo/file_component_events/run.py
@@ -23,6 +23,20 @@ with gr.Blocks() as demo:
             output_file_3 = gr.File(label="Upload Multiple Files Output Image/Video", file_count="multiple")
             num_load_btn_3 = gr.Number(label="# Load Upload Multiple Files Image/Video", value=0)
             file_component_specific.upload(lambda s,n: (s, n + 1), [file_component_specific, num_load_btn_3], [output_file_3, num_load_btn_3])
+    with gr.Row():
+        with gr.Column():
+            file_component_pdf = gr.File(label="Upload PDF File", file_types=["pdf"])
+        with gr.Column():
+            output_file_4 = gr.File(label="Upload PDF File Output")
+            num_load_btn_4 = gr.Number(label="# Load Upload PDF File", value=0)
+            file_component_pdf.upload(lambda s,n: (s, n + 1), [file_component_pdf, num_load_btn_4], [output_file_4, num_load_btn_4])
+    with gr.Row():
+        with gr.Column():
+            file_component_invalid = gr.File(label="Upload File with Invalid file_types", file_types=["invalid file_type"])
+        with gr.Column():
+            output_file_5 = gr.File(label="Upload File with Invalid file_types Output")
+            num_load_btn_5 = gr.Number(label="# Load Upload File with Invalid file_types", value=0)
+            file_component_invalid.upload(lambda s,n: (s, n + 1), [file_component_invalid, num_load_btn_5], [output_file_5, num_load_btn_5])
 
 if __name__ == "__main__":
     demo.launch()
