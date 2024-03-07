d9f3db89a815d5b5cac8519ca0d4cffed33930aa
Abubakar Abid <abubakar@huggingface.co>
2024-03-07 15:21:14 -0800

    clean

--- client/python/gradio_client/client.py
+++ client/python/gradio_client/client.py
@@ -90,7 +90,7 @@ class Client:
             output_dir: The directory to save files that are downloaded from the remote API. If None, reads from the GRADIO_TEMP_DIR environment variable. Defaults to a temporary directory on your machine.
             verbose: Whether the client should print statements to the console.
             headers: Additional headers to send to the remote Gradio app on every request. By default only the HF authorization and user-agent headers are sent. These headers will override the default headers if they have the same keys.
-            upload_files: Whether the client should treat input string filepath as files and upload them to the remote server. If False, the client will treat input string filepaths as strings always and not modify them, and files should be passed in explicitly using `gradio_client.File("path/to/file/or/url")` instead. This parameter will be deleted and False will become the default in a future version.
+            upload_files: Whether the client should treat input string filepath as files and upload them to the remote server. If False, the client will treat input string filepaths as strings always and not modify them, and files should be passed in explicitly using `gradio_client.file("path/to/file/or/url")` instead. This parameter will be deleted and False will become the default in a future version.
             download_files: Whether the client should download output files from the remote API and return them as string filepaths on the local machine. If False, the client will return a FileData dataclass object with the filepath on the remote machine instead.
         """
         self.verbose = verbose
@@ -996,7 +996,6 @@ class Endpoint:
 
             data = self.insert_empty_state(*data)
             data = self.process_input_files(*data)
-            print("input data", data)
             predictions = _predict(*data)
             predictions = self.process_predictions(*predictions)
 
