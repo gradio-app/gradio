94542228821d5ee3313b6cf4ee9be9642e1c40b6
Abubakar Abid <abubakar@huggingface.co>
2024-03-11 15:11:59 -0700

    version

--- test/test_buttons.py
+++ test/test_buttons.py
@@ -2,8 +2,8 @@ import sys
 
 class TestClearButton:
     def test_version(self):
-        print(sys.version)
-        def test(d: dict]str, str]):
+        print("version>>>>>>>", sys.version)
+        def test(d: dict[str, str]):
             return d
-        test({"a": "b"}, "c")
+        test({"a": "b"})
         1/0
