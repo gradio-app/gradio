3dbde434efa4df12ca2c1056219e878d1a1dfed8
Hannah <hannahblair@users.noreply.github.com>
2024-03-19 15:32:07 +0000

    migrate to storybook 8

--- js/storybook/main.ts
+++ js/storybook/main.ts
@@ -4,16 +4,17 @@ import turbosnap from "vite-plugin-turbosnap";
 
 const config: StorybookConfig = {
 	stories: [
-		"../../js/**/*.mdx",
-		"../../js/**/*.stories.@(js|jsx|ts|tsx|svelte|mdx)"
-	],
+        "../../js/**/*.mdx",
+        "../../js/**/*.@(mdx|stories.@(js|jsx|ts|tsx|svelte))"
+    ],
 	addons: [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-interactions",
-		"@storybook/addon-svelte-csf",
-		"@storybook/addon-a11y"
-	],
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        "@storybook/addon-svelte-csf",
+        "@storybook/addon-a11y",
+        "@storybook/addon-mdx-gfm"
+    ],
 	framework: {
 		name: "@storybook/svelte-vite",
 		options: {
