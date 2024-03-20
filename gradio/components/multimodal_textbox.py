86d0e0eded70435cf4ef884bbc80c412800ce91f
Abubakar Abid <abubakar@huggingface.co>
2024-03-20 14:06:45 -0700

    not required

--- gradio/components/multimodal_textbox.py
+++ gradio/components/multimodal_textbox.py
@@ -4,11 +4,12 @@ from __future__ import annotations
 
 import tempfile
 from pathlib import Path
-from typing import Any, Callable, List, Literal, NotRequired, TypedDict
+from typing import Any, Callable, List, Literal, TypedDict
 
 import gradio_client.utils as client_utils
 from gradio_client.documentation import document
 from pydantic import Field
+from typing_extensions import NotRequired
 
 from gradio.components.base import FormComponent
 from gradio.data_classes import FileData, GradioModel
