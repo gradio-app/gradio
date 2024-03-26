24afa3d0b4c0305f03df76b70cdcca89f22f32e9
pngwn <hello@pngwn.io>
2024-03-26 15:44:30 +0000

    more fix

--- demo/image_editor_events/run.py
+++ demo/image_editor_events/run.py
@@ -19,10 +19,13 @@ with gr.Blocks() as demo:
             live=True,
         )
         im_preview = gr.Image()
-    # n_upload = gr.Number(0, label="Number of upload events", step=1)
-    # n_change = gr.Number(0, label="Number of change events", step=1)
+    n_upload = gr.Number(0, label="Number of upload events", step=1)
+    n_change = gr.Number(0, label="Number of change events", step=1)
+    n_input = gr.Number(0, label="Number of input events", step=1)
 
-    # im.upload(lambda x: x + 1, outputs=n_upload, inputs=n_upload)
+    im.upload(lambda x: x + 1, outputs=n_upload, inputs=n_upload)
+    im.change(lambda x: x + 1, outputs=n_change, inputs=n_change)
+    im.input(lambda x: x + 1, outputs=n_input, inputs=n_input)
     im.change(predict, outputs=im_preview, inputs=im, show_progress="hidden")
 
 if __name__ == "__main__":
