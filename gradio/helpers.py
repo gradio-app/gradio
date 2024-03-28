03407bd760c77b514d59e31087cab097f2b26bbc
Abubakar Abid <abubakar@huggingface.co>
2024-03-27 22:41:13 -0700

    check

--- gradio/helpers.py
+++ gradio/helpers.py
@@ -452,8 +452,6 @@ class Examples:
         )
 
     def write_to_cache(self, value):
-        if not self.cache_logger._is_setup:
-            self.cache_logger.setup(self.outputs, self.cached_folder)
         self.cache_logger.flag(value)
 
     def load_from_cache(self, example_id: int) -> list[Any]:
