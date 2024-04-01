cc717192cf659c97c25636d21e50ed1b389625c0
Ali Abid <aliabid94@gmail.com>
2024-04-01 02:02:11 -0700

    changes

--- js/app/test/stream_audio_out.spec.ts
+++ js/app/test/stream_audio_out.spec.ts
@@ -4,14 +4,15 @@ test("audio streams correctly", async ({ page }) => {
 	const uploader = await page.locator("input[type=file]");
 	await uploader.setInputFiles(["../../test/test_files/audio_sample.wav"]);
 	await page.getByRole('button', { name: 'Stream as File' }).click();
+	await page.waitForSelector('audio');
 	const isAudioPlaying = await page.evaluate(async () => {
 		const audio = document.querySelector('audio');
 		if (!audio) {
 			return false;
 		}
 		await audio.play();
-		await new Promise(resolve => setTimeout(resolve, 1000));
-		return !audio.paused && audio.currentTime > 0 && !audio.ended;
+		await new Promise(resolve => setTimeout(resolve, 2000));
+		return audio.currentTime > 0;
 	});
 	await expect(isAudioPlaying).toBeTruthy();
 });
