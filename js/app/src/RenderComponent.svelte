7c9a964ac6b9c2231c7439758e87b4fac56db99f
aliabid94 <aabid94@gmail.com>
2024-04-08 11:34:25 -0700

    Fix programmatic tab selection (#7916)
    * changes
    
    * changes
    
    * add changeset
    
    * changes
    
    * add changeset
    
    * changes
    
    * add changeset
    
    ---------
    
    Co-authored-by: Ali Abid <aliabid94@gmail.com>
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>
    Co-authored-by: pngwn <hello@pngwn.io>
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- js/app/src/RenderComponent.svelte
+++ js/app/src/RenderComponent.svelte
@@ -16,7 +16,7 @@
 	export let gradio: Gradio;
 	export let elem_id: string;
 	export let elem_classes: string[];
-	export let id: number;
+	export let _id: number;
 
 	const s = (id: number, p: string, v: any): CustomEvent =>
 		new CustomEvent("prop_change", { detail: { id, prop: p, value: v } });
@@ -32,7 +32,7 @@
 
 				function report(props: string) {
 					return function (propargs: any) {
-						const ev = s(id, props, propargs);
+						const ev = s(_id, props, propargs);
 						target.dispatchEvent(ev);
 					};
 				}
