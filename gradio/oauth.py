8bccf8e776c3217cdd862409455e333ddc18f358
Wauplin <lucainp@gmail.com>
2024-04-09 12:08:27 +0200

    shoud be fine

--- gradio/oauth.py
+++ gradio/oauth.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import hashlib
 import os
+import traceback
 import typing
 import urllib.parse
 import warnings
@@ -39,7 +40,7 @@ def attach_oauth(app: fastapi.FastAPI):
     # Session Middleware requires a secret key to sign the cookies. Let's use a hash
     # of the OAuth secret key to make it unique to the Space + updated in case OAuth
     # config gets updated.
-    session_secret = (OAUTH_CLIENT_SECRET or "") + "-v3"
+    session_secret = (OAUTH_CLIENT_SECRET or "") + "-v4"
     # ^ if we change the session cookie format in the future, we can bump the version of the session secret to make
     #   sure cookies are invalidated. Otherwise some users with an old cookie format might get a HTTP 500 error.
     app.add_middleware(
@@ -54,6 +55,7 @@ def _add_oauth_routes(app: fastapi.FastAPI) -> None:
     """Add OAuth routes to the FastAPI app (login, callback handler and logout)."""
     try:
         from authlib.integrations.starlette_client import OAuth
+
     except ImportError as e:
         raise ImportError(
             "Cannot initialize OAuth to due a missing library. Please run `pip install gradio[oauth]` or add "
