1090a5e92741f8cf480cca573d14cfb5e2fb1b63
Hannah <hannahblair@users.noreply.github.com>
2024-04-12 17:54:12 +0200

    config tweak

--- client/js/vite.config.js
+++ client/js/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
 	build: {
 		lib: {
-			entry: "src/index.ts",
+			entry: resolve(__dirname, "src/index.ts"),
 			formats: ["es", "umd"],
 			name: "client"
 		},
@@ -19,7 +20,8 @@ export default defineConfig({
 	plugins: [
 		svelte(),
 		dts({
-			insertTypesEntry: true
+			insertTypesEntry: true,
+			rollupTypes: true
 		})
 	],
 
