17e83c958ebb35b3e122ca486067d1bd5ce33a22
Ali Abdalla <ali.si3luwa@gmail.com>
2024-04-23 13:33:45 -0700

    Docs Reorg and Intro Page (#8061)
    * docs intro page and guides
    
    * python library docs and js client
    
    * reorg
    
    * changes
    
    * add better hovering
    
    * fix broken version routing
    
    * add redirects and remove duplicate pages
    
    * fix build issues
    
    * fix issues
    
    * formatting
    
    * add changeset
    
    * add changeset
    
    * Update js/_website/src/routes/[[version]]/docs/+page.svelte
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    
    * Update js/_website/src/routes/[[version]]/docs/+page.svelte
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    
    * Update js/_website/src/routes/[[version]]/docs/+page.svelte
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    
    * Update js/_website/src/routes/[[version]]/docs/+page.svelte
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    
    * Update js/_website/src/routes/[[version]]/docs/+page.svelte
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    
    * Update js/_website/src/routes/[[version]]/docs/+page.svelte
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    
    * styling changes
    
    * formatting
    
    * fix build
    
    * rounded corners on buttons
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- js/_website/src/lib/components/DocsNavCustom.svelte
+++ js/_website/src/lib/components/DocsNavCustom.svelte
@@ -2,11 +2,11 @@
 	// @ts-nocheck
 	import { clickOutside } from "./clickOutside.js";
 
-	export let js_components: any;
+	export let items: any;
+	export let title: string;
 
 	export let current_nav_link = "";
-	export let version_dropdown = false;
-	let docs_type = "js";
+	let docs_type = "python";
 
 	let show_nav = false;
 	let searchTerm = "";
@@ -101,51 +101,27 @@
 			placeholder="Search ⌘-k / ctrl-k"
 			autocomplete="off"
 		/>
-		{#if version_dropdown}
-			<DropDown docs_type="js"></DropDown>
-		{/if}
+		<!-- <DropDown></DropDown> -->
+		<!-- <select
+			bind:value={docs_type}
+			on:change={() => {
+				if (docs_type == "js") {
+					window.location.href = "/main/docs/js/";
+				}
+			}}
+			class="rounded-md border-gray-200 focus:placeholder-transparent focus:shadow-none focus:border-orange-500 focus:ring-0 text-xs mt-2 py-1 pl-2 pr-7 font-mono"
+		>
+			<option value="js">js</option>
+			<option value="python">python</option>
+		</select> -->
 	</div>
 
-	<p
-		class="mx-4 block bg-gradient-to-r from-orange-100 to-orange-50 border border-orange-200 px-4 py-0.5 mr-2 rounded-full text-orange-800 mb-1 w-fit hover:shadow-lg"
-	>
-		<a class="inline-block" href="./storybook">
-			<svg
-				viewBox="0 0 20 20"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-				role="presentation"
-				class="w-4 inline-block mb-1"
-			>
-				<path
-					d="M.62 18.43 0 1.92A1.006 1.006 0 0 1 .944.88L14.984.002a1.005 1.005 0 0 1 1.069 1.004v17.989a1.006 1.006 0 0 1-1.051 1.004L1.58 19.396a1.006 1.006 0 0 1-.96-.967Z"
-					fill="#FF4785"
-				></path>
-				<path
-					fill-rule="evenodd"
-					clip-rule="evenodd"
-					d="m13.88.071-1.932.12-.094 2.267a.15.15 0 0 0 .24.126l.88-.668.744.586a.15.15 0 0 0 .243-.123l-.08-2.308Zm-1.504 7.59c-.353.275-2.989.462-2.989.071.056-1.493-.612-1.558-.984-1.558-.352 0-.946.106-.946.906 0 .815.868 1.275 1.887 1.815 1.447.767 3.2 1.696 3.2 4.032 0 2.24-1.82 3.476-4.14 3.476-2.395 0-4.488-.969-4.252-4.328.093-.394 3.138-.3 3.138 0-.038 1.386.278 1.794 1.076 1.794.613 0 .891-.338.891-.906 0-.861-.904-1.369-1.945-1.953-1.409-.791-3.067-1.722-3.067-3.859 0-2.132 1.466-3.554 4.084-3.554 2.618 0 4.047 1.4 4.047 4.064Z"
-					fill="#fff"
-				></path>
-			</svg>
-
-			Storybook &rarr;</a
-		>
-	</p>
-
-	<p class="font-semibold px-4 my-2 block">Components</p>
-
-	{#each js_components as name}
+	<p class="font-semibold px-4 my-2 block">{title}</p>
+	{#each Object.entries(items) as [name, url] (name)}
 		<a
 			class:current-nav-link={current_nav_link == name}
 			class="px-4 block thin-link"
-			href="./{name}/">{name}</a
+			href={url}>{name}</a
 		>
 	{/each}
-	<p class="font-semibold px-4 my-2 block">Client</p>
-	<a
-		class:current-nav-link={current_nav_link == "js-client"}
-		class="px-4 block thin-link"
-		href="./js-client/">JS Client</a
-	>
 </div>
