fad5ab4b2ef03d2aad2f9ead0d6dc853f9e794eb
pngwn <hello@pngwn.io>
2024-04-24 19:15:37 +0100

    tailwind test

--- js/preview/test/test/frontend/gradio.config.js
+++ js/preview/test/test/frontend/gradio.config.js
@@ -1,19 +1,5 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default {
-	plugins: [
-		{
-			name: "TEST PLUGIN",
-			transform() {
-				console.log("TRANFORMING FROM CUSTOM PLUGIN");
-			}
-		}
-	],
-	svelte: {
-		preprocess: [
-			{
-				markup() {
-					console.log("PREPROCESSING FROM CUSTOM PLUGIN");
-				}
-			}
-		]
-	}
+	plugins: [tailwindcss()]
 };
