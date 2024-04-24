e9ada4724320744cc910bd790d1833426431591c
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-04-24 23:37:33 +0900

    Update gr.JSON's docstring

--- gradio/components/json_component.py
+++ gradio/components/json_component.py
@@ -77,7 +77,7 @@ class JSON(Component):
     def postprocess(self, value: dict | list | str | None) -> dict | list | None:
         """
         Parameters:
-            value: Expects a `str` filepath to a file containing valid JSON -- or a `list` or `dict` that is valid JSON. The JSON value can contain NumPy arrays.
+            value: Expects a valid JSON `str` -- or a `list` or `dict` that can be serialized to a JSON string. The `list` or `dict` value can contain NumPy arrays.
         Returns:
             Returns the JSON as a `list` or `dict`.
         """
