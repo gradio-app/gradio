8e0d09db5d1400ac4a035a8cb585ae2a26f066a5
Tiger3018 <tiger3018of02@gmail.com>
2024-05-03 01:58:25 +0800

    fix: handling SIGINT correctly in reload.py, single entrance of block_thread in blocks.py (#8158)
    * fix: handling SIGINT, single block_thread and fix popen
    
    * Use pass
    
    ---------
    
    Co-authored-by: freddyaboulton <alfonsoboulton@gmail.com>

--- gradio/cli/commands/reload.py
+++ gradio/cli/commands/reload.py
@@ -126,7 +126,11 @@ def main(
             GRADIO_WATCH_DEMO_PATH=str(path),
         ),
     )
-    popen.wait()
+    if popen.poll() is None:
+        try:
+            popen.wait()
+        except (KeyboardInterrupt, OSError):
+            pass
 
 
 if __name__ == "__main__":
