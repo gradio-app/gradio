9a8b484022680278f8e6e63710c6092d7d6b8752
aliabd <ali.si3luwa@gmail.com>
2024-05-05 23:08:22 -0700

    use lite for demos

--- js/_website/src/lib/components/Demos.svelte
+++ js/_website/src/lib/components/Demos.svelte
@@ -13,7 +13,22 @@
 	/>
 </svelte:head>
 
-{#if url_version === "main"}
+{#if url_version === "3.50.2"}
+	<div class="codeblock" id="{name}_code">
+		<a
+			class="clipboard-button m-2"
+			href="https://colab.research.google.com/github/gradio-app/gradio/blob/main/demo/{name}/run.ipynb"
+			target="_blank"
+			style="right:30px"
+		>
+			<img src="https://colab.research.google.com/assets/colab-badge.svg" />
+		</a>
+		<pre class=" max-h-80 overflow-auto"><code class="code language-python"
+				>{@html highlighted_code}</code
+			>
+	</pre>
+	</div>
+{:else }
 	<div class="py-2 max-h-[750px] overflow-y-scroll">
 		{#key name}
 			<button
@@ -31,29 +46,8 @@
 			</gradio-lite>
 		{/key}
 	</div>
-{:else}
-	<div class="codeblock" id="{name}_code">
-		<a
-			class="clipboard-button m-2"
-			href="https://colab.research.google.com/github/gradio-app/gradio/blob/main/demo/{name}/run.ipynb"
-			target="_blank"
-			style="right:30px"
-		>
-			<img src="https://colab.research.google.com/assets/colab-badge.svg" />
-		</a>
-		<pre class=" max-h-80 overflow-auto"><code class="code language-python"
-				>{@html highlighted_code}</code
-			>
-	</pre>
-	</div>
-	{#key name}
-		{#if url_version === "3.50.2"}
-			<gradio-app space={"gradio/" + name + "_3-x"} />
-		{:else}
-			<gradio-app space={"gradio/" + name} />
-		{/if}
-	{/key}
 {/if}
+
 
 <style>
 	.open-btn {
