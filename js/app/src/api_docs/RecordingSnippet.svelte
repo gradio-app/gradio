1b584f631692be5c8537aed3388d07d3be30b390
Abubakar Abid <abubakar@huggingface.co>
2024-05-17 04:54:38 -0700

    add logging

--- js/app/src/api_docs/RecordingSnippet.svelte
+++ js/app/src/api_docs/RecordingSnippet.svelte
@@ -72,14 +72,15 @@
 	}
 
 	onMount(async () => {
+		console.log("mount")
 		const data = await get_info();
+		console.log(data, "data");
 		endpoints_info = data["named_endpoints"];
-		console.loe("data", data)
 		console.log("endpoints_info", endpoints_info)
-
 		let py_api_calls: string[] = api_calls.map((call) =>
 			format_api_call(call, "py")
 		);
+		console.log("py_api_calls", py_api_calls)
 		let js_api_calls: string[] = api_calls.map((call) =>
 			format_api_call(call, "js")
 		);
