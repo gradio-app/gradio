f8cb6d692ba04d446706fbff257152f99780cd0d
Ali Abid <aliabid94@gmail.com>
2024-05-20 15:20:40 -0700

    changes

--- client/js/src/helpers/api_info.ts
+++ client/js/src/helpers/api_info.ts
@@ -206,6 +206,7 @@ export function get_description(
 	return type?.description;
 }
 
+/* eslint-disable complexity */
 export function handle_message(
 	data: any,
 	last_status: Status["stage"]
@@ -338,6 +339,8 @@ export function handle_message(
 
 	return { type: "none", status: { stage: "error", queue } };
 }
+/* eslint-enable complexity */
+
 
 /**
  * Maps the provided `data` to the parameters defined by the `/info` endpoint response.
