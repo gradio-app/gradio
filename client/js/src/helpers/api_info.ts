77766e7dfef194363efda2b3d17149742270c8ed
Ali Abid <aliabid94@gmail.com>
2024-05-21 11:45:40 -0700

    changes

--- client/js/src/helpers/api_info.ts
+++ client/js/src/helpers/api_info.ts
@@ -341,7 +341,6 @@ export function handle_message(
 }
 /* eslint-enable complexity */
 
-
 /**
  * Maps the provided `data` to the parameters defined by the `/info` endpoint response.
  * This allows us to support both positional and keyword arguments passed to the client
