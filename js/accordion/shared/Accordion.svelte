107e1ed940bc053d20b7d4a13fd370631fbefe4b
Hannah <hannahblair@users.noreply.github.com>
2024-05-21 13:02:44 +0200

    add accordion_text_color var

--- js/accordion/shared/Accordion.svelte
+++ js/accordion/shared/Accordion.svelte
@@ -23,7 +23,7 @@
 		justify-content: space-between;
 		cursor: pointer;
 		width: var(--size-full);
-		color: var(--body-text-color);
+		color: var(--accordion_text_color);
 	}
 	.label-wrap.open {
 		margin-bottom: var(--size-2);
