d6db975b345f6f092e4bddc5d0c0c9d108f2e3f8
Ali Abid <aliabid94@gmail.com>
2024-05-29 13:41:27 -0700

    changes

--- client/python/gradio_client/documentation.py
+++ client/python/gradio_client/documentation.py
@@ -58,7 +58,7 @@ _module_prefixes = [
     ("gradio.theme", "themes"),
     ("gradio_client.", "py-client"),
     ("gradio.utils", "helpers"),
-    ("gradio.renderable", "renderable")
+    ("gradio.renderable", "renderable"),
 ]
 
 
