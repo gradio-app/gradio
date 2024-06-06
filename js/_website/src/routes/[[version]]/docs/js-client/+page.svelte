a9e6595817b741c3dcf1eaedf58ee4f901784e57
Ali Abdalla <ali.si3luwa@gmail.com>
2024-06-05 18:21:35 -0700

    Tweak meta titles and descriptions for clients (#8471)
    * tweak title and description for client
    
    * fix title/desc for js client
    
    * formatting
    
    * add changeset
    
    * add changeset
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- js/_website/src/routes/[[version]]/docs/js-client/+page.svelte
+++ js/_website/src/routes/[[version]]/docs/js-client/+page.svelte
@@ -20,10 +20,10 @@
 </script>
 
 <MetaTags
-	title={"Gradio Python Client Docs"}
+	title={"Gradio Javascript Client Docs"}
 	url={$page.url.pathname}
 	canonical={$page.url.pathname}
-	description={"The lightweight Gradio client library that makes it easy to use any Gradio app as an API"}
+	description={"Make programmatic requests to Gradio applications in JavaScript (TypeScript) from the browser or server-side."}
 />
 
 <main class="container mx-auto px-4 flex gap-4">
