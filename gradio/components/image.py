ed48a937b01da945b8ea507ab447b22e22d7e805
Abubakar Abid <abubakar@huggingface.co>
2024-06-20 19:12:36 -0400

    revert

--- gradio/components/image.py
+++ gradio/components/image.py
@@ -168,7 +168,7 @@ class Image(StreamingInput, Component):
             name = "image"
             suffix = "webp"
 
-        if suffix.lower() in "svg":
+        if suffix.lower() != "svg":
             return str(file_path)
 
         im = PIL.Image.open(file_path)
