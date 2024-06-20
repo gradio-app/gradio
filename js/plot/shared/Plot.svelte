30b2375e6242911b764528339a63134b600e6e8b
Ali Abid <aliabid94@gmail.com>
2024-06-20 12:43:05 -0700

    changes

--- js/plot/shared/Plot.svelte
+++ js/plot/shared/Plot.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
 	//@ts-nocheck
-	import PlotlyPlot from "./plot_types/PlotlyPlot.svelte";
-	import BokehPlot from "./plot_types/BokehPlot.svelte";
-	import AltairPlot from "./plot_types/AltairPlot.svelte";
-	import MatplotlibPlot from "./plot_types/MatplotlibPlot.svelte";
 	import { Plot as PlotIcon } from "@gradio/icons";
 	import { Empty } from "@gradio/atoms";
 	import type { ThemeMode } from "js/app/src/components/types";
+	import { beforeUpdate } from "svelte";
 
 	export let value;
 	export let target: HTMLElement;
@@ -16,24 +13,35 @@
 	export let bokeh_version: string | null;
 	export let show_actions_button: bool;
 
-	$: type = value?.type;
+	let PlotComponent: any = null;
+
+	const plotTypeMapping = {
+		plotly: () => import("./plot_types/PlotlyPlot.svelte"),
+		bokeh: () => import("./plot_types/BokehPlot.svelte"),
+		altair: () => import("./plot_types/AltairPlot.svelte"),
+		matplotlib: () => import("./plot_types/MatplotlibPlot.svelte")
+	};
+
+	beforeUpdate(async () => {
+		let type = value?.type;
+		if (type && type in plotTypeMapping) {
+			const module = await plotTypeMapping[type]();
+			PlotComponent = module.default;
+		}
+	});
 </script>
 
-{#if value && type == "plotly"}
-	<PlotlyPlot {value} {target} />
-{:else if type == "bokeh"}
-	<BokehPlot {value} {bokeh_version} />
-{:else if type == "altair"}
-	<AltairPlot
+{#if value && PlotComponent}
+	<svelte:component
+		this={PlotComponent}
 		{value}
 		{target}
 		{colors}
 		{theme_mode}
 		{caption}
+		{bokeh_version}
 		{show_actions_button}
 	/>
-{:else if type == "matplotlib"}
-	<MatplotlibPlot {value} />
 {:else}
 	<Empty unpadded_box={true} size="large"><PlotIcon /></Empty>
 {/if}
