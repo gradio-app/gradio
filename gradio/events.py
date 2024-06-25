5a37d22ad8403655798ed08739c728e2c8ffef6c
Abubakar Abid <abubakar@huggingface.co>
2024-06-25 15:51:10 -0400

    fix syntax

--- gradio/events.py
+++ gradio/events.py
@@ -103,18 +103,18 @@ class EventData:
     that triggered the event, while subclasses of gr.EventData contains additional attributes that are different for each class.
 
     Example:
-    import gradio as gr
-    with gr.Blocks() as demo:
-        table = gr.Dataframe([[1, 2, 3], [4, 5, 6]])
-        gallery = gr.Gallery([("cat.jpg", "Cat"), ("dog.jpg", "Dog")])
-        textbox = gr.Textbox("Hello World!")
-        statement = gr.Textbox()
-        def on_select(value, evt: gr.EventData):
-            return f"The {evt.target} component was selected, and its value was {value}."
-        table.select(on_select, table, statement)
-        gallery.select(on_select, gallery, statement)
-        textbox.select(on_select, textbox, statement)
-    demo.launch()
+        import gradio as gr
+        with gr.Blocks() as demo:
+            table = gr.Dataframe([[1, 2, 3], [4, 5, 6]])
+            gallery = gr.Gallery([("cat.jpg", "Cat"), ("dog.jpg", "Dog")])
+            textbox = gr.Textbox("Hello World!")
+            statement = gr.Textbox()
+            def on_select(value, evt: gr.EventData):
+                return f"The {evt.target} component was selected, and its value was {value}."
+            table.select(on_select, table, statement)
+            gallery.select(on_select, gallery, statement)
+            textbox.select(on_select, textbox, statement)
+        demo.launch()
     Demos: gallery_selections, tictactoe
     """
 
