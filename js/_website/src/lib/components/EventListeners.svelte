dc40972f2f114867b66b5d6ec2625c11f40bab10
Abubakar Abid <abubakar@huggingface.co>
2024-06-26 13:10:30 -0400

    remove

--- js/_website/src/lib/components/EventListeners.svelte
+++ js/_website/src/lib/components/EventListeners.svelte
@@ -66,43 +66,6 @@
 		Event Parameters
 	</h4>
 	<ParamTable parameters={fns[0].parameters}></ParamTable>
-	<!-- <table class="table-fixed w-full leading-loose">
-		<thead class="text-left">
-			<tr>
-				<th class="px-3 pb-3 font-semibold text-gray-700 w-2/5">Parameter</th>
-				<th class="px-3 pb-3 font-semibold text-gray-700">Description</th>
-			</tr>
-		</thead>
-		<tbody
-			class=" rounded-lg bg-gray-50 border border-gray-100 overflow-hidden text-left align-top divide-y"
-		>
-			{#each fns[0].parameters as param}
-				{#if param["name"] != "self"}
-					<tr class="group hover:bg-gray-200/60 odd:bg-gray-100/80">
-						<td class="p-3 w-2/5 break-words">
-							<code class="block">
-								{param["name"]}
-							</code>
-							<p class="text-gray-500 italic">
-								{param["annotation"].replace("Sequence[", "list[")}
-							</p>
-
-							{#if "default" in param}
-								<p class="text-gray-500 font-semibold">
-									default: {param["default"]}
-								</p>
-							{:else if !("kwargs" in param)}
-								<p class="text-orange-600 font-semibold italic">required</p>
-							{/if}
-						</td>
-						<td class="p-3 text-gray-700 break-words">
-							<p>{@html style_formatted_text(param["doc"]) || ""}</p>
-						</td>
-					</tr>
-				{/if}
-			{/each}
-		</tbody>
-	</table> -->
 </div>
 
 <style>
