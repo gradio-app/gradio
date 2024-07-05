681e7ea7cd18cb5966bed3a1be5e3652181a76bc
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-07-05 16:37:27 +0900

    Fix playground to display errors (#8689)
    * Fix the Playground on the website to trigger run_code() and install() with debounce and to display errors
    
    * Remove an unused function, make_full_screen()
    
    * Format demo/hello_world/run.py
    
    * Update notebook
    
    * add changeset
    
    * add changeset
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- demo/hello_world/run.py
+++ demo/hello_world/run.py
@@ -4,6 +4,6 @@ def greet(name):
     return "Hello " + name + "!"
 
 demo = gr.Interface(fn=greet, inputs="textbox", outputs="textbox")
-    
+
 if __name__ == "__main__":
-    demo.launch()   
+    demo.launch()
