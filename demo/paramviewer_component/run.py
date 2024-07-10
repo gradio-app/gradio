4f7df8ccf4f8b0eaf1871110b298c3ce83aeaf61
Abubakar Abid <abubakar@huggingface.co>
2024-07-10 15:21:02 -0700

    changes

--- demo/paramviewer_component/run.py
+++ demo/paramviewer_component/run.py
@@ -16,6 +16,20 @@ with gr.Blocks() as demo:
             }
          }
     )
+    gr.ParamViewer(
+        {
+           "number": { 
+             "type": "int | float", 
+             "description": "The number to round", 
+             "default": None
+            },
+            "ndigits": { 
+             "type": "int", 
+             "description": "The number of digits to round to", 
+             "default": "0"
+            }
+         }
+    )
 
 if __name__ == "__main__":
     demo.launch()
