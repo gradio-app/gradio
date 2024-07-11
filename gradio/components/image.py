3762395a52a98fc5db9b94db9c617ceb4c0b1cfa
Abubakar Abid <abubakar@huggingface.co>
2024-07-11 14:00:36 -0700

    clean

--- gradio/components/image.py
+++ gradio/components/image.py
@@ -208,7 +208,6 @@ class Image(StreamingInput, Component):
         Returns:
             Returns the image as a `FileData` object.
         """
-        print("value", value, type(value))
         if value is None:
             return None
         if isinstance(value, str) and value.lower().endswith(".svg"):
