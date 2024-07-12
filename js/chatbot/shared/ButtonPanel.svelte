b64f1baf14c58a751addb9954490d1ce1d7097e2
Freddy Boulton <alfonsoboulton@gmail.com>
2024-07-12 18:54:57 +0200

    Fix button panel (#8762)

--- js/chatbot/shared/ButtonPanel.svelte
+++ js/chatbot/shared/ButtonPanel.svelte
@@ -4,19 +4,43 @@
 	import type { FileData } from "@gradio/client";
 	import DownloadIcon from "./Download.svelte";
 	import { DownloadLink } from "@gradio/wasm/svelte";
+	import type { NormalisedMessage, TextMessage } from "../types";
+	import { is_component_message } from "./utils";
 
 	export let likeable: boolean;
 	export let show_copy_button: boolean;
 	export let show: boolean;
-	export let message: Record<string, any>;
+	export let message: NormalisedMessage | NormalisedMessage[];
 	export let position: "right" | "left";
 	export let avatar: FileData | null;
 
 	export let handle_action: (selected: string | null) => void;
 	export let layout: "bubble" | "panel";
-	$: show_copy = show_copy_button && message && message?.type === "text";
+
+	function is_all_text(
+		message: NormalisedMessage[] | NormalisedMessage
+	): message is TextMessage[] | TextMessage {
+		return (
+			(Array.isArray(message) &&
+				message.every((m) => typeof m.content === "string")) ||
+			(!Array.isArray(message) && typeof message.content === "string")
+		);
+	}
+
+	function all_text(message: TextMessage[] | TextMessage): string {
+		if (Array.isArray(message)) {
+			return message.map((m) => m.content).join("\n");
+		}
+		return message.content;
+	}
+
+	$: message_text = is_all_text(message) ? all_text(message) : "";
+
+	$: show_copy = show_copy_button && message && is_all_text(message);
 	$: show_download =
-		show_copy_button && (message?.value?.video?.url || message?.value?.url);
+		!Array.isArray(message) &&
+		is_component_message(message) &&
+		message.content.value.url;
 </script>
 
 {#if show}
@@ -25,14 +49,12 @@
 			null && 'with-avatar'}"
 	>
 		{#if show_copy}
-			<Copy value={message.value} />
+			<Copy value={message_text} />
 		{/if}
-		{#if show_download}
+		{#if show_download && !Array.isArray(message) && is_component_message(message)}
 			<DownloadLink
-				href={message?.value?.video?.url || message?.value?.url}
-				download={message?.value?.video?.orig_name ||
-					message.value.orig_name ||
-					"image"}
+				href={message?.content?.value.url}
+				download={message.content.value.orig_name || "image"}
 			>
 				<span class="icon-wrap">
 					<DownloadIcon />
