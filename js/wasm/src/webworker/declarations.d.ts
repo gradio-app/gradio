846d58f7e1f2d85e1181688080e3d970ff080d4f
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-07-14 12:02:16 +0900

    Refactoring loadPyodide typing

--- js/wasm/src/webworker/declarations.d.ts
+++ js/wasm/src/webworker/declarations.d.ts
@@ -1,2 +1,4 @@
+import type { loadPyodide as loadPyodideValue } from "pyodide";
+
 // Declarations for the WebWorker files where some variables are dynamically loaded through importScript.
-declare let loadPyodide: any;
+declare let loadPyodide: typeof loadPyodideValue;
