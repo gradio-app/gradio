df61bcb914706137891a21221c3ee28c1d8c5b7e
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-07-14 23:37:12 +0900

    Fix

--- js/wasm/src/webworker/declarations.d.ts
+++
@@ -1,4 +0,0 @@
-import type { loadPyodide as loadPyodideValue } from "pyodide";
-
-// Declarations for the WebWorker files where some variables are dynamically loaded through importScript.
-declare let loadPyodide: typeof loadPyodideValue;
