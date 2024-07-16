e8c6fbd0942ae07bc510929a82eafcb299aa03f2
pngwn <hello@pngwn.io>
2024-07-16 19:55:14 +0100

    fix

--- js/imageeditor/shared/ImageEditor.svelte
+++ js/imageeditor/shared/ImageEditor.svelte
@@ -40,11 +40,11 @@
 			type: context_type,
 			{
 				reset_fn,
-				init_fn,
+				init_fn
 			}: {
 				reset_fn?: () => void;
 				init_fn?: (dimensions?: [number, number]) => void;
-			},
+			}
 		) => void;
 		reset: (clear_image: boolean, dimensions: [number, number]) => void;
 	}
@@ -94,7 +94,7 @@
 		child_top: 0,
 		child_left: 0,
 		child_right: 0,
-		child_bottom: 0,
+		child_bottom: 0
 	});
 
 	$: height = $editor_box.child_height;
@@ -104,8 +104,8 @@
 		{ x: 0, y: 0 },
 		{
 			stiffness: 0.1,
-			damping: 0.5,
-		},
+			damping: 0.5
+		}
 	);
 	const pixi = writable<PixiApp | null>(null);
 
@@ -144,11 +144,11 @@
 			type: context_type,
 			{
 				reset_fn,
-				init_fn,
+				init_fn
 			}: {
 				reset_fn?: () => void;
 				init_fn?: (dimensions?: [number, number]) => void;
-			},
+			}
 		) => {
 			contexts.update((c) => [...c, type]);
 			init_context.update((c) => ({ ...c, [type]: init_fn }));
@@ -177,7 +177,7 @@
 				CommandManager.reset();
 				$pixi?.resize?.(...dimensions);
 			}
-		},
+		}
 	});
 
 	let pixi_target: HTMLDivElement;
@@ -192,7 +192,7 @@
 			top: parent_top,
 			left: parent_left,
 			right: parent_right,
-			bottom: parent_bottom,
+			bottom: parent_bottom
 		} = canvas_wrap.getBoundingClientRect();
 		const {
 			width: child_width,
@@ -200,7 +200,7 @@
 			top: child_top,
 			left: child_left,
 			right: child_right,
-			bottom: child_bottom,
+			bottom: child_bottom
 		} = child.getBoundingClientRect();
 		editor_box.set({
 			child_width,
@@ -215,7 +215,7 @@
 			parent_left,
 			parent_right,
 			parent_top,
-			parent_bottom,
+			parent_bottom
 		});
 	}
 
@@ -255,9 +255,9 @@
 				Math.round(l * $dimensions[0]),
 				Math.round(t * $dimensions[1]),
 				Math.round(w * $dimensions[0]),
-				Math.round(h * $dimensions[1]),
+				Math.round(h * $dimensions[1])
 			),
-			$dimensions,
+			$dimensions
 		);
 	}
 
@@ -267,7 +267,7 @@
 	export function handle_remove(): void {
 		editor_context.reset(
 			true,
-			orig_canvas_size ? orig_canvas_size : $dimensions,
+			orig_canvas_size ? orig_canvas_size : $dimensions
 		);
 		if (!sources.length) {
 			set_tool("draw");
@@ -282,7 +282,7 @@
 		const app = create_pixi_app({
 			target: pixi_target,
 			dimensions: _size,
-			antialias,
+			antialias
 		});
 
 		function resize(width: number, height: number): void {
