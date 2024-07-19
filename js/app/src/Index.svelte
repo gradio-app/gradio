07fc3c5ae4ccfe3f3b3fae10c2c11219573a6d71
pngwn <hello@pngwn.io>
2024-07-19 17:45:03 +0100

    fix

--- js/app/src/Index.svelte
+++ js/app/src/Index.svelte
@@ -1,7 +1,7 @@
 <script context="module" lang="ts">
 	import { writable } from "svelte/store";
 	import { mount_css as default_mount_css, prefix_css } from "./css";
-	import type { Client as ClientType } from "@gradio/client";
+	import type { client_return, Client as ClientType } from "@gradio/client";
 
 	import type { ComponentMeta, Dependency, LayoutNode } from "./types";
 
@@ -131,12 +131,12 @@
 			css_text_stylesheet = prefix_css(
 				css_string,
 				version,
-				css_text_stylesheet || undefined,
+				css_text_stylesheet || undefined
 			);
 		}
 		await mount_css(
 			config.root + "/theme.css?v=" + config.theme_hash,
-			document.head,
+			document.head
 		);
 		if (!config.stylesheets) return;
 
@@ -153,16 +153,16 @@
 					.then((css_string) => {
 						prefix_css(css_string, version);
 					});
-			}),
+			})
 		);
 	}
 	async function add_custom_html_head(
-		head_string: string | null,
+		head_string: string | null
 	): Promise<void> {
 		if (head_string) {
 			const parser = new DOMParser();
 			const parsed_head_html = Array.from(
-				parser.parseFromString(head_string, "text/html").head.children,
+				parser.parseFromString(head_string, "text/html").head.children
 			);
 
 			if (parsed_head_html) {
@@ -178,7 +178,7 @@
 						newElement.getAttribute("property")
 					) {
 						const domMetaList = Array.from(
-							document.head.getElementsByTagName("meta") ?? [],
+							document.head.getElementsByTagName("meta") ?? []
 						);
 						const matched = domMetaList.find((el) => {
 							return (
@@ -208,7 +208,7 @@
 		} else {
 			const url = new URL(window.location.toString());
 			const url_color_mode: ThemeMode | null = url.searchParams.get(
-				"__theme",
+				"__theme"
 			) as ThemeMode | null;
 			new_theme_mode = theme_mode || url_color_mode || "system";
 		}
@@ -229,7 +229,7 @@
 
 		function update_scheme(): "light" | "dark" {
 			let _theme: "light" | "dark" = window?.matchMedia?.(
-				"(prefers-color-scheme: dark)",
+				"(prefers-color-scheme: dark)"
 			).matches
 				? "dark"
 				: "light";
@@ -255,7 +255,7 @@
 		message: "",
 		load_status: "pending",
 		status: "sleeping",
-		detail: "SLEEPING",
+		detail: "SLEEPING"
 	};
 
 	let app: ClientType;
@@ -282,7 +282,7 @@
 		app = await Client.connect(api_url, {
 			status_callback: handle_status,
 			with_null_state: true,
-			events: ["data", "log", "status", "render"],
+			events: ["data", "log", "status", "render"]
 		});
 
 		if (!app.config) {
@@ -296,7 +296,7 @@
 			message: "",
 			load_status: "complete",
 			status: "running",
-			detail: "RUNNING",
+			detail: "RUNNING"
 		};
 
 		await mount_custom_css(config.css);
@@ -321,7 +321,7 @@
 					app = await Client.connect(api_url, {
 						status_callback: handle_status,
 						with_null_state: true,
-						events: ["data", "log", "status", "render"],
+						events: ["data", "log", "status", "render"]
 					});
 
 					if (!app.config) {
@@ -374,7 +374,7 @@
 			CONFIG_ERROR: $_("errors.config_error"),
 			BUILD_ERROR: $_("errors.build_error"),
 			RUNTIME_ERROR: $_("errors.runtime_error"),
-			PAUSED: $_("errors.space_paused"),
+			PAUSED: $_("errors.space_paused")
 		} as const,
 		title(error: error_types): string {
 			return encodeURIComponent($_("errors.space_not_working"));
@@ -383,9 +383,9 @@
 			return encodeURIComponent(
 				`Hello,\n\nFirstly, thanks for creating this space!\n\nI noticed that the space isn't working correctly because there is ${
 					this.readable_error[error] || "an error"
-				}.\n\nIt would be great if you could take a look at this because this space is being embedded on ${site}.\n\nThanks!`,
+				}.\n\nIt would be great if you could take a look at this because this space is being embedded on ${site}.\n\nThanks!`
 			);
-		},
+		}
 	};
 
 	let new_message_fn: (message: string, type: string) => void;
@@ -399,17 +399,19 @@
 			new CustomEvent("render", {
 				bubbles: true,
 				cancelable: false,
-				composed: true,
-			}),
+				composed: true
+			})
 		);
 	}
 
-	$: mount_space_header(space, is_embed);
+	$: mount_space_header(app.config, is_embed);
 
-	function mount_space_header(space: string | null, is_embed: boolean): void {
-		console.log(space, is_embed);
-		if (space && !is_embed) {
-			init(space);
+	function mount_space_header(
+		config: client_return["config"],
+		is_embed: boolean
+	): void {
+		if (config?.space_id && !is_embed) {
+			init(config?.space_id);
 		}
 	}
 </script>
@@ -454,10 +456,10 @@
 					<p>
 						Please <a
 							href="https://huggingface.co/spaces/{space}/discussions/new?title={discussion_message.title(
-								status?.detail,
+								status?.detail
 							)}&description={discussion_message.description(
 								status?.detail,
-								location.origin,
+								location.origin
 							)}"
 						>
 							contact the author of the space</a
