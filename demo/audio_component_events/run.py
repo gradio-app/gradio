7e8c829aad85d3d5a31a03f4f2e41483d1e6b53e
Abubakar Abid <abubakar@huggingface.co>
2024-07-18 23:17:13 -0700

    Add `.input()` events to `gr.Audio` and `gr.Image` (#8836)
    * add input
    
    * format
    
    * add changeset
    
    * add changeset
    
    * add audio browser tests
    
    * add browser tests
    
    * notebooks
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- demo/audio_component_events/run.py
+++ demo/audio_component_events/run.py
@@ -3,13 +3,14 @@ import gradio as gr
 with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column():
-            input_video = gr.Audio(type="filepath", label="Input Audio", sources=["upload", "microphone"])
+            input_audio = gr.Audio(type="filepath", label="Input Audio", sources=["upload", "microphone"])
         with gr.Column():
-            output_video = gr.Audio(label="Output Audio", sources=["upload", "microphone"])
+            output_audio = gr.Audio(label="Output Audio", sources=["upload", "microphone"])
 
     with gr.Row():
         with gr.Column():
             input_num_change = gr.Number(label="# Input Change Events", value=0)
+            input_num_input = gr.Number(label="# Input Input Events", value=0)
             input_num_load = gr.Number(label="# Input Upload Events", value=0)
             input_num_play = gr.Number(label="# Input Play Events", value=0)
             input_num_pause = gr.Number(label="# Input Pause Events", value=0)
@@ -25,19 +26,20 @@ with gr.Blocks() as demo:
             output_num_pause = gr.Number(label="# Output Pause Events", value=0)
             output_num_stop = gr.Number(label="# Output Stop Events", value=0)
 
-            input_video.upload(lambda s, n: (s, n + 1), [input_video, input_num_load], [output_video, input_num_load])
-            input_video.change(lambda n: n + 1, input_num_change, input_num_change)
-            input_video.play(lambda n: n + 1, input_num_play, input_num_play)
-            input_video.pause(lambda n: n + 1, input_num_pause, input_num_pause)
-            input_video.change(lambda n: n + 1, input_num_change, input_num_change)
+            input_audio.upload(lambda s, n: (s, n + 1), [input_audio, input_num_load], [output_audio, input_num_load])
+            input_audio.change(lambda n: n + 1, input_num_change, input_num_change)
+            input_audio.play(lambda n: n + 1, input_num_play, input_num_play)
+            input_audio.pause(lambda n: n + 1, input_num_pause, input_num_pause)
+            input_audio.change(lambda n: n + 1, input_num_change, input_num_change)
+            input_audio.input(lambda n: n + 1, input_num_input, input_num_input)
 
-            input_video.start_recording(lambda n: n + 1, input_record, input_record)
-            input_video.pause_recording(lambda n: n + 1, input_pause, input_pause)
-            input_video.stop_recording(lambda n: n + 1, input_stop, input_stop)
+            input_audio.start_recording(lambda n: n + 1, input_record, input_record)
+            input_audio.pause_recording(lambda n: n + 1, input_pause, input_pause)
+            input_audio.stop_recording(lambda n: n + 1, input_stop, input_stop)
 
-            output_video.play(lambda n: n + 1, output_num_play, output_num_play)
-            output_video.pause(lambda n: n + 1, output_num_pause, output_num_pause)
-            output_video.stop(lambda n: n + 1, output_num_stop, output_num_stop)
+            output_audio.play(lambda n: n + 1, output_num_play, output_num_play)
+            output_audio.pause(lambda n: n + 1, output_num_pause, output_num_pause)
+            output_audio.stop(lambda n: n + 1, output_num_stop, output_num_stop)
 
 
 
