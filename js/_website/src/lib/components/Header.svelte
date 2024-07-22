44e617f91d6bccf0349caf9e409eabfec0386e9d
aliabd <ali.si3luwa@gmail.com>
2024-07-21 21:49:26 -0700

    style header

--- js/_website/src/lib/components/Header.svelte
+++ js/_website/src/lib/components/Header.svelte
@@ -38,7 +38,8 @@
 	</div>
 	<div
 		class:border={show_nav}
-		class="w-[95%] mx-auto border-gray-300 rounded-lg p-2 mt-2 flex flex-wrap justify-between flex-row relative items-center text-lg z-50 lg:px-4 lg:py-5 lg:gap-6 lg:container lg:border-none"
+		class:shadow={show_nav}
+		class="w-[95%] mx-auto border-gray-200 shadow rounded-lg p-2 mt-2 flex flex-wrap justify-between flex-row relative items-center text-lg z-50 lg:px-4 lg:py-5 lg:gap-6 lg:container lg:border-none lg:shadow-none"
 	>
 		<a href="/">
 			<img src={gradio_logo} alt="Gradio logo" />
