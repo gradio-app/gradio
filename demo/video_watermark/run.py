830bba1416c847ce85c729a578ad4c6bf22be1a3
Abubakar Abid <abubakar@huggingface.co>
2024-07-29 12:18:30 -0700

    demo

--- demo/video_watermark/run.py
+++ demo/video_watermark/run.py
@@ -1,16 +1,16 @@
 import gradio as gr
 import os
 
-a = os.path.join(os.path.dirname(__file__), "files/a.mp4")  # Video
-b = os.path.join(os.path.dirname(__file__), "files/b.mp4")  # Video
-w1 = os.path.join(os.path.dirname(__file__), "files/w1.jpg")  # Watermark
-w2 = os.path.join(os.path.dirname(__file__), "files/w2.png")  # Watermark
+a = os.path.join(os.path.dirname(__file__), "files/a.mp4")
+b = os.path.join(os.path.dirname(__file__), "files/b.mp4")
+w1 = os.path.join(os.path.dirname(__file__), "files/w1.jpg")
+w2 = os.path.join(os.path.dirname(__file__), "files/w2.png")
 
-def generate_video(original_video, watermark_file):
-    return gr.Video(a, watermark_file=watermark_file)
+def generate_video(original_video, watermark):
+    return gr.Video(a, watermark=watermark)
 
 
-demo = gr.Interface(generate_video, [gr.Textbox(), gr.File()], gr.Video(),
+demo = gr.Interface(generate_video, [gr.Video(), gr.File()], gr.Video(),
                     examples=[[a, w1], [b, w2]])
 
 if __name__ == "__main__":
