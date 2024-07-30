5f9b8d093a17f4fcc689cbc83fd6db6c43b5174b
Abubakar Abid <abubakar@huggingface.co>
2024-07-30 16:14:15 -0700

    Fix lazy caching (#8933)
    * fix lazy
    
    * add changeset
    
    * fix
    
    * changes
    
    * hello
    
    * fix test
    
    * add new test
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -595,6 +595,40 @@ class TestProcessExamples:
         response = client.post("/api/load_example/", json={"data": [1]})
         assert response.json()["data"] == ["Michael", "Jordan", "Michael Jordan"]
 
+    def test_end_to_end_lazy_cache_examples(self, patched_cache_folder):
+        def image_identity(image, string):
+            return image
+
+        with gr.Blocks() as demo:
+            i1 = gr.Image()
+            t = gr.Textbox()
+            i2 = gr.Image()
+
+            gr.Examples(
+                examples=[
+                    ["test/test_files/cheetah1.jpg", "cheetah"],
+                    ["test/test_files/bus.png", "bus"],
+                ],
+                inputs=[i1, t],
+                outputs=[i2],
+                fn=image_identity,
+                cache_examples="lazy",
+                api_name="load_example",
+            )
+
+        app, _, _ = demo.launch(prevent_thread_lock=True)
+        client = TestClient(app)
+
+        response = client.post("/api/load_example/", json={"data": [0]})
+        data = response.json()["data"]
+        assert data[0]["value"]["path"].endswith("cheetah1.jpg")
+        assert data[1]["value"] == "cheetah"
+
+        response = client.post("/api/load_example/", json={"data": [1]})
+        data = response.json()["data"]
+        assert data[0]["value"]["path"].endswith("bus.png")
+        assert data[1]["value"] == "bus"
+
 
 def test_multiple_file_flagging(tmp_path):
     with patch("gradio.utils.get_cache_folder", return_value=tmp_path):
