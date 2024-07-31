84a4158310f51fbc591c12387b08e1072c649605
freddyaboulton <alfonsoboulton@gmail.com>
2024-07-31 16:35:42 -0400

    fix audio?

--- js/app/test/stream_audio_out.spec.ts
+++ js/app/test/stream_audio_out.spec.ts
@@ -4,6 +4,10 @@ test("audio streams from wav file correctly", async ({ page }) => {
 	const uploader = await page.locator("input[type=file]");
 	await uploader.setInputFiles(["../../test/test_files/audio_sample.wav"]);
 	await page.getByRole("button", { name: "Stream as File" }).click();
+	// @ts-ignore
+	await page
+		.locator("#stream_as_file_output audio")
+		.evaluate(async (el) => await el.play());
 	await expect
 		.poll(
 			async () =>
@@ -19,6 +23,10 @@ test("audio streams from wav file correctly as bytes", async ({ page }) => {
 	const uploader = await page.locator("input[type=file]");
 	await uploader.setInputFiles(["../../test/test_files/audio_sample.wav"]);
 	await page.getByRole("button", { name: "Stream as Bytes" }).click();
+	// @ts-ignore
+	await page
+		.locator("#stream_as_bytes_output audio")
+		.evaluate(async (el) => await el.play());
 	await expect
 		.poll(
 			async () =>
