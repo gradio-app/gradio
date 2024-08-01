39e3906d5778538114ae0e9ed56171fceee0fc8d
aliabd <ali.si3luwa@gmail.com>
2024-07-31 23:07:24 -0700

    fix header

--- js/_website/src/lib/components/Header.svelte
+++ js/_website/src/lib/components/Header.svelte
@@ -17,7 +17,7 @@
 </script>
 
 <!-- 4.0 Launch BANNER  -->
-<div class="main-header flex-row">
+<div class="main-header flex-row mb-4">
 	<div
 		class="relative isolate flex items-center gap-x-6 overflow-hidden bg-gradient-to-r from-white via-yellow-200 to-white px-6 py-2 sm:px-3.5 sm:before:flex-1 mx-auto"
 	>
@@ -39,7 +39,7 @@
 	<div
 		class:border={show_nav}
 		class:shadow={show_nav}
-		class="w-[95%] mx-auto border-gray-200 shadow rounded-lg p-2 mt-2 flex flex-wrap justify-between flex-row relative items-center text-lg z-50 lg:px-4 lg:py-5 lg:gap-6 lg:container lg:border-none lg:shadow-none"
+		class="w-[95%] mx-auto border-gray-200 rounded-lg p-2 mt-2 flex flex-wrap justify-between flex-row relative items-center text-lg z-50 lg:px-4 lg:py-5 lg:gap-6 lg:container lg:border-none lg:shadow-none"
 	>
 		<a href="/">
 			<img src={gradio_logo} alt="Gradio logo" />
