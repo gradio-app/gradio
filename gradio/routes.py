3a30e09f713ce75bab6163a63b0c86aded07e564
Ali Abid <aliabid94@gmail.com>
2024-08-01 14:19:42 -0700

    changes

--- gradio/routes.py
+++ gradio/routes.py
@@ -217,11 +217,11 @@ class App(FastAPI):
         assert self.blocks  # noqa: S101
         # Don't proxy a URL unless it's a URL specifically loaded by the user using
         # gr.load() to prevent SSRF or harvesting of HF tokens by malicious Spaces.
-        # is_safe_url = any(
-        #     url.host == httpx.URL(root).host for root in self.blocks.proxy_urls
-        # )
-        # if not is_safe_url:
-        #     raise PermissionError("This URL cannot be proxied.")
+        is_safe_url = any(
+            url.host == httpx.URL(root).host for root in self.blocks.proxy_urls
+        )
+        if not is_safe_url:
+            raise PermissionError("This URL cannot be proxied.")
         is_hf_url = url.host.endswith(".hf.space")
         headers = {}
         if Context.hf_token is not None and is_hf_url:
