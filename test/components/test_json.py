b5ff0367081df26a38dfd4e63ca955ae9d6da0d0
Hannah <hannahblair@users.noreply.github.com>
2024-08-06 15:49:26 +0200

    fix test

--- test/components/test_json.py
+++ test/components/test_json.py
@@ -23,7 +23,6 @@ class TestJSON:
             "value": None,
             "show_label": True,
             "label": None,
-            "lines": None,
             "name": "json",
             "proxy_url": None,
             "_selectable": False,
