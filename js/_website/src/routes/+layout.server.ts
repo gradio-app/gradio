7287d5e890b68f6387f556afdf069b7bd2fec8f3
aliabd <ali.si3luwa@gmail.com>
2024-08-09 15:55:09 -0700

    better fix for css trailing slash issue

--- js/_website/src/routes/+layout.server.ts
+++ js/_website/src/routes/+layout.server.ts
@@ -7,7 +7,4 @@ export async function load({ url }: any) {
 	if (url.pathname in redirects) {
 		throw redirect(308, redirects[url.pathname as keyof typeof redirects]);
 	}
-	if (url.pathname.endsWith("/") && url.pathname.length > 1) {
-		throw redirect(308, url.pathname.slice(0, -1));
-	}
 }
