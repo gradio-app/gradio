40742e385e4aaa308f823fda4da10e7581e14a15
Hannah <hannahblair@users.noreply.github.com>
2024-08-12 21:10:29 +0200

    revert onDestroy removal

--- js/json/shared/JSON.svelte
+++ js/json/shared/JSON.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { onDestroy } from "svelte";
 	import { JSON as JSONIcon } from "@gradio/icons";
 	import { Empty } from "@gradio/atoms";
 	import JSONNode from "./JSONNode.svelte";
