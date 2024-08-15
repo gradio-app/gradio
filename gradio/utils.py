6e92eb23069e047458c4e303b02ec218369cadaa
Abubakar Abid <abubakar@huggingface.co>
2024-08-15 16:43:06 -0700

    drop support for python 3.8 and 3.9

--- gradio/utils.py
+++ gradio/utils.py
@@ -898,56 +898,13 @@ def get_cancelled_fn_indices(
 
 
 def get_type_hints(fn):
-    # Importing gradio with the canonical abbreviation. Used in typing._eval_type.
-    import gradio as gr  # noqa: F401
-    from gradio import OAuthProfile, OAuthToken, Request  # noqa: F401
-
     if inspect.isfunction(fn) or inspect.ismethod(fn):
         pass
     elif callable(fn):
         fn = fn.__call__
     else:
         return {}
-
-    try:
-        return typing.get_type_hints(fn)
-    except TypeError:
-        # On Python 3.9 or earlier, get_type_hints throws a TypeError if the function
-        # has a type annotation that include "|". We resort to parsing the signature
-        # manually using inspect.signature.
-        type_hints = {}
-        sig = inspect.signature(fn)
-        for name, param in sig.parameters.items():
-            if param.annotation is inspect.Parameter.empty:
-                continue
-            if param.annotation in ["gr.OAuthProfile | None", "None | gr.OAuthProfile"]:
-                # Special case: we want to inject the OAuthProfile value even on Python 3.9
-                type_hints[name] = Optional[OAuthProfile]
-            if param.annotation == ["gr.OAuthToken | None", "None | gr.OAuthToken"]:
-                # Special case: we want to inject the OAuthToken value even on Python 3.9
-                type_hints[name] = Optional[OAuthToken]
-            if param.annotation in [
-                "gr.Request | None",
-                "Request | None",
-                "None | gr.Request",
-                "None | Request",
-            ]:
-                # Special case: we want to inject the Request value even on Python 3.9
-                type_hints[name] = Optional[Request]
-            if "|" in str(param.annotation):
-                continue
-            # To convert the string annotation to a class, we use the
-            # internal typing._eval_type function. This is not ideal, but
-            # it's the only way to do it without eval-ing the string.
-            # Since the API is internal, it may change in the future.
-            try:
-                type_hints[name] = typing._eval_type(  # type: ignore
-                    typing.ForwardRef(param.annotation), globals(), locals()
-                )
-            except (NameError, TypeError):
-                pass
-        return type_hints
-
+    return typing.get_type_hints(fn)
 
 def is_special_typed_parameter(name, parameter_types):
     from gradio.helpers import EventData
