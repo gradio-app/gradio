b43abdeb79bb4604efbfa55e268719b6a4809674
freddyaboulton <alfonsoboulton@gmail.com>
2024-08-16 11:47:56 -0400

    lint

--- gradio/components/code.py
+++ gradio/components/code.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-from pathlib import Path
 from typing import TYPE_CHECKING, Any, Callable, Literal, Sequence
 
 from gradio_client.documentation import document
