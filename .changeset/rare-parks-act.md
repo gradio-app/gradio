de1f7a2b89e8d2868c7b255bc1d6e6e5c0610d03
pngwn <hello@pngwn.io>
2024-08-19 11:00:03 +0100

    Create test app for components  / SSR testing. (#9138)
    * test app
    
    * test app
    
    * add changeset
    
    * private
    
    * title
    
    * add changeset
    
    * format
    
    * asd
    
    * asd
    
    * asd
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

---
+++ .changeset/rare-parks-act.md
@@ -0,0 +1,5 @@
+---
+"@gradio/component-test": minor
+---
+
+feat:Create test app for components  / SSR testing.
---
+++ js/component-test/src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script>
+	import Header from "./Header.svelte";
+	import "../app.css";
+</script>
+
+<div class="app">
+	<Header />
+
+	<slot />
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
