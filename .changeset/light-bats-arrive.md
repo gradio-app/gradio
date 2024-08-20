f3652ebe08211e12739df73c15fd97e5ff81276a
Dawood Khan <dawoodkhan82@gmail.com>
2024-08-20 11:19:04 -0400

    Video gallery (#9052)
    * video support
    
    * tests and backend changes
    
    * undo main merge
    
    * upload fix
    
    * Revert "undo main merge"
    
    This reverts commit e2a26e6d28772587dadce2bb00c3581f97883664.
    
    * type fixes
    
    * format
    
    * pr fixes
    
    * Update gradio/components/gallery.py
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    
    * Update gradio/components/gallery.py
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    
    * type fix
    
    * thumbnails
    
    * thumbnail type
    
    * remove thumbnail generation
    
    * add changeset
    
    * test fixes
    
    * test fixes
    
    * python test fix
    
    * python test fixc
    
    * fix
    
    * fix
    
    * story fix
    
    ---------
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

---
+++ .changeset/light-bats-arrive.md
@@ -0,0 +1,7 @@
+---
+"@gradio/core": minor
+"@gradio/gallery": minor
+"gradio": minor
+---
+
+feat:Video gallery
--- demo/gallery_component/run.py
+++ demo/gallery_component/run.py
@@ -1,13 +1,13 @@
 import gradio as gr
 
 with gr.Blocks() as demo:
-    cheetahs = [
-        "https://upload.wikimedia.org/wikipedia/commons/0/09/TheCheethcat.jpg",
-        "https://nationalzoo.si.edu/sites/default/files/animals/cheetah-003.jpg",
-        "https://img.etimg.com/thumb/msid-50159822,width-650,imgsize-129520,,resizemode-4,quality-100/.jpg",
-        "https://nationalzoo.si.edu/sites/default/files/animals/cheetah-002.jpg",
-        "https://images.theconversation.com/files/375893/original/file-20201218-13-a8h8uq.jpg?ixlib=rb-1.1.0&rect=16%2C407%2C5515%2C2924&q=45&auto=format&w=496&fit=clip",
+    gallery_items = [
+        ("https://upload.wikimedia.org/wikipedia/commons/0/09/TheCheethcat.jpg", "cheetah1"),
+        ("https://nationalzoo.si.edu/sites/default/files/animals/cheetah-003.jpg", "cheetah2"),
+        ("https://videos.pexels.com/video-files/3209828/3209828-uhd_2560_1440_25fps.mp4", "world"),
+        ("files/cheetah.jpg", "cheetah3"),
+        ("files/world.mp4", "world2")
     ]
-    gr.Gallery(value=cheetahs, columns=4)
+    gr.Gallery(value=gallery_items, columns=4)
 
 demo.launch()
