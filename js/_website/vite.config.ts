183f3f22b7e0fe543ff1336c8be78d0b3fe922c4
pngwn <hello@pngwn.io>
2024-08-21 19:17:17 +0100

    asd

--- js/_website/vite.config.ts
+++ js/_website/vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import { inject_component_loader } from "../build/dist/index.js";
+import { inject_component_loader } from "../build/out/index.js";
 
 //@ts-ignore
 export default defineConfig(({ mode }) => ({
