c8809bd1c1aaaadaefea1c01eb3dcb77d8205684
Abubakar Abid <abubakar@huggingface.co>
2024-08-29 19:28:41 -0700

    tunneling

--- gradio/tunneling.py
+++ gradio/tunneling.py
@@ -18,6 +18,8 @@ CURRENT_TUNNELS: list["Tunnel"] = []
 machine = platform.machine()
 if machine == "x86_64":
     machine = "amd64"
+elif machine == "aarch64":
+    machine = "arm64"
 
 BINARY_REMOTE_NAME = f"frpc_{platform.system().lower()}_{machine.lower()}"
 EXTENSION = ".exe" if os.name == "nt" else ""
