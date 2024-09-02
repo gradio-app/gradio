8e8a55b564a9d0a72720bf8c2f83d64db42854bc
Abubakar Abid <abubakar@huggingface.co>
2024-09-02 09:35:57 -0700

    fix test

--- test/components/test_chatbot.py
+++ test/components/test_chatbot.py
@@ -61,8 +61,6 @@ class TestChatbot:
             "line_breaks": True,
             "layout": None,
             "show_copy_all_button": False,
-            "_retryable": False,
-            "_undoable": False,
         }
 
     def test_avatar_images_are_moved_to_cache(self):
