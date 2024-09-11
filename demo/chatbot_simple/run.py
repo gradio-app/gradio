e29e86203420367f9f1ea20892edd6f2bd4f6dda
Abubakar Abid <abubakar@huggingface.co>
2024-09-11 12:54:01 -0700

    restore

--- demo/chatbot_simple/run.py
+++ demo/chatbot_simple/run.py
@@ -3,14 +3,14 @@ import random
 import time
 
 with gr.Blocks() as demo:
-    chatbot = gr.Chatbot(type="messages", min_height=200, max_height=300, height=None)
+    chatbot = gr.Chatbot()
     msg = gr.Textbox()
     clear = gr.ClearButton([msg, chatbot])
 
     def respond(message, chat_history):
-        bot_message = random.choice(["How are you?", "Today is a great day", "I'm very hungry"])
-        chat_history.append({"role": "user", "content": message})
-        chat_history.append({"role": "assistant", "content": bot_message})
+        bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
+        chat_history.append((message, bot_message))
+        time.sleep(2)
         return "", chat_history
 
     msg.submit(respond, [msg, chatbot], [msg, chatbot])
