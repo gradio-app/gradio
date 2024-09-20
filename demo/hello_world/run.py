58f17072decc2ebdd10a76059f7ddec662e41356
Abubakar Abid <abubakar@huggingface.co>
2024-09-19 23:57:05 -0500

    demo

--- demo/hello_world/run.py
+++ demo/hello_world/run.py
@@ -5,7 +5,7 @@ def greet(name):
     return "Hello " + name + "!"
 
 
-demo = gr.Interface(fn=greet, inputs="textbox", outputs="textbox")
+demo = gr.Interface(fn=greet, inputs="textbox", outputs="textbox", spa_mode=False)
 
 if __name__ == "__main__":
-    demo.launch(ssr_mode=True)
+    demo.launch()
