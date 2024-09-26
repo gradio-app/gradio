f5ac59a5d396845af9e11782ad2a77c556f8d63e
Hannah <hannahblair@users.noreply.github.com>
2024-09-26 14:10:17 +0200

    format

--- js/code/Index.svelte
+++ js/code/Index.svelte
@@ -72,12 +72,9 @@
 		on:clear_status={() => gradio.dispatch("clear_status", loading_status)}
 	/>
 
-	{#if show_label}<BlockLabel
-			Icon={CodeIcon}
-			{show_label}
-			{label}
-			float={false}
-		/>{/if}
+	{#if show_label}
+		<BlockLabel Icon={CodeIcon} {show_label} {label} float={false} />
+	{/if}
 
 	{#if !value && !interactive}
 		<Empty unpadded_box={true} size="large">
