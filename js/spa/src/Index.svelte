240c08c36d28fce1b123e034ec22fdaa2bcc4a98
pngwn <hello@pngwn.io>
2024-09-27 22:26:02 +0100

    format

--- js/spa/src/Index.svelte
+++ js/spa/src/Index.svelte
@@ -134,12 +134,12 @@
 			css_text_stylesheet = prefix_css(
 				css_string,
 				version,
-				css_text_stylesheet || undefined,
+				css_text_stylesheet || undefined
 			);
 		}
 		await mount_css(
 			config.root + "/theme.css?v=" + config.theme_hash,
-			document.head,
+			document.head
 		);
 		if (!config.stylesheets) return;
 
@@ -156,16 +156,16 @@
 					.then((css_string) => {
 						prefix_css(css_string, version);
 					});
-			}),
+			})
 		);
 	}
 	async function add_custom_html_head(
-		head_string: string | null,
+		head_string: string | null
 	): Promise<void> {
 		if (head_string) {
 			const parser = new DOMParser();
 			const parsed_head_html = Array.from(
-				parser.parseFromString(head_string, "text/html").head.children,
+				parser.parseFromString(head_string, "text/html").head.children
 			);
 
 			if (parsed_head_html) {
@@ -181,7 +181,7 @@
 						newElement.getAttribute("property")
 					) {
 						const domMetaList = Array.from(
-							document.head.getElementsByTagName("meta") ?? [],
+							document.head.getElementsByTagName("meta") ?? []
 						);
 						const matched = domMetaList.find((el) => {
 							return (
@@ -211,7 +211,7 @@
 		} else {
 			const url = new URL(window.location.toString());
 			const url_color_mode: ThemeMode | null = url.searchParams.get(
-				"__theme",
+				"__theme"
 			) as ThemeMode | null;
 			new_theme_mode = theme_mode || url_color_mode || "system";
 		}
@@ -232,7 +232,7 @@
 
 		function update_scheme(): "light" | "dark" {
 			let _theme: "light" | "dark" = window?.matchMedia?.(
-				"(prefers-color-scheme: dark)",
+				"(prefers-color-scheme: dark)"
 			).matches
 				? "dark"
 				: "light";
@@ -258,7 +258,7 @@
 		message: "",
 		load_status: "pending",
 		status: "sleeping",
-		detail: "SLEEPING",
+		detail: "SLEEPING"
 	};
 
 	let app: ClientType;
@@ -285,7 +285,7 @@
 		app = await Client.connect(api_url, {
 			status_callback: handle_status,
 			with_null_state: true,
-			events: ["data", "log", "status", "render"],
+			events: ["data", "log", "status", "render"]
 		});
 
 		if (!app.config) {
@@ -299,7 +299,7 @@
 			message: "",
 			load_status: "complete",
 			status: "running",
-			detail: "RUNNING",
+			detail: "RUNNING"
 		};
 
 		await mount_custom_css(config.css);
@@ -324,7 +324,7 @@
 					app = await Client.connect(api_url, {
 						status_callback: handle_status,
 						with_null_state: true,
-						events: ["data", "log", "status", "render"],
+						events: ["data", "log", "status", "render"]
 					});
 
 					if (!app.config) {
@@ -378,7 +378,7 @@
 			CONFIG_ERROR: $_("errors.config_error"),
 			BUILD_ERROR: $_("errors.build_error"),
 			RUNTIME_ERROR: $_("errors.runtime_error"),
-			PAUSED: $_("errors.space_paused"),
+			PAUSED: $_("errors.space_paused")
 		} as const,
 		title(error: error_types): string {
 			return encodeURIComponent($_("errors.space_not_working"));
@@ -387,9 +387,9 @@
 			return encodeURIComponent(
 				`Hello,\n\nFirstly, thanks for creating this space!\n\nI noticed that the space isn't working correctly because there is ${
 					this.readable_error[error] || "an error"
-				}.\n\nIt would be great if you could take a look at this because this space is being embedded on ${site}.\n\nThanks!`,
+				}.\n\nIt would be great if you could take a look at this because this space is being embedded on ${site}.\n\nThanks!`
 			);
-		},
+		}
 	};
 
 	let new_message_fn: (message: string, type: string) => void;
@@ -403,8 +403,8 @@
 			new CustomEvent("render", {
 				bubbles: true,
 				cancelable: false,
-				composed: true,
-			}),
+				composed: true
+			})
 		);
 	}
 
@@ -413,7 +413,7 @@
 
 	async function mount_space_header(
 		space_id: string | null | undefined,
-		is_embed: boolean,
+		is_embed: boolean
 	): Promise<void> {
 		if (space_id && !is_embed && window.self === window.top) {
 			if (spaceheader) {
@@ -471,10 +471,10 @@
 					<p>
 						Please <a
 							href="https://huggingface.co/spaces/{space}/discussions/new?title={discussion_message.title(
-								status?.detail,
+								status?.detail
 							)}&description={discussion_message.description(
 								status?.detail,
-								location.origin,
+								location.origin
 							)}"
 						>
 							contact the author of the space</a
