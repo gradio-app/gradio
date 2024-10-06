d5366ce93b0139760858feead66cd9fca7e92c15
Abubakar Abid <abubakar@huggingface.co>
2024-10-05 17:44:49 -0700

    format

--- test/test_external.py
+++ test/test_external.py
@@ -5,10 +5,10 @@ import warnings
 from pathlib import Path
 from unittest.mock import MagicMock, patch
 
+import huggingface_hub
 import pytest
 from fastapi.testclient import TestClient
 from gradio_client import media_data
-from huggingface_hub import HfFolder
 
 import gradio as gr
 from gradio.context import Context
@@ -31,7 +31,7 @@ pytestmark = pytest.mark.flaky
 
 os.environ["GRADIO_ANALYTICS_ENABLED"] = "False"
 
-HF_TOKEN = os.getenv("HF_TOKEN") or HfFolder.get_token()
+HF_TOKEN = huggingface_hub.get_token()
 
 
 class TestLoadInterface:
@@ -76,7 +76,7 @@ class TestLoadInterface:
     def test_summarization(self):
         model_type = "summarization"
         interface = gr.load(
-            "models/facebook/bart-large-cnn", hf_token=None, alias=model_type
+            "models/facebook/bart-large-cnn", hf_token=HF_TOKEN, alias=model_type
         )
         assert interface.__name__ == model_type
         assert interface.input_components and interface.output_components
@@ -86,7 +86,7 @@ class TestLoadInterface:
     def test_translation(self):
         model_type = "translation"
         interface = gr.load(
-            "models/facebook/bart-large-cnn", hf_token=None, alias=model_type
+            "models/facebook/bart-large-cnn", hf_token=HF_TOKEN, alias=model_type
         )
         assert interface.__name__ == model_type
         assert interface.input_components and interface.output_components
@@ -96,7 +96,7 @@ class TestLoadInterface:
     def test_text2text_generation(self):
         model_type = "text2text-generation"
         interface = gr.load(
-            "models/sshleifer/tiny-mbart", hf_token=None, alias=model_type
+            "models/sshleifer/tiny-mbart", hf_token=HF_TOKEN, alias=model_type
         )
         assert interface.__name__ == model_type
         assert interface.input_components and interface.output_components
@@ -107,7 +107,7 @@ class TestLoadInterface:
         model_type = "text-classification"
         interface = gr.load(
             "models/distilbert-base-uncased-finetuned-sst-2-english",
-            hf_token=None,
+            hf_token=HF_TOKEN,
             alias=model_type,
         )
         assert interface.__name__ == model_type
@@ -117,7 +117,9 @@ class TestLoadInterface:
 
     def test_fill_mask(self):
         model_type = "fill-mask"
-        interface = gr.load("models/bert-base-uncased", hf_token=None, alias=model_type)
+        interface = gr.load(
+            "models/bert-base-uncased", hf_token=HF_TOKEN, alias=model_type
+        )
         assert interface.__name__ == model_type
         assert interface.input_components and interface.output_components
         assert isinstance(interface.input_components[0], gr.Textbox)
@@ -126,7 +128,7 @@ class TestLoadInterface:
     def test_zero_shot_classification(self):
         model_type = "zero-shot-classification"
         interface = gr.load(
-            "models/facebook/bart-large-mnli", hf_token=None, alias=model_type
+            "models/facebook/bart-large-mnli", hf_token=HF_TOKEN, alias=model_type
         )
         assert interface.__name__ == model_type
         assert interface.input_components and interface.output_components
@@ -138,7 +140,7 @@ class TestLoadInterface:
     def test_automatic_speech_recognition(self):
         model_type = "automatic-speech-recognition"
         interface = gr.load(
-            "models/facebook/wav2vec2-base-960h", hf_token=None, alias=model_type
+            "models/facebook/wav2vec2-base-960h", hf_token=HF_TOKEN, alias=model_type
         )
         assert interface.__name__ == model_type
         assert interface.input_components and interface.output_components
@@ -148,7 +150,7 @@ class TestLoadInterface:
     def test_image_classification(self):
         model_type = "image-classification"
         interface = gr.load(
-            "models/google/vit-base-patch16-224", hf_token=None, alias=model_type
+            "models/google/vit-base-patch16-224", hf_token=HF_TOKEN, alias=model_type
         )
         assert interface.__name__ == model_type
         assert interface.input_components and interface.output_components
@@ -159,7 +161,7 @@ class TestLoadInterface:
         model_type = "feature-extraction"
         interface = gr.load(
             "models/sentence-transformers/distilbert-base-nli-mean-tokens",
-            hf_token=None,
+            hf_token=HF_TOKEN,
             alias=model_type,
         )
         assert interface.__name__ == model_type
@@ -171,7 +173,7 @@ class TestLoadInterface:
         model_type = "text-to-speech"
         interface = gr.load(
             "models/julien-c/ljspeech_tts_train_tacotron2_raw_phn_tacotron_g2p_en_no_space_train",
-            hf_token=None,
+            hf_token=HF_TOKEN,
             alias=model_type,
         )
         assert interface.__name__ == model_type
@@ -183,7 +185,7 @@ class TestLoadInterface:
         model_type = "text-to-speech"
         interface = gr.load(
             "models/julien-c/ljspeech_tts_train_tacotron2_raw_phn_tacotron_g2p_en_no_space_train",
-            hf_token=None,
+            hf_token=HF_TOKEN,
             alias=model_type,
         )
         assert interface.__name__ == model_type
@@ -204,7 +206,7 @@ class TestLoadInterface:
 
     def test_sentiment_model(self):
         io = gr.load(
-            "models/distilbert-base-uncased-finetuned-sst-2-english", hf_token=None
+            "models/distilbert-base-uncased-finetuned-sst-2-english", hf_token=HF_TOKEN
         )
         try:
             assert io("I am happy, I love you")["label"] == "POSITIVE"
@@ -212,14 +214,14 @@ class TestLoadInterface:
             pass
 
     def test_image_classification_model(self):
-        io = gr.load(name="models/google/vit-base-patch16-224", hf_token=None)
+        io = gr.load(name="models/google/vit-base-patch16-224", hf_token=HF_TOKEN)
         try:
             assert io("gradio/test_data/lion.jpg")["label"].startswith("lion")
         except TooManyRequestsError:
             pass
 
     def test_translation_model(self):
-        io = gr.load(name="models/t5-base", hf_token=None)
+        io = gr.load(name="models/t5-base", hf_token=HF_TOKEN)
         try:
             output = io("My name is Sarah and I live in London")
             assert output == "Mein Name ist Sarah und ich lebe in London"
@@ -239,7 +241,7 @@ class TestLoadInterface:
             pass
 
     def test_visual_question_answering(self):
-        io = gr.load("models/dandelin/vilt-b32-finetuned-vqa", hf_token=None)
+        io = gr.load("models/dandelin/vilt-b32-finetuned-vqa", hf_token=HF_TOKEN)
         try:
             output = io("gradio/test_data/lion.jpg", "What is in the image?")
             assert isinstance(output, dict) and "label" in output
@@ -247,7 +249,7 @@ class TestLoadInterface:
             pass
 
     def test_image_to_text(self):
-        io = gr.load("models/nlpconnect/vit-gpt2-image-captioning", hf_token=None)
+        io = gr.load("models/nlpconnect/vit-gpt2-image-captioning", hf_token=HF_TOKEN)
         try:
             output = io("gradio/test_data/lion.jpg")
             assert isinstance(output, str)
@@ -255,7 +257,7 @@ class TestLoadInterface:
             pass
 
     def test_speech_recognition_model(self):
-        io = gr.load("models/facebook/wav2vec2-base-960h", hf_token=None)
+        io = gr.load("models/facebook/wav2vec2-base-960h", hf_token=HF_TOKEN)
         try:
             output = io("gradio/test_data/test_audio.wav")
             assert output is not None
@@ -329,7 +331,7 @@ class TestLoadInterface:
     def test_private_space_v4_sse_v1(self):
         io = gr.load(
             "spaces/gradio-tests/not-actually-private-spacev4-sse-v1",
-            hf_token=HfFolder.get_token(),
+            hf_token=HF_TOKEN,
         )
         try:
             output = io("abc")
@@ -359,7 +361,7 @@ class TestLoadInterfaceWithExamples:
                     name="models/google/vit-base-patch16-224",
                     examples=[Path(test_file_dir, "cheetah1.jpg")],
                     cache_examples=True,
-                    hf_token=None,
+                    hf_token=HF_TOKEN,
                 )
             except TooManyRequestsError:
                 pass
