b0f48bb055162dc349af6150ac36716306ca163a
isatyamks <isatyamks@gmail.com>
2024-10-10 02:53:06 +0530

    Refactor slow_echo function to increase delay between message updates

--- demo/chatinterface_streaming_echo/run.py
+++ demo/chatinterface_streaming_echo/run.py
@@ -3,10 +3,10 @@ import gradio as gr
 
 def slow_echo(message, history):
     for i in range(len(message)):
-        time.sleep(0.05)
+        time.sleep(0.5)
         yield "You typed: " + message[: i + 1]
 
-demo = gr.ChatInterface(slow_echo, type="messages")
+demo = gr.ChatInterface(slow_echo).queue()
 
 if __name__ == "__main__":
     demo.launch()
