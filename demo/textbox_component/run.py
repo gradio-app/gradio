876d056dae739e8e70390e431df98b2eac796ff0
Satyam Kumar <isatyamks@gmail.com>
2024-10-10 02:41:34 +0530

    Update run.py

--- demo/textbox_component/run.py
+++ demo/textbox_component/run.py
@@ -1,6 +1,7 @@
 import gradio as gr
 
-with gr.Blocks() as demo:
-    gr.Textbox()
+with gr.Blocks(css=".copy-btn { position: relative; top: -35px; left: 220px; }") as demo:
+    gr.Textbox("Rendered Template", label="Chat Prompt", show_copy_button=True, elem_classes="copy-btn")
 
-demo.launch()
+if __name__ == "__main__":
+    demo.launch()
