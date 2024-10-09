cab50353a8739d6ce9fbe3887a05432f72026b22
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-10-09 13:56:54 +0900

    [wip] Comment out plotly part

--- js/spa/test/outbreak_forecast.spec.ts
+++ js/spa/test/outbreak_forecast.spec.ts
@@ -90,12 +90,12 @@ test("switching between all 4 plot types and pressing submit should update outpu
 	await expect(matplotlib_img_data).toBeTruthy();
 
 	//Plotly
-	await page.getByLabel("Plot Type").click();
-	await page.getByRole("option", { name: "Plotly" }).click();
+	// await page.getByLabel("Plot Type").click();
+	// await page.getByRole("option", { name: "Plotly" }).click();
 
-	await page.click("text=Submit");
-	const plotly = page.getByTestId("plotly");
-	await expect(plotly).toHaveCount(1);
+	// await page.click("text=Submit");
+	// const plotly = page.getByTestId("plotly");
+	// await expect(plotly).toHaveCount(1);
 
 	//Altair
 	await page.getByLabel("Plot Type").click();
