ee6e9ee6bfc2c80f7a5b1a8340aed4979887c729
freddyaboulton <alfonsoboulton@gmail.com>
2024-10-15 17:24:21 -0700

    Fix

--- js/spa/test/image_editor_events.spec.ts
+++ js/spa/test/image_editor_events.spec.ts
@@ -9,49 +9,37 @@ test("upload events work as expected", async ({ page }) => {
 });
 
 test("change events work as expected", async ({ page }) => {
-	await page.getByLabel("Upload button").first().click();
-	const uploader = page.locator("input[type=file]").first();
-	await uploader.setInputFiles(["./test/files/cheetah1.jpg"]);
-
 	const change_text = page.locator("#change h2");
-	await expect(change_text).toContainText("1");
-	await page.waitForTimeout(1000);
 
 	await page.getByLabel("Draw button").first().click();
 	await page.getByLabel("Draw button").first().click();
 	const canvas = page.locator("canvas").first();
+	await canvas.click({ position: { x: 100, y: 100 } });
+	await expect(change_text).toContainText("1");
+
+	await page.getByLabel("Erase button").first().click();
 	await canvas.click({ position: { x: 100, y: 100 } });
 	await expect(change_text).toContainText("2");
 
-	await page.getByLabel("Erase button").first().click();
-	await canvas.click({ position: { x: 100, y: 100 } });
-	await expect(change_text).toContainText("3");
-
 	await page.getByLabel("Clear canvas").first().click();
-	await expect(change_text).toContainText("4");
+	await expect(change_text).toContainText("3");
 });
 
 test("input events work as expected", async ({ page }) => {
-	await page.getByLabel("Upload button").first().click();
-	const uploader = page.locator("input[type=file]").first();
-	await uploader.setInputFiles(["./test/files/cheetah1.jpg"]);
-
 	const input_text = page.locator("#input h2");
-	await expect(input_text).toContainText("1");
-	await page.waitForTimeout(1000);
 
 	await page.getByLabel("Draw button").first().click();
 	await page.getByLabel("Draw button").first().click();
 	const canvas = page.locator("canvas").first();
 	await canvas.click({ position: { x: 100, y: 100 } });
-	await expect(input_text).toContainText("2");
+	await expect(input_text).toContainText("1");
 
 	await page.getByLabel("Erase button").first().click();
 	await canvas.click({ position: { x: 100, y: 100 } });
-	await expect(input_text).toContainText("3");
+	await expect(input_text).toContainText("2");
 
 	await page.getByLabel("Clear canvas").first().click();
-	await expect(input_text).toContainText("4");
+	await expect(input_text).toContainText("3");
 });
 
 test("apply events work as expected", async ({ page }) => {
