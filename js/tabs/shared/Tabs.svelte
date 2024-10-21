dda238f77719381fdf877da549220b8d3f6c4940
pngwn <hello@pngwn.io>
2024-10-21 20:24:20 +0100

    format

--- js/tabs/shared/Tabs.svelte
+++ js/tabs/shared/Tabs.svelte
@@ -33,10 +33,10 @@
 	let tab_nav_el: HTMLDivElement;
 
 	const selected_tab = writable<false | number | string>(
-		selected || tabs[0]?.id || false,
+		selected || tabs[0]?.id || false
 	);
 	const selected_tab_index = writable<number>(
-		tabs.findIndex((t) => t.id === selected) || 0,
+		tabs.findIndex((t) => t.id === selected) || 0
 	);
 	const dispatch = createEventDispatcher<{
 		change: undefined;
@@ -78,7 +78,7 @@
 			}
 		},
 		selected_tab,
-		selected_tab_index,
+		selected_tab_index
 	});
 
 	function change_tab(id: string | number): void {
@@ -142,7 +142,7 @@
 		handle_overflow_has_selected_tab($selected_tab);
 
 	function handle_overflow_has_selected_tab(
-		selected_tab: number | string | false,
+		selected_tab: number | string | false
 	): boolean {
 		if (selected_tab === false) return false;
 		return overflow_tabs.some((t) => t.id === selected_tab);
@@ -150,7 +150,7 @@
 
 	function get_tab_sizes(
 		tabs: Tab[],
-		tab_els: Record<string | number, HTMLElement>,
+		tab_els: Record<string | number, HTMLElement>
 	): Record<string | number, DOMRect> {
 		const tab_sizes: Record<string | number, DOMRect> = {};
 		tabs.forEach((tab) => {
