b8c2fd6ecb5b43f320355ef850e11768c300490c
Ali Abid <aliabid94@gmail.com>
2024-10-21 15:58:52 -0700

    changes

--- js/plot/shared/Plot.svelte
+++ js/plot/shared/Plot.svelte
@@ -4,8 +4,10 @@
 	import { Empty } from "@gradio/atoms";
 	import type { ThemeMode } from "js/core/src/components/types";
 	import type { Gradio, SelectData } from "@gradio/utils";
+	import { _ } from "svelte-i18n";
 
 	export let value;
+	let _value;
 	export let colors: string[] = [];
 	export let theme_mode: ThemeMode;
 	export let caption: string;
@@ -27,36 +29,49 @@
 		matplotlib: () => import("./plot_types/MatplotlibPlot.svelte")
 	};
 
-	const is_browser = typeof window !== "undefined";
+	let loadedPlotTypeMapping = {};
 
-	$: {
+	const is_browser = typeof window !== "undefined";
+	let key = 0;
+
+	$: if (value !== _value) {
+		key += 1;
 		let type = value?.type;
 		if (type !== _type) {
 			PlotComponent = null;
-		}
+		}			
 		if (type && type in plotTypeMapping && is_browser) {
-			plotTypeMapping[type]().then((module) => {
-				PlotComponent = module.default;
-			});
+			if (loadedPlotTypeMapping[type]) {
+				PlotComponent = loadedPlotTypeMapping[type];
+			} else {
+				plotTypeMapping[type]().then((module) => {
+					PlotComponent = module.default;
+					loadedPlotTypeMapping[type] = PlotComponent;
+				});
+			}
 		}
+		_value = value;
+		_type = type;
 	}
 </script>
 
 {#if value && PlotComponent}
-	<svelte:component
-		this={PlotComponent}
-		{value}
-		{colors}
-		{theme_mode}
-		{caption}
-		{bokeh_version}
-		{show_actions_button}
-		{gradio}
-		{_selectable}
-		{x_lim}
-		on:load
-		on:select
-	/>
+	{#key key}
+		<svelte:component
+			this={PlotComponent}
+			{value}
+			{colors}
+			{theme_mode}
+			{caption}
+			{bokeh_version}
+			{show_actions_button}
+			{gradio}
+			{_selectable}
+			{x_lim}
+			on:load
+			on:select
+		/>
+	{/key}
 {:else}
 	<Empty unpadded_box={true} size="large"><PlotIcon /></Empty>
 {/if}
