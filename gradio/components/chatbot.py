c3324d7f7f824f17fd1177b2f6b353ab5c803d05
Abubakar Abid <abubakar@huggingface.co>
2024-11-20 12:57:59 -0800

    Fix issues related to examples and example caching in `gr.ChatInterface` (#9990)
    * changes
    
    * changes
    
    * add functional tests
    
    * add changeset
    
    * revert
    
    * example format
    
    * chat interface
    
    * replace attribute with str
    
    * replace attribute with function
    
    * fix tests
    
    * changes
    
    * fix
    
    * more changes
    
    * changes
    
    * changes
    
    * demo
    
    * more changes
    
    * typing
    
    * demos
    
    * test
    
    * changes
    
    * changes
    
    * functional tests
    
    * add changeset
    
    * fix pytest
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- gradio/components/chatbot.py
+++ gradio/components/chatbot.py
@@ -108,7 +108,7 @@ class ChatbotDataMessages(GradioRootModel):
     root: list[Message]
 
 
-TupleFormat = list[
+TupleFormat = Sequence[
     tuple[Union[str, tuple[str], None], Union[str, tuple[str], None]]
     | list[Union[str, tuple[str], None]]
 ]
@@ -227,7 +227,7 @@ class Chatbot(Component):
         """
         if type is None:
             warnings.warn(
-                "You have not specified a value for the `type` parameter. Defaulting to the 'tuples' format for chatbot messages, but this is deprecated and will be removed in a future version of Gradio. Please set type='messages' instead, which uses openai-style 'role' and 'content' keys.",
+                "You have not specified a value for the `type` parameter. Defaulting to the 'tuples' format for chatbot messages, but this is deprecated and will be removed in a future version of Gradio. Please set type='messages' instead, which uses openai-style dictionaries with 'role' and 'content' keys.",
                 UserWarning,
             )
             type = "tuples"
@@ -315,7 +315,7 @@ class Chatbot(Component):
                                 file_info[i] = file_data
 
     @staticmethod
-    def _check_format(messages: list[Any], type: Literal["messages", "tuples"]):
+    def _check_format(messages: Any, type: Literal["messages", "tuples"]):
         if type == "messages":
             all_valid = all(
                 isinstance(message, dict)
