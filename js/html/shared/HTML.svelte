1ab5fc92a9efaf5ae4cf22c00ee160cf94efbea9
Abubakar Abid <abubakar@huggingface.co>
2024-12-08 00:30:07 -0600

    format

--- js/html/shared/HTML.svelte
+++ js/html/shared/HTML.svelte
@@ -5,13 +5,20 @@
 	export let value: string;
 	export let visible = true;
 
-	const dispatch = createEventDispatcher<{ change: undefined; click: undefined }>();
+	const dispatch = createEventDispatcher<{
+		change: undefined;
+		click: undefined;
+	}>();
 
 	$: value, dispatch("change");
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
-<div class="prose {elem_classes.join(' ')}" class:hide={!visible} on:click={() => dispatch("click")}>
+<div
+	class="prose {elem_classes.join(' ')}"
+	class:hide={!visible}
+	on:click={() => dispatch("click")}
+>
 	{@html value}
 </div>
 
