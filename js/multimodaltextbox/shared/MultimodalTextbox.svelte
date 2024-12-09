afd23902ef05c9abfe18466c563421ff5076c5c1
Hannah <hannahblair@users.noreply.github.com>
2024-12-09 18:02:39 +0000

    add max_plain_text_length param

--- js/multimodaltextbox/shared/MultimodalTextbox.svelte
+++ js/multimodaltextbox/shared/MultimodalTextbox.svelte
@@ -47,7 +47,7 @@
 	export let upload: Client["upload"];
 	export let stream_handler: Client["stream"];
 	export let file_count: "single" | "multiple" | "directory" = "multiple";
-	export let max_text_length = 1000;
+	export let max_plain_text_length = 1000;
 
 	let upload_component: Upload;
 	let hidden_upload: HTMLInputElement;
@@ -200,7 +200,7 @@
 		const items = event.clipboardData.items;
 		const text = event.clipboardData.getData("text");
 
-		if (text && text.length > max_text_length) {
+		if (text && text.length > max_plain_text_length) {
 			event.preventDefault();
 			const file = new window.File([text], "pasted_text.txt", {
 				type: "text/plain",
