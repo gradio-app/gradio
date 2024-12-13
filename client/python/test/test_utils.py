6a41fe7f4be6f7a7c3b7276b6af1bc775314fc22
Abubakar Abid <abubakar@huggingface.co>
2024-12-12 17:42:52 -0800

    format

--- client/python/test/test_utils.py
+++ client/python/test/test_utils.py
@@ -170,7 +170,7 @@ def test_json_schema_to_python_type(schema):
     elif schema == "FileSerializable":
         answer = "str | Dict(name: str (name of file), data: str (base64 representation of file), size: int (size of image in bytes), is_file: bool (true if the file has been uploaded to the server), orig_name: str (original name of the file)) | List[str | Dict(name: str (name of file), data: str (base64 representation of file), size: int (size of image in bytes), is_file: bool (true if the file has been uploaded to the server), orig_name: str (original name of the file))]"
     elif schema == "JSONSerializable":
-        answer = "Dict[Any, Any]"
+        answer = "str | list | dict"
     elif schema == "GallerySerializable":
         answer = "Tuple[Dict(name: str (name of file), data: str (base64 representation of file), size: int (size of image in bytes), is_file: bool (true if the file has been uploaded to the server), orig_name: str (original name of the file)), str | None]"
     elif schema == "SingleFileSerializable":
