96855e87af702e587c27a11f8383e754991dcc32
freddyaboulton <alfonsoboulton@gmail.com>
2024-12-12 17:42:58 -0800

    Fix typecheck

--- js/audio/static/StaticAudio.svelte
+++ js/audio/static/StaticAudio.svelte
@@ -25,7 +25,7 @@
 	export let waveform_options: WaveformOptions;
 	export let editable = true;
 	export let loop: boolean;
-	export let display_icon_button_wrapper_top_border: false;
+	export let display_icon_button_wrapper_top_border = false;
 
 	const dispatch = createEventDispatcher<{
 		change: FileData;
