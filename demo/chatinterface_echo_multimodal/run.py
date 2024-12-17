69f28a9bfbc409fd21e7beb770de8d7581013166
Abubakar Abid <abubakar@huggingface.co>
2024-12-17 12:48:42 -0500

    changes

--- demo/chatinterface_echo_multimodal/run.py
+++ demo/chatinterface_echo_multimodal/run.py
@@ -3,14 +3,16 @@ import gradio as gr
 def echo_multimodal(message, history):
     response = []
     response.append("You wrote: '" + message["text"] + "' and uploaded:")
-    for file in message.get("files", []):
-        response.append((file, ))
+    if message.get("files"):
+        for file in message["files"]:
+            response.append(gr.File(value=file))
     return response
 
 demo = gr.ChatInterface(
     echo_multimodal,
     type="messages",
     multimodal=True,
+    textbox=gr.MultimodalTextbox(file_count="multiple"),
 )
 
 if __name__ == "__main__":
