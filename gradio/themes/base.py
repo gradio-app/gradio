e07bb0107f20143010a967f66c901183108aaec6
Abubakar Abid <abubakar@huggingface.co>
2024-12-23 16:34:57 -0500

    type fixes

--- gradio/themes/base.py
+++ gradio/themes/base.py
@@ -124,10 +124,7 @@ class ThemeClass:
         schema = {"theme": {}}
         for prop in dir(self):
             if (
-                not prop.startswith("_")
-                or prop.startswith("_font")
-                or prop == "_stylesheets"
-                or prop == "name"
+                not prop.startswith("_") or prop.startswith("_font") or prop in ("_stylesheets", "name")
             ) and isinstance(getattr(self, prop), (list, str)):
                 schema["theme"][prop] = getattr(self, prop)
         return schema
