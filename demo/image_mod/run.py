5f3e6367a7ff5bb382dcaae56ed477fdd421a6c2
Abubakar Abid <abubakar@huggingface.co>
2024-12-31 20:10:45 -0500

    changes

--- demo/image_mod/run.py
+++ demo/image_mod/run.py
@@ -1,28 +1,13 @@
 import gradio as gr
-import os
 
 def image_mod(image):
-    return image.rotate(45)
+    return image
 
-new_samples = [
-    [os.path.join(os.path.dirname(__file__), "images/logo.png")],
-    [os.path.join(os.path.dirname(__file__), "images/tower.jpg")],
-]
-
-with gr.Blocks() as demo:
-    interface = gr.Interface(
-        image_mod,
-        gr.Image(type="pil"),
-        "image",
-        flagging_options=["blurry", "incorrect", "other"],
-        examples=[
-            os.path.join(os.path.dirname(__file__), "images/cheetah1.jpg"),
-            os.path.join(os.path.dirname(__file__), "images/lion.jpg"),
-        ],
-    )
-
-    btn = gr.Button("Update Examples")
-    btn.click(lambda : gr.Dataset(samples=new_samples), None, interface.examples_handler.dataset)
+demo = gr.Interface(
+    image_mod,
+    gr.Image(type="filepath"),
+    "image",
+)
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(ssr_mode=True)
--- js/image/shared/Image.svelte
+++ js/image/shared/Image.svelte
@@ -68,10 +68,7 @@
 {/if}
 
 <style>
-	img,
-	:global(svg) {
-		object-fit: contain;
-		width: 100%;
-		height: 100%;
+	img, :global(svg) {
+		object-fit: cover;
 	}
 </style>
