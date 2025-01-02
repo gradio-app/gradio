df7c38845cc158296242c9540609200cbecbd2fa
Abubakar Abid <abubakar@huggingface.co>
2025-01-02 08:10:55 -0500

    add to deployed demos

--- demo/chatinterface_streaming_echo/run.py
+++ demo/chatinterface_streaming_echo/run.py
@@ -6,7 +6,7 @@ def slow_echo(message, history):
         time.sleep(0.05)
         yield "You typed: " + message[: i + 1]
 
-demo = gr.ChatInterface(slow_echo, type="messages")
+demo = gr.ChatInterface(slow_echo, type="messages", save_history=True)
 
 if __name__ == "__main__":
     demo.launch()
