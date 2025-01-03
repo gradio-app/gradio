fc00a7d192b061892843685c0541c19faecc9680
Abubakar Abid <abubakar@huggingface.co>
2025-01-03 09:10:50 -0500

    example

--- demo/chatinterface_streaming_echo/run.py
+++ demo/chatinterface_streaming_echo/run.py
@@ -6,7 +6,13 @@ def slow_echo(message, history):
         time.sleep(0.05)
         yield "You typed: " + message[: i + 1]
 
-demo = gr.ChatInterface(slow_echo, type="messages", flagging_mode="manual", flagging_options=["Like", "Spam", "Inappropriate", "Other"], save_history=True)
+demo = gr.ChatInterface(
+    slow_echo,
+    type="messages",
+    flagging_mode="manual",
+    flagging_options=["Like", "Spam", "Inappropriate", "Other"], 
+    save_history=True,
+)
 
 if __name__ == "__main__":
     demo.launch()
