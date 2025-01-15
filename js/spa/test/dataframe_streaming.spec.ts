af4ad0a49c322fde89c4a10a5af94cf36464db63
Abubakar Abid <abubakar@huggingface.co>
2025-01-14 20:59:17 -0800

    format

--- js/spa/test/dataframe_streaming.spec.ts
+++ js/spa/test/dataframe_streaming.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from "@self/tootils";
 
 test("DataFrame updates correctly when button is clicked", async ({ page }) => {
-    await page.getByRole("button", { name: "Update DataFrame" }).click();
-    await expect(page.getByRole('table', { name: 'Dataframe' }).locator('td').first()).toHaveText("2");
+	await page.getByRole("button", { name: "Update DataFrame" }).click();
+	await expect(
+		page.getByRole("table", { name: "Dataframe" }).locator("td").first()
+	).toHaveText("2");
 });
