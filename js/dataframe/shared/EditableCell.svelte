b658e4764e9a77756429e2d521e06fefe363c1d4
Hannah <hannahblair@users.noreply.github.com>
2025-01-15 15:43:31 +0000

    fix mouse pointer events

--- js/dataframe/shared/EditableCell.svelte
+++ js/dataframe/shared/EditableCell.svelte
@@ -67,6 +67,9 @@
 		class:header
 		tabindex="-1"
 		on:blur={handle_blur}
+		on:mousedown|stopPropagation
+		on:mouseup|stopPropagation
+		on:click|stopPropagation
 		use:use_focus
 		on:keydown={handle_keydown}
 	/>
