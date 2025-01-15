207ea2d6b695d9a2df140776f248326b268aa312
freddyaboulton <alfonsoboulton@gmail.com>
2025-01-15 10:59:38 -0500

    rm for now

--- js/spa/test/image_editor_layers.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from "@self/tootils";
-
-test("ImageEditor layers are properly set", async ({ page }) => {
-	await page.getByRole("button", { name: "Set Layers" }).click();
-	await expect(page.getByLabel("Num Layers")).toHaveValue("1");
-});
