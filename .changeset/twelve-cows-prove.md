48809c722bcbb3aaed72edfdb6c59de5413fdbc4
Abubakar Abid <abubakar@huggingface.co>
2025-01-22 14:19:29 -0800

    Fix bug where dataframe `value` prop was not updating when an input value was changed manually (#10410)
    * dataframe
    
    * add changeset
    
    * comment
    
    * simple unit test
    
    * simple unit test
    
    * add e2e test
    
    * format
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

---
+++ .changeset/twelve-cows-prove.md
@@ -0,0 +1,6 @@
+---
+"@gradio/dataframe": minor
+"gradio": minor
+---
+
+feat:Fix bug where dataframe `value` prop was not updating when an input value was changed manually
--- demo/dataframe_streaming/run.py
+++ demo/dataframe_streaming/run.py
@@ -9,6 +9,9 @@ def update_dataframe(df):
     df.iloc[:, :] = 2
     yield df, 2
 
+def sum_values(df):
+    return pd.to_numeric(df.values.flatten(), errors='coerce').sum()
+
 initial_df = pd.DataFrame(0, index=range(5), columns=range(5))
 
 with gr.Blocks() as demo:
@@ -16,13 +19,16 @@ with gr.Blocks() as demo:
         button = gr.Button("Update DataFrame")
         number = gr.Number(value=0, label="Number")
     dataframe = gr.Dataframe(value=initial_df, label="Dataframe")
+
     button.click(fn=update_dataframe, inputs=dataframe, outputs=[dataframe, number])
     with gr.Row():
         change_events = gr.Number(label="Change events")
         input_events = gr.Number(label="Input events")
+        sum_of_values = gr.Number(label="Sum of values")
 
     dataframe.change(lambda x:x+1, inputs=change_events, outputs=change_events)
     dataframe.input(lambda x:x+1, inputs=input_events, outputs=input_events)
+    dataframe.change(sum_values, inputs=dataframe, outputs=sum_of_values)
 
 if __name__ == "__main__":
     demo.launch()
