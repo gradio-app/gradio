c70ea5660b5f5e71ac7b5429b993cfd87dd209c9
Abubakar Abid <abubakar@huggingface.co>
2025-01-27 12:19:04 -0800

    changes

--- demo/html_component/run.py
+++ demo/html_component/run.py
@@ -1,6 +1,12 @@
 import gradio as gr
 
+value = """
+<script>
+    alert("Hello, world!");
+</script>
+"""
+
 with gr.Blocks() as demo:
-    gr.HTML(value="<p style='margin-top: 1rem, margin-bottom: 1rem'>This <em>example</em> was <strong>written</strong> in <a href='https://en.wikipedia.org/wiki/HTML' _target='blank'>HTML</a> </p>")
+    gr.HTML(value=value, allow_js=False)
 
 demo.launch()
--- js/html/shared/HTML.svelte
+++ js/html/shared/HTML.svelte
@@ -1,43 +1,25 @@
 <script lang="ts">
-	import { createEventDispatcher, onMount } from "svelte";
+	import { createEventDispatcher } from "svelte";
 
 	export let elem_classes: string[] = [];
 	export let value: string;
 	export let visible = true;
-	export let allow_js = false;
 
 	const dispatch = createEventDispatcher<{
 		change: undefined;
 		click: undefined;
 	}>();
 
-	let container: HTMLElement;
-
-	onMount(() => {
-		if (allow_js && container && value) {
-			const parser = new DOMParser();
-			const doc = parser.parseFromString(value, 'text/html');			
-			container.innerHTML = '';
-			doc.body.childNodes.forEach(node => {
-				const importedNode = document.importNode(node, true);
-				container.appendChild(importedNode);
-			});
-		}
-	});
-
 	$: value, dispatch("change");
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
 <div
-	bind:this={container}
 	class="prose {elem_classes.join(' ')}"
 	class:hide={!visible}
 	on:click={() => dispatch("click")}
 >
-	{#if !allow_js}
-		{@html value}
-	{/if}
+	{@html value}
 </div>
 
 <style>
