8dd3042e09d45764425cc21a611f1d018297ff99
Dawood <dawoodkhan82@gmail.com>
2025-01-30 13:24:13 -0500

    format

--- js/spa/test/blocks_sidebar.spec.ts
+++ js/spa/test/blocks_sidebar.spec.ts
@@ -1,11 +1,13 @@
 import { test, expect } from "@self/tootils";
 
 test("shows open sidebar and components in sidebar work", async ({ page }) => {
-	await expect(page.getByRole('heading', { name: '🐾 Pet Name Generator' })).toBeVisible();
-	await expect(page.getByLabel('Choose your pet type')).toBeVisible();
+	await expect(
+		page.getByRole("heading", { name: "🐾 Pet Name Generator" })
+	).toBeVisible();
+	await expect(page.getByLabel("Choose your pet type")).toBeVisible();
 	await expect(page.getByText("Personality type")).toBeVisible();
 
-	await page.getByLabel('Choose your pet type').click();
+	await page.getByLabel("Choose your pet type").click();
 	await page.getByText("Dog").click();
 	await page.getByText("Silly").click();
 	await page.getByRole("button", { name: "Generate Name! 🎲" }).click();
@@ -14,9 +16,11 @@ test("shows open sidebar and components in sidebar work", async ({ page }) => {
 	await expect(nameOutput).toBeVisible();
 	await expect(nameOutput).not.toBeEmpty();
 
-	await page.getByLabel('Toggle Sidebar').click();
+	await page.getByLabel("Toggle Sidebar").click();
 
-	await expect(page.getByRole('heading', { name: '🐾 Pet Name Generator' })).not.toBeInViewport();
+	await expect(
+		page.getByRole("heading", { name: "🐾 Pet Name Generator" })
+	).not.toBeInViewport();
 	await expect(page.getByText("Choose your pet type")).not.toBeInViewport();
 	await expect(page.getByText("Personality type")).not.toBeInViewport();
 });
