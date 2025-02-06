0c62e3e7b9f8cf91b1ddf75e1176f2d6d488a8dc
Ali Abid <aliabid94@gmail.com>
2025-02-05 16:41:37 -0800

    Revert "expand_tabs_in_row"
    This reverts commit 57dd5c7701706f9b3d4c3a1dd6fd5a746f408c14.

--- gradio/layouts/tabs.py
+++ gradio/layouts/tabs.py
@@ -67,7 +67,7 @@ class Tab(BlockContext, metaclass=ComponentMeta):
         id: int | str | None = None,
         elem_id: str | None = None,
         elem_classes: list[str] | str | None = None,
-        scale: int | None = None,
+        scale: int = 0,
         render: bool = True,
     ):
         """
