93acd1472fbffd8ae6852736efb189b1a922f5fd
Abubakar Abid <abubakar@huggingface.co>
2025-02-10 12:47:19 -0800

    changes

--- demo/calculator_js/run.py
+++ demo/calculator_js/run.py
@@ -1,34 +1,37 @@
 import gradio as gr
-from gradio import js
 
-@js
-def calculator(num1: float, operation: str, num2: float) -> float:
+def calculator(num1, operation, num2):
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
         return num1 - num2
     elif operation == "multiply":
         return num1 * num2
-    else:
+    elif operation == "divide":
         return num1 / num2
 
-demo = gr.Interface(
-    calculator,
-    [
-        "number",
-        gr.Radio(["add", "subtract", "multiply", "divide"], value="add"),
-        "number"
-    ],
-    "number",
-    examples=[
-        [45, "add", 3],
-        [3.14, "divide", 2],
-        [144, "multiply", 2.5],
-        [0, "subtract", 1.2],
-    ],
-    title="Toy Calculator",
-    description="Here's a sample toy calculator.",
-)
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            num_1 = gr.Number(value=4)
+            operation = gr.Radio(["add", "subtract", "multiply", "divide"])
+            num_2 = gr.Number(value=0)
+            submit_btn = gr.Button(value="Calculate")
+        with gr.Column():
+            result = gr.Number()
+
+    submit_btn.click(
+        calculator, inputs=[num_1, operation, num_2], outputs=[result], js=True
+    )
+    examples = gr.Examples(
+        examples=[
+            [5, "add", 3],
+            [4, "divide", 2],
+            [-4, "multiply", 2.5],
+            [0, "subtract", 1.2],
+        ],
+        inputs=[num_1, operation, num_2],
+    )
 
 if __name__ == "__main__":
     demo.launch()
