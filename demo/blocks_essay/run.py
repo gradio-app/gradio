837d2f339c291cc1ce1c1441a64c08ff7918c1fb
Abubakar Abid <abubakar@huggingface.co>
2025-02-11 21:07:03 -0800

    changes

--- demo/blocks_essay/run.py
+++ demo/blocks_essay/run.py
@@ -30,6 +30,7 @@ with gr.Blocks() as demo:
     with gr.Row():
         country = gr.Dropdown(list(countries_cities_dict.keys()), label="Country")
         cities = gr.Dropdown([], label="Cities")
+
     @country.change(inputs=country, outputs=cities)
     def update_cities(country):
         cities = list(countries_cities_dict[country])
@@ -38,7 +39,7 @@ with gr.Blocks() as demo:
     def reset_bounds(minimum, maximum):
         return gr.Number(minimum=minimum, maximum=maximum)
 
-    radio.change(fn=change_textbox, inputs=radio, outputs=[text, submit_btn], js=True)
+    radio.change(fn=change_textbox, inputs=radio, outputs=[text, submit_btn], js=True, preprocess=False, postprocess=False)
     gr.on(
         [minimum_slider.change, maximum_slider.change],
         reset_bounds,
