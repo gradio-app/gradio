4d9f1aebc0c74a1df7dd60a9981eed910f756fe6
Abubakar Abid <abubakar@huggingface.co>
2025-02-12 14:21:27 -0800

    changes

--- gradio/external.py
+++ gradio/external.py
@@ -109,6 +109,7 @@ def load(
             @gr.render(triggers=[demo.load])
             def create_blocks(oauth_token: gr.OAuthToken | None):
                 token_value = None if oauth_token is None else oauth_token.token
+                gr.Markdown("token>>>>>")
                 gr.Markdown(token_value)
                 return load_blocks_from_huggingface(
                     name=name, src=src, hf_token=token_value, **kwargs
