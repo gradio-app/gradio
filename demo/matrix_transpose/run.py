16b2cbeec039bb1c4b506bf9cb90655adb46e699
Abubakar Abid <abubakar@huggingface.co>
2025-02-14 15:29:02 -0800

    revert

--- demo/matrix_transpose/run.py
+++ demo/matrix_transpose/run.py
@@ -1,10 +1,23 @@
+import numpy as np
+
 import gradio as gr
 
-with gr.Blocks() as demo:
-    with gr.Row():
-        m = gr.Matrix(headers=["test", "bar"])
-        gr.Button("FoO")
+def transpose(matrix):
+    return matrix.T
 
+demo = gr.Interface(
+    transpose,
+    gr.Dataframe(type="numpy", datatype="number", row_count=5, col_count=3, show_fullscreen_button=True),
+    "numpy",
+    examples=[
+        [np.zeros((3, 3)).tolist()],
+        [np.ones((2, 2)).tolist()],
+        [np.random.randint(0, 10, (3, 10)).tolist()],
+        [np.random.randint(0, 10, (10, 3)).tolist()],
+        [np.random.randint(0, 10, (10, 10)).tolist()],
+    ],
+    cache_examples=False
+)
 
 if __name__ == "__main__":
     demo.launch()
