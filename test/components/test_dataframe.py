d63b28644ba0b3e1377fa13ced2c21a88012a1a9
Hannah <hannahblair@users.noreply.github.com>
2025-02-17 19:02:05 +0000

    fix test

--- test/components/test_dataframe.py
+++ test/components/test_dataframe.py
@@ -29,7 +29,7 @@ class TestDataframe:
         assert dataframe_input.get_config() == {
             "value": {
                 "headers": ["Name", "Age", "Member"],
-                "data": [["", "", ""]],
+                "data": [],
                 "metadata": None,
             },
             "_selectable": False,
@@ -78,7 +78,7 @@ class TestDataframe:
         assert dataframe_output.get_config() == {
             "value": {
                 "headers": ["1", "2", "3"],
-                "data": [["", "", ""]],
+                "data": [],
                 "metadata": None,
             },
             "_selectable": False,
@@ -122,12 +122,6 @@ class TestDataframe:
         postprocess
         """
         dataframe_output = gr.Dataframe()
-        output = dataframe_output.postprocess([]).model_dump()
-        assert output == {
-            "data": [["", "", ""]],
-            "headers": ["1", "2", "3"],
-            "metadata": None,
-        }
         output = dataframe_output.postprocess(np.zeros((2, 2))).model_dump()
         assert output == {
             "data": [[0, 0], [0, 0]],
@@ -378,6 +372,7 @@ class TestDataframe:
         assert df.is_empty(None)
         assert df.is_empty({})
         assert df.is_empty({"data": [], "headers": ["a", "b"]})
+        assert df.is_empty({"data": []})
         assert not df.is_empty({"data": [1, 2]})
         assert not df.is_empty([[1, 2], [3, 4]])
         assert not df.is_empty(pd.DataFrame({"a": [1, 2]}))
@@ -401,7 +396,6 @@ class TestDataframe:
         assert df.get_cell_data(test_data) == [[1, 2], [3, 4]]
         assert df.get_cell_data(test_df) == [[1, 2], [3, 4]]
         assert df.get_cell_data({"data": test_data}) == [[1, 2], [3, 4]]
-        assert df.get_cell_data(np.array([1, 2, 3])) == [[1], [2], [3]]
 
         styled_df = test_df.style
         styled_df.hide(axis=1, subset=["col2"])
