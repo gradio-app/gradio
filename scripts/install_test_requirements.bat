49492dd2fcb3741f5afc033d6da9da300fd0dd6c
Bilal Himite <BilHim@users.noreply.github.com>
2025-02-18 02:11:27 +0100

    Fix subroutine in Windows batch scripts (#10610)

--- scripts/install_test_requirements.bat
+++ scripts/install_test_requirements.bat
@@ -4,11 +4,8 @@ setlocal EnableDelayedExpansion
 :: Change to parent directory of script location
 cd /d "%~dp0.."
 
-:: Source the helpers script
-call scripts\helpers.bat
-
 :: Check for pip
-call :pip_required
+call scripts\helpers.bat pip_required
 
 echo Installing requirements before running tests...
 pip install --upgrade pip
