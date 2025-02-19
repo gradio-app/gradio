d7c7bbfde3f420b505a10d6719b50d0e8b5dfe61
Abubakar Abid <abubakar@huggingface.co>
2025-02-18 17:11:34 -0800

    demo

--- demo/dataframe_colorful/run.py
+++ demo/dataframe_colorful/run.py
@@ -1,32 +1,20 @@
-import pandas as pd 
+import pandas as pd
 import gradio as gr
 
-# Creating a sample dataframe
-def run():
-  df = pd.DataFrame({
-      "A" : [14, 4, 5, 4, -1], 
-      "B" : [5, 2, 54, 3, 2], 
-      "C" : [20, 20, 7, 3, 8], 
-      "D" : [14, 3, 6, 2, 6], 
-      "E" : [23, 45, 64, 32, 23]
-  }) 
-  df = df.style.map(color_num, subset=["A"])
-  print(df.to_html())
-  return df
+df = pd.DataFrame(
+    {
+        "A": [14, 4, 5, 4, 1],
+        "B": [5, 2, 54, 3, 2],
+        "C": [20, 20, 7, 3, 8],
+        "D": [14, 3, 6, 2, 6],
+        "E": [23, 45, 64, 32, 23],
+    }
+)
 
-# Function to apply text color
+t = df.style.highlight_max(color="lightgreen", axis=0)
 
-def color_num(value: float) -> str:
-    color = "red" if value >= 0 else "green"
-    color_style = "color: {}".format(color)
-    return color_style
-
-# Displaying the styled dataframe in Gradio
 with gr.Blocks() as demo:
-    gr.Textbox("{}".format(gr.__version__))
-    a = gr.DataFrame()
-    b = gr.Button("run")
-    b.click(run,outputs=a)
-    
+    gr.Dataframe(t)
+
 if __name__ == "__main__":
     demo.launch()
