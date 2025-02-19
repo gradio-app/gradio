8c08446d454326483b5e092043c896e2ba216f4b
Abubakar Abid <abubakar@huggingface.co>
2025-02-19 00:57:06 -0800

    changes

--- demo/todo_list_js/run.py
+++ demo/todo_list_js/run.py
@@ -6,7 +6,7 @@ import gradio as gr
 
 tasks = ["Get a job", "Marry rich", "", "", "", ""]
 textboxes = []
-
+buttons = []
 with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column(scale=3):
@@ -20,12 +20,13 @@ with gr.Blocks() as demo:
             t = gr.Textbox(tasks[i], placeholder="Enter a task", show_label=False, container=False, scale=7, interactive=True)
             b = gr.Button("✔️", interactive=bool(tasks[i]), variant="primary" if tasks[i] else "secondary")
             textboxes.append(t)
+            buttons.append(b)
         t.change(lambda : gr.Button(interactive=True, variant="primary"), None, b, js=True)
         b.click(lambda : gr.Row(visible=False), None, r, js=True)
     freeze_button.click(lambda : [gr.Textbox(interactive=False), gr.Textbox(interactive=False), gr.Textbox(interactive=False), gr.Textbox(interactive=False), gr.Textbox(interactive=False), gr.Textbox(interactive=False)], None, textboxes, js=True)
     edit_button.click(lambda : [gr.Textbox(interactive=True), gr.Textbox(interactive=True), gr.Textbox(interactive=True), gr.Textbox(interactive=True), gr.Textbox(interactive=True), gr.Textbox(interactive=True)], None, textboxes, js=True)
+    freeze_button.click(lambda : [gr.Button(visible=False), gr.Button(visible=False), gr.Button(visible=False), gr.Button(visible=False), gr.Button(visible=False), gr.Button(visible=False)], None, buttons, js=True)
+    edit_button.click(lambda : [gr.Button(visible=True), gr.Button(visible=True), gr.Button(visible=True), gr.Button(visible=True), gr.Button(visible=True), gr.Button(visible=True)], None, buttons, js=True)
 
-    # Add a button to add a new task
-
-
-demo.launch()
+if __name__ == "__main__":
+    demo.launch()
