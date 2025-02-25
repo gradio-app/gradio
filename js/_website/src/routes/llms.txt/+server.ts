1c35af6f6df0135b712573dbbc3773f5d24ce415
aliabd <ali.si3luwa@gmail.com>
2025-02-25 15:24:11 -0800

    formatting

--- js/_website/src/routes/llms.txt/+server.ts
+++ js/_website/src/routes/llms.txt/+server.ts
@@ -1,39 +1,39 @@
-import { json } from '@sveltejs/kit';
+import { json } from "@sveltejs/kit";
 import SYSTEM_PROMPT from "$lib/json/system_prompt.json";
 
 export async function GET({ url }) {
-    const worker_url = "https://llms-txt.playground-worker.pages.dev/api/prompt";
-    // const worker_url = "https://playground-worker.pages.dev/api/prompt";
-    // const worker_url = "http://localhost:5173/api/prompt";
+	const worker_url = "https://llms-txt.playground-worker.pages.dev/api/prompt";
+	// const worker_url = "https://playground-worker.pages.dev/api/prompt";
+	// const worker_url = "http://localhost:5173/api/prompt";
 
-    const query = url.searchParams.get('q')?.toLowerCase() || '';
-    const format = url.searchParams.get('format')?.toLowerCase() || 'text';
-    
-    const response = await fetch(worker_url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Origin": url.origin
-        },
-        body: JSON.stringify({ 
-            prompt_to_embed: query, 
-            SYSTEM_PROMPT: SYSTEM_PROMPT.SYSTEM,
-            FALLBACK_PROMPT: SYSTEM_PROMPT.FALLBACK
-         })
-    });
+	const query = url.searchParams.get("q")?.toLowerCase() || "";
+	const format = url.searchParams.get("format")?.toLowerCase() || "text";
 
-    const data = await response.json();
+	const response = await fetch(worker_url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Origin: url.origin
+		},
+		body: JSON.stringify({
+			prompt_to_embed: query,
+			SYSTEM_PROMPT: SYSTEM_PROMPT.SYSTEM,
+			FALLBACK_PROMPT: SYSTEM_PROMPT.FALLBACK
+		})
+	});
 
-    console.log(data);
+	const data = await response.json();
 
-    if (format === 'json') {
-        return json(data);
-    } else {
-        return new Response(data.SYS_PROMPT, {
-            headers: {
-                'Content-Type': 'text/plain',
-                'Cache-Control': 'public, max-age=3600'
-            }
-        });
-    }
+	console.log(data);
+
+	if (format === "json") {
+		return json(data);
+	} else {
+		return new Response(data.SYS_PROMPT, {
+			headers: {
+				"Content-Type": "text/plain",
+				"Cache-Control": "public, max-age=3600"
+			}
+		});
+	}
 }
