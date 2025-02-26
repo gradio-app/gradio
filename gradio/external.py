52f0fe39735d20ab7b86ccadc98d2146ac3663b7
Abubakar Abid <abubakar@huggingface.co>
2025-02-26 13:06:44 -0800

    changes

--- gradio/external.py
+++ gradio/external.py
@@ -471,6 +471,8 @@ def from_model(
         except huggingface_hub.errors.HfHubHTTPError as e:  # type: ignore
             if "429" in str(e):
                 raise TooManyRequestsError() from e
+            elif "401" in str(e):
+                raise gr.Error("Unauthorized, please make sure you are logged in.") from e
             else:
                 raise gr.Error(str(e)) from e
 
