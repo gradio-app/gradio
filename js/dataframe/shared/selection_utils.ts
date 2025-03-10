b96f13d509f5f72f83c91698bd60a4a708e22c8e
Abubakar Abid <abubakar@huggingface.co>
2025-03-10 13:21:36 -0700

    remove console

--- js/dataframe/shared/selection_utils.ts
+++ js/dataframe/shared/selection_utils.ts
@@ -56,14 +56,6 @@ export function handle_selection(
 	selected_cells: CellCoordinate[],
 	event: { shiftKey: boolean; metaKey: boolean; ctrlKey: boolean }
 ): CellCoordinate[] {
-	console.log(
-		"current",
-		current,
-		"selected_cells",
-		selected_cells,
-		"event",
-		event
-	);
 	if (event.shiftKey && selected_cells.length > 0) {
 		return get_range_selection(
 			selected_cells[selected_cells.length - 1],
