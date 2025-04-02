be46b94f51c29d66fef9320d59a7f7e5a0df81b4
Hannah <hannahblair@users.noreply.github.com>
2025-04-02 14:57:23 +0100

    Add `rtl` to gr.HighlightedText (#10924)
    * add rtl to highlighted text
    
    * add changeset
    
    * fix test
    
    * Update gradio/components/highlighted_text.py
    
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- js/highlightedtext/HighlightedText.stories.svelte
+++ js/highlightedtext/HighlightedText.stories.svelte
@@ -111,3 +111,16 @@
 		interactive: false
 	}}
 />
+
+<Story
+	name="Highlighted Text with RTL"
+	args={{
+		value: [
+			{ token: "مرحبا", class_or_confidence: "greeting" },
+			{ token: " ", class_or_confidence: null },
+			{ token: "بالعالم", class_or_confidence: "subject" }
+		],
+		rtl: true,
+		color_map: { greeting: "green", subject: "blue" }
+	}}
+/>
