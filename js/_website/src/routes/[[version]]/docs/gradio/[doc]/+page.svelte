e289a8b94c29234b6747c32bfa154d8bb7ef15ee
aliabd <ali.si3luwa@gmail.com>
2025-04-02 18:06:20 -0700

    fix

--- js/_website/src/routes/[[version]]/docs/gradio/[doc]/+page.svelte
+++ js/_website/src/routes/[[version]]/docs/gradio/[doc]/+page.svelte
@@ -117,6 +117,11 @@
 	canonical={$page.url.pathname}
 	description={"Gradio docs for using " + all_headers.page_title.title}
 />
+
+<svelte:head>
+	<link rel="stylesheet" href="https://gradio-hello-world.hf.space/theme.css">
+</svelte:head>
+
 <svelte:window bind:scrollY={y} />
 
 <main class="container mx-auto px-4 flex flex-col gap-4">
