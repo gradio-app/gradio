b1b8d22b6a7e8284a119a031b25e7d5ea866bca5
Abubakar Abid <abubakar@huggingface.co>
2025-04-07 13:44:46 -0700

    interface

--- demo/image_editor_sketchpad/run.py
+++ demo/image_editor_sketchpad/run.py
@@ -6,15 +6,19 @@ def percent_of_pixels_selected(image):
     mask_bool = mask[:,:,3] > 0
     return f"{round(np.sum(mask_bool) / mask_bool.size * 100, 2)}%"
 
-with gr.Blocks() as demo:
-    with gr.Row():
-        image_editor = gr.Sketchpad(
-            type="numpy",
-        )
-        output_image = gr.Label(
-            label="Percent of Pixels Selected"
-        )
-        image_editor.change(fn=percent_of_pixels_selected, inputs=image_editor, outputs=output_image)
+image_editor = gr.Sketchpad(
+    type="numpy",
+)
+output_image = gr.Label(
+    label="Percent of Pixels Selected"
+)
+
+demo = gr.Interface(
+    fn=percent_of_pixels_selected,
+    inputs=image_editor,
+    outputs=output_image,
+    live=True
+)
 
 if __name__ == "__main__":
     demo.launch()
