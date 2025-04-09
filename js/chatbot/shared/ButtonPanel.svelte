cbdc45001f57be733f68461acf8d1bba3d52c7b7
Freddy Boulton <alfonsoboulton@gmail.com>
2025-04-09 12:17:49 -0400

    C

--- js/chatbot/shared/ButtonPanel.svelte
+++ js/chatbot/shared/ButtonPanel.svelte
@@ -6,7 +6,9 @@
 	import { Retry, Undo, Edit, Check, Clear } from "@gradio/icons";
 	import { IconButtonWrapper, IconButton } from "@gradio/atoms";
 	import { all_text, is_all_text } from "./utils";
+	import type { I18nFormatter } from "js/core/src/gradio_helper";
 
+	export let i18n: I18nFormatter;
 	export let likeable: boolean;
 	export let feedback_options: string[];
 	export let show_retry: boolean;
@@ -27,6 +29,7 @@
 
 	$: message_text = is_all_text(message) ? all_text(message) : "";
 	$: show_copy = show_copy_button && message && is_all_text(message);
+	$: console.log("retry", i18n("chatbot.retry"));
 </script>
 
 {#if show_copy || show_retry || show_undo || show_edit || likeable}
@@ -37,13 +40,13 @@
 		<IconButtonWrapper top_panel={false}>
 			{#if in_edit_mode}
 				<IconButton
-					label="Submit"
+					label={i18n("chatbot.submit")}
 					Icon={Check}
 					on:click={() => handle_action("edit_submit")}
 					disabled={generating}
 				/>
 				<IconButton
-					label="Cancel"
+					label={i18n("chatbot.cancel")}
 					Icon={Clear}
 					on:click={() => handle_action("edit_cancel")}
 					disabled={generating}
@@ -59,14 +62,14 @@
 				{#if show_retry}
 					<IconButton
 						Icon={Retry}
-						label="Retry"
+						label={i18n("chatbot.retry")}
 						on:click={() => handle_action("retry")}
 						disabled={generating}
 					/>
 				{/if}
 				{#if show_undo}
 					<IconButton
-						label="Undo"
+						label={i18n("chatbot.undo")}
 						Icon={Undo}
 						on:click={() => handle_action("undo")}
 						disabled={generating}
@@ -74,7 +77,7 @@
 				{/if}
 				{#if show_edit}
 					<IconButton
-						label="Edit"
+						label={i18n("chatbot.edit")}
 						Icon={Edit}
 						on:click={() => handle_action("edit")}
 						disabled={generating}
@@ -85,6 +88,7 @@
 						{handle_action}
 						{feedback_options}
 						selected={current_feedback}
+						{i18n}
 					/>
 				{/if}
 			{/if}
