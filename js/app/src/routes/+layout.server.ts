4d43bf00523c856053bb5288e5b67313fc9db7c7
pngwn <hello@pngwn.io>
2025-04-15 18:00:25 +0100

    fix

--- js/app/src/routes/+layout.server.ts
+++ js/app/src/routes/+layout.server.ts
@@ -1,11 +1,9 @@
 import { redirect } from "@sveltejs/kit";
 import { dev } from "$app/environment";
-import { PUBLIC_API_BASE_URL } from "$env/static/public";
 
 export function load({ url }): void {
 	const { pathname, search } = url;
-	console.log("PUBLIC_API_BASE_URL", PUBLIC_API_BASE_URL);
-	const api_base_url = PUBLIC_API_BASE_URL || "http://127.0.0.1:7860";
+	const api_base_url = "http://127.0.0.1:7860";
 
 	if (dev && url.pathname.startsWith("/theme")) {
 		redirect(308, `${api_base_url}${pathname}${search}`);
