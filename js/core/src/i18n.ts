2cbafdbdcd5bb68b9cbceae308e2f4760e4ce41f
Hannah <hannahblair@users.noreply.github.com>
2025-04-17 12:52:44 +0200

    add locale normalisation to work with browser locale

--- js/core/src/i18n.ts
+++ js/core/src/i18n.ts
@@ -41,9 +41,11 @@ export async function setupi18n(): Promise<void> {
 	}
 
 	const browser_locale = getLocaleFromNavigator();
+	const normalized_locale = browser_locale?.split("-")[0];
+
 	const initial_locale =
-		browser_locale && available_locales.includes(browser_locale)
-			? browser_locale
+		normalized_locale && available_locales.includes(normalized_locale)
+			? normalized_locale
 			: "en";
 
 	await init({
