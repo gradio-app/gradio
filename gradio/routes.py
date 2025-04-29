487b57516b654c7cc53ad4b07d95a14b35a84ace
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-04-29 15:27:06 -0400

    Fix

--- gradio/routes.py
+++ gradio/routes.py
@@ -1406,15 +1406,17 @@ class App(FastAPI):
             return app.get_blocks()._queue.get_status()
 
         @router.get("/upload_progress")
-        def get_upload_progress(upload_id: str, request: fastapi.Request):
+        async def get_upload_progress(upload_id: str, request: fastapi.Request):
             async def sse_stream(request: fastapi.Request):
                 last_heartbeat = time.perf_counter()
                 is_done = False
                 while True:
                     if await request.is_disconnected():
+                        print("disconnected")
                         file_upload_statuses.stop_tracking(upload_id)
                         return
                     if is_done:
+                        print("done")
                         file_upload_statuses.stop_tracking(upload_id)
                         return
 
@@ -1422,24 +1424,36 @@ class App(FastAPI):
                     check_rate = 0.05
                     try:
                         if file_upload_statuses.is_done(upload_id):
+                            print("is_done")
                             message = {"msg": "done"}
                             is_done = True
                         else:
+                            print("not done")
                             update = file_upload_statuses.pop(upload_id)
                             message = {
                                 "msg": "update",
                                 "orig_name": update.filename,
                                 "chunk_size": update.chunk_size,
                             }
+                            print("message", message)
                         yield f"data: {json.dumps(message)}\n\n"
                     except FileUploadProgressNotTrackedError:
+                        print("FileUploadProgressNotTrackedError")
                         return
                     except FileUploadProgressNotQueuedError:
+                        print("FileUploadProgressNotQueuedError")
                         await asyncio.sleep(check_rate)
                         if time.perf_counter() - last_heartbeat > heartbeat_rate:
                             message = {"msg": "heartbeat"}
                             yield f"data: {json.dumps(message)}\n\n"
                             last_heartbeat = time.perf_counter()
+
+            try:
+                await asyncio.wait_for(
+                    file_upload_statuses.is_tracked(upload_id), timeout=10
+                )
+            except (asyncio.TimeoutError, FileUploadProgressNotTrackedError):
+                return PlainTextResponse("Upload not found", status_code=404)
 
             return StreamingResponse(
                 sse_stream(request),
@@ -1459,6 +1473,7 @@ class App(FastAPI):
                 raise HTTPException(status_code=400, detail="Invalid content type.")
 
             try:
+                print("upload_id", upload_id)
                 if upload_id:
                     file_upload_statuses.track(upload_id)
                 max_file_size = app.get_blocks().max_file_size
