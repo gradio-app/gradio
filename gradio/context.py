0fc8db631b5c8f1b74cca3297f0b6eec3680c1e7
Ali Abid <aliabid94@gmail.com>
2025-05-15 16:25:16 -0700

    fix

--- gradio/context.py
+++ gradio/context.py
@@ -32,8 +32,8 @@ class LocalContext:
     event_id: ContextVar[str | None] = ContextVar("event_id", default=None)
     request: ContextVar[Request | None] = ContextVar("request", default=None)
     progress: ContextVar[Progress | None] = ContextVar("progress", default=None)
-    key_to_id_map: ContextVar[dict[int | str | tuple, int]] = ContextVar(
-        "key_to_id_map", default=None
+    key_to_id_map: ContextVar[dict[int | str | tuple[str | int], int] | None] = (
+        ContextVar("key_to_id_map", default=None)
     )
 
 
