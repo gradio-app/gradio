8d26f3376d616b2c72d873fedfdd3c903b0c1f67
Abubakar Abid <abubakar@huggingface.co>
2025-05-28 15:33:10 -0700

    changes

--- gradio/helpers.py
+++ gradio/helpers.py
@@ -111,6 +111,7 @@ class Examples:
         *,
         example_labels: list[str] | None = None,
         visible: bool = True,
+        preload: int | Literal[False] = 0,
         _initiated_directly: bool = True,
     ):
         """
@@ -131,6 +132,7 @@ class Examples:
             batch: If True, then the function should process a batch of inputs, meaning that it should accept a list of input values for each parameter. Used only if cache_examples is not False.
             example_labels: A list of labels for each example. If provided, the length of this list should be the same as the number of examples, and these labels will be used in the UI instead of rendering the example values.
             visible: If False, the examples component will be hidden in the UI.
+            preload: If an integer is provided (and examples are being cached), the example at that index in the examples list will be preloaded when the Gradio app is loaded. If False, no example will be preloaded.
         """
         if _initiated_directly:
             warnings.warn(
@@ -259,6 +261,7 @@ class Examples:
         self.batch = batch
         self.example_labels = example_labels
         self.working_directory = working_directory
+        self.preload = preload
 
         from gradio import components
 
@@ -375,6 +378,18 @@ class Examples:
                     api_name=self.api_name,
                     show_api=False,
                 )
+
+                if self.preload and self.root_block:
+                    self.root_block.load(
+                        load_example_input,
+                        inputs=[self.dataset],
+                        outputs=self.inputs,
+                        show_progress="hidden",
+                        postprocess=False,
+                        queue=False,
+                        show_api=False,
+                    )
+
             else:
 
                 def load_example(example_tuple):
