aed26a64359adb5ed8bc1e524d1dd7fb338858be
Abubakar Abid <abubakar@huggingface.co>
2025-06-10 16:43:26 -0700

    revert

--- demo/datetime_component/run.py
+++ demo/datetime_component/run.py
@@ -1,6 +1,6 @@
 import gradio as gr
 
-demo = gr.Interface(fn=lambda x: x, inputs=gr.DateTime(), outputs=gr.DateTime())
+with gr.Blocks() as demo:
+    gr.DateTime()
 
-if __name__ == "__main__":
-    demo.launch()
+demo.launch()
