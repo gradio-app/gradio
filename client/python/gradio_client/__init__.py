4c35c4fdce486381ade43299c6c7d1908b44c23b
Shiva Kaul <shiva.kaul@gmail.com>
2025-06-12 12:22:10 -0400

    Restore static typing for client

--- client/python/gradio_client/__init__.py
+++ client/python/gradio_client/__init__.py
@@ -1,4 +1,5 @@
 import lazy_loader as lazy
+from typing import TYPE_CHECKING
 
 __getattr__, __dir__, __all__ = lazy.attach(
     __name__,
@@ -7,5 +8,12 @@ __getattr__, __dir__, __all__ = lazy.attach(
         "client": ["Client"],
         "utils": ["file", "handle_file", "__version__"],
         "data_classes": ["FileData"],
-    }
+    },
 )
+
+# Needed so static type inference doesn't break
+# Make sure these imports stay synchronized with the lazy imports above
+if TYPE_CHECKING:
+    from gradio_client.client import Client
+    from gradio_client.data_classes import FileData
+    from gradio_client.utils import __version__, file, handle_file
