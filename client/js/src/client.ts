9c6b93cc75ed8ad694d4abcbd4e4c459cd5f5eee
Ali Abid <aliabid94@gmail.com>
2025-06-17 11:46:57 -0700

    changes

--- client/js/src/client.ts
+++ client/js/src/client.ts
@@ -314,7 +314,6 @@ export class Client {
 		try {
 			// Create base URL
 			let configUrl = `${http_protocol}//${host}`;
-			console.log("url", `${http_protocol}//${host}`)
 			config = await this.resolve_config(configUrl);
 
 			if (!config) {
