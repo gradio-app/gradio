474aa30979c14793e00852ad2f4ef6c12a41b900
Abubakar Abid <abubakar@huggingface.co>
2025-06-27 00:24:26 -0700

    Attach cookies to client after first request (to support multiple-replica setups with cookie-based affinity) (#11041)
    * changes
    
    * add changeset
    
    * clear
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- client/python/gradio_client/client.py
+++ client/python/gradio_client/client.py
@@ -909,6 +909,16 @@ class Client:
             **self.httpx_kwargs,
         )
         if r.is_success:
+            # Cookies are sometimes needed to correctly route requests if the Gradio app is
+            # running on multiple replicas e.g. using cookie session-affinity in Kubernetes.
+            # This approach attaches cookies from the first response to subsequent requests
+            # without overriding existing cookies.
+            new_cookies = {
+                name: value
+                for name, value in r.cookies.items()
+                if value is not None and name not in self.cookies
+            }
+            self.cookies.update(new_cookies)
             return r.json()
         elif r.status_code == 401:
             raise AuthenticationError(
