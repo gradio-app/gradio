b434e648102ba3f1e2db4e5b7c20b32551253853
Abubakar Abid <abubakar@huggingface.co>
2025-06-27 12:51:57 -0700

    format

--- js/textbox/shared/types.ts
+++ js/textbox/shared/types.ts
@@ -1,10 +1,25 @@
 export interface InputHTMLAttributes {
-	autocapitalize?: "off" | "none" | "on" | "sentences" | "words" | "characters" | null;
+	autocapitalize?:
+		| "off"
+		| "none"
+		| "on"
+		| "sentences"
+		| "words"
+		| "characters"
+		| null;
 	autocorrect?: "on" | "off" | null;
 	spellcheck?: boolean | null;
 	autocomplete?: string | null;
 	tabindex?: number | null;
-	enterkeyhint?: "enter" | "done" | "go" | "next" | "previous" | "search" | "send" | null;
+	enterkeyhint?:
+		| "enter"
+		| "done"
+		| "go"
+		| "next"
+		| "previous"
+		| "search"
+		| "send"
+		| null;
 	lang?: string | null;
 	aria_label?: string | null;
 	aria_describedby?: string | null;
