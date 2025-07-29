b13a49b3f50c52a7b6735ef7fac008801d96b59b
Abubakar Abid <abubakar@huggingface.co>
2025-07-29 15:57:57 -0700

    changes

--- test/components/test_button.py
+++ test/components/test_button.py
@@ -3,5 +3,5 @@ import gradio as gr
 
 class TestButton:
     def test_postprocess(self):
-        assert gr.Button().postprocess("6") == "6"
-        assert gr.Button().postprocess(6) == "6"  # type: ignore
+        assert gr.Button().postprocess("5") == "5"
+        assert gr.Button().postprocess(5) == "5"  # type: ignore
