d7b23f5c2fd0351040ab02ad28737b9d363ff244
Abubakar Abid <abubakar@huggingface.co>
2025-07-29 16:38:46 -0700

    test

--- test/test_tunneling.py
+++ test/test_tunneling.py
@@ -1,26 +1,15 @@
+import sys
+
 import pytest
 
 from gradio import Interface, networking
 
+pytestmark = pytest.mark.skipif(sys.platform == "win32", reason="Skipped on Windows as Windows CI doesn't support tunneling")
 
 @pytest.mark.flaky
 def test_setup_tunnel():
     io = Interface(lambda x: x, "number", "number")
     io.launch(show_error=True, prevent_thread_lock=True)
-    share_url = networking.setup_tunnel(
-        io.server_name, io.server_port, io.share_token, io.share_server_address, None
-    )
-    assert isinstance(share_url, str)
-
-
-@pytest.mark.flaky
-def test_setup_custom_tunnel():
-    io = Interface(lambda x: x, "number", "number")
-    io.launch(
-        show_error=True,
-        prevent_thread_lock=True,
-        share_server_address="my-gpt-wrapper.com:7000",
-    )
     share_url = networking.setup_tunnel(
         io.server_name, io.server_port, io.share_token, io.share_server_address, None
     )
