d63a6eb561515654ac6b26d96af2defedce0a425
Dawood <dawoodkhan82@gmail.com>
2025-08-04 15:18:54 -0400

    demos

--- demo/dialogue_component/run.py
+++ demo/dialogue_component/run.py
@@ -2,5 +2,5 @@ import gradio as gr
 
 with gr.Blocks() as demo:
 
-    gr.Dialogue(speakers=["Speaker 1", "Speaker 2"], formatter=None, tags=["laughs", "sighs", "clears throat"])
+    gr.Dialogue(speakers=["Speaker 1", "Speaker 2"], formatter=None, tags=["(laughs)", "(sighs)", "(clears throat)"])
 demo.launch()
