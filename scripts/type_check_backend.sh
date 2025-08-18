ed32402de81c215fc20d6e6a9b3c1d87eecbeeb8
Abubakar Abid <abubakar@huggingface.co>
2025-08-18 11:02:59 -0400

    scripts

--- scripts/type_check_backend.sh
+++ scripts/type_check_backend.sh
@@ -6,12 +6,4 @@ pip_required
 echo "Adding py.typed file to gradio."
 touch gradio/py.typed
 
-ty check \
-  --exclude "gradio/themes/" \
-  --exclude "gradio/_frontend_code/" \
-  --exclude "gradio/components/*_plot.py" \
-  --exclude "gradio/node/*.py" \
-  --exclude "gradio/_frontend_code/*.py" \
-  --exclude "gradio/cli/commands/cli_env_info.py" \
-  --exclude "**/*.ipynb" \
-  gradio demo test
+ty check
