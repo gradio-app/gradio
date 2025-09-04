ca09e2492a2d021b0f64aa1e9745ab8b2bc3ea11
Abubakar Abid <abubakar@huggingface.co>
2025-09-04 11:31:26 -0700

    format

--- gradio/themes/base.py
+++ gradio/themes/base.py
@@ -1788,13 +1788,17 @@ class Base(ThemeClass):
         self.button_cancel_shadow_hover_dark = (
             button_cancel_shadow_hover_dark
             or getattr(
-                self, "button_cancel_shadow_hover_dark", "*button_secondary_shadow_hover"
+                self,
+                "button_cancel_shadow_hover_dark",
+                "*button_secondary_shadow_hover",
             )
         )
         self.button_cancel_shadow_active_dark = (
             button_cancel_shadow_active_dark
             or getattr(
-                self, "button_cancel_shadow_active_dark", "*button_secondary_shadow_active"
+                self,
+                "button_cancel_shadow_active_dark",
+                "*button_secondary_shadow_active",
             )
         )
 
