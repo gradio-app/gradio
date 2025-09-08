ce11ffe86716b8343d5d925b4357a55ce5a5ae1e
Abubakar Abid <abubakar@huggingface.co>
2025-09-08 10:32:38 -0700

    story typing

--- js/navbar/Navbar.stories.svelte
+++ js/navbar/Navbar.stories.svelte
@@ -1,11 +1,19 @@
 <script>
 	import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
-	import Navbar from "./Index.svelte";
+	import { Embed } from "@gradio/core";
+	
+	/** @type {HTMLDivElement} */
+	let wrapper;
+	const pages = /** @type {[string, string][]} */ ([
+		["", "Home"],
+		["page1", "Page 1"],
+		["page2", "Page 2"],
+		["settings", "Settings"]
+	]);
 </script>
 
 <Meta
 	title="Components/Navbar"
-	component={Navbar}
 	argTypes={{
 		visible: {
 			options: [true, false],
@@ -18,25 +26,48 @@
 			description: "The title to display for the home page in the navbar",
 			name: "home_page_title",
 			value: "Home"
-		},
-		elem_id: {
-			control: "text",
-			description:
-				"An optional string that is assigned as the id of this component",
-			name: "elem_id"
-		},
-		elem_classes: {
-			control: "object",
-			description:
-				"An optional list of strings that are assigned as the classes of this component",
-			name: "elem_classes",
-			value: []
 		}
 	}}
 />
 
 <Template let:args>
-	<Navbar {...args} />
+	{#key `${args.visible}-${args.home_page_title}`}
+		<div style="width: 100%; min-height: 400px; border: 1px solid #ddd; border-radius: 8px; overflow: hidden;">
+			<Embed
+				bind:wrapper
+				version="4.0.0"
+				initial_height="400px"
+				fill_width={true}
+				is_embed={false}
+				is_lite={false}
+				space={null}
+				display={true}
+				info={false}
+				loaded={true}
+				pages={pages}
+				current_page=""
+				root=""
+				components={[{
+					id: 1,
+					type: "navbar",
+					props: {
+						visible: args.visible,
+						home_page_title: args.home_page_title
+					}
+				}]}
+			>
+				<div style="padding: 20px;">
+					<h2>Navbar Storybook Demo</h2>
+					<p>The navbar above shows:</p>
+					<ul>
+						<li><strong>Visibility:</strong> {args.visible ? "Visible" : "Hidden"}</li>
+						<li><strong>Home Page Title:</strong> {args.home_page_title}</li>
+					</ul>
+					<p>Use the controls panel to change the navbar properties.</p>
+				</div>
+			</Embed>
+		</div>
+	{/key}
 </Template>
 
 <Story
