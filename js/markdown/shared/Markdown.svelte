5ad07b2b66e570f3d7034218a3ea9d6362184e19
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-09-09 12:55:18 -0400

    Revert markdown css change

--- js/markdown/shared/Markdown.svelte
+++ js/markdown/shared/Markdown.svelte
@@ -54,7 +54,7 @@
 </script>
 
 <div
-	class="prose gr-markdown-container {elem_classes?.join(' ') || ''}"
+	class="prose {elem_classes?.join(' ') || ''}"
 	class:hide={!visible}
 	data-testid="markdown"
 	dir={rtl ? "rtl" : "ltr"}
@@ -104,8 +104,5 @@
 
 	.hide {
 		display: none;
-	}
-	.gr-markdown-container {
-		white-space: pre-wrap;
 	}
 </style>
