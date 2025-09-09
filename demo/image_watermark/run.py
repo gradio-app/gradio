894d811265dcbeab8a0a0d18b349439829fa97c8
meg <90473723+meg-huggingface@users.noreply.github.com>
2025-09-08 18:08:56 -0700

    Removing another image file.

--- demo/image_watermark/run.py
+++ demo/image_watermark/run.py
@@ -2,8 +2,7 @@ import gradio as gr
 import os
 
 base_a = os.path.join(os.path.dirname(__file__), "files/groot.jpeg")
-base_b = os.path.join(os.path.dirname(__file__), "files/lion.png")
-base_c = os.path.join(os.path.dirname(__file__), "files/bird.bmp")
+base_b = os.path.join(os.path.dirname(__file__), "files/bird.bmp")
 
 watermark_a = os.path.join(os.path.dirname(__file__), "files/hf-logo_transpng.png")
 watermark_b = os.path.join(os.path.dirname(__file__), "files/logo_nontrans.png")
@@ -13,7 +12,7 @@ def generate_image(original_image, watermark):
     return gr.Image(original_image, watermark=watermark)
 
 demo = gr.Interface(generate_image, [gr.Image(image_mode=None), gr.Image(image_mode=None)], gr.Image(),
-                    examples=[[base_a, watermark_a], [base_b, watermark_b], [base_c, watermark_c], [base_a, watermark_c]])
+                    examples=[[base_a, watermark_a], [base_b, watermark_b], [base_a, watermark_c], [base_a, watermark_c]])
 
 if __name__ == "__main__":
     demo.launch()
