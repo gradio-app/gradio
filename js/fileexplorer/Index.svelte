82afcef5d04d4502d1bc6f7b97abaebca2455714
Abubakar Abid <abubakar@huggingface.co>
2025-09-10 13:22:47 -0700

    clean

--- js/fileexplorer/Index.svelte
+++ js/fileexplorer/Index.svelte
@@ -16,8 +16,8 @@
 	export let elem_id = "";
 	export let elem_classes: string[] = [];
 	export let visible = true;
-	export let value: string[][] = [];
-	let old_value: string[][] = [];
+	export let value: string[][];
+	let old_value: string[][];
 	export let label: string;
 	export let show_label: boolean;
 	export let height: number | string | undefined;
