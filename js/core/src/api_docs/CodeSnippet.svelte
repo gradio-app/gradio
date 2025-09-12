7010f018032ad44efa46531cfccedb342e1fc71a
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-09-12 12:13:04 -0400

    Fix

--- js/core/src/api_docs/CodeSnippet.svelte
+++ js/core/src/api_docs/CodeSnippet.svelte
@@ -56,23 +56,23 @@
 				<div bind:this={python_code}>
 					<pre><span class="highlight">from</span> gradio_client <span
 							class="highlight">import</span
-					> Client{#if has_file_path}, handle_file{/if}
+						> Client{#if has_file_path}, handle_file{/if}
 
 client = Client(<span class="token string">"{space_id || root}"</span
-					>{#if username !== null}, auth=("{username}", **password**){/if})
+						>{#if username !== null}, auth=("{username}", **password**){/if})
 result = client.<span class="highlight">predict</span
-					>(<!--
+						>(<!--
 -->{#each endpoint_parameters as { python_type, example_input, parameter_name, parameter_has_default, parameter_default }, i}<!--
 		-->
 	{parameter_name
-						? parameter_name + "="
-						: ""}<span
-						>{represent_value(
-							parameter_has_default ? parameter_default : example_input,
-							python_type.type,
-							"py"
-						)}</span
-					>,{/each}<!--
+								? parameter_name + "="
+								: ""}<span
+								>{represent_value(
+									parameter_has_default ? parameter_default : example_input,
+									python_type.type,
+									"py"
+								)}</span
+							>,{/each}<!--
 
 	-->
 	api_name=<span class="api-name">"/{dependency.api_name}"</span><!--
