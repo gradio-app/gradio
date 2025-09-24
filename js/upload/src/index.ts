377ef958c32a72f644d527cf1ee87c1292a1c837
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-09-24 15:15:42 +0200

    Show UploadProgress for Webcam Uploads

--- js/upload/src/index.ts
+++ js/upload/src/index.ts
@@ -1,3 +1,4 @@
 export { default as Upload } from "./Upload.svelte";
 export { default as ModifyUpload } from "./ModifyUpload.svelte";
+export { default as UploadProgress } from "./UploadProgress.svelte";
 export { create_drag } from "./utils";
