82f69dfd76fb78cc3c2c994c1c5263e3cc14cb9c
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-09-30 14:11:28 -0400

    e2e test

---
+++ js/spa/test/tab_render_children.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "@self/tootils";
+
+test("If render_children=True in Tab, the children with visible!=False are rendered", async ({
+	page
+}) => {
+	await expect(page.locator("#invisible-but-rendered")).toBeAttached();
+	await expect(page.locator("#invisible-and-not-rendered")).not.toBeAttached();
+});
