2069fd2a43c294937fbbafddeca13ff3688fd521
Hannah <hannahblair@users.noreply.github.com>
2025-10-02 02:24:30 +0200

    Remove duplicate media files  (#11989)
    * delete home files
    
    * delete unused media files and add media assets dir for demos
    
    * update demos
    
    * add changeset
    
    * refactor media.py
    
    * tweaks
    
    * nb
    
    * lint
    
    * lint
    
    * Update dependency versions and remove duplicates
    
    Updated versioning for dependencies and removed duplicate media files.
    
    * update tests
    
    * nb
    
    * fix test
    
    * fix test
    
    * fix tests
    
    * tweak
    
    * fix test
    
    * update demos
    
    * more deletions
    
    * format
    
    * nb
    
    * lint
    
    * fix tests
    
    * tweak demos
    
    * nb
    
    * clean up
    
    * lint
    
    * fix test
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- demo/model3D/run.py
+++ demo/model3D/run.py
@@ -1,5 +1,6 @@
 import gradio as gr
-import os
+# get_model3d() returns the file path to sample 3D models included with Gradio
+from gradio.media import get_model3d
 
 
 def load_mesh(mesh_file_name):
@@ -13,11 +14,11 @@ demo = gr.Interface(
         clear_color=(0.0, 0.0, 0.0, 0.0), label="3D Model", display_mode="wireframe"
     ),
     examples=[
-        [os.path.join(os.path.dirname(__file__), "files/Bunny.obj")],
-        [os.path.join(os.path.dirname(__file__), "files/Duck.glb")],
-        [os.path.join(os.path.dirname(__file__), "files/Fox.gltf")],
-        [os.path.join(os.path.dirname(__file__), "files/face.obj")],
-        [os.path.join(os.path.dirname(__file__), "files/sofia.stl")],
+        [get_model3d("Bunny.obj")],
+        [get_model3d("Duck.glb")],
+        [get_model3d("Fox.gltf")],
+        [get_model3d("face.obj")],
+        [get_model3d("sofia.stl")],
         [
             "https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/bonsai/bonsai-7k-mini.splat"
         ],
