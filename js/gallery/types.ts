454aced4fb7c11577ecd928171f976954fc06c6e
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-10-28 15:26:30 -0500

    Migrate Gallery

--- js/gallery/types.ts
+++ js/gallery/types.ts
@@ -1,4 +1,4 @@
-import type { FileData } from "@gradio/client";
+import type { FileData, SelectData } from "@gradio/client";
 
 export interface GalleryImage {
 	image: FileData;
@@ -8,4 +8,32 @@ export interface GalleryImage {
 export interface GalleryVideo {
 	video: FileData;
 	caption: string | null;
+}
+
+export type GalleryData = GalleryImage | GalleryVideo;
+
+export interface GalleryProps {
+	value: GalleryData[] | null;
+	file_types: string[] | null;
+	columns: number | number[] | undefined;
+	rows: number | number[] | undefined;
+	height: number | "auto";
+	preview: boolean;
+	allow_preview: boolean;
+	selected_index: number | null;
+	object_fit: "contain" | "cover" | "fill" | "none" | "scale-down";
+	buttons: string[];
+	type: "numpy" | "pil" | "filepath";
+}
+
+export interface GalleryEvents {
+	change: GalleryData[] | null;
+	upload: GalleryData[] | null;
+	select: SelectData;
+	delete: { file: FileData; index: number };
+	preview_open: never;
+	preview_close: never;
+	clear_status: any;
+	share: any;
+	error: any;
 }
