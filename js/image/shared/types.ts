a8e0dde8b6a132c8d97669d3fb41961dca25669f
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-10-28 14:46:37 -0400

    migrate image (#12133)

--- js/image/shared/types.ts
+++ js/image/shared/types.ts
@@ -1,3 +1,6 @@
+import type { LoadingStatus } from "@gradio/statustracker";
+import type { FileData } from "@gradio/client";
+
 export interface Base64File {
 	url: string;
 	alt_text: string;
@@ -6,4 +9,32 @@ export interface Base64File {
 export interface WebcamOptions {
 	mirror: boolean;
 	constraints: MediaStreamConstraints;
+}
+
+export interface ImageProps {
+	_selectable: boolean;
+	sources: ("clipboard" | "webcam" | "upload")[];
+	height: number;
+	width: number;
+	webcam_options: WebcamOptions;
+	value: FileData | null;
+	buttons: string[];
+	pending: boolean;
+	streaming: boolean;
+	stream_every: number;
+	input_ready: boolean;
+	placeholder: string;
+	watermark: FileData | null;
+}
+
+export interface ImageEvents {
+	clear: void;
+    change: any;
+    stream: any;
+    select: any;
+    upload: any;
+    input: any;
+	clear_status: LoadingStatus;
+	share: any;
+	error: any;
 }
