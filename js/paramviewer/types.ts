becdd780d9e2a8f2062b36e6c9d67f5908a82b61
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-10-29 11:28:12 -0400

    ParamViewer (#12147)

---
+++ js/paramviewer/types.ts
@@ -0,0 +1,20 @@
+export interface ParamViewerProps {
+	value: Record<
+		string,
+		{
+			type: string;
+			description: string;
+			default: string;
+		}
+	>;
+	linkify: string[];
+	header: string | null;
+	anchor_links: boolean | string;
+	max_height: number | string | undefined;
+}
+
+export interface ParamViewerEvents {
+	change: never;
+	upload: never;
+	clear_status: never;
+}
