f3a1be36c23f709344a74880a38b08fc1463ed9b
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-10-28 21:30:28 -0400

    Migrate HighlightedText (#12139)

---
+++ js/highlightedtext/types.ts
@@ -0,0 +1,22 @@
+import type { SelectData } from "@gradio/utils";
+import type { LoadingStatus } from "js/statustracker";
+
+export interface HighlightedToken {
+	token: string;
+	class_or_confidence: string | number | null;
+}
+
+export interface HighlightedTextProps {
+	value: HighlightedToken[];
+	show_legend: boolean;
+	show_inline_category: boolean;
+	color_map: Record<string, string>;
+	combine_adjacent: boolean;
+	rtl: boolean;
+}
+
+export interface HighlightedTextEvents {
+	change: never;
+	select: SelectData;
+	clear_status: LoadingStatus
+}
