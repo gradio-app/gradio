aa77189e75a8a5e4464bfeea8344bce6fb889379
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-10-29 10:32:15 -0400

    Migrate DataFrame

--- demo/matrix_transpose/run.py
+++ demo/matrix_transpose/run.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pandas as pd
 import gradio as gr
 
 def transpose(matrix):
@@ -7,15 +7,16 @@ def transpose(matrix):
 
 demo = gr.Interface(
     transpose,
-    gr.Dataframe(type="numpy", datatype="number", row_count=5, col_count=3, show_fullscreen_button=True),
+    gr.Dataframe(type="numpy", datatype="number", row_count=5, col_count=3, show_fullscreen_button=True, value=pd.DataFrame(np.zeros((5, 5), dtype=int), columns=pd.Index([str(i) for i in range(5)]))
+),
     "numpy",
-    examples=[
-        [np.zeros((30, 30)).tolist()],
-        [np.ones((2, 2)).tolist()],
-        [np.random.randint(0, 10, (3, 10)).tolist()],
-        [np.random.randint(0, 10, (10, 3)).tolist()],
-        [np.random.randint(0, 10, (10, 10)).tolist()],
-    ],
+    # examples=[
+    #     [np.zeros((30, 30)).tolist()],
+    #     [np.ones((2, 2)).tolist()],
+    #     [np.random.randint(0, 10, (3, 10)).tolist()],
+    #     [np.random.randint(0, 10, (10, 3)).tolist()],
+    #     [np.random.randint(0, 10, (10, 10)).tolist()],
+    # ],
     cache_examples=False
 )
 
