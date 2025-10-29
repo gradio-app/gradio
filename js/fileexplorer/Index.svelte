339503e6dd2cdb8756aac7b0a8467a406d9f6b89
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-10-29 11:19:52 -0400

    Migrate FileExplorer

--- js/fileexplorer/Index.svelte
+++ js/fileexplorer/Index.svelte
@@ -1,87 +1,66 @@
 <svelte:options accessors={true} />
 
 <script lang="ts">
-	import type { Gradio, SelectData } from "@gradio/utils";
+	import type { FileExplorerProps, FileExplorerEvents } from "./types";
+	import { Gradio } from "@gradio/utils";
 	import { File } from "@gradio/icons";
-	import type { FileNode } from "./shared/types";
 
 	import { Block, BlockLabel } from "@gradio/atoms";
 	import DirectoryExplorer from "./shared/DirectoryExplorer.svelte";
 
 	import { StatusTracker } from "@gradio/statustracker";
-	import type { LoadingStatus } from "@gradio/statustracker";
 
 	import { _ } from "svelte-i18n";
 
-	export let elem_id = "";
-	export let elem_classes: string[] = [];
-	export let visible: boolean | "hidden" = true;
-	export let value: string[][];
-	let old_value: string[][];
-	export let label: string;
-	export let show_label: boolean;
-	export let height: number | string | undefined;
-	export let min_height: number | string | undefined;
-	export let max_height: number | string | undefined;
-	export let file_count: "single" | "multiple" = "multiple";
-	export let root_dir: string;
-	export let glob: string;
-	export let ignore_glob: string;
-	export let loading_status: LoadingStatus;
-	export let container = true;
-	export let scale: number | null = null;
-	export let min_width: number | undefined = undefined;
-	export let gradio: Gradio<{
-		change: never;
-		clear_status: LoadingStatus;
-	}>;
-	export let server: {
-		ls: (path: string[]) => Promise<FileNode[]>;
-	};
-	export let interactive: boolean;
+	const props = $props();
+	const gradio = new Gradio<FileExplorerEvents, FileExplorerProps>(props);
 
-	$: rerender_key = [root_dir, glob, ignore_glob];
+	let old_value = $state(gradio.props.value);
 
-	$: if (JSON.stringify(value) !== JSON.stringify(old_value)) {
-		old_value = value;
-		gradio.dispatch("change");
-	}
+	let rerender_key = $derived([gradio.props.root_dir, gradio.props.glob, gradio.props.ignore_glob]);
+
+	$effect(() => {
+		if (old_value != gradio.props.value) {
+			old_value = gradio.props.value;
+			gradio.dispatch("change");
+		}
+	});
 </script>
 
 <Block
-	{visible}
-	variant={value === null ? "dashed" : "solid"}
+	visible={gradio.shared.visible}
+	variant={gradio.props.value === null ? "dashed" : "solid"}
 	border_mode={"base"}
 	padding={false}
-	{elem_id}
-	{elem_classes}
-	{container}
-	{scale}
-	{min_width}
+	elem_id={gradio.shared.elem_id}
+	elem_classes={gradio.shared.elem_classes}
+	container={gradio.shared.container}
+	scale={gradio.shared.scale}
+	min_width={gradio.shared.min_width}
 	allow_overflow={true}
 	overflow_behavior="auto"
-	{height}
-	{max_height}
-	{min_height}
+	height={gradio.props.height}
+	max_height={gradio.props.max_height}
+	min_height={gradio.props.min_height}
 >
 	<StatusTracker
-		{...loading_status}
-		autoscroll={gradio.autoscroll}
+		{...gradio.shared.loading_status}
+		autoscroll={gradio.shared.autoscroll}
 		i18n={gradio.i18n}
-		on:clear_status={() => gradio.dispatch("clear_status", loading_status)}
+		on:clear_status={() => gradio.dispatch("clear_status", gradio.shared.loading_status)}
 	/>
 	<BlockLabel
-		{show_label}
+		show_label={gradio.shared.show_label}
 		Icon={File}
-		label={label || "FileExplorer"}
+		label={gradio.shared.label || "FileExplorer"}
 		float={false}
 	/>
 	{#key rerender_key}
 		<DirectoryExplorer
-			bind:value
-			{file_count}
-			{interactive}
-			ls_fn={server.ls}
+			bind:value={gradio.props.value}
+			file_count={gradio.props.file_count}
+			interactive={gradio.shared.interactive}
+			ls_fn={gradio.shared.server.ls}
 		/>
 	{/key}
 </Block>
