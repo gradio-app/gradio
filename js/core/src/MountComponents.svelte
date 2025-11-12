6aac6276b837cc158da83b36e34ea68a025eac8c
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-11-11 19:27:15 -0500

    Async rendering fix

--- js/core/src/MountComponents.svelte
+++ js/core/src/MountComponents.svelte
@@ -1,26 +1,22 @@
-<script module>
-	const then = performance.now();
-</script>
-
 <script lang="ts">
 	import Self from "./MountComponents.svelte";
 	let { node } = $props();
-
-	let component = $state(await node.component);
 </script>
 
-{#if node && component}
+{#if node}
 	{#if node.props.shared_props.visible}
-		<svelte:component
-			this={component.default}
-			shared_props={node.props.shared_props}
-			props={node.props.props}
-		>
-			{#if node.children && node.children.length}
-				{#each node.children as _node}
-					<Self node={_node} />
-				{/each}
-			{/if}
-		</svelte:component>
+		{#await node.component then component}
+			<svelte:component
+				this={component.default}
+				shared_props={node.props.shared_props}
+				props={node.props.props}
+			>
+				{#if node.children && node.children.length}
+					{#each node.children as _node}
+						<Self node={_node} />
+					{/each}
+				{/if}
+			</svelte:component>
+		{/await}
 	{/if}
 {/if}
