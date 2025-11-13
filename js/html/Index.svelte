c36c8468bfc221c6febf26b3b73b149695282c6f
aliabid94 <aabid94@gmail.com>
2025-11-13 11:02:32 -0800

    Migrate gr.HTML to svelte 5 (#12315)
    * changes
    
    * add changeset
    
    ---------
    
    Co-authored-by: Ali Abid <aliabid94@gmail.com>
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- js/html/Index.svelte
+++ js/html/Index.svelte
@@ -1,95 +1,91 @@
+<script context="module" lang="ts">
+	export { default as BaseHTML } from "./shared/HTML.svelte";
+	export { default as BaseExample } from "./Example.svelte";
+</script>
+
 <script lang="ts">
-	import type { Gradio } from "@gradio/utils";
+	import { Gradio } from "@gradio/utils";
 	import HTML from "./shared/HTML.svelte";
 	import { StatusTracker } from "@gradio/statustracker";
-	import type { LoadingStatus } from "@gradio/statustracker";
 	import { Block, BlockLabel } from "@gradio/atoms";
 	import { Code as CodeIcon } from "@gradio/icons";
 	import { css_units } from "@gradio/utils";
+	import type { HTMLProps, HTMLEvents } from "./types.ts";
 
-	export let label = "HTML";
-	export let elem_id = "";
-	export let elem_classes: string[] = [];
-	export let visible: boolean | "hidden" = true;
-	export let value = "";
-	export let props: Record<string, any> = {};
-	export let apply_default_css = true;
+	let props = $props();
+	const gradio = new Gradio<HTMLEvents, HTMLProps>(props);
 
-	export let html_template = "${value}";
-	export let css_template = "";
-	export let js_on_load: string | null = null;
+	let _props = $derived({
+		value: gradio.props.value || "",
+		label: gradio.shared.label,
+		visible: gradio.shared.visible,
+		...gradio.props.props
+	});
 
-	let _props: Record<string, any> = { value, label, visible, ...props };
-	$: _props = { ..._props, ...props, value, label, visible };
-
-	export let loading_status: LoadingStatus;
-	export let gradio: Gradio<{
-		change: never;
-		click: never;
-		submit: never;
-		clear_status: LoadingStatus;
-	}>;
-	export let show_label = false;
-	export let min_height: number | undefined = undefined;
-	export let max_height: number | undefined = undefined;
-	export let container = false;
-	export let padding = true;
-	export let autoscroll = false;
-
-	let old_value = value;
-	$: if (JSON.stringify(old_value) !== JSON.stringify(value)) {
-		old_value = value;
-		gradio.dispatch("change");
-	}
+	let old_value = $state(gradio.props.value);
+	$effect(() => {
+		if (JSON.stringify(old_value) !== JSON.stringify(gradio.props.value)) {
+			old_value = gradio.props.value;
+			gradio.dispatch("change");
+		}
+	});
 </script>
 
 <Block
-	{visible}
-	{elem_id}
-	{elem_classes}
-	{container}
+	visible={gradio.shared.visible}
+	elem_id={gradio.shared.elem_id}
+	elem_classes={gradio.shared.elem_classes}
+	container={gradio.shared.container}
 	padding={true}
 	overflow_behavior="visible"
 >
-	{#if show_label}
-		<BlockLabel Icon={CodeIcon} {show_label} {label} float={false} />
+	{#if gradio.props.show_label}
+		<BlockLabel
+			Icon={CodeIcon}
+			show_label={gradio.props.show_label}
+			label={gradio.shared.label}
+			float={false}
+		/>
 	{/if}
 
 	<StatusTracker
-		autoscroll={gradio.autoscroll}
+		autoscroll={gradio.shared.autoscroll}
 		i18n={gradio.i18n}
-		{...loading_status}
+		{...gradio.shared.loading_status}
 		variant="center"
 		on:clear_status={() => gradio.dispatch("clear_status", loading_status)}
 	/>
 	<div
 		class="html-container"
-		class:padding
-		class:pending={loading_status?.status === "pending"}
-		style:min-height={min_height && loading_status?.status !== "pending"
-			? css_units(min_height)
+		class:padding={gradio.shared.padding}
+		class:pending={gradio.shared.loading_status?.status === "pending"}
+		style:min-height={gradio.props.min_height &&
+		gradio.shared.loading_status?.status !== "pending"
+			? css_units(gradio.props.min_height)
 			: undefined}
-		style:max-height={max_height ? css_units(max_height) : undefined}
+		style:max-height={gradio.props.max_height
+			? css_units(gradio.props.max_height)
+			: undefined}
 	>
 		<HTML
 			props={_props}
-			{html_template}
-			{css_template}
-			{js_on_load}
-			{elem_classes}
-			{visible}
-			{autoscroll}
-			{apply_default_css}
+			html_template={gradio.props.html_template}
+			css_template={gradio.props.css_template}
+			js_on_load={gradio.props.js_on_load}
+			elem_classes={gradio.shared.elem_classes}
+			visible={gradio.shared.visible}
+			autoscroll={gradio.shared.autoscroll}
+			apply_default_css={gradio.props.apply_default_css}
 			on:event={(e) => {
 				gradio.dispatch(e.detail.type, e.detail.data);
 			}}
 			on:update_value={(e) => {
 				if (e.detail.property === "value") {
-					value = e.detail.data;
+					gradio.props.value = e.detail.data;
 				} else if (e.detail.property === "label") {
-					label = e.detail.data;
+					gradio.shared.label = e.detail.data;
 				} else if (e.detail.property === "visible") {
-					visible = e.detail.data;
+					gradio.shared.visible = e.detail.data;
 				}
 			}}
 		/>
