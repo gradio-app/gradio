bf3e720fdc23eb8ee5b6b5f228111baa45d93bb0
Abubakar Abid <abubakar@huggingface.co>
2025-11-13 13:58:54 -0800

    changes

--- gradio/components/dataframe.py
+++ gradio/components/dataframe.py
@@ -75,6 +75,7 @@ class Dataframe(Component):
         headers: list[str] | None = None,
         row_count: int | None = None,
         row_limits: tuple[int | None, int | None] | None = None,
+        col_count: None = None,
         column_count: int | None = None,
         column_limits: tuple[int | None, int | None] | None = None,
         datatype: Literal[
@@ -115,6 +116,7 @@ class Dataframe(Component):
             headers: List of str header names. These are used to set the column headers of the dataframe if the value does not have headers. If None, no headers are shown.
             row_count: The number of rows to initially display in the dataframe. If None, the number of rows is determined automatically based on the `value`.
             row_limits: A tuple of two integers specifying the minimum and maximum number of rows that can be created in the dataframe via the UI. If the first element is None, there is no minimum number of rows. If the second element is None, there is no maximum number of rows. Only applies if `interactive` is True.
+            col_count: This parameter is deprecated. Please use `column_count` instead.
             column_count: The number of columns to initially display in the dataframe. If None, the number of columns is determined automatically based on the `value`.
             column_limits: A tuple of two integers specifying the minimum and maximum number of columns that can be created in the dataframe via the UI. If the first element is None, there is no minimum number of columns. If the second element is None, there is no maximum number of columns. Only applies if `interactive` is True.
             datatype: Datatype of values in sheet. Can be provided per column as a list of strings, or for the entire sheet as a single string. Valid datatypes are "str", "number", "bool", "date", and "markdown". Boolean columns will display as checkboxes. If the datatype "auto" is used, the column datatypes are automatically selected based on the value input if possible.
@@ -156,9 +158,15 @@ class Dataframe(Component):
                 UserWarning,
             )
         self.row_limits = row_limits
-        self.column_count = self.__process_counts(
+        self.column_count = self.col_count = self.__process_counts(
             column_count, len(headers) if headers else 3
         )
+        if col_count is not None:
+            warnings.warn(
+                "The `col_count` parameter is deprecated and will be removed. Please use `column_count` instead.",
+                UserWarning,
+            )
+            self.column_count = self.col_count = self.__process_counts(col_count, len(headers) if headers else 3)
         if column_limits is not None:
             warnings.warn(
                 "The `column_limits` parameter is not yet implemented.",
@@ -235,11 +243,6 @@ class Dataframe(Component):
             else "a polars dataframe"
         )
 
-    # TODO: Remove this method once we migrate the dataframe js component to Svelte 5.
-    def get_config(self, cls: None = None) -> dict[str, Any]:  # noqa: ARG002
-        config = super().get_config(self.__class__)
-        config["col_count"] = self.column_count
-        return config
 
     def preprocess(
         self, payload: DataframeData
