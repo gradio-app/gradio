fdefd4ac707baf4351661a731ac3071fea48de28
pngwn <hello@pngwn.io>
2025-11-14 12:39:06 +0700

    fix audio_subtitle demo

--- demo/audio_subtitle/run.py
+++ demo/audio_subtitle/run.py
@@ -1,16 +1,24 @@
 import gradio as gr
-import os
+from gradio.media import get_audio, get_file
+from pathlib import Path
 
-a = os.path.join(os.path.dirname(__file__), "files/a.mp3")
-b = os.path.join(os.path.dirname(__file__), "files/b.mp3")
-s1 = os.path.join(os.path.dirname(__file__), "files/s1.srt")
-s2 = os.path.join(os.path.dirname(__file__), "files/s2.vtt")
+a = get_audio("cate_blanch.mp3")
+b = get_audio("cate_blanch_2.mp3")
+s1 = get_file("s1.srt")
+s2 = get_file("s2.vtt")
+
+p = Path(__file__).parent.parent.parent
+demo_path = Path(p, "gradio/media_assets/").resolve()
+
+print(demo_path)
+
 
 def add_subtitles_to_audio(audio, subtitles=None):
     if subtitles is None:
         return audio
     if subtitles is not None:
         return gr.Audio(label="Out", value=audio, subtitles=subtitles.name)
+
 
 demo = gr.Interface(
     fn=add_subtitles_to_audio,
@@ -23,8 +31,8 @@ demo = gr.Interface(
         [a, s1],
         [b, s2],
     ],
-    api_name="predict"
+    api_name="predict",
 )
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(allowed_paths=[demo_path])
