15d3b6be2d30c85d04a25ffe7f86cf2a91c71aec
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-11-17 15:29:07 -0500

    Revert

--- .config/playwright.config.js
+++ .config/playwright.config.js
@@ -14,12 +14,12 @@ const base = defineConfig({
 			]
 		}
 	},
-	expect: { timeout: 10000 },
-	timeout: 10000,
+	expect: { timeout: 10_000 },
+	timeout: 30_000,
 	testMatch: /.*\.spec\.ts/,
 	testDir: "..",
 	workers: process.env.CI ? 6 : undefined,
-	retries: 0
+	retries: 3
 });
 
 // There are Firefox-specific issues such as https://github.com/gradio-app/gradio/pull/9528 so we want to run the tests on Firefox, but Firefox sometimes fails to start in the GitHub Actions environment so we disable it on CI.
