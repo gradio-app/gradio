8b0c5aab0894fc322ba9bebdce9cdbe6e0735251
aliabid94 <aabid94@gmail.com>
2025-11-20 22:20:37 -0800

    remove_placeholder_type_here (#12423)
    * remove_placeholder_type_here
    
    * add changeset
    
    ---------
    
    Co-authored-by: Ali Abid <aliabid94@gmail.com>
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>
    Co-authored-by: Abubakar Abid <abubakar@huggingface.co>

--- js/multimodaltextbox/shared/MultimodalTextbox.svelte
+++ js/multimodaltextbox/shared/MultimodalTextbox.svelte
@@ -40,7 +40,7 @@
 	export let value_is_output = false;
 	export let lines = 1;
 	export let i18n: I18nFormatter;
-	export let placeholder = "Type here...";
+	export let placeholder = "";
 	export let disabled = false;
 	export let label: string;
 	export let info: string | undefined = undefined;
