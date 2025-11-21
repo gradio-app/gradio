ad52ab81ca59bda5276372164e0787514b3abd02
Ali Abid <aliabid94@gmail.com>
2025-11-20 21:49:01 -0800

    remove_placeholder_type_here

--- js/textbox/shared/Textbox.svelte
+++ js/textbox/shared/Textbox.svelte
@@ -14,7 +14,7 @@
 	export let value = "";
 	export let value_is_output = false;
 	export let lines = 1;
-	export let placeholder = "Type here...";
+	export let placeholder = "";
 	export let label: string;
 	export let info: string | undefined = undefined;
 	export let disabled = false;
