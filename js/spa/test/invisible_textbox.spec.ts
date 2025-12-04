2bcda2014c59ea99cb167e2e03eb33a80ea146fa
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-12-04 17:28:52 -0500

    fix test syntax

--- js/spa/test/invisible_textbox.spec.ts
+++ js/spa/test/invisible_textbox.spec.ts
@@ -3,20 +3,20 @@ import { test, expect } from "@self/tootils";
 test("Textbox visibility can be toggled from the backend. Toggling visibility retains the value", async ({
 	page
 }) => {
-	expect(page.locator("#test-textbox textarea")).toHaveCount(0);
+	await expect(page.locator("#test-textbox textarea")).toHaveCount(0);
 
-	await page.getByLabel("Show").click();
+	await page.click('text="Show"');
 
-	expect(page.locator("#test-textbox textarea")).toHaveCount(1);
+	await expect(page.locator("#test-textbox")).toHaveCount(1);
 
 	const textbox = page.locator("#test-textbox textarea");
 	await textbox.fill("Test value");
 
-	await page.getByLabel("Hide").click();
+	await page.click('text="Hide"');
 
 	await expect(textbox).not.toBeVisible();
 	await expect(textbox).toHaveCount(0);
 
-	await page.getByLabel("Show").click();
+	await page.click('text="Show"');
 	await expect(textbox).toHaveValue("Test value");
 });
