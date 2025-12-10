55288372e6e4e3b72782eb778d0c3a41acb42555
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-12-10 17:55:01 -0500

    revert

--- demo/file_explorer_component_events/run.py
+++ demo/file_explorer_component_events/run.py
@@ -6,23 +6,19 @@ base_root = Path(__file__).parent.resolve()
 with gr.Blocks() as demo:
     with gr.Row():
         dd = gr.Dropdown(label="Select File Explorer Root",
-                        value=str(base_root),
-                        choices=[str(base_root),
-                                 str(base_root / "dir1"),
+                        value=str(base_root / "dir1"),
+                        choices=[str(base_root / "dir1"),
                                  str(base_root / "dir2"),
                                  str(base_root / "dir3")])
         with gr.Group():
             txt_only_glob = gr.Checkbox(label="Show only text files", value=False)
             ignore_txt_in_glob = gr.Checkbox(label="Ignore text files in glob", value=False)
 
-    fe = gr.FileExplorer(root_dir=str(base_root),
+    fe = gr.FileExplorer(root_dir=str(base_root / "dir1"),
                          glob="**/*", interactive=True)
     textbox = gr.Textbox(label="Selected Directory")
-    selected_file = gr.Textbox(label="Last Selected (via .select())")
     run = gr.Button("Run")
-    total_changes = gr.Number(0, elem_id="total-changes", label="# of Change Events")
-    total_inputs = gr.Number(0, elem_id="total-inputs", label="# of Input Events")
-    total_selects = gr.Number(0, elem_id="total-selects", label="# of Select Events")
+    total_changes = gr.Number(0, elem_id="total-changes")
 
     txt_only_glob.select(lambda s: gr.FileExplorer(glob="*.txt" if s else "*") ,
                          inputs=[txt_only_glob], outputs=[fe])
@@ -32,10 +28,6 @@ with gr.Blocks() as demo:
     dd.select(lambda s: gr.FileExplorer(root_dir=s), inputs=[dd], outputs=[fe])
     run.click(lambda s: ",".join(s) if isinstance(s, list) else s, inputs=[fe], outputs=[textbox])
     fe.change(lambda num: num + 1, inputs=total_changes, outputs=total_changes)
-    fe.input(lambda num: num + 1, inputs=total_inputs, outputs=total_inputs)
-    def on_select(evt: gr.SelectData, num: int):
-        return f"Index: {evt.index}, Value: {evt.value}", num + 1
-    fe.select(on_select, inputs=total_selects, outputs=[selected_file, total_selects])
 
     with gr.Row():
         a = gr.Textbox(elem_id="input-box")
