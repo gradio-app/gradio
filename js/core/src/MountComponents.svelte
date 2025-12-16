7b4dec0c0cd8db2cfb77f7bfeba8587a10dab2d6
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-12-16 14:09:26 -0500

    Fix

--- js/core/src/MountComponents.svelte
+++ js/core/src/MountComponents.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
 	import Self from "./MountComponents.svelte";
 	let { node } = $props();
-
-	let component = $derived(await node.component);
 </script>
 
-{#if node && component}
-	{#if node.props.shared_props.visible}
-		<svelte:component
-			this={component.default}
-			shared_props={node.props.shared_props}
-			props={node.props.props}
-		>
-			{#if node.children && node.children.length}
-				{#each node.children as _node}
-					<Self node={_node} />
-				{/each}
-			{/if}
-		</svelte:component>
-	{/if}
+{#if node}
+	{#await node.component then component}
+		{#if component && node.props.shared_props.visible}
+			<svelte:component
+				this={component.default}
+				shared_props={node.props.shared_props}
+				props={node.props.props}
+			>
+				{#if node.children && node.children.length}
+					{#each node.children as _node}
+						<Self node={_node} />
+					{/each}
+				{/if}
+			</svelte:component>
+		{/if}
+	{/await}
 {/if}
