7efafde6d87ffad6d7026656e9db8581f53b964b
Abubakar Abid <islamrealm@gmail.com>
2025-12-15 17:36:12 -0800

    changes

--- gradio/utils.py
+++ gradio/utils.py
@@ -1969,7 +1969,8 @@ async def safe_aclose_iterator(iterator, timeout=60.0, retry_interval=0.05):
 
 
 def set_default_buttons(
-    buttons: Sequence[str | Button] | None = None, default_buttons: list[str] | None = None
+    buttons: Sequence[str | Button] | None = None,
+    default_buttons: list[str] | None = None,
 ) -> list[str | Button]:
     from gradio.components.button import Button
 
