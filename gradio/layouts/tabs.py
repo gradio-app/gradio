a7877b4c3485b46b847fa07b6ee39521b82400bb
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2026-01-06 11:43:02 +0100

    Fix docs for Tab select event

--- gradio/layouts/tabs.py
+++ gradio/layouts/tabs.py
@@ -6,7 +6,7 @@ from gradio_client.documentation import document
 
 from gradio.blocks import BlockContext
 from gradio.component_meta import ComponentMeta
-from gradio.events import Events
+from gradio.events import EventListener, Events
 from gradio.i18n import I18nData
 
 
@@ -65,7 +65,13 @@ class Tab(BlockContext, metaclass=ComponentMeta):
     Guides: controlling-layout
     """
 
-    EVENTS = [Events.select]
+    EVENTS = [
+        EventListener(
+            "select",
+            callback=lambda block: setattr(block, "_selectable", True),
+            doc="Event listener for when the user selects the Tab. Uses event data gradio.SelectData to carry `value` referring to the label of the Tab, and `selected` to refer to state of the Tab. See EventData documentation on how to use this event data",
+        )
+    ]
 
     def __init__(
         self,
