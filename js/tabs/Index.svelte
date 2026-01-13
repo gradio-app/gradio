b48da517d2daa62d3b201f9a84bef9352d485b38
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2026-01-13 11:26:24 -0500

    Fix bug where Walkthrough Component Freezes (#12693)
    * try to fix
    
    * fix
    
    * Fix code
    
    * add changeset
    
    * Remove comments
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- js/tabs/Index.svelte
+++ js/tabs/Index.svelte
@@ -7,24 +7,25 @@
 	import Tabs from "./shared/Tabs.svelte";
 	import Walkthrough from "./shared/Walkthrough.svelte";
 	import type { TabsProps, TabsEvents } from "./types";
+	import { untrack } from "svelte";
 
 	let props = $props();
 	const gradio = new Gradio<TabsEvents, TabsProps>(props);
 
-	let old_selected = $state(gradio.props.selected);
-
 	$effect(() => {
-		if (old_selected !== gradio.props.selected) {
-			const i = gradio.props.initial_tabs.findIndex(
-				(t) => t.id === gradio.props.selected
-			);
-			gradio.dispatch("gradio_tab_select", {
-				value: gradio.props.initial_tabs[i].label,
-				index: i,
-				id: gradio.props.initial_tabs[i].id,
-				component_id: gradio.props.initial_tabs[i].component_id
+		if (gradio.props.selected) {
+			untrack(() => {
+				console.log("Here");
+				const i = gradio.props.initial_tabs.findIndex(
+					(t) => t.id === gradio.props.selected
+				);
+				gradio.dispatch("gradio_tab_select", {
+					value: gradio.props.initial_tabs[i].label,
+					index: i,
+					id: gradio.props.initial_tabs[i].id,
+					component_id: gradio.props.initial_tabs[i].component_id
+				});
 			});
-			old_selected = gradio.props.selected;
 		}
 	});
 </script>
@@ -34,7 +35,7 @@
 		visible={gradio.shared.visible}
 		elem_id={gradio.shared.elem_id}
 		elem_classes={gradio.shared.elem_classes}
-		bind:selected={gradio.props.selected}
+		selected={gradio.props.selected}
 		on:change={() => gradio.dispatch("change")}
 		on:select={(e) => {
 			gradio.dispatch("select", e.detail);
