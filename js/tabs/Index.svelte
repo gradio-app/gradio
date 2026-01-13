c38a87d5d2dbe8ab9e78af8f45759e60715f215e
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2026-01-13 17:07:21 +0100

    Remove comments

--- js/tabs/Index.svelte
+++ js/tabs/Index.svelte
@@ -12,21 +12,13 @@
 	let props = $props();
 	const gradio = new Gradio<TabsEvents, TabsProps>(props);
 
-	// let old_selected = $state(gradio.props.selected);
-
-	$inspect("gradio.props.selected", gradio.props.selected);
-
 	$effect(() => {
-		// console.log("Selected tab changed effect triggered", "selected:", gradio.props.selected, "old_selected:", old_selected);
-		// if (old_selected !== gradio.props.selected) {
 		if (gradio.props.selected) {
 			untrack(() => {
 				console.log("Here");
 				const i = gradio.props.initial_tabs.findIndex(
 					(t) => t.id === gradio.props.selected
 				);
-				// old_selected = gradio.props.selected;
-				// console.log("old_selected updated to", old_selected);
 				gradio.dispatch("gradio_tab_select", {
 					value: gradio.props.initial_tabs[i].label,
 					index: i,
@@ -35,8 +27,6 @@
 				});
 			});
 		}
-
-		// }
 	});
 </script>
 
