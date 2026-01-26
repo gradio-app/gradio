48ec693be5e8dd59ceaebd73042ba7ee14a678ff
unknown <rektpunk@gmail.com>
2026-01-26 11:08:33 +0900

    make it lint

--- demo/llm_langchain/run.py
+++ demo/llm_langchain/run.py
@@ -2,22 +2,24 @@
 # Before running this, make sure you have exported your OpenAI API key as an environment variable:
 # export OPENAI_API_KEY="your-openai-api-key"
 
-from langchain_openai import ChatOpenAI  # type: ignore
-from langchain.messages import AIMessage, HumanMessage  # type: ignore
 import gradio as gr
+from langchain.messages import AIMessage, HumanMessage  # type: ignore
+from langchain_openai import ChatOpenAI  # type: ignore
 
 model = ChatOpenAI(model="gpt-4o-mini")
+
 
 def predict(message, history):
     history_langchain_format = []
     for msg in history:
-        if msg['role'] == "user":
-            history_langchain_format.append(HumanMessage(content=msg['content']))
-        elif msg['role'] == "assistant":
-            history_langchain_format.append(AIMessage(content=msg['content']))
+        if msg["role"] == "user":
+            history_langchain_format.append(HumanMessage(content=msg["content"]))
+        elif msg["role"] == "assistant":
+            history_langchain_format.append(AIMessage(content=msg["content"]))
     history_langchain_format.append(HumanMessage(content=message))
     gpt_response = model.invoke(history_langchain_format)
     return gpt_response.content
+
 
 demo = gr.ChatInterface(
     predict,
