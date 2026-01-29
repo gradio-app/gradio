68d973db46a637572b44a094cf8ef4f57c9e6e21
unknown <rektpunk@gmail.com>
2026-01-29 09:55:02 +0900

    remove duplicated user input in transformers example

--- demo/llm_hf_transformers/run.py
+++ demo/llm_hf_transformers/run.py
@@ -1,20 +1,23 @@
 # type: ignore
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import gradio as gr
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 checkpoint = "HuggingFaceTB/SmolLM2-135M-Instruct"
 device = "cpu"  # "cuda" or "cpu"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 model = AutoModelForCausalLM.from_pretrained(checkpoint).to(device)
 
+
 def predict(message, history):
-    history.append({"role": "user", "content": message})
-    input_text = tokenizer.apply_chat_template(history, tokenize=False)
+    history_transformers_format = history.copy()
+    history_transformers_format.append({"role": "user", "content": message})
+    input_text = tokenizer.apply_chat_template(history_transformers_format, tokenize=False)
     inputs = tokenizer.encode(input_text, return_tensors="pt").to(device)  # type: ignore
     outputs = model.generate(inputs, max_new_tokens=100, temperature=0.2, top_p=0.9, do_sample=True)
     decoded = tokenizer.decode(outputs[0])
     response = decoded.split("<|im_start|>assistant\n")[-1].split("<|im_end|>")[0]
     return response
+
 
 demo = gr.ChatInterface(predict, api_name="chat")
 
