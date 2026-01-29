dae46afd8eedbb3a8f7d85986ded72ce3cee58cb
unknown <rektpunk@gmail.com>
2026-01-29 10:00:36 +0900

    remove history copy and make messgaes

--- demo/llm_hf_transformers/run.py
+++ demo/llm_hf_transformers/run.py
@@ -9,9 +9,8 @@ model = AutoModelForCausalLM.from_pretrained(checkpoint).to(device)
 
 
 def predict(message, history):
-    history_transformers_format = history.copy()
-    history_transformers_format.append({"role": "user", "content": message})
-    input_text = tokenizer.apply_chat_template(history_transformers_format, tokenize=False)
+    messages = history + [{"role": "user", "content": message}]
+    input_text = tokenizer.apply_chat_template(messages, tokenize=False)
     inputs = tokenizer.encode(input_text, return_tensors="pt").to(device)  # type: ignore
     outputs = model.generate(inputs, max_new_tokens=100, temperature=0.2, top_p=0.9, do_sample=True)
     decoded = tokenizer.decode(outputs[0])
