924afd37505b88eab1754c9acdc413381a69d34f
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2026-02-27 08:52:09 -0500

    Back out js/css/ load from hub

--- gradio/blocks.py
+++ gradio/blocks.py
@@ -1261,12 +1261,7 @@ class Blocks(BlockContext, BlocksEvents, metaclass=BlocksMeta):
 
         derived_fields = ["types"]
 
-        with Blocks(
-            theme=config.get("theme", None),
-            css=config.get("css", None),
-            js=config.get("js", None),
-            head=config.get("head", None),
-        ) as blocks:
+        with Blocks(theme=config.get("theme", None)) as blocks:
             # ID 0 should be the root Blocks component
             original_mapping[0] = root_block = Context.root_block or blocks
 
