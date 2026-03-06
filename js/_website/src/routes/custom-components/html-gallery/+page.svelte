932092cdeda9bce8499a40e60da086f70e0b1afa
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2026-03-06 15:34:54 -0600

    Add Navigation to the Guides

--- js/_website/src/routes/custom-components/html-gallery/+page.svelte
+++ js/_website/src/routes/custom-components/html-gallery/+page.svelte
@@ -14,7 +14,7 @@
 	import { theme } from "$lib/stores/theme";
 
 	const BASE_URL =
-		"https://huggingface.co/datasets/gradio/custom-html-gallery/resolve/main";
+		"https://huggingface.co/datasets/gradio/custom-html-gallery/resolve/refs%2Fpr%2F23";
 
 	let manifest: ManifestEntry[] = [];
 	let component_cache: Record<string, HTMLComponentEntry> = {};
