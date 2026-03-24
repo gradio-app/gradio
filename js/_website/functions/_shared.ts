98103f11d2e210fccb099dd0ae79e7a1395e55e0
aliabd <ali.si3luwa@gmail.com>
2026-03-24 21:25:36 +0300

    fix

--- js/_website/functions/_shared.ts
+++ js/_website/functions/_shared.ts
@@ -23,41 +23,21 @@ function isLLMRequest(request: Request): boolean {
 	return AI_UA_PATTERNS.some((re) => re.test(ua));
 }
 
-const MARKDOWN_HEADERS = {
-	"Content-Type": "text/markdown; charset=utf-8",
-	"X-Robots-Tag": "noindex"
-};
-
 export async function serveDocMarkdown(context: any): Promise<Response> {
 	const { request, params, next } = context;
 	if (!isLLMRequest(request)) return next();
 
-	const origin = new URL(request.url).origin;
-	try {
-		const res = await fetch(`${origin}/api/markdown/${params.doc}`);
-		if (res.ok) {
-			const data: any = await res.json();
-			if (data.markdown) {
-				return new Response(data.markdown, { headers: MARKDOWN_HEADERS });
-			}
-		}
-	} catch {}
-	return next();
+	const url = new URL(request.url);
+	return Response.redirect(`${url.origin}/api/markdown/${params.doc}`, 302);
 }
 
 export async function serveGuideMarkdown(context: any): Promise<Response> {
 	const { request, params, next } = context;
 	if (!isLLMRequest(request)) return next();
 
-	const origin = new URL(request.url).origin;
-	try {
-		const res = await fetch(`${origin}/api/markdown/guide/${params.guide}`);
-		if (res.ok) {
-			const data: any = await res.json();
-			if (data.markdown) {
-				return new Response(data.markdown, { headers: MARKDOWN_HEADERS });
-			}
-		}
-	} catch {}
-	return next();
+	const url = new URL(request.url);
+	return Response.redirect(
+		`${url.origin}/api/markdown/guide/${params.guide}`,
+		302
+	);
 }
