9487b60670f6532eaccb3251d15f5505fa23d4e3
pngwn <hello@pngwn.io>
2026-04-01 21:47:53 +0530

    migrate dataframe to svelte 5 (#13150)
    * df migration
    
    * fix tests
    
    * fix
    
    * fix
    
    * fix
    
    * add changeset
    
    * fix
    
    * tweaks
    
    * fix icon
    
    * fix dtype
    
    * fix copy
    
    * fix cell widths
    
    * tweaks
    
    * fix tests
    
    ---------
    
    Co-authored-by: gradio-pr-bot <gradio-pr-bot@users.noreply.github.com>

--- js/dataframe/shared/icons/SortButtonDown.svelte
+++ js/dataframe/shared/icons/SortButtonDown.svelte
@@ -1,14 +1,25 @@
+<script lang="ts">
+	let { size = 16 } = $props();
+</script>
+
 <svg
-	viewBox="0 0 24 24"
+	width={size}
+	height={size}
+	viewBox="0 0 16 16"
 	fill="none"
 	xmlns="http://www.w3.org/2000/svg"
-	focusable="false"
 >
 	<path
-		d="M7 10l5 5 5-5"
+		d="M4 8L8 12L12 8"
 		stroke="currentColor"
-		stroke-width="2"
+		stroke-width="1.5"
 		stroke-linecap="round"
 		stroke-linejoin="round"
+	/>
+	<path
+		d="M8 12V4"
+		stroke="currentColor"
+		stroke-width="1.5"
+		stroke-linecap="round"
 	/>
 </svg>
---
+++ js/dataframe/shared/tanstack/index.ts
@@ -0,0 +1,23 @@
+export { createSvelteTable } from "./table.svelte.js";
+
+export { createSvelteVirtualizer, type VirtualItem } from "./virtual.svelte.js";
+
+export {
+	createColumnHelper,
+	getCoreRowModel,
+	getSortedRowModel,
+	getFilteredRowModel,
+	type ColumnDef,
+	type TableOptions,
+	type Table,
+	type Header,
+	type Cell,
+	type Row,
+	type SortingState,
+	type ColumnFiltersState,
+	type ColumnPinningState,
+	type FilterFn,
+	type SortingFn,
+	type CellContext,
+	type HeaderContext
+} from "@tanstack/table-core";
