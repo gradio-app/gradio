5884adcb8cd63032b459318696fa6b78a2d9f2b6
Abubakar Abid <islamrealm@gmail.com>
2026-04-07 08:10:07 -0700

    changes

--- scripts/create_test_requirements.sh
+++ scripts/create_test_requirements.sh
@@ -8,7 +8,15 @@ pip_required
 echo "Creating test requirements...
 To match the CI environment, this script should be run from a Unix-like system in Python 3.10."
 
+EXCLUDE_NEWER="${UV_EXCLUDE_NEWER:-$(
+  python - <<'PY'
+from datetime import UTC, datetime, timedelta
+
+print((datetime.now(UTC) - timedelta(days=7)).date().isoformat())
+PY
+)}"
+
 uv pip compile \
-  --exclude-newer "${UV_EXCLUDE_NEWER:-7 days}" \
+  --exclude-newer "$EXCLUDE_NEWER" \
   test/requirements.in \
   -o test/requirements.txt
