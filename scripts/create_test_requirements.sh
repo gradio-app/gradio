69e73ec14ab2ee5888ca3e8759a724f4a78440b7
Abubakar Abid <islamrealm@gmail.com>
2026-04-07 13:44:06 -0700

    Use relative uv exclude-newer and bump CI uv

--- scripts/create_test_requirements.sh
+++ scripts/create_test_requirements.sh
@@ -8,15 +8,7 @@ pip_required
 echo "Creating test requirements...
 To match the CI environment, this script should be run from a Unix-like system in Python 3.10."
 
-EXCLUDE_NEWER="${UV_EXCLUDE_NEWER:-$(
-  python - <<'PY'
-from datetime import UTC, datetime, timedelta
-
-print((datetime.now(UTC) - timedelta(days=7)).date().isoformat())
-PY
-)}"
-
 uv pip compile \
-  --exclude-newer "$EXCLUDE_NEWER" \
+  --exclude-newer "${UV_EXCLUDE_NEWER:-7 days}" \
   test/requirements.in \
   -o test/requirements.txt
