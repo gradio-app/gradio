71b304e4e02b7c5188331ca8026a098f420b581b
Abubakar Abid <islamrealm@gmail.com>
2026-08-18 10:25:23 -0700

    Correct mobile fullscreen coverage for ImageSlider

--- js/image/Image.stories.svelte
+++ js/image/Image.stories.svelte
@@ -1,7 +1,6 @@
 <script module>
 	import { defineMeta } from "@storybook/addon-svelte-csf";
 	import StaticImage from "./Index.svelte";
-	import { userEvent, within } from "storybook/test";
 	import { allModes } from "../storybook/modes";
 	import { wrapProps } from "../storybook/wrapProps";
 
@@ -42,35 +41,6 @@
 		<div
 			class="image-container"
 			style="width: 300px; position: relative;border-radius: var(--radius-lg);overflow: hidden;"
-		>
-			<StaticImage {...wrapProps(args)} />
-		</div>
-	{/snippet}
-</Story>
-
-<Story
-	name="Mobile fullscreen"
-	args={{
-		value: {
-			path: cheetah,
-			url: cheetah,
-			orig_name: "cheetah.jpg"
-		},
-		interactive: false,
-		show_label: true,
-		label: "Fullscreen image",
-		buttons: ["fullscreen"],
-		webcam_options: { mirror: true, constraints: null }
-	}}
-	play={async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		await userEvent.click(canvas.getByRole("button", { name: "Fullscreen" }));
-	}}
->
-	{#snippet template(args)}
-		<div
-			class="image-container"
-			style="width: 300px; height: 400px; position: relative;border-radius: var(--radius-lg);overflow: hidden;"
 		>
 			<StaticImage {...wrapProps(args)} />
 		</div>
