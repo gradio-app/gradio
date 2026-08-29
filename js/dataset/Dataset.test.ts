3af3f7b3f3d5909864b03c96a76726c69ca7a362
Dawood <dawoodkhan82@gmail.com>
2026-08-28 20:07:16 -0400

    Add Dataset shared prop tests

--- js/dataset/Dataset.test.ts
+++ js/dataset/Dataset.test.ts
@@ -1,36 +1,45 @@
 import { afterEach, describe, expect, test } from "vitest";
 import { cleanup, render, waitFor } from "@self/tootils/render";
+import { run_shared_prop_tests } from "@self/tootils/shared-prop-tests";
 import event from "@testing-library/user-event";
 
 import Dataset from "./Index.svelte";
 import DatasetRootExample from "./DatasetRootExample.svelte";
 
+const table_props = {
+	components: [
+		{ name: "textbox", class_id: "textbox" },
+		{ name: "textbox", class_id: "textbox" }
+	],
+	component_props: [{}, {}],
+	headers: ["Name", "Field"],
+	samples: [
+		["Ada", "Math"],
+		["Grace", "Compilers"]
+	],
+	sample_labels: null,
+	value: null,
+	root: "",
+	proxy_url: null,
+	samples_per_page: 10,
+	layout: "table" as const,
+	show_label: true,
+	load_component: () => ({
+		component: Promise.resolve({ default: DatasetRootExample }),
+		runtime: false as const
+	})
+};
+
+run_shared_prop_tests({
+	component: Dataset,
+	name: "Dataset",
+	base_props: table_props,
+	has_label: false,
+	has_validation_error: false
+});
+
 describe("Dataset", () => {
 	afterEach(() => cleanup());
-
-	const table_props = {
-		components: [
-			{ name: "textbox", class_id: "textbox" },
-			{ name: "textbox", class_id: "textbox" }
-		],
-		component_props: [{}, {}],
-		headers: ["Name", "Field"],
-		samples: [
-			["Ada", "Math"],
-			["Grace", "Compilers"]
-		],
-		sample_labels: null,
-		value: null,
-		root: "",
-		proxy_url: null,
-		samples_per_page: 10,
-		layout: "table" as const,
-		show_label: true,
-		load_component: () => ({
-			component: Promise.resolve({ default: DatasetRootExample }),
-			runtime: false as const
-		})
-	};
 
 	test("forwards the shared root to example components", async () => {
 		const result = await render(Dataset, {
