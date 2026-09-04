8c71d2069aa4191e23001db037d001ee04ee6dd2
hysts <hysts@users.noreply.github.com>
2026-09-04 14:28:20 +0000

    Revert "Run the streaming test serially"
    This reverts commit f4e9db41a945534eafc347dc9e6ee0065b6b73d8.

--- test/components/test_audio.py
+++ test/components/test_audio.py
@@ -18,7 +18,6 @@ from gradio.media import get_audio
 
 
 class TestAudio:
-    @pytest.mark.serial
     @pytest.mark.asyncio
     async def test_streamed_audio_is_one_continuous_aac_stream(self):
         """Chunks share one encoder, so only the stream gets a priming frame."""
